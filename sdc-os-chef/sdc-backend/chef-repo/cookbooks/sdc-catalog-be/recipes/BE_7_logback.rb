b5b5f8b39b79aeb4a3905c1e8fa49b5dfc45b670
Tal Gitelman <tg851x@intl.att.com>
2017-07-30 14:40:35 +0300

    [sdc] - configuration files copy plug-in fixes
    Change-Id: Ibe82a9adf8d2723e2bfb6d1303bf5a093111304d
    Signed-off-by: Tal Gitelman <tg851x@intl.att.com>

--- sdc-os-chef/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_7_logback.rb
+++ sdc-os-chef/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_7_logback.rb
@@ -1,5 +1,5 @@
 cookbook_file "/var/lib/jetty/config/catalog-be/logback.xml" do
-  source "BE-logback.xml"
+  source "logback.xml"
   mode 0755
   owner "jetty"
   group "jetty"
--- sdc-os-chef/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_8_errors_config.rb
+++ sdc-os-chef/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_8_errors_config.rb
@@ -1,12 +1,12 @@
 cookbook_file "/var/lib/jetty/config/catalog-be/ecomp-error-configuration.yaml" do
-  source "BE-ecomp-error-configuration.yaml"
+  source "ecomp-error-configuration.yaml"
   mode 0755
   owner "jetty"
   group "jetty"
 end
  
 cookbook_file "/var/lib/jetty/config/catalog-be/error-configuration.yaml" do
-  source "BE-error-configuration.yaml"
+  source "error-configuration.yaml"
   mode 0755
   owner "jetty"
   group "jetty"
