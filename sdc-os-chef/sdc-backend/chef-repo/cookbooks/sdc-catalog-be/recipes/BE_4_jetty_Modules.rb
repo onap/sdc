b331b88c509df9e8e54530277b741855d1b91d68
Idan Amit <ia096e@intl.att.com>
2017-08-08 10:32:54 +0300

    [DevOps] - move the keystore copy before the
     jetty modules creation
    
    Change-Id: Ic7d49df2c60ed8385f90c25778ebd4ad747e51e4
    Signed-off-by: Idan Amit <ia096e@intl.att.com>

--- sdc-os-chef/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_4_jetty_Modules.rb
+++ sdc-os-chef/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_4_jetty_Modules.rb
@@ -12,15 +12,4 @@ EOH
 not_if "ls /#{jetty_base}/start.d/https.ini"
 end
 
-###### copy catalog-be.war
-
-
-
-###### copy onboarding-be.war& api-docs.war
-#bash "copy-onboarding-be" do
-#   code <<-EOH
-#       /bin/tar -xvf /var/tmp/onboarding-be.tar -C  /var/tmp/
-#           mv /var/tmp/onboarding-be*.war /var/tmp//api-docs*.war  #{jetty_base}/webapps
-#   EOH
-#end
 
