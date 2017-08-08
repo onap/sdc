8950b158bd9f5994bf266e0d6da13be2223e38c1
Idan Amit <ia096e@intl.att.com>
2017-08-08 15:13:29 +0300

    [DevOps] - Creating etc folder for
    jetty base
    
    Change-Id: I921cc0b5a30c6736be52cdd9ee94cc18f5dbd814
    Signed-off-by: Idan Amit <ia096e@intl.att.com>

--- sdc-os-chef/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_2_setup_configuration.rb
+++ sdc-os-chef/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_2_setup_configuration.rb
@@ -52,7 +52,14 @@ cookbook_file "ArtifactGenerator" do
    mode "0755"
 end
 
-
+directory "Jetty_etcdir_creation" do
+	path "/#{jetty_base}/etc"
+	owner 'jetty'
+	group 'jetty'
+	mode '0755'
+	action :create
+end
+	
 cookbook_file "/#{jetty_base}/etc/keystore" do
    source "keystore"
    owner "jetty"
--- sdc-os-chef/sdc-frontend/chef-repo/cookbooks/sdc-catalog-fe/recipes/FE_7_locate_keystore.rb
+++ sdc-os-chef/sdc-frontend/chef-repo/cookbooks/sdc-catalog-fe/recipes/FE_7_locate_keystore.rb
@@ -1,4 +1,13 @@
 jetty_base="/var/lib/jetty"
+
+directory "Jetty_etcdir_creation" do
+	path "/#{jetty_base}/etc"
+	owner 'jetty'
+	group 'jetty'
+	mode '0755'
+	action :create
+end
+
 cookbook_file "/#{jetty_base}/etc/keystore" do
    source "keystore"
    owner "jetty"
