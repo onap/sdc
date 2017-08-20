405a492b1fe84561edfdf91bab9c24a69a4f0dc2
Tal Gitelman <tg851x@intl.att.com>
2017-08-20 20:49:00 +0300

    [SDC-154] - sdc-simulator docker task
    Change-Id: Iaa4c30237a2eef2c40fb4bfda114bc2eca6df9d5
    Signed-off-by: Tal Gitelman <tg851x@intl.att.com>

--- utils/webseal-simulator/sdc-simulator/chef-repo/cookbooks/sdc-simulator/recipes/SDC_Simulator_2_setup_configuration.rb
+++ utils/webseal-simulator/sdc-simulator/chef-repo/cookbooks/sdc-simulator/recipes/SDC_Simulator_2_setup_configuration.rb
@@ -1,9 +1,14 @@
 jetty_base="/var/lib/jetty"
 
-cookbook_file "webseal.conf" do
+
+template "webseal.conf" do
    path "#{jetty_base}/config/sdc-simulator/webseal.conf"
-   source "webseal.conf"
+   source "SDC-Simulator-webseal.conf.erb"
    owner "jetty"
    group "jetty"
    mode "0755"
+   variables({
+      :fe_host_ip   => node['HOST_IP'],
+      :fe_http_port => "#{node['FE'][:http_port]}"
+   })
 end
