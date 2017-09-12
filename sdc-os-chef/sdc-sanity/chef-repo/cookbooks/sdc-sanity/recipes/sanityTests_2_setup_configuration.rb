7bacb481223bd978dcdd7593e32ab9c074be31bd
Yuli Shlosberg <ys9693@att.com>
2017-09-12 14:11:48 +0300

    Build sanity docker execute automation
    Change-Id: Iab282ea167d12fba4a3f425a5d300397ab435b44
    Issue-Id: SDC-315
    Signed-off-by: Yuli Shlosberg <ys9693@att.com>

---
+++ sdc-os-chef/sdc-sanity/chef-repo/cookbooks/sdc-sanity/recipes/sanityTests_2_setup_configuration.rb
@@ -0,0 +1,43 @@
+tests_base="/var/lib/tests"
+
+template "sdc-yaml-config" do
+    path "#{tests_base}/conf/sdc.yaml"
+    source "sdc-sanity.yaml.erb"
+    owner "root"
+    group "root"
+    mode "0755"
+    variables ({
+        :target_path                  => "#{tests_base}/target",
+        :catalogBE_ip                 => node['Nodes']['BE'],
+        :catalogBE_port               => node['BE'][:http_port],
+        :webportal_ip                 => node['Nodes']['FE'],
+        :webportal_port               => node['FE'][:http_port],
+        :titan_file                   => "#{tests_base}/conf/titan.properties",
+        :tests_base_ci                => "#{tests_base}/CI/tests",
+        :components_path              => "#{tests_base}/CI/components",
+        :importResourceConfigDir      => "#{tests_base}/CI/importResource",
+        :importTypesDir               => "#{tests_base}/CI/importTypesTest",
+        :importResourceTestsConfigDir => "#{tests_base}/CI/importResourceTests",
+        :ConfigurationFile            => "#{tests_base}/conf/configuration.yaml",
+        :errorConfigurationFile       => "#{tests_base}/conf/error-configuration.yaml",
+        :CASSANDRA_IP                 => node['Nodes']['CS'],
+        :CASSANDRA_PWD                => node['cassandra'][:cassandra_password],
+        :CASSANDRA_USR                => node['cassandra'][:cassandra_user]
+    })
+end
+
+replication_factor=1
+template "titan.properties" do
+   path "/#{tests_base}/conf/titan.properties"
+   source "BE-titan.properties.erb"
+   owner "root"
+   group "root"
+   mode "0755"
+   variables({
+      :CASSANDRA_IP => node['Nodes']['CS'],
+      :CASSANDRA_PWD => node['cassandra'][:cassandra_password],
+      :CASSANDRA_USR => node['cassandra'][:cassandra_user],
+      :rep_factor => replication_factor,
+      :DC_NAME      => node['cassandra'][:cluster_name]+node.chef_environment
+   })
+end
