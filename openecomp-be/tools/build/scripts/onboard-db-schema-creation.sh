c24665c64ed719947c16e682035ee3c771b9cde4
Areli Fuss <af732p@att.com>
2017-09-26 11:12:35 +0300

    Add dynamic keyspace creation
    Add dynamic key space creation for
    Amdocs
    
    Change-Id: Ia6fdc921c14231ffad4eaae44d018e012905e698
    Issue-Id: SDC-276
    Signed-off-by: Areli Fuss <af732p@att.com>

--- openecomp-be/tools/build/scripts/onboard-db-schema-creation.sh
+++ openecomp-be/tools/build/scripts/onboard-db-schema-creation.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 
 
@@ -9,7 +9,7 @@ CASSANDRA_ALTER_CQL_FILE=alter_dox_db.cql
 # Generate the create schema CQL file
 ./generate-cassandra-init-cql.sh ./cassandra-commands.json dox no > ${CASSANDRA_CQL_FILE}
 ./generate-application-config-insert-cql.sh vsp.schemaTemplates ../../../tools/install/database/schemaTemplates >> ${CASSANDRA_CQL_FILE}
-./generate-application-config-insert-cql.sh vsp.monitoring ../../..//tools/install/database/monitoring >> ${CASSANDRA_CQL_FILE}
+./generate-application-config-insert-cql.sh vsp.monitoring ../../../tools/install/database/monitoring >> ${CASSANDRA_CQL_FILE}
 ./zusammen-generate-cassandra-init-cql.sh ./zusammen-cassandra-commands.json zusammen_dox no >> ${CASSANDRA_CQL_FILE}
 
 # Generate the alter schema CQL file
