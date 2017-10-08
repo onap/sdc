07f1f710fbd15373e21a62f8481c95ae4d7adf1f
Michael Lando <ml636r@att.com>
2017-10-08 11:05:10 +0300

    add consumer do sdc pre load
    Change-Id: I91bab2fe3a0f4f34ba8668503e25bdd5aa6ab8a5
    Issue-Id: SDC-439
    Signed-off-by: Michael Lando <ml636r@att.com>

--- sdc-os-chef/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/files/default/consumers.py
+++ sdc-os-chef/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/files/default/consumers.py
@@ -52,7 +52,7 @@ def createConsumer( consumerName, consumerSalt, consumerPass ):
 ##############################
 #    Definitions
 ##############################
-consumersList = [ "aai" , "appc" , "dcae" , "mso" , "sdnc" , "vid" , "cognita" ]
+consumersList = [ "aai" , "appc" , "dcae" , "mso" , "sdnc" , "vid" , "cognita", "clamp" , "vfc" ]
 salt = "9cd4c3ad2a6f6ce3f3414e68b5157e63"
 password = "35371c046f88c603ccba152cb3db34ec4475cb2e5713f2fc0a43bf18a5243495"
 beStat=0
