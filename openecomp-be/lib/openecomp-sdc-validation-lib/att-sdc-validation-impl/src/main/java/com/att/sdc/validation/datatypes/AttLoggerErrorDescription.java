15a6fa057331a2060b9446426cf91a99294e41a1
shrikantawachar <shrikant.awachar@amdocs.com>
2017-10-13 19:57:30 +0530

    Refactoring of module in onboarding
    Removed ATT modules.
    
    Change-Id: I9f97b92a1f80483c3422b9b5e6643fdad1bb881d
    Issue-ID: SDC-456
    Signed-off-by: shrikantawachar <shrikant.awachar@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-validation-lib/att-sdc-validation-impl/src/main/java/com/att/sdc/validation/datatypes/AttLoggerErrorDescription.java
+++
@@ -1,9 +0,0 @@
-package com.att.sdc.validation.datatypes;
-
-/**
- * Created by TALIO on 5/21/2017.
- */
-public class AttLoggerErrorDescription {
-
-  public static final String ATT_VALET_IN_USE = "ATT Valet group type is in use";
-}
