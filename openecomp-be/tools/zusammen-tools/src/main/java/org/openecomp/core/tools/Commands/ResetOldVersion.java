f2efa8b46ed86b76925adeddceba7489c87cbe30
shrek2000 <orenkle@amdocs.com>
2017-10-17 09:23:49 +0300

    Reset all version failure
    Change-Id: Iafa72f0101e53ef60cd57ffd71be46db6d5a3c62
    Issue-ID: SDC-469
    Signed-off-by: shrek2000 <orenkle@amdocs.com>

--- openecomp-be/tools/zusammen-tools/src/main/java/org/openecomp/core/tools/Commands/ResetOldVersion.java
+++ openecomp-be/tools/zusammen-tools/src/main/java/org/openecomp/core/tools/Commands/ResetOldVersion.java
@@ -3,24 +3,32 @@ package org.openecomp.core.tools.Commands;
 import com.amdocs.zusammen.datatypes.Id;
 import com.amdocs.zusammen.datatypes.SessionContext;
 import com.amdocs.zusammen.plugin.statestore.cassandra.dao.impl.VersionCassandraDao;
+import com.google.common.collect.Sets;
+import org.openecomp.core.tools.store.ElementHandler;
 import org.openecomp.core.tools.store.VersionInfoCassandraLoader;
 import org.openecomp.core.tools.store.VspGeneralLoader;
-import org.openecomp.core.tools.store.ElementHandler;
 import org.openecomp.core.zusammen.impl.CassandraConnectionInitializer;
 import org.openecomp.core.zusammen.plugin.dao.types.ElementEntity;
 import org.openecomp.sdc.vendorsoftwareproduct.VendorSoftwareProductConstants;
+import org.openecomp.sdc.versioning.dao.types.Version;
 import org.openecomp.sdc.versioning.dao.types.VersionInfoEntity;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class ResetOldVersion {
 
 
-  private static Map<String, List<String>> itemVersionMap = new HashMap<>();
+  public static Map<String, List<String>> itemVersionMap = new HashMap<>();
+  public static Map<String, List<String>> itemChangeRefMap = new HashMap<>();
 
-  private static int count =0;
-  public static void reset(SessionContext context, String oldVersion,String emptyOldVersion) {
+  public static int count = 0;
 
+  public static void reset(SessionContext context, String oldVersion, String emptyOldVersion) {
 
 
     CassandraConnectionInitializer.setCassandraConnectionPropertiesToSystem();
@@ -28,32 +36,42 @@ public class ResetOldVersion {
     loadItemVersionInfo(context);
 
     Map<String, ElementEntity> generalElementMap =
-        VspGeneralLoader.load(context,
-             itemVersionMap);
+            VspGeneralLoader.load(context,
+                    itemVersionMap,itemChangeRefMap);
 
     generalElementMap.values().forEach(elementEntity -> updateOldVersionFlag(elementEntity,
-        oldVersion,"true".equals(emptyOldVersion)));
+            oldVersion, "true".equals(emptyOldVersion)));
 
 
-    itemVersionMap.entrySet().forEach(entry->entry.getValue().stream().filter
-        (version->generalElementMap.containsKey(context.getUser().getUserName()+"_"+entry.getKey()
-            +"_"+version)).forEach(version->ElementHandler.update(context,
-        entry.getKey(),version,generalElementMap.get(context.getUser().getUserName()+"_"+entry.getKey()
-            +"_"+version))));
+    itemVersionMap.entrySet().forEach(entry -> entry.getValue().stream().filter
+            (version -> generalElementMap
+                    .containsKey(context.getUser().getUserName() + "_" + entry.getKey()
+                            + "_" + version)).forEach(version -> ElementHandler.update(context,
+            entry.getKey(), version,null,
+            generalElementMap.get(context.getUser().getUserName() + "_" + entry.getKey()
+                    + "_" + version))));
 
-    System.out.println("nymber of element updated:"+count);
+    itemChangeRefMap.entrySet().forEach(entry -> entry.getValue().stream().filter
+            (changeRef -> generalElementMap
+                    .containsKey(context.getUser().getUserName() + "_" + entry.getKey()
+                            + "_" + changeRef)).forEach(changeref -> ElementHandler.update(context,
+            entry.getKey(),changeref, changeref,
+            generalElementMap.get(context.getUser().getUserName() + "_" + entry.getKey()
+                    + "_" + changeref))));
+
+    System.out.println("number of element updated:" + count);
 
   }
 
   private static void updateOldVersionFlag(ElementEntity elementEntity, String oldVersion,
                                            boolean emptyOldVersion) {
 
-    if(!emptyOldVersion){
-      elementEntity.getInfo().addProperty("oldVersion",oldVersion);
+    if (!emptyOldVersion) {
+      elementEntity.getInfo().addProperty("oldVersion", oldVersion);
       count++;
-    }else if(elementEntity.getInfo().getProperty("oldVersion")== null || ""
-        .equals(elementEntity.getInfo().getProperty("oldVersion"))){
-      elementEntity.getInfo().addProperty("oldVersion",oldVersion);
+    } else if (elementEntity.getInfo().getProperty("oldVersion") == null || ""
+            .equals(elementEntity.getInfo().getProperty("oldVersion"))) {
+      elementEntity.getInfo().addProperty("oldVersion", oldVersion);
       count++;
     }
 
@@ -67,25 +85,51 @@ public class ResetOldVersion {
     System.setProperty("cassandra.dox.keystore", "dox");
     VersionInfoCassandraLoader versionInfoCassandraLoader = new VersionInfoCassandraLoader();
     Collection<VersionInfoEntity> versions =
-        versionInfoCassandraLoader.list();
+            versionInfoCassandraLoader.list();
 
     versions.stream().filter(versionInfoEntity -> versionInfoEntity.getEntityType().equals
-        (VendorSoftwareProductConstants.VENDOR_SOFTWARE_PRODUCT_VERSIONABLE_TYPE)).forEach
-        (versionInfoEntity
-            -> items.add(versionInfoEntity.getEntityId()));
+            (VendorSoftwareProductConstants.VENDOR_SOFTWARE_PRODUCT_VERSIONABLE_TYPE)).forEach
+            (versionInfoEntity -> {
+              items.add(versionInfoEntity.getEntityId());
+              Set<Version> viewableVersions;
+              if (versionInfoEntity.getViewableVersions()!= null && !versionInfoEntity.getViewableVersions().isEmpty()){
+                viewableVersions = versionInfoEntity.getViewableVersions();
+              } else {
+                viewableVersions = Sets.newHashSet(versionInfoEntity.getActiveVersion());
+              }
+              addItemchangeRef(versionInfoEntity.getEntityId(),maxChangeRef(viewableVersions));
+            });
     System.setProperty("cassandra.dox.keystore", "zusammen_dox");
     VersionCassandraDao versionCassandraDao = new VersionCassandraDao();
 
-    items
-        .forEach(itemId -> versionCassandraDao.list(context, context.getUser().getUserName(), new Id
-            (itemId)).forEach(itemVersion -> addItemVersion(itemId, itemVersion.getId())));
+    items.forEach(itemId -> versionCassandraDao.list(context, context.getUser().getUserName(),
+            new Id(itemId)).forEach(itemVersion -> addItemVersion(itemId, itemVersion.getId())));
 
+  }
+
+  private static Id maxChangeRef(Set<Version> viewableVersions) {
+
+    return new Id(viewableVersions.stream().max((o1, o2) -> o1.getMajor() > o2.getMajor()
+            ?1:o1.getMajor
+            () == o2.getMajor() ? (o1.getMinor() > o2.getMinor() ? 1: o1.getMinor() == o2.getMinor()
+            ? 0 : -1) : -1).get().toString());
+
+  }
+
+  private static void addItemchangeRef(String itemId, Id changeRef) {
+    addItemVersion(itemChangeRefMap,itemId,changeRef);
   }
 
   private static void addItemVersion(String itemId, Id versionId) {
-    if (!itemVersionMap.containsKey(itemId)) {
-      itemVersionMap.put(itemId, new ArrayList<>());
+    addItemVersion(itemVersionMap,itemId,versionId);
+  }
+
+  private static void addItemVersion(Map<String,List<String>> itemVersions,String itemId, Id
+          id) {
+    if (!itemVersions.containsKey(itemId)) {
+      itemVersions.put(itemId, new ArrayList<>());
     }
-    itemVersionMap.get(itemId).add(versionId.getValue());
+    itemVersions.get(itemId).add(id.getValue());
   }
 }
+
--- openecomp-be/tools/zusammen-tools/src/main/java/org/openecomp/core/tools/store/ElementHandler.java
+++ openecomp-be/tools/zusammen-tools/src/main/java/org/openecomp/core/tools/store/ElementHandler.java
@@ -2,8 +2,9 @@ package org.openecomp.core.tools.store;
 
 import com.amdocs.zusammen.datatypes.Id;
 import com.amdocs.zusammen.datatypes.SessionContext;
+import com.amdocs.zusammen.datatypes.item.ElementContext;
 import com.amdocs.zusammen.plugin.statestore.cassandra.dao.types.ElementEntityContext;
-import org.openecomp.core.zusammen.plugin.dao.impl.CassandraElementRepository;
+//import org.openecomp.core.zusammen.plugin.dao.impl.CassandraElementRepository;
 import org.openecomp.core.zusammen.plugin.dao.types.ElementEntity;
 
 public class ElementHandler {
@@ -11,14 +12,21 @@ public class ElementHandler {
   private static final String GLOBAL_USER = "GLOBAL_USER";
 
   public static void update(SessionContext context,
-
-                            String itemId, String versionId,
+                            String itemId, String versionId,String changeRef,
                             ElementEntity elementEntity) {
 
     ElementEntityContext elementContext;
-    elementContext = new ElementEntityContext(GLOBAL_USER, new Id(itemId),
-        new Id(versionId));
     CassandraElementRepository cassandraElementRepository = new CassandraElementRepository();
+    if(changeRef== null) {
+
+      elementContext = new ElementEntityContext(GLOBAL_USER, new Id(itemId),
+              new Id(versionId));
+
+    }else{
+      elementContext = new ElementEntityContext(GLOBAL_USER, new ElementContext(new Id(itemId),
+              new Id(versionId),
+              changeRef));
+    }
     cassandraElementRepository.update(context, elementContext, elementEntity);
 
   }
