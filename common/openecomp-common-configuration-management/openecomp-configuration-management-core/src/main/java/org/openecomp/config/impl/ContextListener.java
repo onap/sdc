703240858e26a8efbea199214ebcaf1bd2709163
sheetalm <sheetal.mudholkar@amdocs.com>
2017-11-08 13:09:07 +0530

    Error in Jetty logs
    Fixed code from Configuration Management that was causing exception
    
    Change-Id: Iea5461530a1b8a548a95c86f2d5c38b965dbb45f
    Issue-ID: SDC-580
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

--- common/openecomp-common-configuration-management/openecomp-configuration-management-core/src/main/java/org/openecomp/config/impl/ContextListener.java
+++ common/openecomp-common-configuration-management/openecomp-configuration-management-core/src/main/java/org/openecomp/config/impl/ContextListener.java
@@ -2,18 +2,12 @@ package org.openecomp.config.impl;
 
 import static org.openecomp.config.Constants.MBEAN_NAME;
 
-import org.apache.commons.beanutils.FluentPropertyBeanIntrospector;
-import org.openecomp.config.api.ConfigurationManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.lang.management.ManagementFactory;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import javax.management.ObjectName;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import org.openecomp.config.api.ConfigurationManager;
 
 @WebListener
 public class ContextListener implements ServletContextListener {
@@ -29,20 +23,6 @@ public class ContextListener implements ServletContextListener {
 
   @Override
   public void contextInitialized(ServletContextEvent arg0) {
-    try {
-      ConfigurationManager.lookup();
-      Logger logger = LoggerFactory.getLogger(FluentPropertyBeanIntrospector.class);
-      Method method = logger.getClass().getDeclaredMethod("getLevel");
-      method.setAccessible(true);
-      Object object = method.invoke(logger);
-      method = logger.getClass().getDeclaredMethod("setLevel", object.getClass());
-      method.setAccessible(true);
-      Field field = object.getClass().getDeclaredField("ERROR");
-      field.setAccessible(true);
-      method.invoke(logger, field.get(logger));
-    } catch (Exception exception) {
-      exception.printStackTrace();
-    }
+    ConfigurationManager.lookup();
   }
-
 }
