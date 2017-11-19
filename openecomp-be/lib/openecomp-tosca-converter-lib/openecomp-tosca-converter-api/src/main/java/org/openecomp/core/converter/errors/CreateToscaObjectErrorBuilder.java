b91470aaf45ccd77c56a7882f0babc9b4a2b9637
talio <tali.orenbach@amdocs.com>
2017-11-19 09:35:13 +0200

    import tosca bug
    fix bug when converting parameters, when importing CSAR file
    
    Issue - Id : SDC-646
    
    Change-Id: Ie9c38f5e51e673a7c89add9e4e42fad93f966aa4
    Signed-off-by: talio <tali.orenbach@amdocs.com>

---
+++ openecomp-be/lib/openecomp-tosca-converter-lib/openecomp-tosca-converter-api/src/main/java/org/openecomp/core/converter/errors/CreateToscaObjectErrorBuilder.java
@@ -0,0 +1,19 @@
+package org.openecomp.core.converter.errors;
+
+import org.openecomp.sdc.common.errors.BaseErrorBuilder;
+import org.openecomp.sdc.common.errors.ErrorCategory;
+
+public class CreateToscaObjectErrorBuilder extends BaseErrorBuilder {
+  private static final String CANT_CREATE_OBJECT_FROM_CLASS =
+      "Can't create %s from %s. Reason - %s";
+  private static final String IMPORT_TOSCA = "IMPORT_TOSCA";
+
+  public CreateToscaObjectErrorBuilder(String className,
+                                       String objectId,
+                                       String reason) {
+    getErrorCodeBuilder()
+        .withId(IMPORT_TOSCA)
+        .withCategory(ErrorCategory.APPLICATION)
+        .withMessage(String.format(CANT_CREATE_OBJECT_FROM_CLASS, className, objectId, reason));
+  }
+}
