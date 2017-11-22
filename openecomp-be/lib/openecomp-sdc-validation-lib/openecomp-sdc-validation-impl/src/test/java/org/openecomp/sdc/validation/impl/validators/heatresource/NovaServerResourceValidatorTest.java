49f1676efc2efe8d9e153a61afa692607cb526d3
mojahidi <mojahidul.islam@amdocs.com>
2017-11-22 18:52:02 +0530

    NovaServerResourceValidator
    This task is about  updating  error messages with error codes for NovaServerResourceValidator
    
    Change-Id: I90638b7c736b75b8cdadd440dbaafed146eec140
    Issue-ID: SDC-572
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-impl/src/test/java/org/openecomp/sdc/validation/impl/validators/heatresource/NovaServerResourceValidatorTest.java
+++ openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-impl/src/test/java/org/openecomp/sdc/validation/impl/validators/heatresource/NovaServerResourceValidatorTest.java
@@ -17,37 +17,37 @@ public class NovaServerResourceValidatorTest {
 
   HeatResourceValidator baseValidator = new HeatResourceValidator();
   NovaServerResourceValidator resourceValidator = new NovaServerResourceValidator();
-
+  private static final String PATH = "/org/openecomp/validation/validators/heat_validator/";
   @Test
   public void testNovaPropertiesHasAssignedValue() throws IOException {
     Map<String, MessageContainer> messages = ValidationTestUtil.testValidator(baseValidator,
-        resourceValidator, HeatResourcesTypes.NOVA_SERVER_RESOURCE_TYPE.getHeatResource(),
-        "/org/openecomp/validation/validators/heat_validator/nova_properties_has_assigned_value/negative_test/input");
+            resourceValidator, HeatResourcesTypes.NOVA_SERVER_RESOURCE_TYPE.getHeatResource(),
+            PATH + "nova_properties_has_assigned_value/negative_test/input");
 
     Assert.assertNotNull(messages);
     Assert.assertEquals(messages.size(), 1);
 
     Assert.assertEquals(messages.get("hot-nimbus-psm_v1.0.yaml").getErrorMessageList().size(), 1);
     Assert.assertEquals(
-        messages.get("hot-nimbus-psm_v1.0.yaml").getErrorMessageList().get(0).getMessage(),
-        "ERROR: Missing both Image and Flavor in NOVA Server, Resource ID [nova_server_resource_missing_both]");
+            messages.get("hot-nimbus-psm_v1.0.yaml").getErrorMessageList().get(0).getMessage(),
+            "ERROR: [HNS1]: Missing both Image and Flavor in NOVA Server, Resource ID [nova_server_resource_missing_both]");
   }
 
   @Test
   public void testServerGroupsPointedByServersDefinedCorrectly() throws IOException {
     Map<String, MessageContainer> messages =ValidationTestUtil.testValidator(baseValidator,
-        resourceValidator, HeatResourcesTypes.NOVA_SERVER_RESOURCE_TYPE.getHeatResource(),
-        "/org/openecomp/validation/validators/heat_validator/server_groups_defined_correctly/negative_test/input");
+            resourceValidator, HeatResourcesTypes.NOVA_SERVER_RESOURCE_TYPE.getHeatResource(),
+            PATH + "server_groups_defined_correctly/negative_test/input");
 
     Assert.assertNotNull(messages);
     Assert.assertEquals(messages.size(), 1);
 
     Assert.assertEquals(messages.get("hot-nimbus-pps_v1.0.yaml").getErrorMessageList().size(), 2);
     Assert.assertEquals(
-        messages.get("hot-nimbus-pps_v1.0.yaml").getErrorMessageList().get(0).getMessage(),
-        "ERROR: Missing server group definition - BE_Affinity_2, nova_server_1");
+            messages.get("hot-nimbus-pps_v1.0.yaml").getErrorMessageList().get(0).getMessage(),
+            "ERROR: [HNS2]: Missing server group definition - BE_Affinity_2, nova_server_1");
     Assert.assertEquals(
-        messages.get("hot-nimbus-pps_v1.0.yaml").getErrorMessageList().get(1).getMessage(),
-        "ERROR: Missing server group definition - BE_Affinity_2, nova_server_2");
+            messages.get("hot-nimbus-pps_v1.0.yaml").getErrorMessageList().get(1).getMessage(),
+            "ERROR: [HNS2]: Missing server group definition - BE_Affinity_2, nova_server_2");
   }
 }
