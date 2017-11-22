49817938ed3fcfb62b43954eaa361ff2289f1715
mojahidi <mojahidul.islam@amdocs.com>
2017-11-22 18:37:15 +0530

    NeutronSecurityGroupResourceValidator
    This task is about  updating  error messages with error codes for NeutronSecurityGroupResourceValidator
    
    Change-Id: I6279c76dae5aeff46bee0ffc9d097ba5b7ff267e
    Issue-ID: SDC-572
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-impl/src/main/java/org/openecomp/sdc/validation/impl/validators/heatresource/NeutronSecurityGroupResourceValidator.java
+++ openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-impl/src/main/java/org/openecomp/sdc/validation/impl/validators/heatresource/NeutronSecurityGroupResourceValidator.java
@@ -1,6 +1,7 @@
 package org.openecomp.sdc.validation.impl.validators.heatresource;
 
 import org.apache.commons.collections4.MapUtils;
+import org.openecomp.core.validation.ErrorMessageCode;
 import org.openecomp.core.validation.errors.ErrorMessagesFormatBuilder;
 import org.openecomp.core.validation.types.GlobalValidationContext;
 import org.openecomp.sdc.common.errors.Messages;
@@ -21,47 +22,51 @@ import java.util.Map;
  * Created by TALIO on 2/27/2017.
  */
 public class NeutronSecurityGroupResourceValidator implements ResourceValidator {
+  private static final ErrorMessageCode ERROR_CODE_HSG1 = new ErrorMessageCode("HSG1");
+
   @Override
   public void validate(String fileName, Map.Entry<String, Resource> resourceEntry,
                        GlobalValidationContext globalContext, ValidationContext validationContext) {
 
     HeatResourceValidationContext heatResourceValidationContext =
-        (HeatResourceValidationContext) validationContext;
-    validateSecurityGroupIsUsed(fileName, resourceEntry, heatResourceValidationContext, globalContext);
+            (HeatResourceValidationContext) validationContext;
+    validateSecurityGroupIsUsed(fileName, resourceEntry, heatResourceValidationContext,
+            globalContext);
   }
 
-  public void validateSecurityGroupIsUsed(String fileName, Map.Entry<String, Resource> resourceEntry,
-                                          HeatResourceValidationContext
-                                              heatResourceValidationContext,
-                                          GlobalValidationContext globalContext) {
+  private void validateSecurityGroupIsUsed(String fileName,
+                                           Map.Entry<String, Resource> resourceEntry,
+                                           HeatResourceValidationContext
+                                                   heatResourceValidationContext,
+                                           GlobalValidationContext globalContext) {
 
     Map<String, Map<String, List<String>>> securityGroupsPointedByOtherResources =
-        heatResourceValidationContext.getFileLevelResourceDependencies().
-            get(HeatResourcesTypes.NEUTRON_SECURITY_GROUP_RESOURCE_TYPE.getHeatResource());
+            heatResourceValidationContext.getFileLevelResourceDependencies()
+                    .get(HeatResourcesTypes.NEUTRON_SECURITY_GROUP_RESOURCE_TYPE.getHeatResource());
 
     if (MapUtils.isEmpty(securityGroupsPointedByOtherResources)) {
       return;
     }
 
     Map<String, List<String>> resourcesPointingCurrSecurityGroup =
-        securityGroupsPointedByOtherResources.get(resourceEntry.getKey());
+            securityGroupsPointedByOtherResources.get(resourceEntry.getKey());
 
-    if(isSecurityGroupNotInUse(resourcesPointingCurrSecurityGroup)){
+    if (isSecurityGroupNotInUse(resourcesPointingCurrSecurityGroup)) {
       globalContext.addMessage(
-          fileName,
-          ErrorLevel.WARNING,
-          ErrorMessagesFormatBuilder
-              .getErrorWithParameters(
-                  Messages.RESOURCE_NOT_IN_USE.getErrorMessage(),
-                  ValidatorConstants.Security_Group, resourceEntry.getKey()),
-          LoggerTragetServiceName.VALIDATE_ALL_SERVER_GROUP_OR_SECURITY_GROUP_IN_USE,
-          LoggerErrorDescription.SERVER_GROUP_SECURITY_GROUP_NOT_IN_USE);
+              fileName,
+              ErrorLevel.WARNING,
+              ErrorMessagesFormatBuilder
+                      .getErrorWithParameters(
+                              ERROR_CODE_HSG1, Messages.RESOURCE_NOT_IN_USE.getErrorMessage(),
+                              ValidatorConstants.Security_Group, resourceEntry.getKey()),
+              LoggerTragetServiceName.VALIDATE_ALL_SERVER_GROUP_OR_SECURITY_GROUP_IN_USE,
+              LoggerErrorDescription.SERVER_GROUP_SECURITY_GROUP_NOT_IN_USE);
     }
 
   }
 
-  public boolean isSecurityGroupNotInUse(Map<String, List<String>>
-                                             referencingResourcesToCurrSecurityGroup){
+  private boolean isSecurityGroupNotInUse(Map<String, List<String>>
+                                                  referencingResourcesToCurrSecurityGroup) {
     return MapUtils.isEmpty(referencingResourcesToCurrSecurityGroup);
   }
 }
--- openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-impl/src/test/java/org/openecomp/sdc/validation/impl/validators/heatresource/NeutronSecurityGroupResourceValidatorTest.java
+++ openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-impl/src/test/java/org/openecomp/sdc/validation/impl/validators/heatresource/NeutronSecurityGroupResourceValidatorTest.java
@@ -18,36 +18,37 @@ public class NeutronSecurityGroupResourceValidatorTest {
   HeatResourceValidator baseValidator = new HeatResourceValidator();
   NeutronSecurityGroupResourceValidator resourceValidator = new
       NeutronSecurityGroupResourceValidator();
+  private static final String PATH = "/org/openecomp/validation/validators/heat_validator/";
 
   @Test
   public void testSecurityGroupBaseFileNoPorts() throws IOException {
     Map<String, MessageContainer> messages = ValidationTestUtil.testValidator(baseValidator,
-        resourceValidator,
-        HeatResourcesTypes.NEUTRON_SECURITY_GROUP_RESOURCE_TYPE.getHeatResource(),
-        "/org/openecomp/validation/validators/heat_validator/security_group_base_file_no_ports/input");
+            resourceValidator,
+            HeatResourcesTypes.NEUTRON_SECURITY_GROUP_RESOURCE_TYPE.getHeatResource(),
+            PATH + "security_group_base_file_no_ports/input");
 
     Assert.assertNotNull(messages);
     Assert.assertEquals(messages.size(), 1);
 
     Assert.assertEquals(messages.get("baseFile.yaml").getErrorMessageList().size(), 1);
     Assert.assertEquals(messages.get("baseFile.yaml").getErrorMessageList().get(0).getMessage(),
-        "WARNING: SecurityGroup not in use, Resource Id [jsa_security_group3]");
+            "WARNING: [HSG1]: SecurityGroup not in use, Resource Id [jsa_security_group3]");
   }
 
   @Test
   public void testSecurityGroupsCalledByPort() throws IOException {
     Map<String, MessageContainer> messages =ValidationTestUtil.testValidator(baseValidator,
-        resourceValidator,
-        HeatResourcesTypes.NEUTRON_SECURITY_GROUP_RESOURCE_TYPE.getHeatResource(),
-        "/org/openecomp/validation/validators/heat_validator/security_group_called_by_port/input");
+            resourceValidator,
+            HeatResourcesTypes.NEUTRON_SECURITY_GROUP_RESOURCE_TYPE.getHeatResource(),
+            PATH + "security_group_called_by_port/input");
 
     Assert.assertNotNull(messages);
     Assert.assertEquals(messages.size(), 1);
 
     Assert.assertEquals(messages.get("hot-nimbus-pps_v1.0.yaml").getErrorMessageList().size(), 1);
     Assert.assertEquals(
-        messages.get("hot-nimbus-pps_v1.0.yaml").getErrorMessageList().get(0).getMessage(),
-        "WARNING: SecurityGroup not in use, Resource Id [not_used_security_group]");
+            messages.get("hot-nimbus-pps_v1.0.yaml").getErrorMessageList().get(0).getMessage(),
+            "WARNING: [HSG1]: SecurityGroup not in use, Resource Id [not_used_security_group]");
   }
 
 }
