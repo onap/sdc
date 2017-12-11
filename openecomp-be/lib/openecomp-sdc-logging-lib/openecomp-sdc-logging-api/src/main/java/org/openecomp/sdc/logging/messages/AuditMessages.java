2d1c29146900d54a25d8bf28d9785c16413443f9
mojahidi <mojahidul.islam@amdocs.com>
2017-12-11 15:46:47 +0530

    Fixed sonar issues - AuditMessages
    Added private constructor to hide public one
    
    Change-Id: Icddef8c59eb1f4cc77ed6646cfb7b362d4921da1
    Issue-ID: SDC-343
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-logging-lib/openecomp-sdc-logging-api/src/main/java/org/openecomp/sdc/logging/messages/AuditMessages.java
+++ openecomp-be/lib/openecomp-sdc-logging-lib/openecomp-sdc-logging-api/src/main/java/org/openecomp/sdc/logging/messages/AuditMessages.java
@@ -1,8 +1,21 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package org.openecomp.sdc.logging.messages;
 
-/**
- * Created by ayalaben on 4/20/2017.
- */
 public class AuditMessages {
 
   public static final String AUDIT_MSG = " --Audit-- ";
@@ -38,5 +51,7 @@ public class AuditMessages {
   public static final String ENRICHMENT_ERROR = "Enrichment error: %s. VSP Id: %s";
   public static final String ENRICHMENT_COMPLETED = "Enrichment completed. VSP Id: ";
   public static final String CREATE_PACKAGE = "Created package. VSP Id: ";
+  private AuditMessages (){
 
+  }
 }
