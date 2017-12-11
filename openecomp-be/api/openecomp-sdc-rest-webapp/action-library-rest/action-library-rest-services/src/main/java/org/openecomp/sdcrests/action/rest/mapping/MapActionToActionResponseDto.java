b1ffbbde99b4cfd0a656558cb4f59e9326d605f5
Avi Gaffa <avi.gaffa@amdocs.com>
2017-12-11 09:51:25 +0200

    Change copyright title for onboarding
    Change-Id: I803324e3865f62cf07a69be74e4cdb44cc59220b
    Issue-ID: SDC-738
    Signed-off-by: avigaffa <avi.gaffa@amdocs.com>

--- openecomp-be/api/openecomp-sdc-rest-webapp/action-library-rest/action-library-rest-services/src/main/java/org/openecomp/sdcrests/action/rest/mapping/MapActionToActionResponseDto.java
+++ openecomp-be/api/openecomp-sdc-rest-webapp/action-library-rest/action-library-rest-services/src/main/java/org/openecomp/sdcrests/action/rest/mapping/MapActionToActionResponseDto.java
@@ -1,9 +1,7 @@
 /*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+ *
+ * Copyright © 2016-2017 European Support Limited *
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
