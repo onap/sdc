3d12318f6a6c314a63a304eb2e5180a49ede2e95
ojasdubey <ojas.dubey@amdocs.com>
2017-12-12 15:18:30 +0530

    Fix - Translator core tests stuck
    1. Updated configuration management code to
    prevent repeated database polling for if the
    connection is not successful
    
    2. Sonar violations will be handled as a
    separate task. The commit is specific to the
    build issue fix.
    
    Issue-ID: SDC-748
    Change-Id: Idc691bdc7e2b988654ae87c2581ccbdd86586aae
    Signed-off-by: ojasdubey <ojas.dubey@amdocs.com>

--- common/openecomp-common-configuration-management/openecomp-configuration-management-core/src/main/java/org/openecomp/config/impl/ConfigurationDataSource.java
+++ common/openecomp-common-configuration-management/openecomp-configuration-management-core/src/main/java/org/openecomp/config/impl/ConfigurationDataSource.java
@@ -52,6 +52,9 @@ public final class ConfigurationDataSource {
   public static BasicDataSource initDataSource() throws Exception {
     ImmutableConfiguration dbConfig = ConfigurationRepository.lookup()
         .getConfigurationFor(Constants.DEFAULT_TENANT, Constants.DB_NAMESPACE);
+    if ("0.0.0.0".equals(dbConfig.getString("dbhost"))) {
+      return null;
+    }
     BasicDataSource datasource = new BasicDataSource();
     String driverClassName = dbConfig.getString("driverClassName");
     String jdbcUrl = dbConfig.getString("jdbcURL");
