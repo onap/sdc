f783bfa7035656e68ed06ba1039ee17ce056a733
sheetalm <sheetal.mudholkar@amdocs.com>
2017-12-12 18:04:46 +0530

    Fixed sonar issues - ServiceArtifactEntity
    Add override annotations to methods. Remove no arg default constructor
    Corrected Copyright. Not fixing duplicate code violation since same
    code block exists but for different entities
    
    Change-Id: I64a25f2fe9805774f704700cd5a345db461f152a
    Issue-ID: SDC-343
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-model-lib/openecomp-sdc-model-api/src/main/java/org/openecomp/core/model/types/ServiceArtifactEntity.java
+++ openecomp-be/lib/openecomp-sdc-model-lib/openecomp-sdc-model-api/src/main/java/org/openecomp/core/model/types/ServiceArtifactEntity.java
@@ -1,21 +1,17 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.core.model.types;
@@ -61,9 +57,6 @@ public class ServiceArtifactEntity implements ServiceElementEntity {
   @Column(name = "content_data")
   public ByteBuffer contentData;
 
-  public ServiceArtifactEntity() {
-  }
-
   /**
    * Instantiates a new Service artifact entity.
    *
@@ -96,10 +89,12 @@ public class ServiceArtifactEntity implements ServiceElementEntity {
     return getId();
   }
 
+  @Override
   public String getId() {
     return id;
   }
 
+  @Override
   public void setId(String id) {
     this.id = id;
   }
@@ -114,6 +109,7 @@ public class ServiceArtifactEntity implements ServiceElementEntity {
     this.version = version;
   }
 
+  @Override
   public String getName() {
     return name;
   }
@@ -122,6 +118,7 @@ public class ServiceArtifactEntity implements ServiceElementEntity {
     this.name = name;
   }
 
+  @Override
   public ByteBuffer getContentData() {
     return contentData;
   }
