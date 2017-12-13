dedb4e0212df3c71e57e061ba382272f17200171
ojasdubey <ojas.dubey@amdocs.com>
2017-12-13 16:23:43 +0530

    Updated global type map as final
    Updated the global type initializer map as final
    
    Note: Sonar violations will be handled as a separate
    task
    
    Change-Id: I862247f5c693f4d64dff4ce18f2f71e42c85355a
    Issue-ID: SDC-749
    Signed-off-by: ojasdubey <ojas.dubey@amdocs.com>

--- openecomp-be/lib/openecomp-core-lib/openecomp-utilities-lib/src/main/java/org/openecomp/core/utilities/orchestration/OnboardingTypesEnum.java
+++ openecomp-be/lib/openecomp-core-lib/openecomp-utilities-lib/src/main/java/org/openecomp/core/utilities/orchestration/OnboardingTypesEnum.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecomp.core.utilities.orchestration;
 
 import java.util.Optional;
 
 import static java.util.Arrays.asList;
 public enum OnboardingTypesEnum {
-    CSAR("csar"), ZIP("zip"), NONE("none");
+    CSAR("csar"), ZIP("zip"), MANUAL("manual"), NONE("none");
     private String type;
 
     OnboardingTypesEnum(String type) {
