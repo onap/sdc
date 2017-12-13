ed3eebd8461c326c77f84e44d98e7f05734c7771
sheetalm <sheetal.mudholkar@amdocs.com>
2017-12-13 16:45:34 +0530

    Fixed sonar issues - ServiceTemplateEntity
    Add override annotations to methods. Remove no arg default constructor
    Corrected Copyright. Not fixing duplicate code violation since same
    code block exists but for different entities
    
    Change-Id: Ib776e280fb7e67100ac75f3312ad0c19dc709d46
    Issue-ID: SDC-343
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-model-lib/openecomp-sdc-model-api/src/main/java/org/openecomp/core/model/types/ServiceTemplateEntity.java
+++ openecomp-be/lib/openecomp-sdc-model-lib/openecomp-sdc-model-api/src/main/java/org/openecomp/core/model/types/ServiceTemplateEntity.java
@@ -1,21 +1,17 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.core.model.types;
@@ -66,9 +62,6 @@ public class ServiceTemplateEntity implements ServiceElementEntity {
   @Column(name = "base_name")
   private String baseName;
 
-  public ServiceTemplateEntity() {
-  }
-
   /**
    * Instantiates a new Service template entity.
    *
@@ -109,10 +102,12 @@ public class ServiceTemplateEntity implements ServiceElementEntity {
     return getId();
   }
 
+  @Override
   public String getId() {
     return id;
   }
 
+  @Override
   public void setId(String id) {
     this.id = id;
   }
@@ -127,6 +122,7 @@ public class ServiceTemplateEntity implements ServiceElementEntity {
     this.version = version;
   }
 
+  @Override
   public String getName() {
     return name;
   }
@@ -135,6 +131,7 @@ public class ServiceTemplateEntity implements ServiceElementEntity {
     this.name = name;
   }
 
+  @Override
   public ByteBuffer getContentData() {
     return contentData;
   }
