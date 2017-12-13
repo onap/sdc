f1fcfe3765ccbb40f95099a6f6f447122d3b7cee
mojahidi <mojahidul.islam@amdocs.com>
2017-12-13 17:04:17 +0530

    Fixed sonar issues - HeatResourceValidator
    Removed duplicate code block,
    removed unused method parameters and fixed other sonar violations
    
    Change-Id: Id1cd659af6ddd9e24d281a1bb0d1ae9431bcaf98
    Issue-ID: SDC-343
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-impl/src/test/java/org/openecomp/sdc/validation/impl/validators/HeatResourceValidatorTest.java
+++ openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-impl/src/test/java/org/openecomp/sdc/validation/impl/validators/HeatResourceValidatorTest.java
@@ -18,7 +18,7 @@ public class HeatResourceValidatorTest {
         "/org/openecomp/validation/validators/guideLineValidator/baseHeatDoesNotExposeNetwork/parseException/");
     Assert.assertEquals(messages.size(), 1);
     Assert.assertEquals(messages.get("first.yaml").getErrorMessageList().get(0).getMessage(),
-        "ERROR: [HTR1]: Invalid HEAT format problem - [while scanning for the next token\n" +
+        "ERROR: [RBV2]: Invalid HEAT format problem - [while scanning for the next token\n" +
             "found character '\\t(TAB)' that cannot start any token. (Do not use \\t(TAB) for indentation)\n" +
             " in 'reader', line 5, column 1:\n" +
             "    \t\t\tresources:\n" +
