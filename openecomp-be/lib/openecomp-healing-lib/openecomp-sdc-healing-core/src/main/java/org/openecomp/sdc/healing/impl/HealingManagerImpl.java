46058d74f8190307d6b568471d7d24f73c8afe5a
sheetalm <sheetal.mudholkar@amdocs.com>
2017-12-14 12:34:27 +0530

    Fixed sonar issues - HealingManagerImpl
    Make constant field as final. Corrected Copyright.
    
    Change-Id: I11a75f94c2aca1dd885780d92e1b7329b5a78750
    Issue-ID: SDC-343
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

--- openecomp-be/lib/openecomp-healing-lib/openecomp-sdc-healing-core/src/main/java/org/openecomp/sdc/healing/impl/HealingManagerImpl.java
+++ openecomp-be/lib/openecomp-healing-lib/openecomp-sdc-healing-core/src/main/java/org/openecomp/sdc/healing/impl/HealingManagerImpl.java
@@ -1,21 +1,17 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.sdc.healing.impl;
@@ -39,11 +35,8 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
 
-/**
- * Created by Talio on 11/29/2016.
- */
 public class HealingManagerImpl implements HealingManager {
-  private static String HEALING_CONF_FILE = "healingConfiguration.json";
+  private static final String HEALING_CONF_FILE = "healingConfiguration.json";
   private static Map<String, String> healerCodeToImplClass = initHealers();
 
   @Override
