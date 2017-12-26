342febacf55cc84403a72c4c677326648c8cf3cb
siddharth0905 <siddharth.singh4@amdocs.com>
2017-12-26 14:52:15 +0530

    Fix sonar issues
    Argument name change and no-op comment
    
    Change-Id: I3087310fe8dfedadbeac8408051fded0225838f2
    Issue-ID: SDC-343
    Signed-off-by: siddharth0905 <siddharth.singh4@amdocs.com>

--- openecomp-be/lib/openecomp-core-lib/openecomp-zusammen-lib/openecomp-zusammen-core/src/main/java/org/openecomp/core/zusammen/db/impl/ZusammenConnectorImpl.java
+++ openecomp-be/lib/openecomp-core-lib/openecomp-zusammen-lib/openecomp-zusammen-core/src/main/java/org/openecomp/core/zusammen/db/impl/ZusammenConnectorImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openecomp.core.zusammen.db.impl;
 
 import com.amdocs.zusammen.adaptor.inbound.api.health.HealthAdaptorFactory;
@@ -366,8 +381,8 @@ public class ZusammenConnectorImpl implements ZusammenConnector {
 
   @Override
   public void resetVersionHistory(SessionContext context, Id itemId, Id versionId,
-                                  String revision_id) {
-
+                                  String revisionId) {
+    // no-op, required by the interface
   }
 
   private RuntimeException buildGetElementException(ElementContext elementContext, Id elementId,
