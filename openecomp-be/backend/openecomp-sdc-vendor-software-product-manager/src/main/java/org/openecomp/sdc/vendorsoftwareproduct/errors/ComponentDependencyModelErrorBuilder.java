c07a90cf5ec29eaf8b3148f9cb3fdb2be4e5d0a0
mojahidi <mojahidul.islam@amdocs.com>
2017-12-26 20:10:02 +0530

    Fixed sonar issues - ComponentDependencyModel
    Fixed all sonar issues
    
    Change-Id: I4469b63b6a2291a7780e591edda36827c7bf51a1
    Issue-ID: SDC-343
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/errors/ComponentDependencyModelErrorBuilder.java
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/errors/ComponentDependencyModelErrorBuilder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecomp.sdc.vendorsoftwareproduct.errors;
 
 
@@ -21,7 +37,9 @@ public class ComponentDependencyModelErrorBuilder {
   private static final String SOURCE_TARGET_COMPONENT_EQUAL_MSG = "Source and target components "
       + "are same.";
 
+  private ComponentDependencyModelErrorBuilder(){
 
+  }
 
   public static ErrorCode getcyclicDependencyComponentErrorBuilder() {
     ErrorCode.ErrorCodeBuilder builder = new ErrorCode.ErrorCodeBuilder();
