d8e3fde0c8138fb2fbca5fcce1cf7dba2e835d4f
mojahidi <mojahidul.islam@amdocs.com>
2017-12-26 12:43:36 +0530

    Fixed sonar issues - LimitErrorBuilder
    Fixed all sonar issues
    
    Change-Id: I6243f40a4bf400f834166f813154455cae9b7ab1
    Issue-ID: SDC-343
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/backend/openecomp-sdc-vendor-license-manager/src/main/java/org/openecomp/sdc/vendorlicense/errors/LimitErrorBuilder.java
+++ openecomp-be/backend/openecomp-sdc-vendor-license-manager/src/main/java/org/openecomp/sdc/vendorlicense/errors/LimitErrorBuilder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecomp.sdc.vendorlicense.errors;
 
 
@@ -12,6 +28,10 @@ public class LimitErrorBuilder {
   private static final String DUPLICATE_LIMIT_NAME_NOT_ALLOWED_MSG =
       "Invalid request, Limit with name %s already exists for type %s.";
 
+  private LimitErrorBuilder(){
+
+  }
+
   public static ErrorCode getInvalidValueErrorBuilder(String attribute, String errorCode) {
     ErrorCode.ErrorCodeBuilder builder = new ErrorCode.ErrorCodeBuilder();
     builder.withId(errorCode);
@@ -24,7 +44,7 @@ public class LimitErrorBuilder {
     ErrorCode.ErrorCodeBuilder builder = new ErrorCode.ErrorCodeBuilder();
     builder.withId(VendorLicenseErrorCodes.DUPLICATE_LIMIT_NAME_NOT_ALLOWED);
     builder.withCategory(ErrorCategory.APPLICATION);
-    builder.withMessage(String.format(DUPLICATE_LIMIT_NAME_NOT_ALLOWED_MSG, name, type ));
+    builder.withMessage(String.format (DUPLICATE_LIMIT_NAME_NOT_ALLOWED_MSG, name, type ));
     return builder.build();
   }
 }
