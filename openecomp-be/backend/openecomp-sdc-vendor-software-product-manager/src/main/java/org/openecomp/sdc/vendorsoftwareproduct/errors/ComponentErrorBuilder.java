300a2f773b28201afec3a3b512b9fa49f185cd13
mojahidi <mojahidul.islam@amdocs.com>
2017-12-26 20:12:24 +0530

    Fixed sonar issues - ComponentErrorBuilder
    Fixed all sonar issues
    
    Change-Id: Ibcd3596e6935247c6f244be05d795caf9de30450
    Issue-ID: SDC-343
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/errors/ComponentErrorBuilder.java
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/errors/ComponentErrorBuilder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecomp.sdc.vendorsoftwareproduct.errors;
 
 import org.openecomp.sdc.common.errors.ErrorCategory;
@@ -9,8 +25,12 @@ public class ComponentErrorBuilder {
   private static final String VFC_INVALID_MISSING_IMAGE_MSG =
       "All VFC need to have atleast a single Image specified. Please fix the VFC Images and re-submit the VSP";
 
+  private ComponentErrorBuilder(){
 
-  public static ErrorCode VfcMissingImageErrorBuilder() {
+  }
+
+
+  public static ErrorCode vfcMissingImageErrorBuilder() {
     ErrorCode.ErrorCodeBuilder builder = new ErrorCode.ErrorCodeBuilder();
     builder.withId(VendorSoftwareProductErrorCodes.VFC_INVALID);
     builder.withCategory(ErrorCategory.APPLICATION);
