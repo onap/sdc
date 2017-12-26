8b3aaa6d0cceb2e80ec401e138fd192938e69568
sheetalm <sheetal.mudholkar@amdocs.com>
2017-12-26 10:45:35 +0530

    Fixed sonar issues - Healer
    Add functional interface annotation. Corrected Copyright
    
    Change-Id: Ia271a0c335756c8d4369d6921d43c9b9282bd412
    Issue-ID: SDC-343
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

--- openecomp-be/lib/openecomp-healing-lib/openecomp-sdc-healing-api/src/main/java/org/openecomp/sdc/healing/interfaces/Healer.java
+++ openecomp-be/lib/openecomp-healing-lib/openecomp-sdc-healing-api/src/main/java/org/openecomp/sdc/healing/interfaces/Healer.java
@@ -1,30 +1,24 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.sdc.healing.interfaces;
 
 import org.openecomp.sdc.versioning.dao.types.Version;
 
-/**
- * Created by Talio on 11/29/2016.
- */
+@FunctionalInterface
 public interface Healer {
     Object heal(String itemId, Version version) throws Exception;
 }
