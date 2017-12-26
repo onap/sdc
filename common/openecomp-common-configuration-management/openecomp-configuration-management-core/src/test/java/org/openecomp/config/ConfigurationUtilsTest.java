02ef1500af0a5e0ff9ed0fa9fd91e0092d3667af
shrek2000 <orenkle@amdocs.com>
2017-12-26 17:26:00 +0200

    Add a private constructor
    Change-Id: I540a7d6109e3b668e2e50e9501b0e4dcffc13ab9
    Issue-ID: SDC-793
    Signed-off-by: shrek2000 <orenkle@amdocs.com>

---
+++ common/openecomp-common-configuration-management/openecomp-configuration-management-core/src/test/java/org/openecomp/config/ConfigurationUtilsTest.java
@@ -0,0 +1,24 @@
+package org.openecomp.config;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class ConfigurationUtilsTest {
+    @Test
+    public void testCommaList() {
+        List list = Arrays.asList("1", "2", 3);
+        String commaSeparatedList = ConfigurationUtils.getCommaSeparatedList(list);
+        list.forEach(o -> assertTrue(commaSeparatedList.contains(o.toString())));
+    }
+
+    @Test
+    public void testCommaListWithNullAndEmptyStrings() {
+        List list = Arrays.asList(null, "");
+        String commaSeparatedList = ConfigurationUtils.getCommaSeparatedList(list);
+        assertTrue(commaSeparatedList.isEmpty());
+    }
+}
