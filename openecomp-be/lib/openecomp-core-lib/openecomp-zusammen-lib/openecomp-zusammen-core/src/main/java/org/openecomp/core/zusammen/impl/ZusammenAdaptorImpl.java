f3d102d49d04f8027ede3df4b4ded3fc3f436cfa
siddharth0905 <siddharth.singh4@amdocs.com>
2017-12-26 14:41:57 +0530

    Fix sonar issues
    Code correction as per suggestion
    
    Change-Id: Ia7772c1f257b90fc38ff4b12ee71b37354f7606b
    Issue-ID: SDC-343
    Signed-off-by: siddharth0905 <siddharth.singh4@amdocs.com>

--- openecomp-be/lib/openecomp-core-lib/openecomp-zusammen-lib/openecomp-zusammen-core/src/main/java/org/openecomp/core/zusammen/impl/ZusammenAdaptorImpl.java
+++ openecomp-be/lib/openecomp-core-lib/openecomp-zusammen-lib/openecomp-zusammen-core/src/main/java/org/openecomp/core/zusammen/impl/ZusammenAdaptorImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openecomp.core.zusammen.impl;
 
 import com.amdocs.zusammen.adaptor.inbound.api.types.item.Element;
@@ -57,8 +72,8 @@ public class ZusammenAdaptorImpl implements ZusammenAdaptor {
     Predicate<ElementInfo> elementInfoPredicate = elementInfo -> elementInfo.getInfo() != null
         && elementName.equals(elementInfo.getInfo().getName());
     return getFirstElementInfo(elementInfos, elementInfoPredicate)
-        .map(elementInfo -> getElement(context, elementContext, elementInfo.getId().getValue()))
-        .orElse(Optional.empty());
+        .flatMap(elementInfo -> getElement(context, elementContext,
+            elementInfo.getId().getValue()));
   }
 
   @Override
@@ -101,8 +116,8 @@ public class ZusammenAdaptorImpl implements ZusammenAdaptor {
     Collection<ElementInfo> elementInfos =
         connector.listElements(context, elementContext, parentElementId);
     return getFirstElementInfo(elementInfos,
-        elementInfo -> elementInfo.getInfo() != null &&
-            elementName.equals(elementInfo.getInfo().getName()));
+        elementInfo -> elementInfo.getInfo() != null
+           && elementName.equals(elementInfo.getInfo().getName()));
   }
 
   @Override
@@ -278,7 +293,7 @@ public class ZusammenAdaptorImpl implements ZusammenAdaptor {
 
   private static void sortItemVersionListByModificationTimeDescOrder(
       List<ItemVersion> itemVersions) {
-    itemVersions.sort((o1, o2) -> ((Integer) o2.getId().getValue().length())
-        .compareTo((o1.getId().getValue().length())));
+    itemVersions.sort((o1, o2) -> Integer.compare(o2.getId().getValue().length(),
+        o1.getId().getValue().length()));
   }
 }
