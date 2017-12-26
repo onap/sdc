e83f803552fcb8e5b9b6d17e0fbc33f85f0bf050
sheetalm <sheetal.mudholkar@amdocs.com>
2017-12-26 11:11:31 +0530

    Fixed sonar issues - ToscaExtensionYamlUtil
    Introduced new variable instead of using parameter. Added Copyright
    
    Change-Id: I8f7ff38c987acb1d6919c95e4a430df29927c1f9
    Issue-ID: SDC-343
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

--- common/openecomp-tosca-datatype/src/main/java/org/openecomp/sdc/tosca/services/ToscaExtensionYamlUtil.java
+++ common/openecomp-tosca-datatype/src/main/java/org/openecomp/sdc/tosca/services/ToscaExtensionYamlUtil.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecomp.sdc.tosca.services;
 
 import org.yaml.snakeyaml.constructor.Constructor;
@@ -25,16 +41,17 @@ public class ToscaExtensionYamlUtil extends YamlUtil {
     @Override
     public Property getProperty(Class<? extends Object> type, String name)
         throws IntrospectionException {
+      Class<? extends Object> classType = type;
       try {
         if (type
             .equals(Class.forName("org.openecomp.sdc.tosca.datatypes.model.ParameterDefinition"))) {
-          type = Class
+          classType = Class
               .forName("org.openecomp.sdc.tosca.datatypes.model.heatextend.ParameterDefinitionExt");
         }
       } catch (ClassNotFoundException ex) {
         throw new RuntimeException(ex);
       }
-      return super.getProperty(type, name);
+      return super.getProperty(classType, name);
     }
   }
 
