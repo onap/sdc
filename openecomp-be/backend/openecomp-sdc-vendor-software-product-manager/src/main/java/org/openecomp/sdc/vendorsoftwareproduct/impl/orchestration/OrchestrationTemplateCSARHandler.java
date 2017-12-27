5f52ba7ed8baf8a35a260c5a8ca87a2dbc044001
mojahidi <mojahidul.islam@amdocs.com>
2017-12-27 13:20:19 +0530

    Fixed sonar issues-OrchestrationTemplateCSARHandle
    Fixed all sonar issues
    
    Change-Id: I06e91fa886fd6dfc2b45fa5c8b0fdfc797ffd889
    Issue-ID: SDC-343
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/impl/orchestration/OrchestrationTemplateCSARHandler.java
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/impl/orchestration/OrchestrationTemplateCSARHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecomp.sdc.vendorsoftwareproduct.impl.orchestration;
 
 import org.apache.commons.lang3.tuple.Pair;
@@ -44,10 +60,12 @@ public class OrchestrationTemplateCSARHandler extends BaseOrchestrationTemplateH
       contentMap = fileContentMapFromOrchestrationCandidateZip.getKey();
       folderList = fileContentMapFromOrchestrationCandidateZip.getRight();
     } catch (IOException exception) {
+      logger.error("Invalid csar file ", exception);
       uploadFileResponse.addStructureError(
           SdcCommon.UPLOAD_FILE,
           new ErrorMessage(ErrorLevel.ERROR, Messages.INVALID_CSAR_FILE.getErrorMessage()));
     } catch (CoreException coreException) {
+      logger.error("Unexpected error occucrred ", coreException);
       uploadFileResponse.addStructureError(
           SdcCommon.UPLOAD_FILE, new ErrorMessage(ErrorLevel.ERROR, coreException.getMessage()));
     }
@@ -159,10 +177,4 @@ public class OrchestrationTemplateCSARHandler extends BaseOrchestrationTemplateH
     return OnboardingTypesEnum.CSAR;
   }
 
-  @Override
-  protected boolean isInvalidRawZipData(UploadFileResponse uploadFileResponse,
-                                        byte[] uploadedFileData,
-                                        CandidateService candidateService) {
-    return super.isInvalidRawZipData(uploadFileResponse, uploadedFileData, candidateService);
-  }
 }
