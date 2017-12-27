295a837eab12d44f5cf54589f8fd66b0123ad67f
mojahidi <mojahidul.islam@amdocs.com>
2017-12-27 11:18:42 +0530

    Fixed sonar issues - NotSupportedHeatOnboardMethod
    Fixed all sonar issues
    
    Change-Id: I877e1bdccf63a20a618dabfa9176d94cf023d2b8
    Issue-ID: SDC-343
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/errors/NotSupportedHeatOnboardMethodErrorBuilder.java
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/errors/NotSupportedHeatOnboardMethodErrorBuilder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecomp.sdc.vendorsoftwareproduct.errors;
 
 import org.openecomp.sdc.common.errors.ErrorCategory;
@@ -24,12 +40,16 @@ public class NotSupportedHeatOnboardMethodErrorBuilder {
   private static final String EDIT_DEPLOYMENT_FLAVOR_NOT_ALLOWED_MSG =
       "Deployment Flavor cannot be edited for VSPs onboarded with HEAT.";
 
+  private NotSupportedHeatOnboardMethodErrorBuilder() {
+    
+  }
+
 
   public static ErrorCode getAddNicNotSupportedHeatOnboardMethodErrorBuilder() {
     ErrorCode.ErrorCodeBuilder builder = new ErrorCode.ErrorCodeBuilder();
     builder.withId(ADD_NIC_NOT_ALLOWED_IN_HEAT_ONBOARDING);
     builder.withCategory(ErrorCategory.APPLICATION);
-    builder.withMessage(String.format(ADD_NIC_NOT_ALLOWED_IN_HEAT_ONBOARDING_MSG));
+    builder.withMessage(ADD_NIC_NOT_ALLOWED_IN_HEAT_ONBOARDING_MSG);
     return builder.build();
   }
 
@@ -37,7 +57,7 @@ public class NotSupportedHeatOnboardMethodErrorBuilder {
     ErrorCode.ErrorCodeBuilder builder = new ErrorCode.ErrorCodeBuilder();
     builder.withId(ADD_COMPUTE_NOT_ALLOWED_IN_HEAT_ONBOARDING);
     builder.withCategory(ErrorCategory.APPLICATION);
-    builder.withMessage(String.format(ADD_COMPUTE_NOT_ALLOWED_IN_HEAT_ONBOARDING_MSG));
+    builder.withMessage(ADD_COMPUTE_NOT_ALLOWED_IN_HEAT_ONBOARDING_MSG);
     return builder.build();
   }
 
@@ -45,7 +65,7 @@ public class NotSupportedHeatOnboardMethodErrorBuilder {
     ErrorCode.ErrorCodeBuilder builder = new ErrorCode.ErrorCodeBuilder();
     builder.withId(ADD_IMAGE_NOT_ALLOWED_IN_HEAT_ONBOARDING);
     builder.withCategory(ErrorCategory.APPLICATION);
-    builder.withMessage(String.format(IMAGE_ADD_NOT_ALLOWED_IN_HEAT_ONBOARDING_MSG));
+    builder.withMessage(IMAGE_ADD_NOT_ALLOWED_IN_HEAT_ONBOARDING_MSG);
     return builder.build();
   }
 
@@ -53,7 +73,7 @@ public class NotSupportedHeatOnboardMethodErrorBuilder {
     ErrorCode.ErrorCodeBuilder builder = new ErrorCode.ErrorCodeBuilder();
     builder.withId(DELETE_IMAGE_NOT_ALLOWED);
     builder.withCategory(ErrorCategory.APPLICATION);
-    builder.withMessage(String.format(DELETE_IMAGE_NOT_ALLOWED_MSG));
+    builder.withMessage(DELETE_IMAGE_NOT_ALLOWED_MSG);
     return builder.build();
   }
 
@@ -61,7 +81,7 @@ public class NotSupportedHeatOnboardMethodErrorBuilder {
     ErrorCode.ErrorCodeBuilder builder = new ErrorCode.ErrorCodeBuilder();
     builder.withId(DELETE_DEPLOYMENT_FLAVOR_NOT_ALLOWED_IN_HEAT_ONBOARDING);
     builder.withCategory(ErrorCategory.APPLICATION);
-    builder.withMessage(String.format(DELETE_DEPLOYMENT_FLAVOR_NOT_ALLOWED_MSG));
+    builder.withMessage(DELETE_DEPLOYMENT_FLAVOR_NOT_ALLOWED_MSG);
     return builder.build();
   }
 
@@ -69,7 +89,7 @@ public class NotSupportedHeatOnboardMethodErrorBuilder {
     ErrorCode.ErrorCodeBuilder builder = new ErrorCode.ErrorCodeBuilder();
     builder.withId(EDIT_DEPLOYMENT_FLAVOR_NOT_ALLOWED_IN_HEAT_ONBOARDING);
     builder.withCategory(ErrorCategory.APPLICATION);
-    builder.withMessage(String.format(EDIT_DEPLOYMENT_FLAVOR_NOT_ALLOWED_MSG));
+    builder.withMessage(EDIT_DEPLOYMENT_FLAVOR_NOT_ALLOWED_MSG);
     return builder.build();
   }
 
