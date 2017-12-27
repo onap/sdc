cded31cc93ffc0c72e279674d322a4621db297e8
sheetalm <sheetal.mudholkar@amdocs.com>
2017-12-27 15:44:02 +0530

    Fixed sonar issues ToscaFileOutputServiceCsarImpl
    Remove unwanted parenthesis. Corrected Copyright
    
    Change-Id: If7cb2a722aa486156d6dc2c1fb90877d0442b3a6
    Issue-ID: SDC-343
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

--- openecomp-be/lib/openecomp-tosca-lib/src/main/java/org/openecomp/sdc/tosca/services/impl/ToscaFileOutputServiceCsarImpl.java
+++ openecomp-be/lib/openecomp-tosca-lib/src/main/java/org/openecomp/sdc/tosca/services/impl/ToscaFileOutputServiceCsarImpl.java
@@ -1,21 +1,17 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.sdc.tosca.services.impl;
@@ -119,7 +115,7 @@ public class ToscaFileOutputServiceCsarImpl implements ToscaFileOutputService {
       throws IOException {
     String metaFile = createMetaFile(entryDefinitionsFileName);
     zos.putNextEntry(
-        new ZipEntry((TOSCA_META_FOLDER_NAME + FILE_SEPARATOR + TOSCA_META_FILE_NAME)));
+        new ZipEntry(TOSCA_META_FOLDER_NAME + FILE_SEPARATOR + TOSCA_META_FILE_NAME));
     writeBytesToZip(zos, new ByteArrayInputStream(metaFile.getBytes()));
   }
 
@@ -160,7 +156,7 @@ public class ToscaFileOutputServiceCsarImpl implements ToscaFileOutputService {
 
     for (String fileName : artifacts.getFileList()) {
       try {
-        zos.putNextEntry(new ZipEntry((ARTIFACTS_FOLDER_NAME + FILE_SEPARATOR + fileName)));
+        zos.putNextEntry(new ZipEntry(ARTIFACTS_FOLDER_NAME + FILE_SEPARATOR + fileName));
         writeBytesToZip(zos, artifacts.getFileContent(fileName));
 
       } catch (IOException ex) {
