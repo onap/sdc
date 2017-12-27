edfe54deac57b23d32d1fe3ac97b9084acccef9d
mojahidi <mojahidul.islam@amdocs.com>
2017-12-27 11:44:56 +0530

    Fixed sonar issues - NicErrorBuilder
    Fixed all sonar issues
    
    Change-Id: I379a055c19334721d430032c660668dcb6393ee8
    Issue-ID: SDC-343
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/errors/NicErrorBuilder.java
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/errors/NicErrorBuilder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecomp.sdc.vendorsoftwareproduct.errors;
 
 import org.openecomp.sdc.common.errors.ErrorCategory;
@@ -11,13 +27,15 @@ import static org.openecomp.sdc.vendorsoftwareproduct.errors.VendorSoftwareProdu
 public class NicErrorBuilder {
     private static final String NIC_NAME_FORMAT_MSG = "Field does not conform to predefined criteria"
             + ": name : must match %s";
+private NicErrorBuilder() {
 
+}
     /**
      * Gets image name format error builder.
      *
      * @return the image name format error builder
      */
-    public static ErrorCode getNicNameFormatErrorBuilder(String imageName, String pattern) {
+    public static ErrorCode getNicNameFormatErrorBuilder(String pattern) {
         ErrorCode.ErrorCodeBuilder builder = new ErrorCode.ErrorCodeBuilder();
         builder.withId(NIC_NAME_FORMAT_NOT_ALLOWED);
         builder.withCategory(ErrorCategory.APPLICATION);
