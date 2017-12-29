fbf3adc0a20be82e55a088b466e0e30862710d1b
Idan Amit <ia096e@intl.att.com>
2017-12-29 09:40:43 +0200

    SDC Designers Integration - Part 1
    Finished first part of designers integration
    Added templated configuration file
    Added tests
    
    Change-Id: I76e8b413063b939e1eb58da4a5502c4b526168f2
    Issue-ID: SDC-687
    Signed-off-by: Idan Amit <ia096e@intl.att.com>

--- catalog-ui/src/app/ng2/app.module.ts
+++ catalog-ui/src/app/ng2/app.module.ts
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -50,7 +50,10 @@ import { TranslationServiceConfig } from "./config/translation.service.config";
 export const upgradeAdapter = new UpgradeAdapter(forwardRef(() => AppModule));
 
 export function configServiceFactory(config:ConfigService) {
-    return () => config.loadValidationConfiguration();
+    return () => {
+        config.loadValidationConfiguration();
+        config.loadDesignersConfiguration();
+    }
 }
 
 
