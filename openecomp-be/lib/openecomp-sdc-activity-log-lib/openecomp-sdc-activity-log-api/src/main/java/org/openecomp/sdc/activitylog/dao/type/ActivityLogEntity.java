1de05ba162b61b32976b0e7d2e29fe3182a106cc
vempo <vitaliy.emporopulo@amdocs.com>
2017-12-29 16:22:45 +0200

    Default constructors in activity-log-api module
    Change-Id: Ibf61aaae8a8023b528eac5982e1488c26060a3e1
    Issue-ID: SDC-343
    Signed-off-by: vempo <vitaliy.emporopulo@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-activity-log-lib/openecomp-sdc-activity-log-api/src/main/java/org/openecomp/sdc/activitylog/dao/type/ActivityLogEntity.java
+++ openecomp-be/lib/openecomp-sdc-activity-log-lib/openecomp-sdc-activity-log-api/src/main/java/org/openecomp/sdc/activitylog/dao/type/ActivityLogEntity.java
@@ -44,7 +44,13 @@ public class ActivityLogEntity {
   private String message;
   private String comment;
 
+  /**
+   * Every entity class must have a default constructor according to
+   * <a href="http://docs.datastax.com/en/developer/java-driver/2.1/manual/object_mapper/creating/">
+   * Definition of mapped classes</a>.
+   */
   public ActivityLogEntity() {
+    // Don't delete! Default constructor is required by DataStax driver
   }
 
   public ActivityLogEntity(String itemId, Version version) {
