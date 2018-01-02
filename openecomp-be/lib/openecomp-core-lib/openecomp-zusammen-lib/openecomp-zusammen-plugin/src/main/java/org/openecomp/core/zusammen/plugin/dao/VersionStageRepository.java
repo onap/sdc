b38f18bb99a603012ec406053531c45fb4643d62
shalomb <shalomb@amdocs.com>
2018-01-02 13:48:40 +0200

    Remove Zusammen cassandra plugin
    Remove Zusammen cassandra plugin
    Change dependency
    Modify imports
    
    Change-Id: Ice66fdd9f3970961723df476c8d1e38040504189
    Issue-ID: SDC-862
    Signed-off-by: shalomb <shalomb@amdocs.com>

--- openecomp-be/lib/openecomp-core-lib/openecomp-zusammen-lib/openecomp-zusammen-plugin/src/main/java/org/openecomp/core/zusammen/plugin/dao/VersionStageRepository.java
+++
@@ -1,9 +0,0 @@
-package org.openecomp.core.zusammen.plugin.dao;
-
-
-import org.openecomp.core.zusammen.plugin.dao.types.VersionContext;
-import org.openecomp.core.zusammen.plugin.dao.types.VersionEntity;
-
-public interface VersionStageRepository extends StageRepository<VersionContext, VersionEntity> {
-
-}
