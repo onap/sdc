1fe0ae4026dcd55ec5eaa267546ed382dfea863d
Abhai Singh <Abhai.Singh@amdocs.com>
2018-01-03 16:28:47 +0530

    Fix Sonar violations - ValidationErrorCodes
    Fixed reported sonar issues by converting class with constant to enum
    
    Change-Id: Ica5835575222e96abc02132734b1e354d696ae35
    Issue-ID: SDC-343
    Signed-off-by: Abhai Singh <Abhai.Singh@amdocs.com>

--- openecomp-be/backend/openecomp-sdc-validation-manager/src/main/java/org/openecomp/sdc/validation/errors/ValidationErrorCodes.java
+++ openecomp-be/backend/openecomp-sdc-validation-manager/src/main/java/org/openecomp/sdc/validation/errors/ValidationErrorCodes.java
@@ -1,21 +1,17 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.sdc.validation.errors;
@@ -23,7 +19,16 @@ package org.openecomp.sdc.validation.errors;
 /**
  * Created by TALIO on 4/24/2016.
  */
-public class ValidationErrorCodes {
-  public static final String VALIDATION_INVALID = "VALIDATION_INVALID";
+public enum ValidationErrorCodes {
+  VALIDATION_INVALID("VALIDATION_INVALID");
+  private String val;
+
+  ValidationErrorCodes(String val) {
+    this.val = val;
+  }
+
+  public String getVal() {
+    return val;
+  }
 
 }
--- openecomp-be/backend/openecomp-sdc-validation-manager/src/main/java/org/openecomp/sdc/validation/errors/ValidationInvalidErrorBuilder.java
+++ openecomp-be/backend/openecomp-sdc-validation-manager/src/main/java/org/openecomp/sdc/validation/errors/ValidationInvalidErrorBuilder.java
@@ -1,21 +1,17 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.sdc.validation.errors;
@@ -37,7 +33,7 @@ public class ValidationInvalidErrorBuilder extends BaseErrorBuilder {
    * @param errors the errors
    */
   public ValidationInvalidErrorBuilder(Map<String, List<ErrorMessage>> errors) {
-    getErrorCodeBuilder().withId(ValidationErrorCodes.VALIDATION_INVALID);
+    getErrorCodeBuilder().withId(ValidationErrorCodes.VALIDATION_INVALID.getVal());
     getErrorCodeBuilder().withCategory(ErrorCategory.APPLICATION);
     getErrorCodeBuilder()
         .withMessage(String.format(VALIDATION_INVALID_DETAILED_MSG, toString(errors)));
@@ -47,7 +43,7 @@ public class ValidationInvalidErrorBuilder extends BaseErrorBuilder {
    * Instantiates a new Validation invalid error builder.
    */
   public ValidationInvalidErrorBuilder() {
-    getErrorCodeBuilder().withId(ValidationErrorCodes.VALIDATION_INVALID);
+    getErrorCodeBuilder().withId(ValidationErrorCodes.VALIDATION_INVALID.getVal());
     getErrorCodeBuilder().withCategory(ErrorCategory.APPLICATION);
     getErrorCodeBuilder().withMessage(VALIDATION_INVALID_MSG);
   }
