e7203a7c849491b4dfdada98cfe0041e9fd9cf2b
mojahidi <mojahidul.islam@amdocs.com>
2018-01-04 12:51:39 +0530

    Fixed sonar issues-MapImageRequestDtoToImageEntity
    Removed commented code
    
    Change-Id: I6b6b78653936800e76957d638df3574306605696
    Issue-ID: SDC-343
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/api/openecomp-sdc-rest-webapp/vendor-software-products-rest/vendor-software-products-rest-services/src/main/java/org/openecomp/sdcrests/vsp/rest/mapping/MapImageRequestDtoToImageEntity.java
+++ openecomp-be/api/openecomp-sdc-rest-webapp/vendor-software-products-rest/vendor-software-products-rest-services/src/main/java/org/openecomp/sdcrests/vsp/rest/mapping/MapImageRequestDtoToImageEntity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecomp.sdcrests.vsp.rest.mapping;
 
 import org.openecomp.sdc.vendorsoftwareproduct.dao.type.ImageEntity;
@@ -12,21 +28,6 @@ public class MapImageRequestDtoToImageEntity extends MappingBase<ImageRequestDto
     Image image = new Image();
     image.setFileName(source.getFileName());
     image.setDescription(source.getDescription());
-    /*try {
-      if (source.getFormat() != null) {
-        final ImageFormat imageFormat = ImageFormat.valueOf(source.getFormat());
-        image.setFormat(source.getFormat());
-      }
-    } catch (IllegalArgumentException exception) {
-      ErrorCode errorCode = ImageErrorBuilder.getInvalidImageFormatErrorBuilder();
-      MdcDataErrorMessage.createErrorMessageAndUpdateMdc(LoggerConstants.TARGET_ENTITY_DB,
-          LoggerTragetServiceName.CREATE_IMAGE, ErrorLevel.ERROR.name(),
-          errorCode.id(), errorCode.message() );
-      throw new CoreException(errorCode);
-    }
-    image.setMd5(source.getMd5());
-    image.setVersion(source.getVersion());
-    //image.setProvidedBy(source.getProvidedBy());*/
     target.setImageCompositionData(image);
   }
 
