f1a146d745f907445b2540c8830b668e03893bf7
Abhai Singh <Abhai.Singh@amdocs.com>
2018-01-04 19:07:11 +0530

    Fix VendorSoftwareProductErrorCodes
    Fixed reported sonar issues
    
    Change-Id: I48dfd9333ec0ce5bb105c13e7fcd648290236bf2
    Issue-ID: SDC-343
    Signed-off-by: Abhai Singh <Abhai.Singh@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-vendor-software-product-lib/openecomp-sdc-vendor-software-product-core/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/errors/VendorSoftwareProductErrorCodes.java
+++ openecomp-be/lib/openecomp-sdc-vendor-software-product-lib/openecomp-sdc-vendor-software-product-core/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/errors/VendorSoftwareProductErrorCodes.java
@@ -1,21 +1,17 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.sdc.vendorsoftwareproduct.errors;
@@ -129,4 +125,5 @@ public class VendorSoftwareProductErrorCodes {
   public static final String VSP_ONBOARD_METHOD_UPDATE_NOT_ALLOWED =
       "VSP_ONBOARD_METHOD_UPDATE_NOT_ALLOWED";
 
+  private VendorSoftwareProductErrorCodes(){}
 }
