9093c74016f85811f8fd4d455551874421e13fa3
shrikantawachar <shrikant.awachar@amdocs.com>
2018-01-04 18:33:53 +0530

    Fixing sonar qube issues
    EchoMapping.java is not used anywhere hence deleting it.
    
    Change-Id: Ifb26e29bacdd62d1137184b50b0b83a6fb2896e5
    Issue-ID: SDC-343
    Signed-off-by: shrikantawachar <shrikant.awachar@amdocs.com>

--- openecomp-be/api/openecomp-sdc-rest-webapp/openecomp-sdc-common-rest/src/main/java/org/openecomp/sdcrests/mapping/EchoMapping.java
+++
@@ -1,8 +0,0 @@
-package org.openecomp.sdcrests.mapping;
-
-public class EchoMapping extends MappingBase <Object,Object>{
-  @Override
-  public void doMapping(Object source, Object target) {
-    target = source;
-  }
-}
