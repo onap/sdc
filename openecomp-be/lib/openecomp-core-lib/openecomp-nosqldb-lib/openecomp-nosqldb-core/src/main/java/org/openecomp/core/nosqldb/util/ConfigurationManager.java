0fe4cb7ad3e5e91d6715f85dac5474eff1543149
siddharth0905 <siddharth.singh4@amdocs.com>
2018-01-08 12:19:42 +0530

    Fix sonar issues - ConfigurationManager
    NOSONAR from password constant and removed commented code
    
    Change-Id: If20c5a61ffdb4887b9baa4c049cabdf67c350fa3
    Issue-ID: SDC-343
    Signed-off-by: siddharth0905 <siddharth.singh4@amdocs.com>

--- openecomp-be/lib/openecomp-core-lib/openecomp-nosqldb-lib/openecomp-nosqldb-core/src/main/java/org/openecomp/core/nosqldb/util/ConfigurationManager.java
+++ openecomp-be/lib/openecomp-core-lib/openecomp-nosqldb-lib/openecomp-nosqldb-core/src/main/java/org/openecomp/core/nosqldb/util/ConfigurationManager.java
@@ -49,19 +49,20 @@ public class ConfigurationManager {
   private static final String CASSANDRA_DOX_KEY_STORE = "cassandra.dox.keystore";
   private static final String CASSANDRA_AUTHENTICATE = "cassandra.authenticate";
   private static final String CASSANDRA_USER = "cassandra.user";
-  private static final String CASSANDRA_PASSWORD = "cassandra.password";
+  private static final String CASSANDRA_PASSWORD = "cassandra.password";    //NOSONAR
   private static final String CASSANDRA_PORT = "cassandra.port";
   private static final String CASSANDRA_SSL = "cassandra.ssl";
   private static final String CASSANDRA_TRUSTSTORE = "cassandra.Truststore";
-  private static final String CASSANDRA_TRUSTSTORE_PASSWORD = "cassandra.TruststorePassword";
+  private static final String CASSANDRA_TRUSTSTORE_PASSWORD =               //NOSONAR
+      "cassandra.TruststorePassword";
   private static final String CASSANDRA_HOSTS_KEY = "cassandraHosts";
   private static final String CASSANDRA_PORT_KEY = "port";
   private static final String CASSANDRA_USERNAME_KEY = "username";
-  private static final String CASSANDRA_PASSWORD_KEY = "password";
+  private static final String CASSANDRA_PASSWORD_KEY = "password";          //NOSONAR
   private static final String CASSANDRA_AUTHENTICATE_KEY = "authenticate";
   private static final String CASSANDRA_SSL_KEY = "ssl";
   private static final String CASSANDRA_TRUSTSTORE_PATH_KEY = "truststorePath";
-  private static final String CASSANDRA_TRUSTSTORE_PASSWORD_KEY = "truststorePassword";
+  private static final String CASSANDRA_TRUSTSTORE_PASSWORD_KEY = "truststorePassword";   //NOSONAR
   private static final String CONSISTENCY_LEVEL = "cassandra.consistencyLevel";
   private static final String CONSISTENCY_LEVEL_KEY = "consistencyLevel";
   private static final String LOCAL_DATA_CENTER_KEY = "localDataCenter";
@@ -69,14 +70,14 @@ public class ConfigurationManager {
   private static ConfigurationManager instance = null;
   private final LinkedHashMap<String, Object> cassandraConfiguration;
 
-  private final Logger log = (Logger) LoggerFactory.getLogger(this.getClass().getName());
+  private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
 
   private ConfigurationManager() {
 
     String configurationYamlFile = System.getProperty(CONFIGURATION_YAML_FILE);
 
-    Function<InputStream, Map<String, LinkedHashMap<String, Object>>> reader = (is) -> {
+    Function<InputStream, Map<String, LinkedHashMap<String, Object>>> reader = is -> {
       YamlUtil yamlUtil = new YamlUtil();
       return yamlUtil.yamlToMap(is);
     };
@@ -135,8 +136,6 @@ public class ConfigurationManager {
   public String getKeySpace() {
     String keySpace = System.getProperty(CASSANDRA_DOX_KEY_STORE);
     if (Objects.isNull(keySpace)) {
-      //keySpace = cassandraConfiguration.get(cassandraKeySpaceKey);
-      //if (keySpace == null)
       keySpace = DEFAULT_KEYSPACE_NAME;
     }
     return keySpace;
