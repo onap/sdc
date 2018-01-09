81ad9aa3964eec60a1d431290fc23139aee2204d
vempo <vitaliy.emporopulo@amdocs.com>
2018-01-09 14:25:08 +0200

    Add Togglz REST API
    Supporting REST API, and Javascript for Togglz
    Issue-ID: SDC-881
    
    Change-Id: I026d3f190cb95c8943a85b26dfbcd13e856abe64
    Signed-off-by: shrek2000 <orenkle@amdocs.com>
    (cherry picked from commit 5a33f027cd2bc4253d5ebb7a71889ead65d095b2)
    Signed-off-by: vempo <vitaliy.emporopulo@amdocs.com>

---
+++ openecomp-be/api/openecomp-sdc-rest-webapp/togglz-rest/togglz-rest-services/src/main/java/org/openecomp/sdcrests/togglz/rest/mapping/MapToggleableFeatureToDto.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openecomp.sdcrests.togglz.rest.mapping;
+
+import org.openecomp.sdcrests.mapping.MappingBase;
+import org.openecomp.sdcrests.togglz.types.FeatureDto;
+import org.openecomp.sdcrests.togglz.types.FeatureSetDto;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+import  org.openecomp.sdc.common.togglz.ToggleStatus;
+
+public class MapToggleableFeatureToDto extends MappingBase<Collection<ToggleStatus>, FeatureSetDto> {
+
+    @Override
+    public void doMapping(Collection<ToggleStatus> source, FeatureSetDto target) {
+        if (source != null && !source.isEmpty()){
+            Set<FeatureDto> fDtos = source.stream().map(f -> new FeatureDto(f.name(), f.isActive()))
+                .collect(Collectors.toSet());
+            target.setFeatures(fDtos);
+        } else {
+            target.setFeatures(Collections.emptySet());
+        }
+    }
+}
