5a33f027cd2bc4253d5ebb7a71889ead65d095b2
shrek2000 <orenkle@amdocs.com>
2018-01-09 11:17:20 +0200

    Add Togglz REST API
    Supporting REST API, and Javascript for Togglz
    Issue-ID: SDC-881
    
    Change-Id: I026d3f190cb95c8943a85b26dfbcd13e856abe64
    Signed-off-by: shrek2000 <orenkle@amdocs.com>

---
+++ openecomp-be/api/openecomp-sdc-rest-webapp/togglz-rest/togglz-rest-types/src/main/java/org/openecomp/sdcrests/togglz/types/FeatureDto.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openecomp.sdcrests.togglz.types;
+
+import java.util.Objects;
+
+public class FeatureDto {
+    
+    private final String name;
+    private final boolean active;
+
+    public FeatureDto(String name, boolean active) {
+        this.name = name;
+        this.active = active;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FeatureDto)) return false;
+        FeatureDto that = (FeatureDto) o;
+        return Objects.equals(getName(), that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("FeatureDto{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", active=").append(active);
+        sb.append('}');
+        return sb.toString();
+    }
+}
