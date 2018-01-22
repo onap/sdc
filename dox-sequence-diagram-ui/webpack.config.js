9b49cd824fa759e2c9de65c9a0d1e3089ba457fa
svishnev <shlomo-stanisla.vishnevetskiy@amdocs.com>
2018-01-22 14:43:14 +0200

    Babel version upgarde in dox-sequence
    Issue-ID: SDC-944
    Change-Id: I4933e2904a01b96719e8d9ea04e00e1d94af6306
    Signed-off-by: svishnev <shlomo-stanisla.vishnevetskiy@amdocs.com>

--- dox-sequence-diagram-ui/webpack.config.js
+++ dox-sequence-diagram-ui/webpack.config.js
@@ -10,7 +10,6 @@ var devmode = (process.env.npm_lifecycle_event === 'start');
 
 var entry = [];
 if (devmode) {
-	entry.push('babel-polyfill');
 	entry.push(path.resolve(PATHS.SRC, 'lib/main.jsx'));
 } else {
 	entry.push(path.resolve(PATHS.SRC, 'lib/ecomp/asdc/sequencer/Sequencer.jsx'));
@@ -46,11 +45,6 @@ var config = {
                 test: /\.html$/,
                 include: path.join(PATHS.SRC, 'lib'),
                 loaders: ['raw-loader']
-            },
-			{
-                test: /\.json$/,
-                include: path.join(PATHS.SRC, 'lib'),
-                loaders: ['json-loader']
             },
             {
                 test: /\.svg$/,
