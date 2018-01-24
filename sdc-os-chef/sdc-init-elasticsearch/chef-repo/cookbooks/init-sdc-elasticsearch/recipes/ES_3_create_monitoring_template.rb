e7a5aa84fa596f7b86bf1004c8237595a75c793f
Tal Gitelman <tg851x@intl.att.com>
2018-01-24 17:59:53 +0200

    ES OOM alignment
    Change-Id: I986fc13f8e32a103e3868f4da9ffb41631c70201
    Issue-ID: SDC-912
    Signed-off-by: Tal Gitelman <tg851x@intl.att.com>

--- sdc-os-chef/sdc-init-elasticsearch/chef-repo/cookbooks/init-sdc-elasticsearch/recipes/ES_3_create_monitoring_template.rb
+++ sdc-os-chef/sdc-init-elasticsearch/chef-repo/cookbooks/init-sdc-elasticsearch/recipes/ES_3_create_monitoring_template.rb
@@ -2,8 +2,7 @@ ruby_block "check_ElasticSearch_Cluster_Health" do
     block do
       #tricky way to load this Chef::Mixin::ShellOut utilities
       Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
-      #curl_command = "http://#{node['ipaddress']}:9200/_cluster/health?pretty=true"
-      curl_command = "http://localhost:9200/_cluster/health?pretty=true"
+      curl_command = "http://#{node['Nodes']['ES']}:9200/_cluster/health?pretty=true"
       resp = Net::HTTP.get_response URI.parse(curl_command)
       stat = JSON.parse(resp.read_body)['status']
 
@@ -14,6 +13,7 @@ ruby_block "check_ElasticSearch_Cluster_Health" do
             printf("\033[33m%s\n\033[0m", "  ElasticSearch Cluster status is yellow...")
          when "red"
             printf("\033[31m%s\n\033[0m", "  ElasticSearch Cluster status is red!")
+            break;
       end
    end
    retries 10
@@ -48,6 +48,6 @@ bash "create monitoring mapping" do
 				}
 			},
 			"aliases": { "last_3_months": {} }
-		}' http://localhost:9200/_template/monitoring_template
+		}' http://#{node['Nodes']['ES']}:9200/_template/monitoring_template
 	EOH
 end
