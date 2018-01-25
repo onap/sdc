abe01de6b943c6e9bd7a3e662b347cd33b841906
Tal Gitelman <tg851x@intl.att.com>
2018-01-25 16:05:11 +0200

    ES OOM alignment
    Change-Id: I4cbae682df510f1b301c40ef0a1d4cc5d244fce8
    Issue-ID: SDC-912
    Signed-off-by: Tal Gitelman <tg851x@intl.att.com>

--- sdc-os-chef/sdc-elasticsearch/chef-repo/cookbooks/sdc-elasticsearch/recipes/ES_1_setup_elasticsearch.rb
+++ sdc-os-chef/sdc-elasticsearch/chef-repo/cookbooks/sdc-elasticsearch/recipes/ES_1_setup_elasticsearch.rb
@@ -1,12 +1,10 @@
-clusterName = node['elasticsearch'][:cluster_name]+node.chef_environment
-
 template "/usr/share/elasticsearch/config/elasticsearch.yml" do
    source "ES-elasticsearch.yml.erb"
    owner "elasticsearch"
    group "elasticsearch"
    mode "0755"
    variables({
-        :cluster_name => "#{clusterName}",
+        :cluster_name => node['elasticsearch'][:cluster_name]+node.chef_environment,
         :node_name => node[:hostname],
         :ES_IP => node['Nodes']['ES'],
         :num_of_shards => node['elasticsearch'][:num_of_shards],
