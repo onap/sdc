edd7859b25d2ec9cf910ca316f9e3ed345619131
vempo <vitaliy.emporopulo@amdocs.com>
2018-01-29 15:26:59 +0200

    Removed MdcUtil
    Validation messages are propagated to the client and should not be logged as errors.
    No application modules can depend on logging-core.
    
    Change-Id: I36eb2f9de195e77d5008cc69dbc7b65643fe2322
    Issue-ID: SDC-875
    Signed-off-by: vempo <vitaliy.emporopulo@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-api/src/main/java/org/openecomp/core/validation/types/GlobalValidationContext.java
+++ openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-api/src/main/java/org/openecomp/core/validation/types/GlobalValidationContext.java
@@ -1,21 +1,17 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.core.validation.types;
@@ -25,9 +21,6 @@ import org.openecomp.core.validation.ErrorMessageCode;
 import org.openecomp.sdc.datatypes.error.ErrorLevel;
 import org.openecomp.sdc.logging.api.Logger;
 import org.openecomp.sdc.logging.api.LoggerFactory;
-import org.openecomp.sdc.logging.context.MdcUtil;
-import org.openecomp.sdc.logging.types.LoggerConstants;
-import org.openecomp.sdc.logging.types.LoggerErrorCode;
 
 import java.io.InputStream;
 import java.util.Collection;
@@ -39,9 +32,9 @@ import java.util.stream.Collectors;
 
 public class GlobalValidationContext {
 
-  private static Logger logger = (Logger) LoggerFactory.getLogger(GlobalValidationContext.class);
-  private Map<String, FileValidationContext> fileContextMap = new HashMap<>();
-  private Map<String, MessageContainer> messageContainerMap = new HashMap<>();
+  private static final Logger LOGGER = LoggerFactory.getLogger(GlobalValidationContext.class);
+  private final Map<String, FileValidationContext> fileContextMap = new HashMap<>();
+  private final Map<String, MessageContainer> messageContainerMap = new HashMap<>();
   private ErrorMessageCode messageCode;
 
   public ErrorMessageCode getMessageCode() {
@@ -59,28 +52,21 @@ public class GlobalValidationContext {
    * @param fileName the file name
    * @param level    the level
    * @param message  the message
-   * @param targetServiceName  the target service name
-   * @param errorDescription  the error details
+   * @param targetService  the target service name
+   * @param description  validation event details
    */
   public void addMessage(String fileName, ErrorLevel level, String message,
-                         String targetServiceName, String errorDescription) {
+                         String targetService, String description) {
 
-    printLog(fileName, message, level, targetServiceName, errorDescription);
+    LOGGER.debug("{}: {} [{}]. Target: {}. Description: {}", fileName, message, level, targetService, description);
 
     if (fileContextMap.containsKey(fileName)) {
       fileContextMap.get(fileName).getMessageContainer().getMessageBuilder()
           .setMessage(level.toString() + ": " + message).setLevel(level).create();
     } else {
-//      if (CommonMethods.isEmpty(fileName)) {
-//        fileName = SdcCommon.UPLOAD_FILE;
-//      }
       MessageContainer messageContainer;
       synchronized (this) {
-        messageContainer = messageContainerMap.get(fileName);
-        if (messageContainer == null) {
-          messageContainer = new MessageContainer();
-          messageContainerMap.put(fileName, messageContainer);
-        }
+        messageContainer = messageContainerMap.computeIfAbsent(fileName, k -> new MessageContainer());
       }
       messageContainer.getMessageBuilder().setMessage(level.toString() + ": " + message)
           .setLevel(level).create();
@@ -127,29 +113,6 @@ public class GlobalValidationContext {
   public Map<String, FileValidationContext> getFileContextMap() {
     return fileContextMap;
   }
-
-  private void printLog(String fileName, String message, ErrorLevel level, String targetServiceName,
-                        String errorDescription) {
-
-    String messageToPrint = message + " in file[" + fileName + "]";
-    MdcUtil.setValuesForMdc(LoggerConstants.TARGET_ENTITY_API, targetServiceName, level.name(),
-        LoggerErrorCode.DATA_ERROR.getErrorCode(), errorDescription);
-
-    switch (level) {
-      case ERROR:
-        logger.error(messageToPrint);
-        break;
-      case WARNING:
-        logger.warn(messageToPrint);
-        break;
-      case INFO:
-        logger.info(messageToPrint);
-        break;
-      default:
-        break;
-    }
-  }
-
 
   public Collection<String> files(BiPredicate<String, GlobalValidationContext> func) {
     return fileContextMap.keySet().stream().filter(t -> func.test(t, this))
