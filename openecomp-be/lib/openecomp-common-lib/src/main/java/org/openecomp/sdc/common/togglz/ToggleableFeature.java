a628c458288d43d1e26bf750e08205e3559f93c3
katy.rotman <katy.rotman@amdocs.com>
2018-02-04 16:38:32 +0200

    Added validation-VirtualMachineInterface resource
    Issue-ID: SDC-994
    Change-Id: I0fd23862eebf65437ab0e1a1ad1d44c9a2944990
    Signed-off-by: katy.rotman <katy.rotman@amdocs.com>

--- openecomp-be/lib/openecomp-common-lib/src/main/java/org/openecomp/sdc/common/togglz/ToggleableFeature.java
+++ openecomp-be/lib/openecomp-common-lib/src/main/java/org/openecomp/sdc/common/togglz/ToggleableFeature.java
@@ -1,15 +1,20 @@
 package org.openecomp.sdc.common.togglz;
 
-import org.togglz.core.Feature;
 import org.togglz.core.annotation.Label;
 import org.togglz.core.context.FeatureContext;
 
-public enum ToggleableFeature implements Feature {
+public enum ToggleableFeature implements ToggleStatus {
 
   @Label ("Forwarder Capability")
-  FORWARDER_CAPABILITY;
+  FORWARDER_CAPABILITY,
 
+  @Label ("VLAN Tagging")
+  VLAN_TAGGING;
+
+  @Override
   public boolean isActive() {
     return FeatureContext.getFeatureManager().isActive(this);
   }
+
+
 }
