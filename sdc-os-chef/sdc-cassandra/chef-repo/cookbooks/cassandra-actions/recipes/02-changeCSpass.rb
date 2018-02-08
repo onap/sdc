eeec504fb7251cc282482d505fcf35ae3e7097f2
Areli Fuss <af732p@att.com>
2018-02-09 00:08:52 +0200

    Add K8S deployment above Vagrant
    Set deployment manifest files and scripts
    for deploy SDC over Kubernetes inside
    Vagrant
    Preparation for OOM integration
    
    Change-Id: If5d6b4131b75eb4795ce8bfd8ec228808938d40e
    Issue-ID: SDC-907
    Signed-off-by: Areli Fuss <af732p@att.com>

--- sdc-os-chef/sdc-cassandra/chef-repo/cookbooks/cassandra-actions/recipes/02-changeCSpass.rb
+++ sdc-os-chef/sdc-cassandra/chef-repo/cookbooks/cassandra-actions/recipes/02-changeCSpass.rb
@@ -1,9 +1,17 @@
+interface = node['interfaces']['application']
+application_host = ''
+node['network']['interfaces'][interface][:addresses].each do | addr , details |
+    if details['family'] == ('inet')
+        application_host = addr
+    end
+end
+
 template "/tmp/change_cassandra_pass.sh" do
   source "change_cassandra_pass.sh.erb"
   sensitive true
   mode 0755
   variables({
-     :cassandra_ip => node['Nodes']['CS'],
+     :cassandra_ip => application_host,
      :cassandra_pwd => ENV['CS_PASSWORD']
   })
 end
--- sdc-os-chef/sdc-cassandra/chef-repo/cookbooks/cassandra-actions/recipes/03-prepareProbeFile.rb
+++ sdc-os-chef/sdc-cassandra/chef-repo/cookbooks/cassandra-actions/recipes/03-prepareProbeFile.rb
@@ -1,9 +1,17 @@
+interface = node['interfaces']['application']
+application_host = ''
+node['network']['interfaces'][interface][:addresses].each do | addr , details |
+    if details['family'] == ('inet')
+        application_host = addr
+    end
+end
+
 template "/var/lib/ready-probe.sh" do
   source "ready-probe.sh.erb"
   sensitive true
   mode 0755
   variables({
-     :cassandra_ip => node['Nodes']['CS'],
+     :cassandra_ip => application_host,
      :cassandra_pwd => ENV['CS_PASSWORD']
   })
 end
