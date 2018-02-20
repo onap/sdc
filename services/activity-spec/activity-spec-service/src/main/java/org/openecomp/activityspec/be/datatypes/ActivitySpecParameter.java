b7fb942c128ebc0931dd6cab9242fc2d264bd05d
sheetalm <sheetal.mudholkar@amdocs.com>
2018-02-20 11:23:22 +0530

    Activity Spec Service - Data Types and DTOs
    Adding Activity Spec Data Types and DTOs.
    POM files will be added in coming reviews with other code.
    
    Change-Id: I10d489c469bbf345b3da2d5748f5bade29115bbf
    Issue-ID: SDC-1048
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

---
+++ services/activity-spec/activity-spec-service/src/main/java/org/openecomp/activityspec/be/datatypes/ActivitySpecParameter.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2016-2018 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openecomp.activityspec.be.datatypes;
+
+public class ActivitySpecParameter {
+  private String name;
+  private String type;
+  private String value;
+
+  public ActivitySpecParameter(){/*default constructor*/}
+
+
+  public ActivitySpecParameter(String name, String type) {
+    this.name=name;
+    this.type=type;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value){
+    this.value = value;
+  }
+}
