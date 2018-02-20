089f643ece95d05f09378df392cd78531379e3b3
Michael Lando <ml636r@att.com>
2018-02-20 16:14:11 +0200

    add https support to simulato
    Change-Id: I16f68532a7e7a09fbf96c09dff19d857c03fedd8
    Issue-ID: SDC-832
    Signed-off-by: Michael Lando <ml636r@att.com>

--- utils/webseal-simulator/sdc-simulator/chef-repo/cookbooks/sdc-simulator/recipes/SDC_Simulator_5_create_jetty_modules.rb
+++ utils/webseal-simulator/sdc-simulator/chef-repo/cookbooks/sdc-simulator/recipes/SDC_Simulator_5_create_jetty_modules.rb
@@ -1,7 +1,6 @@
 jetty_base="/var/lib/jetty"
 jetty_home="/usr/local/jetty"
 
-###### create Jetty modules
 bash "create-jetty-modules" do
 cwd "#{jetty_base}"
 code <<-EOH
@@ -9,6 +8,29 @@ code <<-EOH
    java -jar "/#{jetty_home}"/start.jar --add-to-start=deploy
    java -jar "/#{jetty_home}"/start.jar --add-to-startd=http,https,logging,setuid
 EOH
-not_if "ls /#{jetty_base}/start.d/https.ini"
+end
+
+template "ssl-ini" do
+   path "/#{jetty_base}/start.d/ssl.ini"
+   source "SDC-Simulator-ssl-ini.erb"
+   owner "jetty"
+   group "jetty"
+   mode "0755"
+   variables({
+        :https_port           => "8443",
+        :jetty_keystore_pwd   => "OBF:1vny1zlo1x8e1vnw1vn61x8g1zlu1vn4",
+        :jetty_keymanager_pwd => "OBF:1u2u1wml1z7s1z7a1wnl1u2g"
+   })
+end
+
+template "https-ini" do
+   path "/#{jetty_base}/start.d/https.ini"
+   source "SDC-Simulator-https-ini.erb"
+   owner "jetty"
+   group "jetty"
+   mode "0755"
+   variables ({
+        :https_port => "8443"
+   })
 end
 
