f7550f359e506b1eb8f75a818b1c1a9508f0e9b6
Michael Lando <ml636r@att.com>
2018-02-20 16:14:11 +0200

    add https support to simulato
    Change-Id: I16f68532a7e7a09fbf96c09dff19d857c03fedd8
    Issue-ID: SDC-832
    Signed-off-by: Michael Lando <ml636r@att.com>

--- utils/webseal-simulator/sdc-simulator/startup.sh
+++ utils/webseal-simulator/sdc-simulator/startup.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
-export CHEFNAME=${ENVNAME}
-cd /root/chef-solo
-echo "normal['HOST_IP'] = \"${HOST_IP}\"" > /root/chef-solo/cookbooks/sdc-simulator/attributes/default.rb
-chef-solo -c solo.rb -E ${CHEFNAME}
 
-sed -i '/^set -e/aJAVA_OPTIONS=\"-Xdebug -Xmx128m -Xms128m -Xss1m -Dconfig.home=${JETTY_BASE}/config/sdc-simulator -Dlog.home=${JETTY_BASE}/logs -Dlogback.configurationFile=${JETTY_BASE}/config/sdc-simulator/logback.xml -Djetty.logging.dir=${JETTY_BASE}/logs -Djetty.base=${JETTY_BASE} \"' /docker-entrypoint.sh 
-sed -i '/^set -e/aTMPDIR=${JETTY_BASE}\/temp' /docker-entrypoint.sh
+
+cd /root/chef-solo
+chef-solo -c solo.rb -E ${ENVNAME}
+rc=$?
+if [[ $rc != 0 ]]; then
+    echo "Chef exaction failed."
+    exit $rc;
+fi
+
+JAVA_OPTIONS=" ${JAVA_OPTIONS} \
+                -Dconfig.home=${JETTY_BASE}/config/sdc-simulator \
+                -Dlog.home=${JETTY_BASE}/logs \
+                -Dlogback.configurationFile=${JETTY_BASE}/config/sdc-simulator/logback.xml \
+                -Djetty.logging.dir=${JETTY_BASE}/logs"
+
+echo "---------------------------------------------"
+echo $TMPDIR
+echo $JAVA_OPTIONS
+echo $JETTY_BASE
+echo "---------------------------------------------"
 
 cd /var/lib/jetty
 /docker-entrypoint.sh 
