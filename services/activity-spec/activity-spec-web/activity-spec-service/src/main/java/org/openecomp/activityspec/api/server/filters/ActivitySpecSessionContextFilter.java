60b0696dfa7e48f5de4721bc6a6bcd75490b6c78
sheetalm <sheetal.mudholkar@amdocs.com>
2018-02-20 19:06:27 +0530

    Activity Spec Service - Implementation
    Patch Set 4 Not fixing Spring related review comments in this patch set
    Patch Set 4 Fixed review comments related to unused dependency
    Fixed review comments for plugin version. Corrected License
    Adding Activity Spec Service Implementation
    Not adding to main sdc pom as of now
    
    Change-Id: I5285c0ab3b71b492d18ca442ebd52b59ab0eabdc
    Issue-ID: SDC-1048
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

--- services/activity-spec/activity-spec-web/activity-spec-service/src/main/java/org/openecomp/activityspec/api/server/filters/ActivitySpecSessionContextFilter.java
+++ services/activity-spec/activity-spec-web/activity-spec-service/src/main/java/org/openecomp/activityspec/api/server/filters/ActivitySpecSessionContextFilter.java
@@ -14,12 +14,24 @@
  * limitations under the License.
  */
 
-package org.openecomp.sdc.versioning.dao.types;
+package org.openecomp.activityspec.api.server.filters;
 
-public enum VersionStatus {
-  Draft,
-  Locked, /* todo remove!!*/
-  Certified,
-  Deprecated,
-  Deleted
+import org.openecomp.sdcrests.filters.SessionContextFilter;
+
+import javax.servlet.ServletRequest;
+
+public class ActivitySpecSessionContextFilter extends SessionContextFilter {
+
+  private static final String TENANT = "activity_spec";
+  private static final String USER = "activity_spec_USER";
+
+  @Override
+  public String getUser(ServletRequest servletRequest) {
+    return USER;
+  }
+
+  @Override
+  public String getTenant(ServletRequest servletRequest) {
+    return TENANT;
+  }
 }
