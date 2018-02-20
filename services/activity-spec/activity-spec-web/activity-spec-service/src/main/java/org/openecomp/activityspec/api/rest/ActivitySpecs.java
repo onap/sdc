55081a77f482aeaeecdad3d11577f68f67bbf336
sheetalm <sheetal.mudholkar@amdocs.com>
2018-02-20 19:06:27 +0530

    Activity Spec Service - Implementation
    Changing structrue as per review comments
    Adding Activity Spec Service Implementation
    Not adding to main sdc pom as of now
    
    Change-Id: I5285c0ab3b71b492d18ca442ebd52b59ab0eabdc
    Issue-ID: SDC-1048
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

---
+++ services/activity-spec/activity-spec-web/activity-spec-service/src/main/java/org/openecomp/activityspec/api/rest/ActivitySpecs.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright © 2016-2018 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openecomp.activityspec.api.rest;
+
+import static org.openecomp.sdcrests.common.RestConstants.USER_ID_HEADER_PARAM;
+import static org.openecomp.sdcrests.common.RestConstants.USER_MISSING_ERROR_MSG;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.openecomp.activityspec.api.rest.types.ActivitySpecActionRequestDto;
+import org.openecomp.sdcrests.common.RestConstants;
+import org.openecomp.activityspec.api.rest.types.ActivitySpecRequestDto;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/v1.0/activity-spec/")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Api(value = "Activity Specs")
+@Validated
+public interface ActivitySpecs {
+  @POST
+  @Path("/")
+  @ApiOperation(value = "Create Activity Spec")
+  Response createActivitySpec(@Valid ActivitySpecRequestDto request,
+                              @NotNull(message = USER_MISSING_ERROR_MSG)
+                              @HeaderParam(RestConstants.USER_ID_HEADER_PARAM) String user);
+
+  @GET
+  @Path("/{id}/versions/{versionId}")
+  @ApiOperation(value = "Get Activity Spec")
+  Response getActivitySpec(@ApiParam(value = "Activity Spec Id") @PathParam("id")
+                                  String id,
+                              @ApiParam(value = "Version Id") @PathParam("versionId")
+                                  String versionId,
+                              @NotNull(message = USER_MISSING_ERROR_MSG)
+                              @HeaderParam(RestConstants.USER_ID_HEADER_PARAM) String user);
+
+  @PUT
+  @Path("/{id}/versions/{versionId}")
+  @ApiOperation(value = "Update Activity Spec")
+  Response updateActivitySpec(@Valid ActivitySpecRequestDto request,
+                              @ApiParam(value = "Activity Spec Id") @PathParam("id")
+                                  String id,
+                              @ApiParam(value = "Version Id") @PathParam("versionId")
+                                  String versionId,
+                              @NotNull(message = USER_MISSING_ERROR_MSG)
+                              @HeaderParam(RestConstants.USER_ID_HEADER_PARAM) String user);
+
+  @PUT
+  @Path("/{id}/versions/{versionId}/actions")
+  @ApiOperation(value = "Actions on a activity spec",
+      notes = "Performs one of the following actions on a activity spec: |"
+          + "Submit: Finalize its active version.|"
+          + "Deprecate: Deprecate activity spec.|")
+  Response actOnActivitySpec(ActivitySpecActionRequestDto request,
+      @ApiParam(value = "Activity Spec Id") @PathParam("id") String id,
+      @ApiParam(value = "Version Id") @PathParam("versionId") String versionId,
+      @NotNull(message = USER_MISSING_ERROR_MSG)
+      @HeaderParam(USER_ID_HEADER_PARAM) String user);
+
+  @GET
+  @Path("/")
+  @ApiOperation(value = "Get list of activity specs ",
+      responseContainer = "List")
+  Response list(@ApiParam(
+      value = "Currently supported values: 'Certified' - only activity specs with Certified status")
+                    @QueryParam("status") String versionStatus,
+                    @NotNull(message = USER_MISSING_ERROR_MSG)
+                    @HeaderParam(USER_ID_HEADER_PARAM) String user);
+}
---
+++ services/activity-spec/activity-spec-web/activity-spec-service/src/main/java/org/openecomp/activityspec/api/rest/services/ActivitySpecsImpl.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright © 2016-2018 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openecomp.activityspec.api.rest.services;
+
+import org.openecomp.activityspec.api.rest.ActivitySpecs;
+import org.openecomp.activityspec.api.rest.mapping.MapActivitySpecRequestDtoToActivitySpecEntity;
+import org.openecomp.activityspec.api.rest.mapping.MapActivitySpecToActivitySpecCreateResponse;
+import org.openecomp.activityspec.api.rest.mapping.MapActivitySpecToActivitySpecGetResponse;
+import org.openecomp.activityspec.api.rest.mapping.MapItemToListResponseDto;
+import org.openecomp.activityspec.api.rest.types.ActivitySpecActionRequestDto;
+import org.openecomp.activityspec.api.rest.types.ActivitySpecCreateResponse;
+import org.openecomp.activityspec.api.rest.types.ActivitySpecGetResponse;
+import org.openecomp.activityspec.api.rest.types.ActivitySpecListResponseDto;
+import org.openecomp.activityspec.api.rest.types.ActivitySpecRequestDto;
+import org.openecomp.activityspec.be.ActivitySpecManager;
+import org.openecomp.activityspec.be.dao.impl.ActivitySpecDaoZusammenImpl;
+import org.openecomp.activityspec.be.dao.types.ActivitySpecEntity;
+import org.openecomp.activityspec.be.impl.ActivitySpecManagerImpl;
+import org.openecomp.core.dao.UniqueValueDaoFactory;
+import org.openecomp.core.zusammen.api.ZusammenAdaptorFactory;
+import org.openecomp.sdc.versioning.ItemManagerFactory;
+import org.openecomp.sdc.versioning.VersioningManagerFactory;
+import org.openecomp.sdc.versioning.dao.types.Version;
+
+import org.openecomp.sdcrests.wrappers.GenericCollectionWrapper;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import javax.inject.Named;
+import javax.ws.rs.core.Response;
+
+@Named
+@Service("activitySpecs")
+@Scope(value = "prototype")
+@Validated
+public class ActivitySpecsImpl implements ActivitySpecs {
+
+
+  private final ActivitySpecManager activitySpecManager =
+      new ActivitySpecManagerImpl(ItemManagerFactory.getInstance().createInterface(),
+          VersioningManagerFactory.getInstance().createInterface(),
+          new ActivitySpecDaoZusammenImpl(ZusammenAdaptorFactory.getInstance().createInterface()),
+          UniqueValueDaoFactory.getInstance().createInterface());
+
+  @Override
+  public Response createActivitySpec(ActivitySpecRequestDto request, String user) {
+    ActivitySpecEntity activitySpec = new MapActivitySpecRequestDtoToActivitySpecEntity()
+        .applyMapping(request, ActivitySpecEntity.class);
+
+    activitySpec = activitySpecManager.createActivitySpec(activitySpec);
+    ActivitySpecCreateResponse createActivitySpecResponse =
+        new MapActivitySpecToActivitySpecCreateResponse().applyMapping(activitySpec,
+            ActivitySpecCreateResponse.class);
+
+    return Response.ok(createActivitySpecResponse).build();
+  }
+
+  @Override
+  public Response getActivitySpec(String activitySpecId, String versionId, String user) {
+    ActivitySpecEntity activitySpec = new ActivitySpecEntity();
+    activitySpec.setId(activitySpecId);
+    activitySpec.setVersion(new Version(versionId));
+    final ActivitySpecEntity retrieved = activitySpecManager.get(activitySpec);
+    ActivitySpecGetResponse getResponse = new MapActivitySpecToActivitySpecGetResponse()
+        .applyMapping(retrieved, ActivitySpecGetResponse.class);
+    return Response.ok(getResponse).build();
+  }
+
+  @Override
+  public Response updateActivitySpec(ActivitySpecRequestDto request, String activitySpecId,
+                                     String versionId, String user) {
+    ActivitySpecEntity activitySpec = new MapActivitySpecRequestDtoToActivitySpecEntity()
+        .applyMapping(request, ActivitySpecEntity.class);
+
+    activitySpec.setId(activitySpecId);
+    activitySpec.setVersion(new Version(versionId));
+
+    activitySpecManager.update(activitySpec);
+
+    return Response.ok().build();
+  }
+
+  @Override
+  public Response actOnActivitySpec(ActivitySpecActionRequestDto request, String activitySpecId,
+                                    String versionId, String user) {
+    activitySpecManager.actOnAction(activitySpecId, versionId, request.getAction());
+    return Response.ok().build();
+  }
+
+  @Override
+  public Response list(String versionStatus, String user) {
+
+    GenericCollectionWrapper<ActivitySpecListResponseDto> results = new GenericCollectionWrapper<>();
+    MapItemToListResponseDto mapper = new MapItemToListResponseDto();
+    activitySpecManager.list(versionStatus).stream()
+        .sorted((o1, o2) -> o2.getModificationTime().compareTo(o1.getModificationTime()))
+        .forEach(activitySpecItem -> results.add(mapper.applyMapping(activitySpecItem,
+            ActivitySpecListResponseDto.class)));
+
+    return Response.ok(results).build();
+  }
+
+}
