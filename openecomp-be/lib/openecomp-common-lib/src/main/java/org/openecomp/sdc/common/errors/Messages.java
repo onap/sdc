2c9cafd12d60298db262b17dd5f0c686a3c7b839
katy.rotman <katy.rotman@amdocs.com>
2018-02-21 10:18:53 +0200

    Heat Resource Validator code +tests
    Change-Id: Ic1e10c15fbbc01580311057e71edf05506bd9e8f
    Issue-ID: SDC-994
    Signed-off-by: katy.rotman <katy.rotman@amdocs.com>

--- openecomp-be/lib/openecomp-common-lib/src/main/java/org/openecomp/sdc/common/errors/Messages.java
+++ openecomp-be/lib/openecomp-common-lib/src/main/java/org/openecomp/sdc/common/errors/Messages.java
@@ -1,22 +1,19 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2016-2018 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
  */
+
 
 package org.openecomp.sdc.common.errors;
 
@@ -124,10 +121,6 @@ public enum Messages {
       "Wrong value assigned to a ResourceGroup index_var property (functions are not allowed"
           + " but only strings), Resource ID [%s]"),
   CONTRAIL_2_IN_USE("Contrail 2.x deprecated resource is in use, Resource ID [%s]"),
-  VLAN_SUBINTERFACE_MORE_THAN_ONE_PORT("More than one parent port found, there should be only one parent port for a VLAN sub-interface ID [%s]"),
-  VLAN_SUBINTERFACE_MISSING_TAG_PROPERTY("VLAN Tag property " +
-      "virtual_machine_interface_properties_sub_interface_vlan_tag is missing in VLAN Resource ID [%s]"),
-  VLAN_SUBINTERFACE_MISSING_REFS_PROPERTY("Parent port property virtual_machine_interface_refs is missing in VLAN Resource ID [%s]"),
 
   /* warnings */
   REFERENCED_RESOURCE_NOT_FOUND("Referenced resource - %s not found"),
