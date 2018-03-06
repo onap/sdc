db56a57ca0ba2c7a80a1e4050849ecd511224f50
ojasdubey <ojas.dubey@amdocs.com>
2018-03-06 12:01:54 +0530

    VLAN tagging - VFC Instance Group
    1. Create VFC instance group per sub-interface
    network role
    2. Refactored PortTemplateConsolidationData and
    its references based on review comments
    
    Change-Id: I423593c5156ecae5eff3a4f2ce8591c85db727cc
    Issue-ID: SDC-1073
    Signed-off-by: ojasdubey <ojas.dubey@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-translator-lib/openecomp-sdc-translator-core/src/main/java/org/openecomp/sdc/translator/datatypes/heattotosca/unifiedmodel/consolidation/PortTemplateConsolidationData.java
+++ openecomp-be/lib/openecomp-sdc-translator-lib/openecomp-sdc-translator-core/src/main/java/org/openecomp/sdc/translator/datatypes/heattotosca/unifiedmodel/consolidation/PortTemplateConsolidationData.java
@@ -19,9 +19,17 @@ package org.openecomp.sdc.translator.datatypes.heattotosca.unifiedmodel.consolid
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Multimaps;
+import org.apache.commons.collections4.CollectionUtils;
+import org.openecomp.core.utilities.file.FileUtils;
+import org.openecomp.sdc.heat.datatypes.model.Resource;
+import org.openecomp.sdc.tosca.datatypes.ToscaNodeType;
+import org.openecomp.sdc.tosca.datatypes.model.NodeTemplate;
+import org.openecomp.sdc.translator.services.heattotosca.HeatToToscaUtil;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 
 /**
@@ -44,13 +52,32 @@ public class PortTemplateConsolidationData extends EntityConsolidationData {
     this.networkRole = networkRole;
   }
 
-  public List<SubInterfaceTemplateConsolidationData> getSubInterfaceConsolidationData(
-      String subInterfaceType) {
-    return this.subInterfaceConsolidationData.get(subInterfaceType);
-  }
-
-  public Set<String> getAllSubInterfaceNodeTypes() {
-    return this.subInterfaceConsolidationData.keySet();
+  public SubInterfaceTemplateConsolidationData getSubInterfaceResourceTemplateConsolidationData(Resource resource,
+                                                                                      String subInterfaceNodeTemplateId,
+                                                                                      String parentPortNodeTemplateId) {
+    String subInterfaceType = ToscaNodeType.VLAN_SUB_INTERFACE_RESOURCE_TYPE_PREFIX
+        + FileUtils.getFileWithoutExtention(HeatToToscaUtil.getSubInterfaceResourceType(resource));
+    SubInterfaceTemplateConsolidationData data = new SubInterfaceTemplateConsolidationData();
+    data.setNodeTemplateId(subInterfaceNodeTemplateId);
+    data.setParentPortNodeTemplateId(parentPortNodeTemplateId);
+    if (CollectionUtils.isNotEmpty(subInterfaceConsolidationData.get(subInterfaceType))) {
+      boolean isNewSubInterface = true;
+      List<SubInterfaceTemplateConsolidationData> subInterfaceTemplateConsolidationDataList =
+          subInterfaceConsolidationData.get(subInterfaceType);
+      for (SubInterfaceTemplateConsolidationData subInterfaceTemplateConsolidationData :
+          subInterfaceTemplateConsolidationDataList) {
+        if (subInterfaceNodeTemplateId.equals(subInterfaceTemplateConsolidationData.getNodeTemplateId())) {
+          data = subInterfaceTemplateConsolidationData;
+          isNewSubInterface = false;
+        }
+      }
+      if (isNewSubInterface) {
+        addSubInterfaceConsolidationData(subInterfaceType, data);
+      }
+    } else {
+      addSubInterfaceConsolidationData(subInterfaceType, data);
+    }
+    return data;
   }
 
   public void addSubInterfaceConsolidationData(String subPortType,
@@ -79,6 +106,25 @@ public class PortTemplateConsolidationData extends EntityConsolidationData {
               subInterfaceType -> calculateSize(other.subInterfaceConsolidationData.get(subInterfaceType))
                   ==  calculateSize(this.subInterfaceConsolidationData.get(subInterfaceType)));
 
+  }
+
+  public List<String> getSubInterfaceNodeTemplateIdsByType(String subInterfaceType) {
+    List<String> subInterfaceNodeTemplateIds = new ArrayList<>();
+    List<SubInterfaceTemplateConsolidationData> subInterfaceTemplateConsolidationDataList =
+        this.subInterfaceConsolidationData.get(subInterfaceType);
+    if (CollectionUtils.isNotEmpty(subInterfaceTemplateConsolidationDataList)) {
+      subInterfaceNodeTemplateIds = subInterfaceTemplateConsolidationDataList.stream()
+          .map(SubInterfaceTemplateConsolidationData::getNodeTemplateId)
+          .collect(Collectors.toList());
+    }
+    return subInterfaceNodeTemplateIds;
+  }
+
+  public boolean isSubInterfaceNodeTemplateIdParameter(NodeTemplate nodeTemplate) {
+    List<SubInterfaceTemplateConsolidationData> subInterfaceTemplateConsolidationDataList =
+        this.subInterfaceConsolidationData.get(nodeTemplate.getType());
+    return (Objects.nonNull(subInterfaceTemplateConsolidationDataList)
+        && subInterfaceTemplateConsolidationDataList.size() > 1) ;
   }
 
   private int calculateSize(List<SubInterfaceTemplateConsolidationData> subInterfaces) {
