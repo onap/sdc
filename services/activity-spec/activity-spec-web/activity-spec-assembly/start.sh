870df02d1e7d5f1f11f6b32ad57933de815172d7
sheetalm <sheetal.mudholkar@amdocs.com>
2018-03-13 19:32:08 +0530

    Activity Spec Service - Dockerization
    Fix issue with swagger. Unix Line terminators for .sh files
    Configuring docker registry to push images and adding docker profile
    Add maven docker module to generate below docker images
    1 activity-spec-init:1.2.0-SNAPSHOT - creates cassandra keyspace and tables
      in already running cassandra docker image
    2 activity-spec-be:1.2.0-SNAPSHOT - deploys activity spec war and swagger war on base jetty docker image
    
    Change-Id: Ic638e2cb9c224e9e6c91cf0f7198594593a77ae5
    Issue-ID: SDC-1048
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

---
+++ services/activity-spec/activity-spec-web/activity-spec-assembly/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [[ -z "${CASSANDRA_HOST}" ]]; then
+	echo "CASSANDRA_HOST environment variable must be set"
+	exit 1
+fi
+
+#Replace 'CASSANDRA_HOST' in configuration.yaml with value of CASSANDRA_HOST environment variable
+sed -i "s/CASSANDRA_HOST/${CASSANDRA_HOST}/" configuration.yaml
+
+java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 "$JETTY_HOME/start.jar" -Dconfiguration.yaml=configuration.yaml
