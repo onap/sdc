d770405d052cc74287147037b23a9d7a6701c856
sheetalm <sheetal.mudholkar@amdocs.com>
2018-03-13 19:32:08 +0530

    Activity Spec Service - Dockerization
    Fixing todo items after rebase.
    URL to be /activity-spec-api. Removing unused image. Renaming css
    Configuring docker registry to push images and adding docker profile
    Add maven docker module to generate below docker images
    1 activity-spec-init:1.2.0-SNAPSHOT - creates cassandra keyspace and tables
      in already running cassandra docker image
    2 activity-spec-be:1.2.0-SNAPSHOT - deploys activity spec war and swagger war on base jetty docker image
    
    Change-Id: Ic638e2cb9c224e9e6c91cf0f7198594593a77ae5
    Issue-ID: SDC-1048
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

--- services/activity-spec/activity-spec-web/activity-spec-service/src/test/java/org/openecomp/activityspec/mocks/ItemManagerMock.java
+++ services/activity-spec/activity-spec-web/activity-spec-service/src/test/java/org/openecomp/activityspec/mocks/ItemManagerMock.java
@@ -18,9 +18,7 @@ package org.openecomp.activityspec.mocks;
 
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.openecomp.sdc.versioning.dao.types.VersionStatus;
 import org.openecomp.sdc.versioning.types.Item;
 import org.openecomp.sdc.versioning.ItemManager;
@@ -30,15 +28,14 @@ import java.util.function.Predicate;
 
 public class ItemManagerMock implements ItemManager {
 
-  public String id;
-  public Item item;
+  private String id;
+  private Item item;
 
 
   @Override
   public Collection<Item> list(Predicate<Item> predicate) {
     List<Item> items = new ArrayList<>();
     items.add(item);
-    Collection<Item> collection = items;
     return items;
   }
 
@@ -57,10 +54,19 @@ public class ItemManagerMock implements ItemManager {
 
   @Override
   public void updateVersionStatus(String itemId, VersionStatus addedVersionStatus,
-                                  VersionStatus removedVersionStatus) {
+      VersionStatus removedVersionStatus) {
 
   }
 
+  @Override
+  public void archive(Item item) {
+
+  }
+
+  @Override
+  public void restore(Item item) {
+
+  }
 
 
   @Override
