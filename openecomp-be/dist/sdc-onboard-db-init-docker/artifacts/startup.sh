96874555685f295e32fd15f235f15a225c1ea6f4
Avi Ziv <avi.ziv@amdocs.com>
2018-03-15 13:21:44 +0200

    Create on boarding docker
    Add BE and Cassandra init docker images
    
    Change-Id: Id9d767c73fc946819cec821b4634ce650cbb22fd
    Issue-ID: SDC-781
    Signed-off-by: Michael Lando <ml636r@att.com>

---
+++ openecomp-be/dist/sdc-onboard-db-init-docker/artifacts/startup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd /root
+
+CS_PORT=""
+CS_HOST=127.0.0.1
+
+if [ ! -z "${CS_HOST_IP}" ]; then
+    CS_HOST=$CS_HOST_IP
+fi
+
+if [ ! -z "${CS_HOST_PORT}" ]; then
+    CS_PORT=$CS_HOST_PORT
+fi
+
+echo "[Info] Going to initialize sdc onboard cassandra: user=$SDC_USER; host=$CS_HOST; port=$CS_PORT"
+echo "[Info] Initializing onboard keyspaces"
+cqlsh -u $SDC_USER -p $SDC_PASSWORD -f init_keyspaces.cql $CS_HOST $CS_PORT
+
+echo "[Info] Initializing onboard schemas"
+cqlsh -u $SDC_USER -p $SDC_PASSWORD -f init_schemas.cql $CS_HOST $CS_PORT
+
+rc=$?
+if [[ $rc != 0 ]]; then exit $rc; fi
