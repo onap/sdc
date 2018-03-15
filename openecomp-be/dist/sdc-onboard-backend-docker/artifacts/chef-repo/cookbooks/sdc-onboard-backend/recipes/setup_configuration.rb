7fed3d3836826b3dbc07a9a56fae572b927999e1
Avi Ziv <avi.ziv@amdocs.com>
2018-03-15 13:21:44 +0200

    Create on boarding docker
    Add BE and Cassandra init docker images
    
    Change-Id: Id9d767c73fc946819cec821b4634ce650cbb22fd
    Issue-ID: SDC-781
    Signed-off-by: Avi Ziv <avi.ziv@amdocs.com>

---
+++ openecomp-be/dist/sdc-onboard-backend-docker/artifacts/chef-repo/cookbooks/sdc-onboard-backend/recipes/setup_configuration.rb
@@ -0,0 +1,20 @@
+template "onboard-be-config" do
+   path "#{ENV['JETTY_BASE']}/config/onboarding-be/onboarding_configuration.yaml"
+   source "configuration.yaml.erb"
+   owner "jetty"
+   group "jetty"
+   mode "0755"
+   variables({
+          :onboard_ip             => node['Nodes']['BE'],
+          :onboard_port           => node['BE'][:http_port],
+          :ssl_port               => node['BE'][:https_port],
+          :cassandra_ip           => node['Nodes']['CS'],
+      :rep_factor             => 1,
+      :DC_NAME                => node['cassandra'][:cluster_name]+node.chef_environment,
+      :socket_connect_timeout => node['cassandra']['socket_connect_timeout'],
+      :socket_read_timeout    => node['cassandra']['socket_read_timeout'],
+      :cassandra_pwd          => node['cassandra'][:cassandra_password],
+      :cassandra_usr          => node['cassandra'][:cassandra_user]
+
+   })
+end
