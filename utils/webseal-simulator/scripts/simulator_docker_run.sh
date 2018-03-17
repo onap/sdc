908e6091628b7448a46f6a6f194c821dc6214de9
Victor Morales <victor.morales@intel.com>
2018-03-16 17:21:42 -0700

    Use getopts in simulator_docker_run.sh
    The simulator_docker_run.sh script can use getopts builtin fuction
    instead of while/case combination. This helps to clean the code and
    reuse some existing functions.
    
    Change-Id: I6a0a7ddd25d0da980e3d870bebddb8913bcdfdf7
    Signed-off-by: Victor Morales <victor.morales@intel.com>
    Issue-ID: SDC-993

--- utils/webseal-simulator/scripts/simulator_docker_run.sh
+++ utils/webseal-simulator/scripts/simulator_docker_run.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 
+export IP=`ifconfig eth0 | awk -F: '/inet addr/ {gsub(/ .*/,"",$2); print $2}'`
+export PREFIX=${NEXUS_DOCKER_REPO}'/onap'
 
-function usage {
-    echo "usage: simulator_docker_run.sh [ -r|--release <RELEASE-NAME> ]  [ -e|--environment <ENV-NAME> ] [ -p|--port <Docker-hub-port>] [ -h|--help ]"
-}
-
-
-function cleanup {
-	echo "performing old dockers cleanup"
-	docker_ids=`docker ps -a | egrep "onap/sdc-simulator|Exit" | awk '{print $1}'`
-	for X in ${docker_ids}
-	do
-	   docker rm -f ${X}
-	done
-}
-
-
-function dir_perms {
-	mkdir -p /data/logs/WS/
-	chmod -R 777 /data/logs
-}
-
-
+PREFIX='onap'
 RELEASE=latest
 LOCAL=true
 [ -f /opt/config/env_name.txt ] && DEP_ENV=$(cat /opt/config/env_name.txt) || DEP_ENV=__ENV-NAME__
@@ -29,26 +11,53 @@ LOCAL=true
 [ -f /opt/config/nexus_password.txt ] && NEXUS_PASSWD=$(cat /opt/config/nexus_password.txt)      || NEXUS_PASSWD=sfWU3DFVdBr7GVxB85mTYgAW
 [ -f /opt/config/nexus_docker_repo.txt ] && NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt) || NEXUS_DOCKER_REPO=ecomp-nexus:${PORT}
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -r | --release )
+function usage {
+    cat <<EOF
+usage: simulator_docker_run.sh [-e <ENV-NAME>] [-r <RELEASE-NAME>] [-p <PORT>] [-h]
+Optional arguments:
+    -e, --environment
+        Environment Name
+    -r, --release
+        Release Name
+    -p, --port
+        Docker Hub port number
+    -h, --help
+        Help
+EOF
+}
+
+function cleanup {
+    echo "performing old dockers cleanup"
+    for old_container in $(docker ps -a | egrep "onap/sdc-simulator|Exit" | awk '{print $1}'); do
+       docker rm -f ${old_container}
+    done
+}
+
+function dir_perms {
+    mkdir -p /data/logs/WS/
+    chmod -R 777 /data/logs
+}
+
+while getopts "r:e:p:" OPTION "${@}"; do
+    case $OPTION in
+        -r | --release)
             shift
             RELEASE=${1}
             ;;
-        -e | --environment )
-			shift
+        -e | --environment)
+            shift
             DEP_ENV=${1}
             ;;
-		-p | --port )
+        -p | --port)
             shift
             PORT=${1}
-			;;
-        -h | --help )
-			usage
+            ;;
+        -h | --help)
+            usage
             exit
             ;;
-        * ) 
-    		usage
+        *)
+            usage
             exit 1
     esac
     shift
@@ -56,27 +65,33 @@ done
 
 [ -f /opt/config/nexus_username.txt ] && docker login -u $NEXUS_USERNAME -p $NEXUS_PASSWD $NEXUS_DOCKER_REPO
 
-
 cleanup
 
-
-export IP=`ifconfig eth0 | awk -F: '/inet addr/ {gsub(/ .*/,"",$2); print $2}'`
-export PREFIX=${NEXUS_DOCKER_REPO}'/onap'
-
-PREFIX='onap'
-
-echo ""
-echo "${PREFIX}"
+echo "\n${PREFIX}"
 
 dir_perms
 
-JAVA_OPTIONS=" -Xmx128m -Xms128m -Xss1m"
-
 # SDC-Simulator
-docker run --detach --name sdc-sim  --env JAVA_OPTIONS="${JAVA_OPTIONS}" --env ENVNAME="${DEP_ENV}" --env http_proxy=${http_proxy} --env https_proxy=${https_proxy} --env no_proxy=${no_proxy}  --volume /etc/localtime:/etc/localtime:ro --volume /data/logs/WS/:/var/lib/jetty/logs --volume /data/environments:/root/chef-solo/environments --publish 8285:8080 --publish 8286:8443 ${PREFIX}/sdc-simulator:${RELEASE}
+docker run --detach --name sdc-sim \
+    --env HOST_IP=${IP} \
+    --env ENVNAME="${DEP_ENV}" \
+    --env http_proxy=${http_proxy} \
+    --env https_proxy=${https_proxy} \
+    --env no_proxy=${no_proxy} \
+    --log-driver=json-file \
+    --log-opt max-size=100m \
+    --log-opt max-file=10 \
+    --ulimit memlock=-1:-1 \
+    --memory 256m \
+    --memory-swap=256m \
+    --ulimit nofile=4096:100000 \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /data/logs/WS/:/var/lib/jetty/logs \
+    --volume /data/environments:/root/chef-solo/environments \
+    --publish 8285:8080 \
+        ${PREFIX}/sdc-simulator:${RELEASE}
 
 
 if [ $? -ne 0 ]; then
     exit 1
 fi
-
