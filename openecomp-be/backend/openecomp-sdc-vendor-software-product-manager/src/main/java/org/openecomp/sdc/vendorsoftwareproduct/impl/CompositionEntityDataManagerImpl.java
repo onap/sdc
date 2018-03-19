7d6ba1060ceae2eded6561e46ecf75a605e7b6d1
lding7 <liang.ding@intel.com>
2018-03-19 16:22:01 +0800

    fix sonar complaints NullPointerException in SDC
    bugs from https://sonar.onap.org/project/issues?id=
    org.openecomp.sdc%3Asdc-main%3A1.2.0-SNAPSHOT&open=
    AWIFjPUXRGy6eclHC_Q6&resolved=false&types=BUG
    and https://sonar.onap.org/project/issues?id=
    org.openecomp.sdc%3Asdc-main%3A1.2.0-SNAPSHOT&open=
    AWIFjPUXRGy6eclHC_Q5&resolved=false&types=BUG
    
    Change-Id: I88225e4da197bed62d588026564bd688a9627c8a
    Issue-ID: SDC-1146
    Signed-off-by: lding7 <liang.ding@intel.com>

--- openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/impl/CompositionEntityDataManagerImpl.java
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/impl/CompositionEntityDataManagerImpl.java
@@ -68,6 +68,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -214,7 +215,11 @@ public class CompositionEntityDataManagerImpl implements CompositionEntityDataMa
   public Set<CompositionEntityValidationData> getAllErrorsByVsp(String vspId) {
     CompositionEntityValidationData matchVsp = null;
     Set<CompositionEntityValidationData> entitiesWithErrors = new HashSet<>();
+    Iterator<CompositionEntityValidationData> iterator = roots.iterator();
     for (CompositionEntityValidationData root : roots) {
+      if (iterator.hasNext() && (iterator.next() == null)) {
+        iterator.remove();
+      }
       if (root.getEntityId().equals(vspId)) {
         matchVsp = root;
         break;
@@ -449,6 +454,9 @@ public class CompositionEntityDataManagerImpl implements CompositionEntityDataMa
 
       case vsp:
         CompositionEntityData vspEntity = getCompositionEntityDataById(entity);
+        if (Objects.isNull(vspEntity)) {
+          return null;
+        }
         VspQuestionnaireEntity vspQuestionnaireEntity = (VspQuestionnaireEntity) vspEntity.entity;
         VspDetails vspDetails =
             vspInfoDao.get(new VspDetails(vspQuestionnaireEntity.getId(),
