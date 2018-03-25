6bc90bd78bdd9a24c7ba76d5856f650d4fcc050d
talio <tali.orenbach@amdocs.com>
2018-03-25 13:25:43 +0300

    Interface definition
    Add handling in reading and writing operation definition
    
    Change-Id: I0fd5770b19a8cf5d5a8d2b93a549fd66a3b1e728
    Issue-ID: SDC-1161
    Signed-off-by: talio <tali.orenbach@amdocs.com>

--- common/openecomp-tosca-datatype/src/main/java/org/openecomp/sdc/tosca/datatypes/model/Implementation.java
+++ common/openecomp-tosca-datatype/src/main/java/org/openecomp/sdc/tosca/datatypes/model/Implementation.java
@@ -1,22 +1,6 @@
-/*
- * Copyright © 2016-2018 European Support Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.openecomp.sdc.tosca.datatypes.model;
 
 import java.util.List;
-import java.util.Objects;
 
 
 public class Implementation {
@@ -48,14 +32,20 @@ public class Implementation {
     if (!(o instanceof Implementation)) {
       return false;
     }
+
     Implementation that = (Implementation) o;
-    return Objects.equals(primary, that.primary) &&
-        Objects.equals(dependencies, that.dependencies);
+
+    if (!getPrimary().equals(that.getPrimary())) {
+      return false;
+    }
+    return getDependencies() != null ? getDependencies().equals(that.getDependencies())
+        : that.getDependencies() == null;
   }
 
   @Override
   public int hashCode() {
-
-    return Objects.hash(primary, dependencies);
+    int result = getPrimary().hashCode();
+    result = 31 * result + (getDependencies() != null ? getDependencies().hashCode() : 0);
+    return result;
   }
 }
--- common/openecomp-tosca-datatype/src/main/java/org/openecomp/sdc/tosca/datatypes/model/InterfaceDefinitionType.java
+++ common/openecomp-tosca-datatype/src/main/java/org/openecomp/sdc/tosca/datatypes/model/InterfaceDefinitionType.java
@@ -1,6 +1,9 @@
-/*
- * Copyright © 2016-2018 European Support Limited
- *
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,6 +15,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.sdc.tosca.datatypes.model;
