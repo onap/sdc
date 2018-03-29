d551595ef176ce838dd6899ae8a7fccb4a0c173b
Murali-P <murali.p@huawei.com>
2018-03-29 17:46:39 +0530

    Integrate VNF Repository in Beijing release
    Migrate the code
    
    Change-Id: Ifccacf83634af32b034fd9c413e68f894f06d2f7
    Issue-ID: VNFSDK-155
    Signed-off-by: Murali-P <murali.p@huawei.com>

--- catalog-be/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_2_setup_configuration.rb
+++ catalog-be/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_2_setup_configuration.rb
@@ -56,3 +56,16 @@ cookbook_file "ArtifactGenerator" do
    group "jetty"
    mode "0755"
 end
+
+
+template "VnfrepoConfiguration" do
+   path "#{ENV['JETTY_BASE']}/config/onboarding-be/config-vnfrepo.yaml"
+   source "BE-vnfrepo-configuration.yaml.erb"
+   owner "jetty"
+   group "jetty"
+   mode "0755"
+   variables({
+      :VNFREPO_IP   => node['VnfRepo']['vnfRepoHost'],
+      :VNFREPO_PORT => node['VnfRepo']['vnfRepoPort']
+   })
+end
