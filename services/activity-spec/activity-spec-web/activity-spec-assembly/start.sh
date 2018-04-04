e3cbadafcdc6709220430686d9f4ddbeb173fee5
sheetalm <sheetal.mudholkar@amdocs.com>
2018-04-04 15:06:18 +0530

    Activity Spec - Logging changes
    Resolved review comments
    1 Add logback.xml
    2 Configure LoggingRequestFilter in beans-services.xml
    3 Remove unwanted LoggingFilter from web.xml
    
    Change-Id: I00d2c168458ad60517fc9dd4389c2125bc63bc25
    Issue-ID: SDC-1048
    Signed-off-by: sheetalm <sheetal.mudholkar@amdocs.com>

--- services/activity-spec/activity-spec-web/activity-spec-assembly/start.sh
+++ services/activity-spec/activity-spec-web/activity-spec-assembly/start.sh
@@ -8,4 +8,4 @@ fi
 #Replace 'CASSANDRA_HOST' in configuration.yaml with value of CASSANDRA_HOST environment variable
 sed -i "s/CASSANDRA_HOST/${CASSANDRA_HOST}/" configuration.yaml
 
-java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 "$JETTY_HOME/start.jar" -Dconfiguration.yaml=configuration.yaml
+java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 "$JETTY_HOME/start.jar" -Dconfiguration.yaml=configuration.yaml -Dlogback.configurationFile=${LOGBACK_FILE_DIR}/logback.xml
