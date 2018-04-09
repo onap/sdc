bf67540a5813318c33a83ac213ff40239571be9a
Areli, Fuss (af732p) <af732p@att.com>
2018-04-09 18:27:46 +0300

    Add support of http and https
    Add support of http and https
    
    Change-Id: I8c79155037ddaa5f904ecd85de797675a6be9436
    Issue-ID: SDC-1180
    Signed-off-by: Areli, Fuss (af732p) <af732p@att.com>

--- catalog-be/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_5_setup_elasticsearch.rb
+++ catalog-be/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_5_setup_elasticsearch.rb
@@ -1,14 +1,13 @@
 clusterName = node['elasticsearch'][:cluster_name]+node.chef_environment
 
 template "elasticsearch.yml-config" do
-   path "#{ENV['JETTY_BASE']}/config/elasticsearch.yml"
-   source "BE-elasticsearch.yml.erb"
-   owner "jetty"
-   group "jetty"
-   mode "0755"
-   variables({
-        :cluster_name => "#{clusterName}",
-        :es_host_ip => node['Nodes']['ES']
-   })
+  path "#{ENV['JETTY_BASE']}/config/elasticsearch.yml"
+  source "BE-elasticsearch.yml.erb"
+  owner "jetty"
+  group "jetty"
+  mode "0755"
+  variables ({
+    :cluster_name => "#{clusterName}",
+    :es_host_ip => node['Nodes']['ES']
+  })
 end
-
--- catalog-fe/sdc-frontend/chef-repo/cookbooks/sdc-catalog-fe/recipes/FE_7_create_jetty_modules.rb
+++ catalog-fe/sdc-frontend/chef-repo/cookbooks/sdc-catalog-fe/recipes/FE_7_create_jetty_modules.rb
@@ -1,3 +1,12 @@
+#
+#Set the http module option
+if node['disableHttp']
+  http_option = "#--module=http"
+else
+  http_option = "--module=http"
+end
+
+
 bash "create-jetty-modules" do
 cwd "#{ENV['JETTY_BASE']}"
 code <<-EOH
@@ -8,24 +17,26 @@ EOH
 end
 
 
-
-template "FE-http-ini" do
+template "http-ini" do
    path "#{ENV['JETTY_BASE']}/start.d/http.ini"
-   source "FE-http-ini.erb"
+   source "http-ini.erb"
    owner "jetty"
    group "jetty"
    mode "0755"
-   variables :FE_http_port => "#{node['FE'][:http_port]}"
+   variables ({
+     :http_option => http_option ,
+     :http_port => "#{node['FE'][:http_port]}"
+    })
 end
 
 
-template "FE-https-ini" do
+template "https-ini" do
    path "#{ENV['JETTY_BASE']}/start.d/https.ini"
-   source "FE-https-ini.erb"
+   source "https-ini.erb"
    owner "jetty"
    group "jetty"
    mode "0755"
-   variables :FE_https_port => "#{node['FE'][:https_port]}"
+   variables :https_port => "#{node['FE'][:https_port]}"
 end
 
 
@@ -35,7 +46,10 @@ template "ssl-ini" do
    owner "jetty"
    group "jetty"
    mode "0755"
-   variables :https_port => "#{node['FE'][:https_port]}"
+   variables ({
+     :https_port => "#{node['FE'][:https_port]}" ,
+     :jetty_keystore_pwd => "#{node['jetty'][:keystore_pwd]}" ,
+     :jetty_keymanager_pwd => "#{node['jetty'][:keymanager_pwd]}" ,
+     :jetty_truststore_pwd => "#{node['jetty'][:truststore_pwd]}"
+   })
 end
-
-
