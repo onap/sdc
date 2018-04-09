57d7f55afe4c21d4cd6ad121d61c0f5239483ef4
Areli, Fuss (af732p) <af732p@att.com>
2018-04-09 19:01:44 +0300

    Add support of http and https
    Add support of http and https
    
    Change-Id: Iddf6282151a578c6b6c5abcd3945567952ed9238
    Issue-ID: SDC-1180
    Signed-off-by: Areli, Fuss (af732p) <af732p@att.com>

--- catalog-be/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/attributes/default.rb
+++ catalog-be/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/attributes/default.rb
@@ -1,2 +1,10 @@
 #
+default['BE'][:http_port] = 8080
+default['BE'][:https_port] = 8443
+default['FE'][:http_port] = 8181
+default['FE'][:https_port] = 9443
+default['disableHttp'] = true
 default['cassandra'][:truststore_password] = "Aa123456"
+default['jetty'][:keystore_pwd] = "OBF:1cp61iuj194s194u194w194y1is31cok"
+default['jetty'][:keymanager_pwd] = "OBF:1cp61iuj194s194u194w194y1is31cok"
+default['jetty'][:truststore_pwd] = "OBF:1cp61iuj194s194u194w194y1is31cok"
