189ea57b899a346f5af26ddc88eff7a12611b560
siddharth0905 <siddharth.singh4@amdocs.com>
2018-04-20 12:29:22 +0530

    Healing getting failed
    Due to previous build changes healing was failing
    
    Change-Id: I6acdc323399862ee37d6e206825097040dd1daf6
    Issue-ID: SDC-1244
    Signed-off-by: siddharth0905 <siddharth.singh4@amdocs.com>

--- openecomp-be/tools/zusammen-tools/src/main/resources/zusammenMainTool.sh
+++ openecomp-be/tools/zusammen-tools/src/main/resources/zusammenMainTool.sh
@@ -22,6 +22,7 @@ else
    CONF_FILE_LOCATION="/apps/jetty/base/be/config/catalog-be/configuration.yaml"
 fi
 echo "Configuration file location:  ${CONF_FILE_LOCATION}"
+mv lib/openecomp-zusammen-tools*.jar openecomp-zusammen-tools.jar  &>/dev/null
 
-java -Dconfig.home=/opt/app/jetty/base/be/config -Dlog.home=/apps/jetty/base/be/logs -Dconfiguration.yaml=${CONF_FILE_LOCATION}  -classpath lib/* org.openecomp.core.tools.main.ZusammenMainTool $1 $2 $3 $4 $5 $6
+java -Dconfig.home=/opt/app/jetty/base/be/config -Dlog.home=/apps/jetty/base/be/logs -Dconfiguration.yaml=${CONF_FILE_LOCATION}  -classpath openecomp-zusammen-tools.jar:lib/* org.openecomp.core.tools.main.ZusammenMainTool $1 $2 $3 $4 $5 $6
 STATUS="${?}" echo "${STATUS}"
