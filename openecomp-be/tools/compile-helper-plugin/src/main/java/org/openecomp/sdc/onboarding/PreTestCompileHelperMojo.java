6b44c41e9456277004af6a7c94a41d5ea8159f73
Gautam Shah <gautams@amdocs.com>
2018-04-21 16:18:37 +0530

    Reducing onboarding backend maven build time
    Reducing onboarding backend maven build time
    
    Change-Id: Id0178cbf010e46f741f4bff11e14b0801f14d4b8
    Issue-ID: SDC-1189
    Signed-off-by: GAUTAMS <gautams@amdocs.com>

---
+++ openecomp-be/tools/compile-helper-plugin/src/main/java/org/openecomp/sdc/onboarding/PreTestCompileHelperMojo.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2018 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on a "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openecomp.sdc.onboarding;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(name = "pre-test-compile-helper", threadSafe = true, defaultPhase = LifecyclePhase.GENERATE_TEST_RESOURCES,
+        requiresDependencyResolution = ResolutionScope.COMPILE)
+public class PreTestCompileHelperMojo extends AbstractMojo {
+
+    @Parameter
+    private File compiledFilesList;
+    private static final String JAVA_EXT = "java";
+    @Parameter
+    private Long staleThreshold;
+    @Parameter
+    private File inputTestFilesList;
+    @Parameter
+    private BuildState buildState;
+
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if (compiledFilesList.exists()
+                    && compiledFilesList.lastModified() > System.currentTimeMillis() - staleThreshold) {
+            try {
+                buildState.markModuleDirty(inputTestFilesList);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+    }
+}
