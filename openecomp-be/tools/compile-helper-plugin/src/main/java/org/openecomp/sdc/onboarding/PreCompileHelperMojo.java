80676e1e655f1ed1e99fcb5ab95c4f2df0270a17
Gautam Shah <gautams@amdocs.com>
2018-04-21 16:18:37 +0530

    Reducing onboarding backend maven build time
    Reducing onboarding backend maven build time
    
    Change-Id: Id0178cbf010e46f741f4bff11e14b0801f14d4b8
    Issue-ID: SDC-1189
    Signed-off-by: GAUTAMS <gautams@amdocs.com>

---
+++ openecomp-be/tools/compile-helper-plugin/src/main/java/org/openecomp/sdc/onboarding/PreCompileHelperMojo.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright © 2016-2017 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on a "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openecomp.sdc.onboarding;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name = "pre-compile-helper", threadSafe = true, defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+        requiresDependencyResolution = ResolutionScope.COMPILE)
+public class PreCompileHelperMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project}", readonly = true)
+    private MavenProject project;
+    @Parameter(defaultValue = "${project.artifact.groupId}:${project.artifact.artifactId}")
+    private String moduleCoordinates;
+
+    private static final String JAVA_EXT = "java";
+
+    @Parameter
+    private String excludePackaging;
+    @Parameter
+    private List<String> excludeDependencies;
+    @Parameter
+    private File mainCompiledLocation;
+    @Parameter
+    private File testCompiledLocation;
+    @Parameter
+    private File inputSourceFilesList;
+    @Parameter
+    private File inputTestFilesList;
+    @Parameter
+    private BuildState buildState;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if (project.getPackaging().equals(excludePackaging)) {
+            return;
+        }
+
+        Map<String, String> moduleBuildData = getCurrentModuleBuildData();
+        Map<String, String> lastTimeModuleBuildData = buildState.readModuleBuildData();
+
+        boolean buildDataSame = lastTimeModuleBuildData.equals(moduleBuildData);
+
+        if (buildState.getBuildTime(moduleCoordinates) == 0 || !buildDataSame || buildState.isCompileMust(
+                moduleCoordinates, moduleBuildData.keySet())) {
+            try {
+                buildState.markModuleDirty(inputSourceFilesList);
+                buildState.markModuleDirty(inputTestFilesList);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+
+        if (!buildDataSame) {
+            buildState.addModuleBuildData(moduleCoordinates, moduleBuildData);
+        }
+        if (inputSourceFilesList.isFile() && inputSourceFilesList.length() == 0) {
+            inputSourceFilesList.delete();
+        }
+        if (inputTestFilesList.isFile() && inputTestFilesList.length() == 0) {
+            inputTestFilesList.delete();
+        }
+    }
+
+    private Map<String, String> getCurrentModuleBuildData() {
+        Map<String, String> moduleBuildData = new HashMap<>();
+        if (project.getDependencies() == null || project.getDependencies().isEmpty()) {
+            return moduleBuildData;
+        }
+        for (Dependency dependency : project.getDependencies()) {
+            if (excludeDependencies.contains(dependency.getScope())) {
+                continue;
+            }
+            moduleBuildData.put(dependency.getGroupId() + ":" + dependency.getArtifactId(), dependency.getVersion());
+        }
+        return moduleBuildData;
+    }
+}
