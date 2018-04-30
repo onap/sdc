9b8f36d618e7ce55becb4c3f79069d5d8c4b30dc
priyanshu <pagarwal@amdocs.com>
2018-04-30 15:02:45 +0530

    Activity Spec docker enhancements
    Activity Spec Changes for docker in Kubernetes
    
    Change-Id: I987fe097792ccd5d41a0ad811602dad72259d45f
    Issue-ID: SDC-1048
    Signed-off-by: priyanshu <pagarwal@amdocs.com>

--- services/activity-spec/activity-spec-web/activity-spec-assembly/start.sh
+++ services/activity-spec/activity-spec-web/activity-spec-assembly/start.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-if [[ -z "${CASSANDRA_HOST}" ]]; then
-	echo "CASSANDRA_HOST environment variable must be set"
-	exit 1
-fi
+JAVA_OPTIONS=" ${JAVA_OPTIONS} \
+            -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
+            -Dconfig.home=${CONFIG_FILES_DIR} \
+            -Dlog.home=${LOGS_DIR} \
+            -Dlogback.configurationFile=${CONFIG_FILES_DIR}/logback.xml \
+            -Dconfiguration.yaml=${CONFIG_FILES_DIR}/configuration.yaml"
 
-#Replace 'CASSANDRA_HOST' in configuration.yaml with value of CASSANDRA_HOST environment variable
-sed -i "s/CASSANDRA_HOST/${CASSANDRA_HOST}/" configuration.yaml
-
-java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 "$JETTY_HOME/start.jar" -Dconfiguration.yaml=configuration.yaml -Dlogback.configurationFile=${LOGBACK_FILE_DIR}/logback.xml
+java -jar "$JETTY_HOME/start.jar" ${JAVA_OPTIONS}
