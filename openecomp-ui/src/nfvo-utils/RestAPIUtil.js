3b0bea525ed2051f3635dfd6800f156601aa8c0d
svishnev <shlomo-stanisla.vishnevetskiy@amdocs.com>
2018-05-08 16:15:21 +0300

    nic creation issue fix
    Issue-ID: SDC-1274
    Change-Id: Ifd9297d3559f1b8820b0a64f063f1211b6957dd4
    Signed-off-by: svishnev <shlomo-stanisla.vishnevetskiy@amdocs.com>

--- openecomp-ui/src/nfvo-utils/RestAPIUtil.js
+++ openecomp-ui/src/nfvo-utils/RestAPIUtil.js
@@ -79,10 +79,6 @@ function handleSuccess(responseHeaders, requestHeaders) {
 
 class RestAPIUtil {
     handleRequest(url, type, options = {}, data) {
-        if (DEBUG) {
-            console.log('axios --> Making REST call (' + type + '): ' + url);
-        }
-
         applySecurity(options, data);
 
         // TODO see ig necessary or in transformrequest funtion
