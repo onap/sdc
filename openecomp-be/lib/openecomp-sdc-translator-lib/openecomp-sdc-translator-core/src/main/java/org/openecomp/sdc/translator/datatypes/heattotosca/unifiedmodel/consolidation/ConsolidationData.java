9b026c3b228df4748ba563f20e54fb89e7669c46
eleonorali <eleonoral@amdocs.com>
2018-05-14 16:44:04 +0300

    TOSCA Consolidation Data handling - Refactoring
    Create new Interface, Base and Implementation classes for Consolidation Data Handlers
    
    Change-Id: I9638a7b57321aca00b458b244b278807a99b25f2
    Issue-ID: SDC-1330
    Signed-off-by: eleonorali <eleonoral@amdocs.com>
    Signed-off-by: vempo <vitaliy.emporopulo@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-translator-lib/openecomp-sdc-translator-core/src/main/java/org/openecomp/sdc/translator/datatypes/heattotosca/unifiedmodel/consolidation/ConsolidationData.java
+++ openecomp-be/lib/openecomp-sdc-translator-lib/openecomp-sdc-translator-core/src/main/java/org/openecomp/sdc/translator/datatypes/heattotosca/unifiedmodel/consolidation/ConsolidationData.java
@@ -1,81 +1,139 @@
+/*
+ * Copyright © 2016-2018 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecomp.sdc.translator.datatypes.heattotosca.unifiedmodel.consolidation;
 
-/**
- * The type Consolidation data.
- */
+import java.util.Optional;
+import org.openecomp.sdc.translator.services.heattotosca.ConsolidationEntityType;
+
 public class ConsolidationData {
 
-  //Compute consolidation data
-  private ComputeConsolidationData computeConsolidationData;
+    private ComputeConsolidationData computeConsolidationData;
+    private final ComputeConsolidationDataHandler computeConsolidationDataHandler;
 
-  //Port Consolidation data
-  private PortConsolidationData portConsolidationData;
+    private PortConsolidationData portConsolidationData;
+    private final PortConsolidationDataHandler portConsolidationDataHandler;
+    private final SubInterfaceConsolidationDataHandler subInterfaceConsolidationDataHandler;
 
-  //Nested Consolidation data
-  private NestedConsolidationData nestedConsolidationData;
+    private NestedConsolidationData nestedConsolidationData;
+    private final NestedConsolidationDataHandler nestedConsolidationDataHandler;
 
+    public ConsolidationData() {
 
-  /**
-   * Instantiates a new Consolidation data.
-   */
-  public ConsolidationData() {
-    computeConsolidationData = new ComputeConsolidationData();
-    portConsolidationData = new PortConsolidationData();
-    nestedConsolidationData = new NestedConsolidationData();
-  }
+        computeConsolidationData = new ComputeConsolidationData();
+        computeConsolidationDataHandler = new ComputeConsolidationDataHandler(computeConsolidationData);
 
-  /**
-   * Gets compute consolidation data.
-   *
-   * @return the compute consolidation data
-   */
-  public ComputeConsolidationData getComputeConsolidationData() {
-    return computeConsolidationData;
-  }
+        portConsolidationData = new PortConsolidationData();
+        portConsolidationDataHandler = new PortConsolidationDataHandler(portConsolidationData);
+        subInterfaceConsolidationDataHandler = new SubInterfaceConsolidationDataHandler(portConsolidationData);
 
-  /**
-   * Sets compute consolidation data.
-   *
-   * @param computeConsolidationData the compute consolidation data
-   */
-  public void setComputeConsolidationData(ComputeConsolidationData computeConsolidationData) {
-    this.computeConsolidationData = computeConsolidationData;
-  }
+        nestedConsolidationData = new NestedConsolidationData();
+        nestedConsolidationDataHandler = new NestedConsolidationDataHandler(nestedConsolidationData);
 
-  /**
-   * Gets port consolidation data.
-   *
-   * @return the port consolidation data
-   */
-  public PortConsolidationData getPortConsolidationData() {
-    return portConsolidationData;
-  }
+    }
 
-  /**
-   * Sets port consolidation data.
-   *
-   * @param portConsolidationData the port consolidation data
-   */
-  public void setPortConsolidationData(
-      PortConsolidationData portConsolidationData) {
-    this.portConsolidationData = portConsolidationData;
-  }
+    /**
+     * Get Consolidation data handler by entity type.
+     *
+     * @return If there is no consolidation handler for a type, return an empty {@link Optional}.
+     */
+    public Optional<ConsolidationDataHandler> getConsolidationDataHandler(ConsolidationEntityType type) {
 
-  /**
-   * Gets nested consolidation data.
-   *
-   * @return the nested consolidation data
-   */
-  public NestedConsolidationData getNestedConsolidationData() {
-    return nestedConsolidationData;
-  }
+        switch (type) {
+            case COMPUTE:
+                return Optional.of(getComputeConsolidationDataHelper());
+            case PORT:
+                return Optional.of(getPortConsolidationDataHandler());
+            case SUB_INTERFACE:
+                return Optional.of(getSubInterfaceConsolidationDataHandler());
+            case NESTED:
+            case VFC_NESTED:
+                return Optional.of(getNestedConsolidationDataHandler());
+            default:
+                return Optional.empty();
+        }
+    }
 
-  /**
-   * Sets nested consolidation data.
-   *
-   * @param nestedConsolidationData the nested consolidation data
-   */
-  public void setNestedConsolidationData(NestedConsolidationData nestedConsolidationData) {
-    this.nestedConsolidationData = nestedConsolidationData;
-  }
+    public ComputeConsolidationDataHandler getComputeConsolidationDataHelper() {
+        return computeConsolidationDataHandler;
+    }
+
+    public PortConsolidationDataHandler getPortConsolidationDataHandler() {
+        return portConsolidationDataHandler;
+    }
+
+    public NestedConsolidationDataHandler getNestedConsolidationDataHandler() {
+        return nestedConsolidationDataHandler;
+    }
+
+    public SubInterfaceConsolidationDataHandler getSubInterfaceConsolidationDataHandler() {
+        return subInterfaceConsolidationDataHandler;
+    }
+
+    /**
+     * Gets compute consolidation data.
+     *
+     * @return the compute consolidation data
+     */
+    public ComputeConsolidationData getComputeConsolidationData() {
+        return computeConsolidationData;
+    }
+
+    /**
+     * Sets compute consolidation data.
+     *
+     * @param computeConsolidationData the compute consolidation data
+     */
+    public void setComputeConsolidationData(ComputeConsolidationData computeConsolidationData) {
+        this.computeConsolidationData = computeConsolidationData;
+    }
+
+    /**
+     * Gets port consolidation data.
+     *
+     * @return the port consolidation data
+     */
+    public PortConsolidationData getPortConsolidationData() {
+        return portConsolidationData;
+    }
+
+    /**
+     * Sets port consolidation data.
+     *
+     * @param portConsolidationData the port consolidation data
+     */
+    public void setPortConsolidationData(PortConsolidationData portConsolidationData) {
+        this.portConsolidationData = portConsolidationData;
+    }
+
+    /**
+     * Gets nested consolidation data.
+     *
+     * @return the nested consolidation data
+     */
+    public NestedConsolidationData getNestedConsolidationData() {
+        return nestedConsolidationData;
+    }
+
+    /**
+     * Sets nested consolidation data.
+     *
+     * @param nestedConsolidationData the nested consolidation data
+     */
+    public void setNestedConsolidationData(NestedConsolidationData nestedConsolidationData) {
+        this.nestedConsolidationData = nestedConsolidationData;
+    }
 }
---
+++ openecomp-be/lib/openecomp-sdc-translator-lib/openecomp-sdc-translator-core/src/main/java/org/openecomp/sdc/translator/datatypes/heattotosca/unifiedmodel/consolidation/ConsolidationDataHandler.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2016-2018 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openecomp.sdc.translator.datatypes.heattotosca.unifiedmodel.consolidation;
+
+import org.onap.sdc.tosca.datatypes.model.RequirementAssignment;
+import org.onap.sdc.tosca.datatypes.model.ServiceTemplate;
+import org.openecomp.sdc.heat.datatypes.model.HeatOrchestrationTemplate;
+import org.openecomp.sdc.translator.datatypes.heattotosca.to.TranslateTo;
+
+public interface ConsolidationDataHandler {
+
+    /**
+     * Add node connected out from current consolidation entity.
+     *
+     * @param nodeTemplateId        the node template id of target node - node connected out from current node
+     * @param requirementId         the requirement id of requirement assignment
+     * @param requirementAssignment the requirement assignment data connected to target node
+     */
+    void addNodesConnectedOut(TranslateTo translateTo, String nodeTemplateId,
+                                     String requirementId, RequirementAssignment requirementAssignment);
+
+    /**
+     * Add source node connected in to target node consolidation entity.
+     *
+     * @param sourceNodeTemplateId  the node template id of source node connected to consolidation entity
+     * @param targetNodeTemplateId  the node template id of consolidation entity node
+     * @param targetResourceId      the resource id of consolidation entity node
+     * @param requirementId         the requirement id of source node
+     * @param requirementAssignment the requirement assignment data of source node
+     */
+    void addNodesConnectedIn(TranslateTo translateTo,  String sourceNodeTemplateId,
+                                    String targetNodeTemplateId,
+                                    String targetResourceId, String requirementId,
+                                    RequirementAssignment requirementAssignment);
+
+    void removeParamNameFromAttrFuncList(ServiceTemplate serviceTemplate,
+                                                HeatOrchestrationTemplate heatOrchestrationTemplate,
+                                                String paramName, String contrailSharedResourceId,
+                                                String sharedTranslatedResourceId);
+}
