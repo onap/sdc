97782d80dffc669e831b920d04e99c8eb314d33b
mojahidi <mojahidul.islam@amdocs.com>
2018-05-15 11:59:53 +0530

    Handle  logger.debug(, exception)
    Removed logger.debug(, exception) from code
    
    Change-Id: I92a10d4581511e2cd0c073a9519dd161c4abe570
    Issue-ID: SDC-836
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-vendor-license-lib/openecomp-sdc-vendor-license-core/src/main/java/org/openecomp/sdc/vendorlicense/licenseartifacts/impl/types/XmlArtifact.java
+++ openecomp-be/lib/openecomp-sdc-vendor-license-lib/openecomp-sdc-vendor-license-core/src/main/java/org/openecomp/sdc/vendorlicense/licenseartifacts/impl/types/XmlArtifact.java
@@ -1,36 +1,29 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2018 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
- */
+*/
 
 package org.openecomp.sdc.vendorlicense.licenseartifacts.impl.types;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.openecomp.sdc.common.errors.CoreException;
-import org.openecomp.sdc.logging.api.Logger;
-import org.openecomp.sdc.logging.api.LoggerFactory;
 import org.openecomp.sdc.vendorlicense.VendorLicenseConstants;
 import org.openecomp.sdc.vendorlicense.errors.JsonErrorBuilder;
 
 public abstract class XmlArtifact {
 
   XmlMapper xmlMapper = new XmlMapper();
-  private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
   abstract void initMapper();
 
@@ -46,8 +39,8 @@ public abstract class XmlArtifact {
     try {
       xml = xmlMapper.writeValueAsString(this);
     } catch (com.fasterxml.jackson.core.JsonProcessingException exception) {
-      log.debug("",exception);
-      throw new CoreException(new JsonErrorBuilder(exception.getMessage()).build());
+      throw new CoreException(new JsonErrorBuilder("Failed to write xml value as string ")
+              .build(), exception);
     }
 
     return xml.replaceAll(VendorLicenseConstants.VENDOR_LICENSE_MODEL_ARTIFACT_REGEX_REMOVE, "");
