ad673d8dc6b3d3ffeb100637b2d7763d7c889017
mojahidi <mojahidul.islam@amdocs.com>
2018-05-15 11:09:21 +0530

    Handle  logger.debug(, exception) from code
    Removed logger.debug(, exception) from AbstractServiceModelDao,
     ActionAuthenticationFilter,ApplicationConfigManagerImpl
    
    Change-Id: I58665a7dfd7e3e29d630d136af1bbf65228a9a7d
    Issue-ID: SDC-836
    Signed-off-by: mojahidi <mojahidul.islam@amdocs.com>

--- openecomp-be/backend/openecomp-sdc-application-config-manager/src/main/java/org/openecomp/sdc/applicationconfig/impl/ApplicationConfigManagerImpl.java
+++ openecomp-be/backend/openecomp-sdc-application-config-manager/src/main/java/org/openecomp/sdc/applicationconfig/impl/ApplicationConfigManagerImpl.java
@@ -1,24 +1,22 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+/*
+ * Copyright © 2018 European Support Limited
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
- */
+*/
 
 package org.openecomp.sdc.applicationconfig.impl;
+
+import java.util.Collection;
 
 import org.openecomp.core.utilities.applicationconfig.ApplicationConfig;
 import org.openecomp.core.utilities.applicationconfig.dao.type.ApplicationConfigEntity;
@@ -28,17 +26,11 @@ import org.openecomp.sdc.applicationconfig.ApplicationConfigManager;
 import org.openecomp.sdc.common.errors.CoreException;
 import org.openecomp.sdc.common.errors.ErrorCategory;
 import org.openecomp.sdc.common.errors.ErrorCode;
-import org.openecomp.sdc.logging.api.Logger;
-import org.openecomp.sdc.logging.api.LoggerFactory;
-
-import java.util.Collection;
 
 /**
  * Created by Talio on 8/8/2016.
  */
 public class ApplicationConfigManagerImpl implements ApplicationConfigManager {
-
-  private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
   private static final String SCHEMA_GENERATOR_INITIALIZATION_ERROR =
       "SCHEMA_GENERATOR_INITIALIZATION_ERROR";
@@ -51,10 +43,9 @@ public class ApplicationConfigManagerImpl implements ApplicationConfigManager {
     try {
       applicationConfig.insertValue(namespace, key, value);
     } catch (Exception exception) {
-      log.debug("",exception);
       throw new CoreException(new ErrorCode.ErrorCodeBuilder().withCategory(ErrorCategory
           .APPLICATION).withId(SCHEMA_GENERATOR_INITIALIZATION_ERROR).withMessage(
-          SCHEMA_GENERATOR_INITIALIZATION_ERROR_MSG).build());
+          SCHEMA_GENERATOR_INITIALIZATION_ERROR_MSG).build(), exception);
     }
   }
 
