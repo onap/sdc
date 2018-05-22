2a142ba307c41d80fcdd66a47bf7217adf0e0444
eleonorali <eleonoral@amdocs.com>
2018-05-22 16:26:35 +0300

    TOSCA Consolidation Data - Refactoring Part 2
    Refactor get Compute/Port/Nested/SubInterface
    Consolidation Data APIs
    
    Change-Id: Ibce3abcd766555fe0eeaaf2f728d3a384935877d
    Issue-ID: SDC-1330
    Signed-off-by: eleonorali <eleonoral@amdocs.com>

---
+++ openecomp-be/lib/openecomp-sdc-translator-lib/openecomp-sdc-translator-core/src/test/java/org/openecomp/sdc/translator/datatypes/heattotosca/unifiedmodel/consolidation/ConsolidationDataTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright © 2016-2018 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openecomp.sdc.translator.datatypes.heattotosca.unifiedmodel.consolidation;
+
+import java.util.Optional;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.openecomp.sdc.translator.datatypes.heattotosca.TranslationContext;
+import org.openecomp.sdc.translator.services.heattotosca.ConsolidationEntityType;
+
+public class ConsolidationDataTest {
+
+    private final TranslationContext translationContext = new TranslationContext();
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetConsolidationDataHandler() {
+
+        verifyHandlerIsValid(ConsolidationEntityType.COMPUTE, ComputeConsolidationDataHandler.class);
+        verifyHandlerIsValid(ConsolidationEntityType.PORT, PortConsolidationDataHandler.class);
+        verifyHandlerIsValid(ConsolidationEntityType.SUB_INTERFACE, SubInterfaceConsolidationDataHandler.class);
+        verifyHandlerIsValid(ConsolidationEntityType.NESTED, NestedConsolidationDataHandler.class);
+        verifyHandlerIsValid(ConsolidationEntityType.VFC_NESTED, NestedConsolidationDataHandler.class);
+    }
+
+    @Test
+    public void testGetConsolidationDataHandler_Negative() {
+        verifyHandlerNotExist(ConsolidationEntityType.OTHER);
+        verifyHandlerNotExist(ConsolidationEntityType.VOLUME);
+    }
+
+    private void verifyHandlerIsValid(ConsolidationEntityType consolidationEntityType, Class cls) {
+
+        ConsolidationData consolidationData = translationContext.getConsolidationData();
+        Optional<ConsolidationDataHandler> consolidationDataHandler =
+                consolidationData.getConsolidationDataHandler(consolidationEntityType);
+        Assert.assertTrue(consolidationDataHandler.isPresent());
+        cls.isInstance(consolidationDataHandler.get());
+    }
+
+    private void verifyHandlerNotExist(ConsolidationEntityType consolidationEntityType) {
+        Optional<ConsolidationDataHandler> consolidationDataHandler =
+                translationContext.getConsolidationData().getConsolidationDataHandler(consolidationEntityType);
+        Assert.assertFalse(consolidationDataHandler.isPresent());
+    }
+
+}
