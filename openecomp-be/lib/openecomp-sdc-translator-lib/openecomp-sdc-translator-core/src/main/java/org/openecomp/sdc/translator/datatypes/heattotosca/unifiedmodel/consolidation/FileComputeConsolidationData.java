280cf3ca300cf167870007a5a3c17ff2bcfeab7d
siddharth0905 <siddharth.singh4@amdocs.com>
2018-05-23 16:32:25 +0530

    Refactoring - TOSCA Consolidation Data handling
    Changes as per the comments
    
    Change-Id: Ia6c5cbc68cd976452625eec28ac0bb8a065ae4e0
    Issue-ID: SDC-1330
    Signed-off-by: siddharth0905 <siddharth.singh4@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-translator-lib/openecomp-sdc-translator-core/src/main/java/org/openecomp/sdc/translator/datatypes/heattotosca/unifiedmodel/consolidation/FileComputeConsolidationData.java
+++ openecomp-be/lib/openecomp-sdc-translator-lib/openecomp-sdc-translator-core/src/main/java/org/openecomp/sdc/translator/datatypes/heattotosca/unifiedmodel/consolidation/FileComputeConsolidationData.java
@@ -10,47 +10,65 @@ import java.util.Set;
  */
 public class FileComputeConsolidationData {
 
-  //key - compute node type name (vm_type)
-  private Map<String, TypeComputeConsolidationData> typeComputeConsolidationData;
+    //key - compute node type name (vm_type)
+    private Map<String, TypeComputeConsolidationData> typeComputeConsolidationData;
 
-  /**
-   * Instantiates a new File compute consolidation data.
-   */
-  public FileComputeConsolidationData() {
-    typeComputeConsolidationData = new HashMap<>();
-  }
+    /**
+     * Instantiates a new File compute consolidation data.
+     */
+    public FileComputeConsolidationData() {
+        typeComputeConsolidationData = new HashMap<>();
+    }
 
-  /**
-   * Gets all compute types.
-   *
-   * @return the all compute types
-   */
-  public Set<String> getAllComputeTypes() {
-    return typeComputeConsolidationData.keySet();
-  }
+    /**
+     * Gets all compute types.
+     *
+     * @return the all compute types
+     */
+    public Set<String> getAllComputeTypes() {
+        return typeComputeConsolidationData.keySet();
+    }
 
-  public Collection<TypeComputeConsolidationData> getAllTypeComputeConsolidationData(){
-    return typeComputeConsolidationData.values();
-  }
+    /**
+     * Gets all type compute consolidation data.
+     *
+     * @return the all type compute consolidation data
+     */
+    public Collection<TypeComputeConsolidationData> getAllTypeComputeConsolidationData() {
+        return typeComputeConsolidationData.values();
+    }
 
-  /**
-   * Gets type compute consolidation data.
-   *
-   * @param computeType the compute type
-   * @return the type compute consolidation data
-   */
-  public TypeComputeConsolidationData getTypeComputeConsolidationData(String computeType) {
-    return typeComputeConsolidationData.get(computeType);
-  }
+    /**
+     * Gets type compute consolidation data.
+     *
+     * @param computeType the compute type
+     * @return the type compute consolidation data
+     */
+    public TypeComputeConsolidationData getTypeComputeConsolidationData(String computeType) {
+        return typeComputeConsolidationData.get(computeType);
+    }
 
-  /**
-   * Sets type compute consolidation data.
-   *
-   * @param computeType                  the compute type
-   * @param typeComputeConsolidationData the type compute consolidation data
-   */
-  public void setTypeComputeConsolidationData(String computeType, TypeComputeConsolidationData
-      typeComputeConsolidationData) {
-    this.typeComputeConsolidationData.put(computeType, typeComputeConsolidationData);
-  }
+    /**
+     * Sets type compute consolidation data.
+     *
+     * @param computeType                  the compute type
+     * @param typeComputeConsolidationData the type compute consolidation data
+     */
+    public void setTypeComputeConsolidationData(String computeType,
+                                                       TypeComputeConsolidationData typeComputeConsolidationData) {
+        this.typeComputeConsolidationData.put(computeType, typeComputeConsolidationData);
+    }
+
+    /**
+     * Is number of compute types legal boolean.
+     *
+     * @return the boolean
+     */
+    public boolean isNumberOfComputeTypesLegal() {
+        Collection<TypeComputeConsolidationData> typeComputeConsolidationDataCollection =
+                getAllTypeComputeConsolidationData();
+        return typeComputeConsolidationDataCollection.size() == 1
+                && typeComputeConsolidationDataCollection.iterator().next()
+                                                         .isNumberOfComputeConsolidationDataPerTypeLegal();
+    }
 }
---
+++ openecomp-be/lib/openecomp-sdc-translator-lib/openecomp-sdc-translator-core/src/test/java/org/openecomp/sdc/translator/datatypes/heattotosca/unifiedmodel/consolidation/EntityConsolidationDataTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright © 2016-2018 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openecomp.sdc.translator.datatypes.heattotosca.unifiedmodel.consolidation;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EntityConsolidationDataTest {
+    @Test
+    public void testIsGetAttrOutFromEntityLegal() {
+        Map<String, List<String>> ports = new HashMap<>();
+        ports.put("server_networkrole_1_port",
+                Arrays.asList("server_0_networkrole_1_port", "server_1_networkrole_1_port"));
+
+        GetAttrFuncData getAttrFuncData = new GetAttrFuncData("vmac_address", "accessIPv4");
+        Map<String, List<GetAttrFuncData>> getAttOutMap = new HashMap<>();
+        getAttOutMap.put("server_0_networkrole_1_port", Collections.singletonList(getAttrFuncData));
+
+        GetAttrFuncData getAttrFuncData1 = new GetAttrFuncData("vmac_address", "accessIPv4");
+        Map<String, List<GetAttrFuncData>> getAttOutMap1 = new HashMap<>();
+        getAttOutMap1.put("server_1_networkrole_1_port", Collections.singletonList(getAttrFuncData1));
+
+
+        EntityConsolidationData entityConsolidationData = new EntityConsolidationData();
+        entityConsolidationData.setNodesGetAttrOut(getAttOutMap);
+
+        EntityConsolidationData entityConsolidationData1 = new EntityConsolidationData();
+        entityConsolidationData1.setNodesGetAttrOut(getAttOutMap1);
+
+        List<EntityConsolidationData> entityConsolidationDataList =
+                Arrays.asList(entityConsolidationData, entityConsolidationData1);
+
+        Assert.assertTrue(entityConsolidationData
+                                  .isGetAttrOutFromEntityLegal(entityConsolidationDataList, ports));
+    }
+
+    @Test
+    public void testIsGetAttrOutFromEntityLegalNegative() {
+        Map<String, List<String>> ports = new HashMap<>();
+        ports.put("server_networkrole_1_port",
+                Arrays.asList("server_0_networkrole_1_port", "server_0_networkrole_2_port"));
+
+        GetAttrFuncData getAttrFuncData = new GetAttrFuncData("vmac_address", "accessIPv4");
+        Map<String, List<GetAttrFuncData>> getAttOutMap = new HashMap<>();
+        getAttOutMap.put("server_0_networkrole_1_port", Collections.singletonList(getAttrFuncData));
+
+        GetAttrFuncData getAttrFuncData1 = new GetAttrFuncData("vmac_address", "accessIPv4");
+        Map<String, List<GetAttrFuncData>> getAttOutMap1 = new HashMap<>();
+        getAttOutMap.put("server_0_networkrole_2_port", Collections.singletonList(getAttrFuncData1));
+
+
+        EntityConsolidationData entityConsolidationData = new EntityConsolidationData();
+        entityConsolidationData.setNodesGetAttrOut(getAttOutMap);
+
+        EntityConsolidationData entityConsolidationData1 = new EntityConsolidationData();
+        //entityConsolidationData1.setNodesGetAttrOut(getAttOutMap1);
+
+        List<EntityConsolidationData> entityConsolidationDataList =
+                Arrays.asList(entityConsolidationData, entityConsolidationData1);
+
+        Assert.assertFalse(entityConsolidationData
+                                  .isGetAttrOutFromEntityLegal(entityConsolidationDataList, ports));
+    }
+
+    @Test
+    public void testIsGetAttrOutFromEntityLegalMultiplePortWithDiffAttr() {
+        Map<String, List<String>> ports = new HashMap<>();
+        ports.put("server_networkrole_1_port",
+                Arrays.asList("server_0_networkrole_1_port", "server_1_networkrole_1_port"));
+
+        ports.put("server_networkrole_2_port",
+                Arrays.asList("server_0_networkrole_2_port", "server_0_networkrole_2_port"));
+
+        GetAttrFuncData getAttrFuncData = new GetAttrFuncData("vmac_address", "accessIPv4");
+        Map<String, List<GetAttrFuncData>> getAttOutMap = new HashMap<>();
+        getAttOutMap.put("server_0_networkrole_1_port", Collections.singletonList(getAttrFuncData));
+        getAttOutMap.put("server_0_networkrole_2_port", Collections.singletonList(getAttrFuncData));
+
+        GetAttrFuncData getAttrFuncData1 = new GetAttrFuncData("vmac_address", "accessIPv4");
+        Map<String, List<GetAttrFuncData>> getAttOutMap1 = new HashMap<>();
+        getAttOutMap.put("server_0_networkrole_1_port", Collections.singletonList(getAttrFuncData1));
+
+
+        EntityConsolidationData entityConsolidationData = new EntityConsolidationData();
+        entityConsolidationData.setNodesGetAttrOut(getAttOutMap);
+
+        EntityConsolidationData entityConsolidationData1 = new EntityConsolidationData();
+        entityConsolidationData1.setNodesGetAttrOut(getAttOutMap1);
+
+        List<EntityConsolidationData> entityConsolidationDataList =
+                Arrays.asList(entityConsolidationData, entityConsolidationData1);
+
+        Assert.assertFalse(entityConsolidationData
+                                   .isGetAttrOutFromEntityLegal(entityConsolidationDataList, ports));
+    }
+}
