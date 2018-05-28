daf0dfe8308a259d168734d3fe98212cadad1a9e
priyanshu <pagarwal@amdocs.com>
2018-05-28 15:29:26 +0300

    Activity Spec docker enhancements
    Activity Spec Changes for docker in Kubernetes
    
    Change-Id: I987fe097792ccd5d41a0ad811602dad72259d45f
    Issue-ID: SDC-1048
    Signed-off-by: priyanshu <pagarwal@amdocs.com>

--- services/activity-spec/activity-spec-init/start.sh
+++ services/activity-spec/activity-spec-init/start.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-if [[ -z "${CASSANDRA_HOST}" ]]; then
-	echo "CASSANDRA_HOST environment variable must be set"
+if [[ -z "${CS_USER}" ]]; then
+	echo "CS_USER environment variable must be set"
 	exit 1
 fi
 
-cqlsh -f /create_activityspec_db.cql $CASSANDRA_HOST $CASSANDRA_PORT
+if [[ -z "${CS_PASSWORD}" ]]; then
+	echo "CS_PASSWORD environment variable must be set"
+	exit 1
+fi
+
+if [[ -z "${CS_HOST}" ]]; then
+	echo "CS_HOST environment variable must be set"
+	exit 1
+fi
+
+cqlsh -u $CS_USER -p $CS_PASSWORD -f /create_activityspec_db.cql $CS_HOST $CS_PORT
---
+++ services/activity-spec/activity-spec-web/activity-spec-docker-assembly/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+JAVA_OPTIONS=" ${JAVA_OPTIONS} \
+            -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
+            -Dconfig.home=${CONFIG_FILES_DIR}/config \
+            -Dlog.home=${LOGS_DIR} \
+            -Dlogback.configurationFile=${CONFIG_FILES_DIR}/logback.xml \
+            -Dconfiguration.yaml=${CONFIG_FILES_DIR}/config/configuration.yaml"
+
+java -jar "$JETTY_HOME/start.jar" ${JAVA_OPTIONS}
