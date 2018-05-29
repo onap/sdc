82085090bfd57f8ca7f191f3765f78e104b103d1
Gautam Shah <gautams@amdocs.com>
2018-05-29 16:40:44 +0530

    Adding Proxy settings for pulling PMD artifact.
    Proxy settings applied.
    
    Change-Id: I9c32894239c998a497a6b59e79b3c41dd7d5df14
    Issue-ID: SDC-1189
    Signed-off-by: GAUTAMS <gautams@amdocs.com>

--- openecomp-be/tools/artifact-copy-plugin/src/main/java/org/openecomp/sdc/onboarding/util/CopyArtifactPlugin.java
+++ openecomp-be/tools/artifact-copy-plugin/src/main/java/org/openecomp/sdc/onboarding/util/CopyArtifactPlugin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2018 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on a "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecomp.sdc.onboarding.util;
 
 import java.io.File;
@@ -49,12 +65,11 @@ public class CopyArtifactPlugin extends AbstractMojo {
         }
         boolean isSnapshot = version.contains("SNAPSHOT");
         List<ArtifactRepository> artRepoList = artifactHelper.getRepositories(isSnapshot);
-        String resolvedVersion =
-                project.getProperties().getProperty("resolvedVersion");
+        String resolvedVersion = project.getProperties().getProperty("resolvedVersion");
         try {
             if (!version.equals(resolvedVersion)) {
-                if(copyResolvedArtifact(artRepoList, resolvedVersion) && getLog().isInfoEnabled()){
-                    getLog().info("Data Artifact Copied with "+resolvedVersion);
+                if (copyResolvedArtifact(artRepoList, resolvedVersion) && getLog().isInfoEnabled()) {
+                    getLog().info("Data Artifact Copied with " + resolvedVersion);
                 }
 
             }
@@ -100,7 +115,7 @@ public class CopyArtifactPlugin extends AbstractMojo {
     }
 
 
-    private boolean copyResolvedArtifact(List<ArtifactRepository> list, String resolvedVersion){
+    private boolean copyResolvedArtifact(List<ArtifactRepository> list, String resolvedVersion) {
         for (ArtifactRepository repo : list) {
             try {
                 writeContents(
@@ -110,7 +125,7 @@ public class CopyArtifactPlugin extends AbstractMojo {
                                         + ".jar"));
                 return true;
             } catch (IOException e) {
-                getLog().warn(e);
+                getLog().debug(e);
             }
         }
         return false;
