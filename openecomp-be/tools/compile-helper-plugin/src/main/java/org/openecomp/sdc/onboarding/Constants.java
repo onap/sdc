4c82af6ab4374b61952d7ddd7b58638641e175bb
Gautam Shah <gautams@amdocs.com>
2018-06-01 08:38:32 +0530

    Fixing Merge Build issue and Sonar issues.
    fixing build issue and Sonar issues.
    
    Change-Id: I4209b79ab2a3646839df50be4b58f8f230b6d710
    Issue-ID: SDC-1189
    Signed-off-by: GAUTAMS <gautams@amdocs.com>

--- openecomp-be/tools/compile-helper-plugin/src/main/java/org/openecomp/sdc/onboarding/Constants.java
+++ openecomp-be/tools/compile-helper-plugin/src/main/java/org/openecomp/sdc/onboarding/Constants.java
@@ -22,7 +22,6 @@ public class Constants {
     public static final String UNICORN = "unicorn";
     public static final String EMPTY_STRING = "";
     public static final String PREFIX = System.getProperties().contains(UNICORN) ? EMPTY_STRING : UNICORN;
-    ;
     public static final String JACOCO_SKIP = "jacoco.skip";
     public static final String FORK_COUNT = "fork.count";
     public static final String FORK_MODE = "fork.mode";
@@ -39,6 +38,7 @@ public class Constants {
     public static final String SKIP_MAIN_SOURCE_COMPILE = PREFIX + "skipMainSourceCompile";
     public static final String SKIP_TEST_SOURCE_COMPILE = PREFIX + "skipTestSourceCompile";
     public static final String MAIN_CHECKSUM = "mainChecksum";
+    public static final String CHECKSUM = "checksum";
     public static final String TEST_CHECKSUM = "testChecksum";
     public static final String RESOURCE_CHECKSUM = "resourceChecksum";
     public static final String MAIN_SOURCE_CHECKSUM = "mainSourceChecksum";
@@ -65,4 +65,6 @@ public class Constants {
 
     private Constants() {
     }
+
+
 }
