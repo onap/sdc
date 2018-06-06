1f9ed0292ef527e1823b0929edad5498b1a8d403
Tal Gitelman <tg851x@intl.att.com>
2018-06-06 19:15:13 +0300

    new unit tests for sdc-model
    Change-Id: I14b1d527b18b354b8c29149135a1dbb800a454bb
    Issue-ID: SDC-1333
    Signed-off-by: Tal Gitelman <tg851x@intl.att.com>

--- catalog-model/src/test/java/org/openecomp/sdc/be/model/PropertyDefinitionTest.java
+++ catalog-model/src/test/java/org/openecomp/sdc/be/model/PropertyDefinitionTest.java
@@ -1,8 +1,11 @@
 package org.openecomp.sdc.be.model;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Test;
+import org.openecomp.sdc.be.datatypes.elements.PropertyDataDefinition;
+import org.openecomp.sdc.be.model.PropertyDefinition.PropertyNames;
 
 
 public class PropertyDefinitionTest {
@@ -10,7 +13,44 @@ public class PropertyDefinitionTest {
 	private PropertyDefinition createTestSubject() {
 		return new PropertyDefinition();
 	}
-
+	
+	@Test
+	public void testCtor() throws Exception {
+		new PropertyDefinition(new PropertyDefinition());
+		new PropertyDefinition(new PropertyDataDefinition());
+	}
+	
+	@Test
+	public void testPropertyNames() throws Exception {
+		PropertyNames availabiltyZoneCount = PropertyDefinition.PropertyNames.AVAILABILTY_ZONE_COUNT;
+	}
+	
+	@Test
+	public void testPropertyNames_GetPropertyName() throws Exception {
+		PropertyDefinition.PropertyNames.AVAILABILTY_ZONE_COUNT.getPropertyName();
+	}
+	
+	@Test
+	public void testPropertyNames_GetUpdateBehavior() throws Exception {
+		PropertyDefinition.PropertyNames.AVAILABILTY_ZONE_COUNT.getUpdateBehavior();
+	}
+	
+	@Test
+	public void testPropertyNames_FindName() throws Exception {
+		PropertyDefinition.PropertyNames.findName(null);
+		PropertyDefinition.PropertyNames.findName(PropertyDefinition.PropertyNames.AVAILABILTY_ZONE_COUNT.getPropertyName());
+	}
+	
+	@Test
+	public void testGroupInstancePropertyValueUpdateBehavior_GetLevelName() throws Exception {
+		PropertyDefinition.GroupInstancePropertyValueUpdateBehavior.NOT_RELEVANT.getLevelName();
+	}
+	
+	@Test
+	public void testGroupInstancePropertyValueUpdateBehavior_GetLevelNumber() throws Exception {
+		PropertyDefinition.GroupInstancePropertyValueUpdateBehavior.NOT_RELEVANT.getLevelNumber();
+	}
+	
 	@Test
 	public void testGetConstraints() throws Exception {
 		PropertyDefinition testSubject;
@@ -80,5 +120,18 @@ public class PropertyDefinitionTest {
 		// default test
 		testSubject = createTestSubject();
 		result = testSubject.equals(obj);
+		
+		result = testSubject.equals(new Object());
+		result = testSubject.equals(testSubject);
+		PropertyDefinition testSubject2 = createTestSubject();
+		result = testSubject.equals(testSubject2);
+		testSubject2.setConstraints(new LinkedList<>());
+		result = testSubject.equals(testSubject2);
+		testSubject.setConstraints(new LinkedList<>());
+		result = testSubject.equals(testSubject2);
+		testSubject2.setName("mock");
+		result = testSubject.equals(testSubject2);
+		testSubject.setName("mock");
+		result = testSubject.equals(testSubject2);
 	}
 }
---
+++ catalog-model/src/test/java/org/openecomp/sdc/be/model/cache/ApplicationDataTypeCacheTest.java
@@ -0,0 +1,169 @@
+package org.openecomp.sdc.be.model.cache;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.openecomp.sdc.be.dao.titan.TitanOperationStatus;
+import org.openecomp.sdc.be.model.DataTypeDefinition;
+import org.openecomp.sdc.be.model.operations.impl.PropertyOperation;
+import org.openecomp.sdc.be.resources.data.DataTypeData;
+import org.openecomp.sdc.be.unittests.utils.ModelConfDependentTest;
+
+import fj.data.Either;
+import mockit.Deencapsulation;
+
+public class ApplicationDataTypeCacheTest extends ModelConfDependentTest{
+
+	@InjectMocks
+	private ApplicationDataTypeCache testSubject;
+	
+	@Mock
+	PropertyOperation propertyOperation;
+
+	@Before
+	public void setUpMocks() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+
+	@Test
+	public void testInit() throws Exception {
+		testSubject.init();
+	}
+
+	@Test
+	public void testDestroy() throws Exception {
+		testSubject.init();
+		Deencapsulation.invoke(testSubject, "destroy");
+	}
+
+	@Test
+	public void testShutdownExecutor() throws Exception {
+
+		// default test
+		Deencapsulation.invoke(testSubject, "shutdownExecutor");
+	}
+
+	@Test
+	public void testGetAllDataTypesFromGraph() throws Exception {
+		Either<Map<String, DataTypeDefinition>, TitanOperationStatus> result;
+
+		// default test
+		result = Deencapsulation.invoke(testSubject, "getAllDataTypesFromGraph");
+	}
+
+	@Test
+	public void testGetAll() throws Exception {
+		Either<Map<String, DataTypeDefinition>, TitanOperationStatus> result;
+
+		// default test
+		result = testSubject.getAll();
+	}
+
+	@Test
+	public void testGet() throws Exception {
+		String uniqueId = "";
+		Either<DataTypeDefinition, TitanOperationStatus> result;
+
+		// default test
+		result = testSubject.get(uniqueId);
+	}
+
+	@Test
+	public void testGet2() throws Exception {
+		String uniqueId = "";
+		Either<DataTypeDefinition, TitanOperationStatus> result;
+
+		HashMap<String, DataTypeDefinition> a = new HashMap<>();
+		DataTypeDefinition value1 = new DataTypeDefinition();
+		value1.setUniqueId("mock");
+		a.put("mock", value1);
+		Either<Map<String, DataTypeDefinition>, TitanOperationStatus> value = Either.left(a);
+		Mockito.when(propertyOperation.getAllDataTypes()).thenReturn(value);
+		// default test
+		Deencapsulation.invoke(testSubject, "replaceAllData");
+		result = testSubject.get(uniqueId);
+	}
+	
+	@Test
+	public void testRun() throws Exception {
+		testSubject.run();
+	}
+
+	@Test
+	public void testRun2() throws Exception {
+		Either<List<DataTypeData>, TitanOperationStatus> value = Either.right(TitanOperationStatus.GENERAL_ERROR);
+		Mockito.when(propertyOperation.getAllDataTypeNodes()).thenReturn(value);
+		testSubject.run();
+	}
+	
+	@Test
+	public void testRun3() throws Exception {
+		LinkedList<DataTypeData> a = new LinkedList<>();
+		a.add(new DataTypeData());
+		Either<List<DataTypeData>, TitanOperationStatus> value = Either.left(a);
+		Mockito.when(propertyOperation.getAllDataTypeNodes()).thenReturn(value);
+		
+		HashMap<String, DataTypeDefinition> a1 = new HashMap<>();
+		DataTypeDefinition value1 = new DataTypeDefinition();
+		value1.setUniqueId("mock");
+		a1.put("mock", value1);
+		Either<Map<String, DataTypeDefinition>, TitanOperationStatus> value2 = Either.left(a1);
+		Mockito.when(propertyOperation.getAllDataTypes()).thenReturn(value2);
+		
+		Deencapsulation.invoke(testSubject, "replaceAllData");
+		testSubject.run();
+	}
+	
+	@Test
+	public void testCompareDataTypes() throws Exception {
+		Map<String, ImmutablePair<Long, Long>> dataTypeNameToModificationTime = new HashMap<>();
+		Map<String, ImmutablePair<Long, Long>> currentDataTypeToModificationTime = new HashMap<>();
+		boolean result;
+
+		// default test
+		result = Deencapsulation.invoke(testSubject, "compareDataTypes", dataTypeNameToModificationTime, currentDataTypeToModificationTime);
+	}
+
+	@Test
+	public void testCompareDataTypes2() throws Exception {
+		Map<String, ImmutablePair<Long, Long>> dataTypeNameToModificationTime = new HashMap<>();
+		Map<String, ImmutablePair<Long, Long>> currentDataTypeToModificationTime = new HashMap<>();
+		boolean result;
+		
+		currentDataTypeToModificationTime.put("mock", ImmutablePair.of(1L, 2L));
+		dataTypeNameToModificationTime.put("mock", ImmutablePair.of(5L, 6L));
+		
+		// default test
+		result = Deencapsulation.invoke(testSubject, "compareDataTypes", dataTypeNameToModificationTime, currentDataTypeToModificationTime);
+	}
+	
+	@Test
+	public void testReplaceAllData() throws Exception {
+		HashMap<String, DataTypeDefinition> a = new HashMap<>();
+		DataTypeDefinition value1 = new DataTypeDefinition();
+		value1.setUniqueId("mock");
+		a.put("mock", value1);
+		Either<Map<String, DataTypeDefinition>, TitanOperationStatus> value = Either.left(a);
+		Mockito.when(propertyOperation.getAllDataTypes()).thenReturn(value);
+		// default test
+		Deencapsulation.invoke(testSubject, "replaceAllData");
+	}
+	
+	@Test
+	public void testReplaceAllData2() throws Exception {
+		Either<Map<String, DataTypeDefinition>, TitanOperationStatus> value = Either.right(TitanOperationStatus.GENERAL_ERROR);
+		Mockito.when(propertyOperation.getAllDataTypes()).thenReturn(value);
+		// default test
+		Deencapsulation.invoke(testSubject, "replaceAllData");
+	}
+}
