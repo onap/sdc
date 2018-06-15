e3b0c307dc0b5cfe8a6b18fae27e22e6100a4707
Piotr Darosz <piotr.darosz@nokia.com>
2018-06-15 08:28:00 +0200

    Add tests for SdcSchema classes
    Introduce Cassandra Unit tool and tests for schema-related classes
    
    Change-Id: I14da602c42056730e7992dd92b4da8b87dd9fb0d
    Issue-ID: SDC-1358
    Signed-off-by: Piotr Darosz <piotr.darosz@nokia.com>

--- catalog-be/src/main/java/org/openecomp/sdc/be/components/impl/CassandraHealthCheck.java
+++ catalog-be/src/main/java/org/openecomp/sdc/be/components/impl/CassandraHealthCheck.java
@@ -1,3 +1,24 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ * Modifications copyright (c) 2018 Nokia
+ * ================================================================================
+ */
 package org.openecomp.sdc.be.components.impl;
 
 import com.datastax.driver.core.Cluster;
@@ -12,6 +33,7 @@ import org.openecomp.sdc.common.util.GeneralUtility;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.*;
@@ -27,6 +49,8 @@ public class CassandraHealthCheck {
     private Set<String> sdcKeyspaces = new HashSet<>();
 
     private int HC_FormulaNumber;
+
+    private SdcSchemaUtils sdcSchemaUtils;
 
     @PostConstruct
     private void init() {
@@ -61,14 +85,14 @@ public class CassandraHealthCheck {
         }
 
         log.info("All sdc keyspaces are : {}", sdcKeyspaces);
-
+        sdcSchemaUtils = new SdcSchemaUtils();
         //Calculate the Formula of Health Check
         Cluster cluster = null;
         try {
 
             log.info("creating cluster for Cassandra Health Check.");
             //Create cluster from nodes in cassandra configuration
-            cluster = SdcSchemaUtils.createCluster();
+            cluster = sdcSchemaUtils.createCluster();
             if (cluster == null) {
                 log.error("Failure create cassandra cluster.");
                 return;
@@ -141,7 +165,7 @@ public class CassandraHealthCheck {
         Session session = null;
         try {
             log.info("creating cluster for Cassandra for monitoring.");
-            cluster = SdcSchemaUtils.createCluster();
+            cluster = sdcSchemaUtils.createCluster();
             if (cluster == null) {
                 log.error("Failure create cassandra cluster.");
                 return false;
--- catalog-dao/src/test/java/org/openecomp/sdc/be/dao/cassandra/schema/SdcSchemaBuilderTest.java
+++ catalog-dao/src/test/java/org/openecomp/sdc/be/dao/cassandra/schema/SdcSchemaBuilderTest.java
@@ -1,104 +1,105 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ * Modifications copyright (c) 2018 Nokia
+ * ================================================================================
+ */
 package org.openecomp.sdc.be.dao.cassandra.schema;
 
+import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
-import mockit.Deencapsulation;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mockito;
-import org.openecomp.sdc.be.config.Configuration.CassandrConfig.KeyspaceConfig;
-import org.openecomp.sdc.be.dao.cassandra.schema.SdcSchemaBuilder.ReplicationStrategy;
-import org.openecomp.sdc.be.utils.DAOConfDependentTest;
+import org.openecomp.sdc.be.config.Configuration;
+import org.openecomp.sdc.be.utils.CassandraTestHelper;
 
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-public class SdcSchemaBuilderTest extends DAOConfDependentTest{
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
 
-	private SdcSchemaBuilder createTestSubject() {
-		return new SdcSchemaBuilder();
-	}
+public class SdcSchemaBuilderTest {
 
-	@Test
-	public void testHandle1707OSMigration() throws Exception {
-		Map<String, Map<String, List<String>>> cassndraMetadata = new HashMap<>();
-		Map<String, List<ITableDescription>> schemeData = new HashMap<>();
+    @BeforeClass
+    public static void startServer() {
+        CassandraTestHelper.startServer();
+    }
 
-		// default test
-		Deencapsulation.invoke(SdcSchemaBuilder.class, "handle1707OSMigration", cassndraMetadata, schemeData);
-	}
+    @Test
+    public void testCreateSchema() {
+        SdcSchemaUtils utils = mock(SdcSchemaUtils.class);
+        when(utils.createCluster()).thenReturn(CassandraTestHelper.createCluster());
+        SdcSchemaBuilder sdcSchemaBuilder = new SdcSchemaBuilder(utils, SdcSchemaBuilderTest::createCassandraConfig);
+        final boolean result = sdcSchemaBuilder.createSchema();
+        Assert.assertTrue(result);
+    }
 
-	@Test
-	public void testCreateSchema() throws Exception {
-		boolean result;
+    @Test
+    public void testDeleteSchemaNoKeyspaces() {
+        SdcSchemaUtils utils = mock(SdcSchemaUtils.class);
+        when(utils.createCluster()).thenReturn(CassandraTestHelper.createCluster());
+        SdcSchemaBuilder sdcSchemaBuilder = new SdcSchemaBuilder(utils, SdcSchemaBuilderTest::createCassandraConfig);
+        final boolean result = sdcSchemaBuilder.deleteSchema();
+        Assert.assertTrue(result);
+    }
 
-		// default test
-		result = SdcSchemaBuilder.createSchema();
-	}
+    @Test
+    public void testDeleteSchemaWithKeyspacesExisting() {
+        Configuration.CassandrConfig throwAwayConfig = createCassandraConfig();
+        Cluster cluster = CassandraTestHelper.createCluster();
+        createTestKeyspaces(cluster, throwAwayConfig.getKeySpaces());
+        SdcSchemaUtils utils = mock(SdcSchemaUtils.class);
+        when(utils.createCluster()).thenReturn(cluster);
+        SdcSchemaBuilder sdcSchemaBuilder = new SdcSchemaBuilder(utils, SdcSchemaBuilderTest::createCassandraConfig);
+        final boolean result = sdcSchemaBuilder.deleteSchema();
+        Assert.assertTrue(result);
+    }
 
-	@Test
-	public void testDeleteSchema() throws Exception {
-		boolean result;
+    private static Configuration.CassandrConfig createCassandraConfig() {
+        Configuration.CassandrConfig cfg = new Configuration.CassandrConfig();
+        Set<String> requiredKeyspaces = Arrays.stream(Table.values())
+                .map(t -> t.getTableDescription().getKeyspace().toLowerCase())
+                .collect(Collectors.toSet());
+        List<Configuration.CassandrConfig.KeyspaceConfig> createdKeyspaces = requiredKeyspaces.stream()
+                .map(k -> {
+                    Configuration.CassandrConfig.KeyspaceConfig keyspace = new Configuration.CassandrConfig.KeyspaceConfig();
+                    keyspace.setName(k);
+                    keyspace.setReplicationInfo(Collections.singletonList("1"));
+                    keyspace.setReplicationStrategy(SdcSchemaBuilder.ReplicationStrategy.SIMPLE_STRATEGY.getStrategyName());
+                    return keyspace;})
+                .collect(Collectors.toList());
+        cfg.setKeySpaces(createdKeyspaces);
+        return cfg;
+    }
 
-		// default test
-		result = SdcSchemaBuilder.deleteSchema();
-	}
-
-	@Test
-	public void testCreateIndexName() throws Exception {
-		String table = "";
-		String column = "";
-		String result;
-
-		// default test
-		result = Deencapsulation.invoke(SdcSchemaBuilder.class, "createIndexName", table, column);
-	}
-
-	@Test
-	public void testCreateKeyspace() throws Exception {
-		String keyspace = "mock";
-		Map<String, Map<String, List<String>>> cassndraMetadata = new HashMap<>();
-		Session session = Mockito.mock(Session.class);
-		boolean result;
-
-		// default test
-		result = Deencapsulation.invoke(SdcSchemaBuilder.class, "createKeyspace",
-				keyspace, cassndraMetadata, session);
-		
-		cassndraMetadata.put(keyspace, new HashMap<>());
-		result = Deencapsulation.invoke(SdcSchemaBuilder.class, "createKeyspace",
-				keyspace, cassndraMetadata, session);
-	}
-
-	@Test
-	public void testGetSchemeData() throws Exception {
-		Map<String, List<ITableDescription>> result;
-
-		// default test
-		result = Deencapsulation.invoke(SdcSchemaBuilder.class, "getSchemeData");
-	}
-
-	@Test
-	public void testCreateKeyspaceQuereyString() throws Exception {
-		String keyspace = "mock";
-		KeyspaceConfig keyspaceInfo = new KeyspaceConfig();
-		String result;
-
-		// default test
-		result = Deencapsulation.invoke(SdcSchemaBuilder.class, "createKeyspaceQuereyString", keyspace, keyspaceInfo);
-		
-		keyspaceInfo.setReplicationStrategy(ReplicationStrategy.NETWORK_TOPOLOGY_STRATEGY.getStrategyName());
-		LinkedList<String> replicationInfo = new LinkedList<>();
-		keyspaceInfo.setReplicationInfo(replicationInfo);
-		//Test1
-		result = Deencapsulation.invoke(SdcSchemaBuilder.class, "createKeyspaceQuereyString", keyspace, keyspaceInfo);
-		replicationInfo.add("mock1");
-		replicationInfo.add("mock2");
-		
-		result = Deencapsulation.invoke(SdcSchemaBuilder.class, "createKeyspaceQuereyString", keyspace, keyspaceInfo);
-		
-		//Test2
-		keyspaceInfo.setReplicationStrategy(ReplicationStrategy.SIMPLE_STRATEGY.getStrategyName());
-		result = Deencapsulation.invoke(SdcSchemaBuilder.class, "createKeyspaceQuereyString", keyspace, keyspaceInfo);
-	}
+    private static void createTestKeyspaces(Cluster cluster, List<Configuration.CassandrConfig.KeyspaceConfig> keyspaceConfig) {
+        try(Session session = cluster.connect()) {
+            keyspaceConfig.forEach(keyspace -> {
+                String query = String
+                        .format("CREATE KEYSPACE IF NOT EXISTS %s WITH replication = {'class':'%s', 'replication_factor' : %s};",
+                                keyspace.getName(), keyspace.getReplicationStrategy(), keyspace.getReplicationInfo().get(0));
+                session.execute(query);
+            });
+        }
+    }
 }
