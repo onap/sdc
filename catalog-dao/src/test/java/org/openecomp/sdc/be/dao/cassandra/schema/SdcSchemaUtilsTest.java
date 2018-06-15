56db7265da84b1c1251d96fe0d0546d478cb7a54
Piotr Darosz <piotr.darosz@nokia.com>
2018-06-15 08:28:00 +0200

    Add tests for SdcSchema classes
    Introduce Scassandra tool and tests for schema-related classes
    
    Change-Id: I14da602c42056730e7992dd92b4da8b87dd9fb0d
    Issue-ID: SDC-1358
    Signed-off-by: Piotr Darosz <piotr.darosz@nokia.com>

--- catalog-dao/src/test/java/org/openecomp/sdc/be/dao/cassandra/schema/SdcSchemaUtilsTest.java
+++ catalog-dao/src/test/java/org/openecomp/sdc/be/dao/cassandra/schema/SdcSchemaUtilsTest.java
@@ -1,38 +1,150 @@
 package org.openecomp.sdc.be.dao.cassandra.schema;
+import com.datastax.driver.core.Cluster;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.openecomp.sdc.be.config.Configuration;
+import org.openecomp.sdc.be.utils.CassandraTestHelper;
+import org.scassandra.Scassandra;
 
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
 
-import org.junit.Test;
-import org.openecomp.sdc.be.config.ConfigurationManager;
-import org.openecomp.sdc.be.utils.DAOConfDependentTest;
+import static org.mockito.Mockito.when;
 
+public class SdcSchemaUtilsTest {
+	private static final String SINGLE_STATEMENT = "";
+	private static final String[] MULTIPLE_STATEMENTS = new String[] {"",""};
+	private static final List<String> CASSANDRA_HOSTS = Collections.singletonList(CassandraTestHelper.SERVER);
+	private static final String CASSANDRA_USERNAME = "username";
+	private static final String CASSANDRA_PASSWORD = "password";
+	private static final String TRUSTSTORE_PATH = "pathToTruststore";
+	private static final String TRUSTSTORE_PASSWORD = "passwordToTruststore";
+	private static Scassandra server = null;
 
-public class SdcSchemaUtilsTest extends DAOConfDependentTest{
-	
+	@BeforeClass
+	public static void startServer() {
+		server = CassandraTestHelper.getServer();
+		server.start();
+	}
+
+	@AfterClass
+	public static void stopServer() {
+		server.stop();
+	}
+
 	@Test
-	public void testExecuteStatement() throws Exception {
-		String statement = "";
-		boolean result;
-
-		// default test
-		result = SdcSchemaUtils.executeStatement(statement);
-		
-		List<String> cassandraHosts = new LinkedList<>();
-		ConfigurationManager.getConfigurationManager().getConfiguration().getCassandraConfig().setCassandraHosts(cassandraHosts);
-		ConfigurationManager.getConfigurationManager().getConfiguration().getCassandraConfig().setAuthenticate(true);
-		ConfigurationManager.getConfigurationManager().getConfiguration().getCassandraConfig().setSsl(true);
-		
-		result = SdcSchemaUtils.executeStatement(statement);
+	public void testExecuteSingleStatement() throws Exception {
+		SdcSchemaUtils sdcSchemaUtils = new SdcSchemaUtils();
+		final boolean result = sdcSchemaUtils.executeStatement(CassandraTestHelper::createCluster, SINGLE_STATEMENT);
+		Assert.assertTrue(result);
 	}
 
 	
 	@Test
-	public void testExecuteStatements() throws Exception {
-		String[] statements = new String[] { "" };
-		boolean result;
+	public void testExecuteStatementsSuccessfullScenario() throws Exception {
+		SdcSchemaUtils sdcSchemaUtils = new SdcSchemaUtils();
+		final boolean result = sdcSchemaUtils.executeStatements(CassandraTestHelper::createCluster, MULTIPLE_STATEMENTS);
+		Assert.assertTrue(result);
+	}
 
-		// default test
-		result = SdcSchemaUtils.executeStatements(statements);
+	@Test
+	public void testExecuteStatementsClusterFail() throws Exception {
+		SdcSchemaUtils sdcSchemaUtils = new SdcSchemaUtils();
+		final boolean result = sdcSchemaUtils.executeStatements(() -> null, MULTIPLE_STATEMENTS);
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void testExecuteStatementsSessionFail() throws Exception {
+		SdcSchemaUtils sdcSchemaUtils = new SdcSchemaUtils();
+		final boolean result = sdcSchemaUtils.executeStatements(CassandraTestHelper::createClusterWithNoSession, MULTIPLE_STATEMENTS);
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void testCreateClusterNoAuthNoSsl() {
+		Configuration.CassandrConfig cfg = new Configuration.CassandrConfig();
+		cfg.setCassandraHosts(CASSANDRA_HOSTS);
+
+		SdcSchemaUtils sdcSchemaUtils = Mockito.mock(SdcSchemaUtils.class);
+		when(sdcSchemaUtils.getCassandraConfig()).thenReturn(cfg);
+		when(sdcSchemaUtils.createCluster()).thenCallRealMethod();
+
+		try(Cluster cluster = sdcSchemaUtils.createCluster()) {
+			Assert.assertNotNull(cluster);
+		}
+	}
+
+	@Test
+	public void testCreateClusterFailOnLackOfCassandraNodes() {
+		Configuration.CassandrConfig cfg = new Configuration.CassandrConfig();
+		cfg.setCassandraHosts(null);
+
+		SdcSchemaUtils sdcSchemaUtils = Mockito.mock(SdcSchemaUtils.class);
+		when(sdcSchemaUtils.getCassandraConfig()).thenReturn(cfg);
+		when(sdcSchemaUtils.createCluster()).thenCallRealMethod();
+
+		try(Cluster cluster = sdcSchemaUtils.createCluster()) {
+			Assert.assertNull(cluster);
+		}
+	}
+
+	@Test
+	public void testCreateClusterFailOnAuthEnabledWithNoCredentials() {
+		Configuration.CassandrConfig cfg = new Configuration.CassandrConfig();
+		cfg.setAuthenticate(true);
+		cfg.setCassandraHosts(CASSANDRA_HOSTS);
+		cfg.setUsername(null);
+		cfg.setPassword(null);
+
+		SdcSchemaUtils sdcSchemaUtils = Mockito.mock(SdcSchemaUtils.class);
+		when(sdcSchemaUtils.getCassandraConfig()).thenReturn(cfg);
+		when(sdcSchemaUtils.createCluster()).thenCallRealMethod();
+
+		try(Cluster cluster = sdcSchemaUtils.createCluster()) {
+			Assert.assertNull(cluster);
+		}
+	}
+
+	@Test
+	public void testCreateClusterFailOnSSLWithNoCredentials() {
+		Configuration.CassandrConfig cfg = new Configuration.CassandrConfig();
+		cfg.setCassandraHosts(CASSANDRA_HOSTS);
+		cfg.setSsl(true);
+		cfg.setTruststorePath(null);
+		cfg.setTruststorePassword(null);
+
+		SdcSchemaUtils sdcSchemaUtils = Mockito.mock(SdcSchemaUtils.class);
+		when(sdcSchemaUtils.getCassandraConfig()).thenReturn(cfg);
+		when(sdcSchemaUtils.createCluster()).thenCallRealMethod();
+
+		try(Cluster cluster = sdcSchemaUtils.createCluster()) {
+			Assert.assertNull(cluster);
+		}
+	}
+
+	@Test
+	public void testCreateClusterWithAuthSsl() {
+		Configuration.CassandrConfig cfg = new Configuration.CassandrConfig();
+		cfg.setAuthenticate(true);
+		cfg.setCassandraHosts(CASSANDRA_HOSTS);
+		cfg.setUsername(CASSANDRA_USERNAME);
+		cfg.setPassword(CASSANDRA_PASSWORD);
+		cfg.setSsl(true);
+		cfg.setTruststorePath(TRUSTSTORE_PATH);
+		cfg.setTruststorePassword(TRUSTSTORE_PASSWORD);
+
+		SdcSchemaUtils sdcSchemaUtils = Mockito.mock(SdcSchemaUtils.class);
+		when(sdcSchemaUtils.getCassandraConfig()).thenReturn(cfg);
+		when(sdcSchemaUtils.createCluster()).thenCallRealMethod();
+
+		try(Cluster cluster = sdcSchemaUtils.createCluster()) {
+			Assert.assertNotNull(cluster);
+			Assert.assertEquals(System.getProperty("javax.net.ssl.trustStore"), TRUSTSTORE_PATH);
+			Assert.assertEquals(System.getProperty("javax.net.ssl.trustStorePassword"), TRUSTSTORE_PASSWORD);
+		}
 	}
 }
