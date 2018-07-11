6d4baa05bf44d6a5d9eba932fe84df0c0cdef805
siddharth0905 <siddharth.singh4@amdocs.com>
2018-07-11 16:34:03 +0530

    Response changed
    Error Response changed while collaboration changes
    
    Change-Id: I1e634c5da3b2bfb3cf385efe1a015bc3c2cd890f
    Issue-ID: SDC-1496
    Signed-off-by: siddharth0905 <siddharth.singh4@amdocs.com>

--- openecomp-be/api/openecomp-sdc-rest-webapp/openecomp-sdc-common-rest/src/main/java/org/openecomp/sdcrests/errors/DefaultExceptionMapper.java
+++ openecomp-be/api/openecomp-sdc-rest-webapp/openecomp-sdc-common-rest/src/main/java/org/openecomp/sdcrests/errors/DefaultExceptionMapper.java
@@ -16,6 +16,21 @@
 
 package org.openecomp.sdcrests.errors;
 
+import com.amdocs.zusammen.datatypes.response.Module;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
 import org.codehaus.jackson.map.JsonMappingException;
 import org.hibernate.validator.internal.engine.path.PathImpl;
 import org.openecomp.core.utilities.CommonMethods;
@@ -27,135 +42,151 @@ import org.openecomp.sdc.common.errors.ErrorCode;
 import org.openecomp.sdc.common.errors.ErrorCodeAndMessage;
 import org.openecomp.sdc.common.errors.GeneralErrorBuilder;
 import org.openecomp.sdc.common.errors.JsonMappingErrorBuilder;
+import org.openecomp.sdc.common.errors.Messages;
+import org.openecomp.sdc.common.errors.SdcRuntimeException;
 import org.openecomp.sdc.common.errors.ValidationErrorBuilder;
 import org.openecomp.sdc.logging.api.Logger;
 import org.openecomp.sdc.logging.api.LoggerFactory;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import javax.validation.Path;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 public class DefaultExceptionMapper implements ExceptionMapper<Exception> {
-  private static final String ERROR_CODES_TO_RESPONSE_STATUS_MAPPING_FILE =
-      "errorCodesToResponseStatusMapping.json";
-  @SuppressWarnings("unchecked")
-  private static final Map<String, String> ERROR_CODE_TO_RESPONSE_STATUS =
-          FileUtils.readViaInputStream(ERROR_CODES_TO_RESPONSE_STATUS_MAPPING_FILE,
-              stream -> JsonUtil.json2Object(stream, Map.class));
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(DefaultExceptionMapper
-      .class);
+    private static final String VLM_VSP_VERSION_ID_DOES_NOT_EXISTS =
+            Module.ZDB.name() + "-" + com.amdocs.zusammen.datatypes.response.ErrorCode.ZU_ITEM_VERSION_NOT_EXIST;
+    private static final String VLM_VSP_ITEM_ID_DOES_NOT_EXISTS =
+            Module.ZDB.name() + "-" + com.amdocs.zusammen.datatypes.response.ErrorCode.ZU_ITEM_DOES_NOT_EXIST;
+    private static final String SUB_ENTITY_ID_DOES_NOT_EXISTS =
+            Module.ZDB.name() + "-" + com.amdocs.zusammen.datatypes.response.ErrorCode.ZU_ELEMENT_GET_INFO;
+    private static final String FAILED_TO_SYNC =
+            Module.ZDB.name() + "-" + com.amdocs.zusammen.datatypes.response.ErrorCode.ZU_ITEM_VERSION_SYNC;
+    private static final String FAILED_TO_PUBLISH_OUT_OF_SYNC =
+            Module.ZDB.name() + "-" + com.amdocs.zusammen.datatypes.response.ErrorCode
+                                              .ZU_ITEM_VERSION_PUBLISH_NOT_ALLOWED;
+    private static final String ERROR_CODES_TO_RESPONSE_STATUS_MAPPING_FILE = "errorCodesToResponseStatusMapping.json";
+    @SuppressWarnings("unchecked")
+    private static final Map<String, String> ERROR_CODE_TO_RESPONSE_STATUS = FileUtils.readViaInputStream(
+            ERROR_CODES_TO_RESPONSE_STATUS_MAPPING_FILE, stream -> JsonUtil.json2Object(stream, Map.class));
 
-  @Override
-  public Response toResponse(Exception exception) {
-    Response response;
-    if (exception instanceof CoreException) {
-      response = transform(CoreException.class.cast(exception));
-    } else if (exception instanceof ConstraintViolationException) {
-      response = transform(ConstraintViolationException.class.cast(exception));
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultExceptionMapper.class);
 
-    } else if (exception instanceof JsonMappingException) {
-      response = transform(JsonMappingException.class.cast(exception));
+    @Override
+    public Response toResponse(Exception exception) {
+        Response response;
+        if (exception instanceof CoreException) {
+            response = transform(CoreException.class.cast(exception));
+        } else if (exception instanceof ConstraintViolationException) {
+            response = transform(ConstraintViolationException.class.cast(exception));
 
-    } else {
-      response = transform(exception);
+        } else if (exception instanceof JsonMappingException) {
+            response = transform(JsonMappingException.class.cast(exception));
+
+        } else if (exception instanceof SdcRuntimeException) {
+            response = transform(SdcRuntimeException.class.cast(exception));
+
+        } else {
+            response = transform(exception);
+        }
+
+        List<Object> contentTypes = new ArrayList<>();
+        contentTypes.add(MediaType.APPLICATION_JSON);
+        response.getMetadata().put("Content-Type", contentTypes);
+        return response;
     }
 
-    List<Object> contentTypes = new ArrayList<>();
-    contentTypes.add(MediaType.APPLICATION_JSON);
-    response.getMetadata().put("Content-Type", contentTypes);
-    return response;
-  }
+    private Response transform(CoreException coreException) {
+        Response response;
+        ErrorCode code = coreException.code();
+        LOGGER.error(code.message(), coreException);
 
-  private Response transform(CoreException coreException) {
-    Response response;
-    ErrorCode code = coreException.code();
-    LOGGER.error(code.message(), coreException);
+        if (coreException.code().category().equals(ErrorCategory.APPLICATION)) {
+            if (Response.Status.NOT_FOUND.name().equals(ERROR_CODE_TO_RESPONSE_STATUS.get(code.id()))) {
+                response = Response.status(Response.Status.NOT_FOUND).entity(toEntity(Response.Status.NOT_FOUND, code))
+                                   .build();
+            } else if (Response.Status.BAD_REQUEST.name().equals(ERROR_CODE_TO_RESPONSE_STATUS.get(code.id()))) {
+                response =
+                        Response.status(Response.Status.BAD_REQUEST).entity(toEntity(Response.Status.BAD_REQUEST, code))
+                                .build();
+            } else {
+                response = Response.status(Response.Status.EXPECTATION_FAILED)
+                                   .entity(toEntity(Response.Status.EXPECTATION_FAILED, code)).build();
+            }
+        } else {
+            response = Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                               .entity(toEntity(Response.Status.INTERNAL_SERVER_ERROR, code)).build();
+        }
 
-    if (coreException.code().category().equals(ErrorCategory.APPLICATION)) {
-      if (Response.Status.NOT_FOUND.name().equals(ERROR_CODE_TO_RESPONSE_STATUS.get(code.id()))) {
-        response = Response
-            .status(Response.Status.NOT_FOUND)
-            .entity(toEntity(Response.Status.NOT_FOUND, code))
-            .build();
-      } else if (Response.Status.BAD_REQUEST.name()
-          .equals(ERROR_CODE_TO_RESPONSE_STATUS.get(code.id()))) {
-        response = Response
-            .status(Response.Status.BAD_REQUEST)
-            .entity(toEntity(Response.Status.BAD_REQUEST, code))
-            .build();
-      } else {
-        response = Response
-            .status(Response.Status.EXPECTATION_FAILED)
-            .entity(toEntity(Response.Status.EXPECTATION_FAILED, code))
-            .build();
-      }
-    } else {
-      response = Response
-          .status(Response.Status.INTERNAL_SERVER_ERROR)
-          .entity(toEntity(Response.Status.INTERNAL_SERVER_ERROR, code))
-          .build();
+
+        return response;
     }
 
+    private Response transform(ConstraintViolationException validationException) {
+        Set<ConstraintViolation<?>> constraintViolationSet = validationException.getConstraintViolations();
+        String message;
 
-    return response;
-  }
+        String fieldName = null;
+        if (!CommonMethods.isEmpty(constraintViolationSet)) {
+            // getting the first violation message for the output response.
+            ConstraintViolation<?> constraintViolation = constraintViolationSet.iterator().next();
+            message = constraintViolation.getMessage();
+            fieldName = getFieldName(constraintViolation.getPropertyPath());
 
-  private Response transform(ConstraintViolationException validationException) {
-    Set<ConstraintViolation<?>> constraintViolationSet =
-        validationException.getConstraintViolations();
-    String message;
+        } else {
+            message = validationException.getMessage();
+        }
 
-    String fieldName = null;
-    if (!CommonMethods.isEmpty(constraintViolationSet)) {
-      // getting the first violation message for the output response.
-      ConstraintViolation<?> constraintViolation = constraintViolationSet.iterator().next();
-      message = constraintViolation.getMessage();
-      fieldName = getFieldName(constraintViolation.getPropertyPath());
+        ErrorCode validationErrorCode = new ValidationErrorBuilder(message, fieldName).build();
 
-    } else {
-      message = validationException.getMessage();
+        LOGGER.error(validationErrorCode.message(), validationException);
+        return Response.status(Response.Status.EXPECTATION_FAILED) //error 417
+                       .entity(toEntity(Response.Status.EXPECTATION_FAILED, validationErrorCode)).build();
     }
 
-    ErrorCode validationErrorCode = new ValidationErrorBuilder(message, fieldName).build();
+    private Response transform(JsonMappingException jsonMappingException) {
+        ErrorCode jsonMappingErrorCode = new JsonMappingErrorBuilder().build();
+        LOGGER.error(jsonMappingErrorCode.message(), jsonMappingException);
+        return Response.status(Response.Status.EXPECTATION_FAILED) //error 417
+                       .entity(toEntity(Response.Status.EXPECTATION_FAILED, jsonMappingErrorCode)).build();
+    }
 
-    LOGGER.error(validationErrorCode.message(), validationException);
-    return Response
-        .status(Response.Status.EXPECTATION_FAILED) //error 417
-        .entity(toEntity(Response.Status.EXPECTATION_FAILED, validationErrorCode))
-        .build();
-  }
+    private Response transform(Exception exception) {
+        ErrorCode errorCode = new GeneralErrorBuilder().build();
+        LOGGER.error(errorCode.message(), exception);
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                       .entity(toEntity(Response.Status.INTERNAL_SERVER_ERROR, errorCode)).build();
+    }
 
-  private Response transform(JsonMappingException jsonMappingException) {
-    ErrorCode jsonMappingErrorCode = new JsonMappingErrorBuilder().build();
-    LOGGER.error(jsonMappingErrorCode.message(), jsonMappingException);
-    return Response
-        .status(Response.Status.EXPECTATION_FAILED) //error 417
-        .entity(toEntity(Response.Status.EXPECTATION_FAILED, jsonMappingErrorCode))
-        .build();
-  }
+    private Response transform(SdcRuntimeException exception) {
+        if (Stream.of(VLM_VSP_ITEM_ID_DOES_NOT_EXISTS, VLM_VSP_VERSION_ID_DOES_NOT_EXISTS)
+                  .anyMatch(exception.getMessage()::contains)) {
+            return generateSdcErrorResponse(Messages.ENTITY_NOT_FOUND, Response.Status.NOT_FOUND,
+                    new SdcRuntimeException(Messages.ENTITY_NOT_FOUND.getErrorMessage(), exception));
+        } else if (exception.getMessage().contains(SUB_ENTITY_ID_DOES_NOT_EXISTS)) {
+            return generateSdcErrorResponse(Messages.SUB_ENTITY_NOT_FOUND, Response.Status.NOT_FOUND,
+                    new SdcRuntimeException(Messages.SUB_ENTITY_NOT_FOUND.getErrorMessage(), exception));
+        } else if (exception.getMessage().contains(FAILED_TO_SYNC)) {
+            return generateSdcErrorResponse(Messages.FAILED_TO_SYNC, Response.Status.EXPECTATION_FAILED,
+                    new SdcRuntimeException(Messages.FAILED_TO_SYNC.getErrorMessage(), exception));
+        } else if (exception.getMessage().contains(FAILED_TO_PUBLISH_OUT_OF_SYNC)) {
+            return generateSdcErrorResponse(Messages.FAILED_TO_PUBLISH_OUT_OF_SYNC, Response.Status.EXPECTATION_FAILED,
+                    new SdcRuntimeException(Messages.FAILED_TO_PUBLISH_OUT_OF_SYNC.getErrorMessage(), exception));
+        }
 
-  private Response transform(Exception exception) {
-    ErrorCode errorCode = new GeneralErrorBuilder().build();
-    LOGGER.error(errorCode.message(), exception);
-    return Response
-        .status(Response.Status.INTERNAL_SERVER_ERROR)
-        .entity(toEntity(Response.Status.INTERNAL_SERVER_ERROR, errorCode))
-        .build();
-  }
+        return transform(new Exception());
+    }
 
-  private String getFieldName(Path propertyPath) {
-    return ((PathImpl) propertyPath).getLeafNode().toString();
-  }
+    private Response generateSdcErrorResponse(Messages messages, Response.Status status, Exception exception) {
+        ErrorCode errorCode = new ErrorCode.ErrorCodeBuilder()
+                                      .withId(messages.name())
+                                      .withMessage(exception.getMessage()).build();
 
-  private Object toEntity(Response.Status status, ErrorCode code) {
-    return new ErrorCodeAndMessage(status, code);
-  }
+        LOGGER.error(errorCode.message(), exception);
+        return Response.status(status).entity(toEntity(status, errorCode)).build();
+    }
+
+    private String getFieldName(Path propertyPath) {
+        return ((PathImpl) propertyPath).getLeafNode().toString();
+    }
+
+    private Object toEntity(Response.Status status, ErrorCode code) {
+        return new ErrorCodeAndMessage(status, code);
+    }
 }
