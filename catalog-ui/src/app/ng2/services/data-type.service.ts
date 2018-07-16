a555c46e2065c2de1de87f26f5a829be5ef0d403
Idan Amit <ia096e@intl.att.com>
2018-07-16 19:22:02 +0300

    Fixed not needed derived from properties
    Removed derived from property that we're added before
    Fixed the properties assignments page to work with the new state
    
    Change-Id: Id8d8e9437a9927660cfd373b24efae63f4d13347
    Issue-ID: SDC-1514
    Signed-off-by: Idan Amit <ia096e@intl.att.com>

--- catalog-ui/src/app/ng2/services/data-type.service.ts
+++ catalog-ui/src/app/ng2/services/data-type.service.ts
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -57,7 +57,7 @@ export class DataTypeService {
             });
         }
         //recurse parent (derivedFrom), in case one of parents contains properties
-        if (PROPERTY_DATA.ROOT_DATA_TYPE !== dataTypeObj.derivedFrom.name) {
+        if (dataTypeObj.derivedFrom && PROPERTY_DATA.ROOT_DATA_TYPE == dataTypeObj.derivedFrom.name) {
             this.getDerivedDataTypeProperties(dataTypeObj.derivedFrom, propertiesArray, parentName);
         }
     }
@@ -65,7 +65,7 @@ export class DataTypeService {
     /**
      * Checks for custom behavior for a given data type by checking if a function exists within data-type.service with that name
      * Additional custom behavior can be added by adding a function with the given dataType name
-     */    
+     */
     public checkForCustomBehavior = (property:PropertyFEModel) => {
         let shortTypeName:string = property.type.split('.').pop();
         if (this[shortTypeName]) {
