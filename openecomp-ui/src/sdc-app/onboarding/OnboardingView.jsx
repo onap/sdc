625fac15f480c932a357a90ec7ee8059e7700940
svishnev <shlomo-stanisla.vishnevetskiy@amdocs.com>
2018-07-17 10:37:38 +0300

    ui loop fix
    Issue-ID: SDC-1525
    Change-Id: I09cf3751881c49361315b2c80c91e7402872c8a4
    Signed-off-by: svishnev <shlomo-stanisla.vishnevetskiy@amdocs.com>

--- openecomp-ui/src/sdc-app/onboarding/OnboardingView.jsx
+++ openecomp-ui/src/sdc-app/onboarding/OnboardingView.jsx
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -74,11 +74,12 @@ export default class OnboardingView extends React.Component {
     }
 
     render() {
-        let { currentScreen } = this.props;
+        let { currentScreen, isLoading } = this.props;
         let { screen, props } = currentScreen;
-
+        const preventClicks = isLoading ? 'no-pointer-events' : '';
         return (
-            <div className="dox-ui dox-ui-punch-out dox-ui-punch-out-full-page">
+            <div
+                className={`dox-ui dox-ui-punch-out dox-ui-punch-out-full-page ${preventClicks}`}>
                 {(() => {
                     switch (screen) {
                         case enums.SCREEN.ONBOARDING_CATALOG:
