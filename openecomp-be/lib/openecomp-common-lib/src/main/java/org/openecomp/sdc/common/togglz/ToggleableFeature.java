14411bd756955f4508552c790709c96b8c203d3e
Shnaider, Marina (ms7589) <ms7589@intl.att.com>
2018-08-12 10:55:05 +0300

    Toggle for SRIOV
    Adding Toggle for Fabric Configuration feature
    
    Change-Id: I4c8c23c67d215597b0a32212f24416de977ef2eb
    Issue-ID: SDC-1623
    Signed-off-by: Shnaider, Marina (ms7589) <ms7589@intl.att.com>

--- openecomp-be/lib/openecomp-common-lib/src/main/java/org/openecomp/sdc/common/togglz/ToggleableFeature.java
+++ openecomp-be/lib/openecomp-common-lib/src/main/java/org/openecomp/sdc/common/togglz/ToggleableFeature.java
@@ -1,11 +1,16 @@
 package org.openecomp.sdc.common.togglz;
 
 import org.togglz.core.Feature;
+import org.togglz.core.annotation.Label;
 import org.togglz.core.context.FeatureContext;
 
 public enum ToggleableFeature implements Feature {
 
-  ;
+  @Label(("MRN"))
+  MRN,
+  
+  @Label("Fabric Configuration")
+  FABRIC_CONFIGURATION;
 
   public boolean isActive() {
     return FeatureContext.getFeatureManager().isActive(this);
