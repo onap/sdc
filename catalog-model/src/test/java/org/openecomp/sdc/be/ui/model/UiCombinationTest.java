7561d89d4c68135c820f509b5bb0ed822392220d
Satyaki Mallick <satyaki.mallick@huawei.com>
2018-09-19 14:29:57 +0530

    Fix addition for Component Combination
    Add combination business logic, operation and servlet.
    Modify existing classes to support combination. Added test class for Business Logic
    Reworked for the comments raised by reviewers
    
    Issue-ID: SDC-1460
    Change-Id: Ieeece15073b959a8f578e4398e07024716cf7b0d
    Signed-off-by: Satyaki Mallick <satyaki.mallick@huawei.com>

---
+++ catalog-model/src/test/java/org/openecomp/sdc/be/ui/model/UiCombinationTest.java
@@ -0,0 +1,98 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2018 Huawei Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+
+package org.openecomp.sdc.be.ui.model;
+
+import org.junit.Test;
+import org.openecomp.sdc.be.model.operations.impl.UniqueIdBuilder;
+import org.openecomp.sdc.be.unittests.utils.ModelConfDependentTest;
+
+public class UiCombinationTest {
+    private UiCombination createTestSubject() {
+        return new UiCombination();
+    }
+
+    @Test
+    public void testCtor() throws Exception {
+        String uniqueId = UniqueIdBuilder.generateUUID();
+        new UiCombination(uniqueId, "testUiCombinationName", "testUiCombinationDescription");
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+        UiCombination testSubject;
+        String result;
+
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.getName();
+    }
+
+    @Test
+    public void testSetName() throws Exception {
+        UiCombination testSubject;
+        String result = "";
+
+        // default test
+        testSubject = createTestSubject();
+        testSubject.setName(result);
+    }
+
+    @Test
+    public void testGetDescription() throws Exception {
+        UiCombination testSubject;
+        String result;
+
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.getDescription();
+    }
+
+    @Test
+    public void testSetDescription() throws Exception {
+        UiCombination testSubject;
+        String result = "";
+
+        // default test
+        testSubject = createTestSubject();
+        testSubject.setDescription(result);
+    }
+
+    @Test
+    public void testGetUniqueId() throws Exception {
+        UiCombination testSubject;
+        String result;
+
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.getUniqueId();
+    }
+
+    @Test
+    public void testSetUniqueId() throws Exception {
+        UiCombination testSubject;
+        String result = "";
+
+        // default test
+        testSubject = createTestSubject();
+        testSubject.setUniqueId(result);
+    }
+}
