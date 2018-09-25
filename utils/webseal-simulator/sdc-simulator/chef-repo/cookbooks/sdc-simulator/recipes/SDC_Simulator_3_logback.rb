b4e32fec0abedc244bd20d2ee8c09fe9dc67eff1
Yuli Shlosberg <ys9693@att.com>
2018-09-25 17:16:48 +0300

    fix security violation
    Change-Id: I78adf42e6a328e782b4db394fa1a90c17bea9f17
    Issue-ID: SDC-1725
    Signed-off-by: Yuli Shlosberg <ys9693@att.com>

--- utils/webseal-simulator/sdc-simulator/chef-repo/cookbooks/sdc-simulator/recipes/SDC_Simulator_3_logback.rb
+++ utils/webseal-simulator/sdc-simulator/chef-repo/cookbooks/sdc-simulator/recipes/SDC_Simulator_3_logback.rb
@@ -1,9 +1,8 @@
 jetty_base="/var/lib/jetty"
 
-
-cookbook_file "logback.xml" do
-   path "#{jetty_base}/config/sdc-simulator/logback.xml"
-   source "logback.xml"
+cookbook_file "log4j.properties" do
+   path "#{jetty_base}/config/sdc-simulator/log4j.properties"
+   source "log4j.properties"
    owner "jetty"
    group "jetty"
    mode "0755"
