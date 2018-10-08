9dfe518345aa9e38a0d3367fdc82c843dd3f3415
siddharth0905 <siddharth.singh4@amdocs.com>
2018-10-08 17:06:27 +0530

    Test coverage : openecomp-nosqldb-api
    Increase test coverage
    
    Change-Id: If8b4b8e961278eb1448e8467cd7ac8e136c16315
    Issue-ID: SDC-1673
    Signed-off-by: siddharth0905 <siddharth.singh4@amdocs.com>

--- openecomp-be/lib/openecomp-core-lib/openecomp-nosqldb-lib/openecomp-nosqldb-api/src/main/java/org/openecomp/core/dao/BaseDao.java
+++ openecomp-be/lib/openecomp-core-lib/openecomp-nosqldb-lib/openecomp-nosqldb-api/src/main/java/org/openecomp/core/dao/BaseDao.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,13 +24,13 @@ import java.util.Collection;
 
 public interface BaseDao<T> {
 
-  Collection<T> list(T entity);
+    Collection<T> list(T entity);
 
-  void create(T entity);
+    void create(T entity);
 
-  void update(T entity);
+    void update(T entity);
 
-  T get(T entity);
+    T get(T entity);
 
-  void delete(T entity);
+    void delete(T entity);
 }
--- openecomp-be/lib/openecomp-core-lib/openecomp-nosqldb-lib/openecomp-nosqldb-api/src/main/java/org/openecomp/core/nosqldb/api/NoSqlDb.java
+++ openecomp-be/lib/openecomp-core-lib/openecomp-nosqldb-lib/openecomp-nosqldb-api/src/main/java/org/openecomp/core/nosqldb/api/NoSqlDb.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,15 +25,15 @@ import com.datastax.driver.mapping.MappingManager;
 
 public interface NoSqlDb {
 
-  //TODO: remove cassandra types from here!! (like done in SDC...).
+    //TODO: remove cassandra types from here!! (like done in SDC...).
 
-  void insert(String tableName, String[] colNames, Object[] values);
+    void insert(String tableName, String[] colNames, Object[] values);
 
-  ResultSet execute(String statement);
+    ResultSet execute(String statement);
 
-  ResultSet execute(String statementName, Object... values);
+    ResultSet execute(String statementName, Object... values);
 
-  MappingManager getMappingManager();
+    MappingManager getMappingManager();
 
-  String getVersion();
+    String getVersion();
 }
