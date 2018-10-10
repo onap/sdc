5e7c8524f0098b2a183e76b02caa58cf636cd798
siddharth0905 <siddharth.singh4@amdocs.com>
2018-10-10 14:34:47 +0530

    Test coverage : openecomp-sdc-notification-api
    Increase test coverage
    
    Change-Id: I13ddec81fd06542d3d11e4236d76416309ae32d9
    Issue-ID: SDC-1673
    Signed-off-by: siddharth0905 <siddharth.singh4@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-notification-lib/openecomp-sdc-notification-api/src/main/java/org/openecomp/sdc/notification/dao/types/LastSeenNotificationEntity.java
+++ openecomp-be/lib/openecomp-sdc-notification-lib/openecomp-sdc-notification-api/src/main/java/org/openecomp/sdc/notification/dao/types/LastSeenNotificationEntity.java
@@ -20,74 +20,26 @@ import com.datastax.driver.mapping.annotations.Column;
 import com.datastax.driver.mapping.annotations.PartitionKey;
 import com.datastax.driver.mapping.annotations.Table;
 
-import java.util.Objects;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "lastEventId")
 @Table(keyspace = "dox", name = "last_notification")
 public class LastSeenNotificationEntity {
-  public static final String ENTITY_TYPE = "Event Notification";
 
-  @PartitionKey
-  @Column(name = "owner_id")
-  private String ownerId;
+    public static final String ENTITY_TYPE = "Event Notification";
 
-  @Column(name = "event_id")
-  private UUID lastEventId;
+    @PartitionKey
+    @Column(name = "owner_id")
+    private String ownerId;
 
-  /**
-   * Every entity class must have a default constructor according to
-   * <a href="http://docs.datastax.com/en/developer/java-driver/2.1/manual/object_mapper/creating/">
-   * Definition of mapped classes</a>.
-   */
-  public LastSeenNotificationEntity() {
-    // Don't delete! Default constructor is required by DataStax driver
-  }
-
-  /**
-   * Instantiates a new Notification entity.
-   *
-   * @param ownerId      the owner id
-   * @param lastEventId  the last event id
-   */
-  public LastSeenNotificationEntity(String ownerId, UUID lastEventId) {
-    this.ownerId = ownerId;
-    this.lastEventId = lastEventId;
-  }
-
-  public String getOwnerId() {
-    return ownerId;
-  }
-
-  public void setOwnerId(String ownerId) {
-    this.ownerId = ownerId;
-  }
-
-  public UUID getLastEventId() {
-    return lastEventId;
-  }
-
-  public void setLastEventId(UUID lastEventId) {
-    this.lastEventId = lastEventId;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    LastSeenNotificationEntity that = (LastSeenNotificationEntity) o;
-    return Objects.equals(ownerId, that.ownerId) &&
-            Objects.equals(lastEventId, that.lastEventId);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(ownerId, lastEventId);
-  }
-
-  @Override
-  public String toString() {
-    return "LastSeenNotificationEntity {"
-        + "ownerId='" + ownerId + '\''
-       + '}';
-  }
+    @Column(name = "event_id")
+    private UUID lastEventId;
 }
