65c7d5b11362ae0d420efbbdba9d0e19b6255e62
Satyaki Mallick <satyaki.mallick@huawei.com>
2018-10-15 18:54:22 +0530

    Add Unit Test for LicenseKeyGroupEntity.java
    Add Unit Test for LicenseKeyGroupEntity.java
    
    Change-Id: I23a1de1950eb74d2a776a4bb32f833efaf66eade
    Issue-ID: SDC-1774
    Signed-off-by: Satyaki Mallick <satyaki.mallick@huawei.com>

---
+++ openecomp-be/lib/openecomp-sdc-enrichment-lib/openecomp-sdc-enrichment-core/src/test/java/org/openecomp/sdc/vendorlicense/dao/types/LicenseKeyGroupEntityTest.java
@@ -0,0 +1,89 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2018 Huawei Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdc.vendorlicense.dao.types;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LicenseKeyGroupEntityTest {
+
+    LicenseKeyGroupEntity licenseKeyGroupEntity = new LicenseKeyGroupEntity();
+
+    @Before
+    public void setup() {
+        licenseKeyGroupEntity.setDescription("new Desc");
+        licenseKeyGroupEntity.setId("1234");
+        licenseKeyGroupEntity.setName("License");
+        licenseKeyGroupEntity.setStartDate("18101993");
+        licenseKeyGroupEntity.setExpiryDate("19101993");
+        licenseKeyGroupEntity.setIncrements("1");
+        licenseKeyGroupEntity.setThresholdValue(10);
+        licenseKeyGroupEntity.setVersionUuId("7733-9998");
+    }
+
+    @Test
+    public void testDesc() {
+        assertEquals(licenseKeyGroupEntity.getDescription(), "new Desc");
+    }
+
+    @Test
+    public void testId() {
+        assertEquals(licenseKeyGroupEntity.getId(), "1234");
+    }
+
+    @Test
+    public void testName() {
+        assertEquals(licenseKeyGroupEntity.getName(), "License");
+    }
+
+    @Test
+    public void testStartDate() {
+        assertEquals(licenseKeyGroupEntity.getStartDate(), "18101993");
+    }
+
+    @Test
+    public void testExpiryDate() {
+        assertEquals(licenseKeyGroupEntity.getExpiryDate(), "19101993");
+    }
+
+    @Test
+    public void testIncrements() {
+        assertEquals(licenseKeyGroupEntity.getIncrements(), "1");
+    }
+
+    @Test
+    public void testThresholdValue() {
+        assertEquals(licenseKeyGroupEntity.getThresholdValue(), new Integer(10));
+    }
+
+    @Test
+    public void testVersionUuid() {
+        assertEquals(licenseKeyGroupEntity.getVersionUuId(), "7733-9998");
+    }
+    @Test
+    public void testSPLimits() {
+        Collection<LimitEntity> limits = null;
+        licenseKeyGroupEntity.getSPLimits();
+    }
+}
