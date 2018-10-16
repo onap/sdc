a39ee5ceaf6c3ccbc91eddec7c642abec541adbe
Satyaki Mallick <satyaki.mallick@huawei.com>
2018-10-16 12:42:38 +0530

    Add Unit Test for Action.java
    Add Unit Test for Action.java
    
    Issue-ID: SDC-1774
    
    Change-Id: I77993b5e33b5dd32a3e1ebda89edc97c7f7849c9
    Signed-off-by: Satyaki Mallick <satyaki.mallick@huawei.com>

---
+++ openecomp-be/backend/openecomp-sdc-action-manager/src/test/java/org/openecomp/sdc/action/types/ActionTest.java
@@ -0,0 +1,39 @@
+package org.openecomp.sdc.action.types;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ActionTest {
+
+    Action action = new Action();
+
+    @Before
+    public void setup() {
+        action.setActionUuId("123");
+        action.setActionInvariantUuId("12345");
+        action.setData("long data");
+        action.setName("Action Name");
+    }
+
+    @Test
+    public void testActionUuid() {
+        assertEquals(action.getActionUuId(), "123");
+    }
+
+    @Test
+    public void testActionInvariantUuid() {
+        assertEquals(action.getActionInvariantUuId(), "12345");
+    }
+
+    @Test
+    public void testData() {
+        assertEquals(action.getData(), "long data");
+    }
+
+    @Test
+    public void testName() {
+        assertEquals(action.getName(), "Action Name");
+    }
+}
