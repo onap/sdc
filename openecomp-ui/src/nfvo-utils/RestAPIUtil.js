7d1a5c25c8e848a9c2449de8633b8d9d1d0f3e96
Malek <malek.zoabi@amdocs.com>
2018-10-16 16:44:41 +0300

    Add worker to onboard notification
    Issue-ID: SDC-1840
    Change-Id: I0506df2073beb14de2aea5a01233f05ce9a6d7f7
    Signed-off-by: Malek <malek.zoabi@amdocs.com>

--- openecomp-ui/src/nfvo-utils/RestAPIUtil.js
+++ openecomp-ui/src/nfvo-utils/RestAPIUtil.js
@@ -1,18 +1,19 @@
 /*
- * Copyright © 2016-2018 European Support Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright © 2018 European Support Limited
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http: //www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 import uuid from 'uuid-js';
 import md5 from 'md5';
 import axios from 'axios';
@@ -39,7 +40,7 @@ const STORAGE_AUTH_KEY = 'sdc-auth-token';
 const REQUEST_ID_HEADER = 'X-ECOMP-RequestID';
 const CONTENT_MD5_HEADER = 'Content-MD5';
 
-function applySecurity(options, data) {
+export function applySecurity(options, data) {
     let headers = options.headers || (options.headers = {});
     if (options.isAnonymous) {
         return;
