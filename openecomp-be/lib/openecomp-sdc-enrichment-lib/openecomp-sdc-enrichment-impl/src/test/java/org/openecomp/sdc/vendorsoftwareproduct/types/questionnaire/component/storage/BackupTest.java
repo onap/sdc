5fde9a21e8bd3e9418b7ddf894e411935a5cfcac
Satyaki Mallick <satyaki.mallick@huawei.com>
2018-10-18 09:49:46 +0530

    UT for Backup.java using Bean Matcher
    UT for Backup.java using Bean Matcher. Also modified pom to with bean matcher dependancy
    
    Issue-ID: SDC-1774
    
    Change-Id: I8a25c2cff3701a26e286b7da02ded5f5f8d91668
    Signed-off-by: Satyaki Mallick <satyaki.mallick@huawei.com>

---
+++ openecomp-be/lib/openecomp-sdc-enrichment-lib/openecomp-sdc-enrichment-impl/src/test/java/org/openecomp/sdc/vendorsoftwareproduct/types/questionnaire/component/storage/BackupTest.java
@@ -0,0 +1,20 @@
+package org.openecomp.sdc.vendorsoftwareproduct.types.questionnaire.component.storage;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class BackupTest {
+
+    @Test
+    public void testConstructor() {
+        assertThat(Backup.class, hasValidBeanConstructor());
+    }
+
+    @Test
+    public void shouldHaveValidGettersAndSetters() {
+        assertThat(Backup.class, hasValidGettersAndSetters());
+    }
+}
