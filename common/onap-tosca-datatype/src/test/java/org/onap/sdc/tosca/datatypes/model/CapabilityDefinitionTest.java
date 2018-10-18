21c00e5abf6675ea5054c75541e6f69ca2127a99
Satyaki Mallick <satyaki.mallick@huawei.com>
2018-10-18 15:28:36 +0530

    Add UT for CapabilityDefinition
    Add UT for CapabilityDefinition
    
    Issue-ID: SDC-1774
    
    Change-Id: I141489dab051eb1e7ae6e33353ca378dfad7325f
    Signed-off-by: Satyaki Mallick <satyaki.mallick@huawei.com>

---
+++ common/onap-tosca-datatype/src/test/java/org/onap/sdc/tosca/datatypes/model/CapabilityDefinitionTest.java
@@ -0,0 +1,33 @@
+package org.onap.sdc.tosca.datatypes.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+
+public class CapabilityDefinitionTest {
+
+
+    CapabilityDefinition capabilityDefinition;
+
+    @Before
+    public void initialize() {
+        capabilityDefinition = new CapabilityDefinition();
+    }
+
+    @Test
+    public void testClone() {
+        CapabilityDefinition cap2 = capabilityDefinition.clone();
+        assertIfObjectsAreSame(cap2, capabilityDefinition);
+        assertEquals(cap2, capabilityDefinition);
+    }
+
+    private void assertIfObjectsAreSame(CapabilityDefinition expected, CapabilityDefinition actual) {
+        assertEquals(expected.getAttributes(), actual.getAttributes());
+        assertEquals(expected.getDescription(), actual.getDescription());
+        assertEquals(expected.getOccurrences(), actual.getOccurrences());
+        assertEquals(expected.getProperties(), actual.getProperties());
+        assertEquals(expected.getType(), actual.getType());
+    }
+}
