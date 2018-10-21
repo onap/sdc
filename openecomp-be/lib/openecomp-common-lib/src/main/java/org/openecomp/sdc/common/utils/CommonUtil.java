b6ad1cbd56e4f810f2b7fef914ffc045d56fbfca
Satyaki Mallick <satyaki.mallick@huawei.com>
2018-10-21 19:19:42 +0530

    Add UT for CommonUtil
    Add UT for CommonUtil in openecomp-be.
    
    Issue-ID: SDC-1774
    
    Change-Id: Id197e79ede009f3ca34043f907a48b26af17be53
    Signed-off-by: Satyaki Mallick <satyaki.mallick@huawei.com>

--- openecomp-be/lib/openecomp-common-lib/src/main/java/org/openecomp/sdc/common/utils/CommonUtil.java
+++ openecomp-be/lib/openecomp-common-lib/src/main/java/org/openecomp/sdc/common/utils/CommonUtil.java
@@ -1,9 +1,6 @@
-/*-
- * ============LICENSE_START=======================================================
- * SDC
- * ================================================================================
+/*
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.sdc.common.utils;
@@ -53,8 +49,8 @@ import java.util.zip.ZipException;
 import java.util.zip.ZipInputStream;
 
 public class CommonUtil {
-  private static final String DEFAULT = "default";
-  private static final String _DEFAULT = "_default";
+  static final String DEFAULT = "default";
+  static final String _DEFAULT = "_default";
 
   private CommonUtil() {
     // prevent instantiation
