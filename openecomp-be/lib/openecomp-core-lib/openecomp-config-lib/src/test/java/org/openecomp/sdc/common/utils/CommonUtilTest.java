de346ef1ac69be729181d73bf1964bf8ca4f1741
Satyaki Mallick <satyaki.mallick@huawei.com>
2018-10-21 19:19:42 +0530

    Add UT for CommonUtil
    Add UT for CommonUtil in openecomp-be.
    
    Issue-ID: SDC-1774
    
    Change-Id: Id197e79ede009f3ca34043f907a48b26af17be53
    Signed-off-by: Satyaki Mallick <satyaki.mallick@huawei.com>

---
+++ openecomp-be/lib/openecomp-core-lib/openecomp-config-lib/src/test/java/org/openecomp/sdc/common/utils/CommonUtilTest.java
@@ -0,0 +1,22 @@
+package org.openecomp.sdc.common.utils;
+
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.testng.Assert.assertTrue;
+
+public class CommonUtilTest {
+
+    @Test
+    public void testGetObjectAsMap() {
+        Map<String, String> obj = new HashMap<>(1);
+        obj.put(CommonUtil.DEFAULT, "");
+        Map<String, Object> newMap = CommonUtil.getObjectAsMap(obj);
+
+        boolean exists = newMap.containsKey(CommonUtil._DEFAULT);
+
+        assertTrue(exists);
+    }
+}
