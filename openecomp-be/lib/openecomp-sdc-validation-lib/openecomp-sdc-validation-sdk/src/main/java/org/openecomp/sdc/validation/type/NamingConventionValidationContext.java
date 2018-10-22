c75d4cbd19982216ee3a9ac0269f194f81c3db5f
talio <tali.orenbach@amdocs.com>
2018-10-22 16:51:53 +0300

    test coverage
    add unit tests for ResourceBaseValidator
    
    Change-Id: Ib81a2f51d935156597eb055028d7e6da6e8d5834
    Issue-ID: SDC-1864
    Signed-off-by: talio <tali.orenbach@amdocs.com>

--- openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-sdk/src/main/java/org/openecomp/sdc/validation/type/NamingConventionValidationContext.java
+++ openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-sdk/src/main/java/org/openecomp/sdc/validation/type/NamingConventionValidationContext.java
@@ -1,11 +1,31 @@
+/*
+ * Copyright © 2018 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openecomp.sdc.validation.type;
 
 import org.openecomp.sdc.heat.datatypes.model.HeatOrchestrationTemplate;
 import org.openecomp.sdc.validation.ValidationContext;
 
+import lombok.Getter;
+import lombok.EqualsAndHashCode;
+
 /**
  * Created by TALIO on 2/23/2017.
  */
+@Getter
+@EqualsAndHashCode
 public class NamingConventionValidationContext implements ValidationContext {
 
   private HeatOrchestrationTemplate heatOrchestrationTemplate;
@@ -16,13 +36,5 @@ public class NamingConventionValidationContext implements ValidationContext {
       String envFileName) {
     this.heatOrchestrationTemplate = heatOrchestrationTemplate;
     this.envFileName = envFileName;
-  }
-
-  public HeatOrchestrationTemplate getHeatOrchestrationTemplate() {
-    return heatOrchestrationTemplate;
-  }
-
-  public String getEnvFileName() {
-    return envFileName;
   }
 }
--- openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-sdk/src/test/java/org/openecomp/sdc/validation/base/ResourceBaseValidatorTest.java
+++ openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-sdk/src/test/java/org/openecomp/sdc/validation/base/ResourceBaseValidatorTest.java
@@ -1,19 +1,40 @@
+/*
+ * Copyright © 2018 European Support Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openecomp.sdc.validation.base;
 
+import org.apache.commons.collections4.MapUtils;
 import org.openecomp.core.validation.types.MessageContainer;
+import org.openecomp.sdc.datatypes.configuration.ImplementationConfiguration;
+import org.openecomp.sdc.validation.type.ConfigConstants;
 import org.openecomp.sdc.validation.util.ValidationTestUtil;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @Test
 public class ResourceBaseValidatorTest {
-
-ResourceBaseValidator resourceBaseValidator=new ResourceBaseValidator();
+private String testValidator = "testValidator";
 
   @Test
   public void testInvalidResourceType(){
+    ResourceBaseValidator resourceBaseValidator = new ResourceBaseValidator();
     Map<String, MessageContainer> messages = ValidationTestUtil.testValidator(
         resourceBaseValidator, "/InvalidResourceType");
     Assert.assertEquals(messages.get("first.yaml").getErrorMessageList().get(0).getMessage(),
@@ -23,6 +44,7 @@ ResourceBaseValidator resourceBaseValidator=new ResourceBaseValidator();
 
   @Test
   public void testInvalidHeatStructure(){
+    ResourceBaseValidator resourceBaseValidator = new ResourceBaseValidator();
     Map<String, MessageContainer> messages = ValidationTestUtil.testValidator(resourceBaseValidator,
         "/InvalidHeatStructure");
     Assert.assertEquals(messages.get("first.yaml").getErrorMessageList().get(0).getMessage(),
@@ -33,5 +55,61 @@ ResourceBaseValidator resourceBaseValidator=new ResourceBaseValidator();
         "    \t\t\tresources:\n" +
         "    ^\n" +
         "]");
+  }
+
+  @Test
+  public void testInitWithEmptyPropertiesMap() {
+    ResourceBaseValidator resourceBaseValidator = new ResourceBaseValidator();
+    Map<String, Object> properties = new HashMap<>();
+    resourceBaseValidator.init(properties);
+
+    Assert.assertTrue(MapUtils.isEmpty(resourceBaseValidator.getResourceTypeToImpl()));
+  }
+
+  @Test
+  public void testInitPropertiesMap() {
+    ResourceBaseValidator resourceBaseValidator = new ResourceBaseValidator();
+    initProperties(resourceBaseValidator, getValidImplementationConfiguration());
+
+    Map<String, ImplementationConfiguration> resourceTypeToImpl =
+        resourceBaseValidator.getResourceTypeToImpl();
+    Assert.assertTrue(MapUtils.isNotEmpty(resourceTypeToImpl));
+    Assert.assertTrue(resourceTypeToImpl.containsKey(testValidator));
+  }
+
+  @Test
+  public void testInitPropertiesWithString() {
+    ResourceBaseValidator resourceBaseValidator = new ResourceBaseValidator();
+    Map<String, Object> properties = new HashMap<>();
+    properties.put(testValidator, "invalidValue");
+
+    resourceBaseValidator.init(properties);
+
+    Assert.assertTrue(MapUtils.isEmpty(resourceBaseValidator.getResourceTypeToImpl()));
+  }
+
+  @Test
+  public void testInitPropertiesWithoutImplClass() {
+    ResourceBaseValidator resourceBaseValidator = new ResourceBaseValidator();
+    initProperties(resourceBaseValidator, new HashMap<>());
+
+    Assert.assertTrue(MapUtils.isEmpty(resourceBaseValidator.getResourceTypeToImpl()));
+  }
+
+  public Map<String, Object> getValidImplementationConfiguration() {
+    Map<String, Object> implConfiguration = new HashMap<>();
+    implConfiguration.put(
+        ConfigConstants.Impl_Class, "org.openecomp.sdc.validation.impl.validators.ForbiddenResourceGuideLineValidator");
+    implConfiguration.put(ConfigConstants.Enable, true);
+
+    return implConfiguration;
+  }
+
+  private void initProperties(ResourceBaseValidator resourceBaseValidator,
+                              Map<String, Object> implementationConfiguration) {
+    Map<String, Object> properties = new HashMap<>();
+    properties.put(testValidator, implementationConfiguration);
+
+    resourceBaseValidator.init(properties);
   }
 }
