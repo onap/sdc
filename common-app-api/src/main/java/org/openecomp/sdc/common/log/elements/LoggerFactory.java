6f730ac3142e467379564d9e4b97a0389e3bef28
Parshad Patel <pars.patel@samsung.com>
2018-11-12 21:02:13 +0900

    Fix sonar critical issue in common-app-api
    Fix use "isAssignableFrom" instead issue
    
    Issue-ID: SDC-1895
    Change-Id: Ifb51c4bca8df5dd7cdb95bbf5baa1b39004dd1ea
    Signed-off-by: Parshad Patel <pars.patel@samsung.com>

--- common-app-api/src/main/java/org/openecomp/sdc/common/log/elements/LoggerFactory.java
+++ common-app-api/src/main/java/org/openecomp/sdc/common/log/elements/LoggerFactory.java
@@ -15,19 +15,19 @@ public class LoggerFactory {
     @SuppressWarnings("unchecked")
     public static <T, V> V getLogger(Class<T> type, Logger logger) {
 
-        if (type.getName().equals(LoggerAudit.class.getName())) {
+        if (type.isAssignableFrom(LoggerAudit.class)) {
             return (V) new LoggerAudit(new LogFieldsMdcHandler(), logger);
         }
 
-        if (type.getName().equals(LoggerDebug.class.getName())) {
+        if (type.isAssignableFrom(LoggerDebug.class)) {
             return (V) new LoggerDebug(new LogFieldsMdcHandler(), logger);
         }
 
-        if (type.getName().equals(LoggerMetric.class.getName())) {
+        if (type.isAssignableFrom(LoggerMetric.class)) {
             return (V) new LoggerMetric(new LogFieldsMdcHandler(), logger);
         }
 
-        if (type.getName().equals(LoggerError.class.getName())) {
+        if (type.isAssignableFrom(LoggerError.class)) {
             return (V) new LoggerError(new LogFieldsMdcHandler(), logger);
         }
 
@@ -37,19 +37,19 @@ public class LoggerFactory {
     @SuppressWarnings("unchecked")
     public static <T, V> V getMdcLogger(Class<T> type, Logger logger) {
 
-        if (type.getName().equals(LoggerAudit.class.getName())) {
+        if (type.isAssignableFrom(LoggerAudit.class)) {
             return (V) new LoggerAudit(LogFieldsMdcHandler.getInstance(), logger);
         }
 
-        if (type.getName().equals(LoggerDebug.class.getName())) {
+        if (type.isAssignableFrom(LoggerDebug.class)) {
             return (V) new LoggerDebug(LogFieldsMdcHandler.getInstance(), logger);
         }
 
-        if (type.getName().equals(LoggerMetric.class.getName())) {
+        if (type.isAssignableFrom(LoggerMetric.class)) {
             return (V) new LoggerMetric(LogFieldsMdcHandler.getInstance(), logger);
         }
 
-        if (type.getName().equals(LoggerError.class.getName())) {
+        if (type.isAssignableFrom(LoggerError.class)) {
             return (V) new LoggerError(LogFieldsMdcHandler.getInstance(), logger);
         }
 
