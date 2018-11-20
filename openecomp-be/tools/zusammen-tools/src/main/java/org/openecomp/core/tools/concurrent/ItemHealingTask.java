f011df07dfdc0b8cd7704b9dbe912fbf195bab02
Sonsino, Ofir (os0695) <os0695@intl.att.com>
2018-11-20 15:32:06 +0200

    Fix Sonar issues
    Change-Id: I487f54714f87e993b9fa4a3b43de7a65692922d5
    Issue-ID: SDC-1925
    Signed-off-by: Sonsino, Ofir (os0695) <os0695@intl.att.com>

--- openecomp-be/tools/zusammen-tools/src/main/java/org/openecomp/core/tools/concurrent/ItemHealingTask.java
+++ openecomp-be/tools/zusammen-tools/src/main/java/org/openecomp/core/tools/concurrent/ItemHealingTask.java
@@ -11,45 +11,43 @@ import java.util.concurrent.Callable;
  */
 public class ItemHealingTask implements Callable<String> {
 
-  private String itemId;
-  private Version version;
-  private VendorSoftwareProductManager vspManager;
-  private HealingManager healingManager;
+    private String itemId;
+    private Version version;
+    private VendorSoftwareProductManager vspManager;
+    private HealingManager healingManager;
 
-  public String getItemId() {
-    return itemId;
-  }
+    public ItemHealingTask(String itemId, String versionId, VendorSoftwareProductManager vspManager,
+            HealingManager healingManager) {
+        // this.itemId = itemId;
+        // this.version = new Version(versionId);
+        this.vspManager = vspManager;
+        this.healingManager = healingManager;
+    }
 
-  public String getVersionId() {
-//    return version.getId();
-    return null;
-  }
+    public String getItemId() {
+        return itemId;
+    }
 
+    public String getVersionId() {
+        // return version.getId();
+        return null;
+    }
 
-  public ItemHealingTask(String itemId, String versionId,
-                         VendorSoftwareProductManager vspManager,
-                         HealingManager healingManager) {
-//    this.itemId = itemId;
-//    this.version = new Version(versionId);
-    this.vspManager = vspManager;
-    this.healingManager = healingManager;
-
-  }
-
-  @Override
-  public String call() throws Exception {
-//    VspDetails vspDetails = vspManager.getVsp(itemId, version);
-//
-//    try {
-//      healingManager.healItemVersion(itemId, version, ItemType.vsp, true);
-//    } catch (Exception e) {
-//      return (String
-//          .format("healing failed on vsp: %s with id: %s, versionId: %s, with message: %s",
-//              vspDetails.getName(), itemId, version.getId(), e.getMessage()));
-//    }
-//
-//    return String.format("healed vsp: %s, with id: %s, versionId: %s",
-//        vspDetails.getName(), itemId, version.getId());
-    return null;
-  }
+    @Override
+    public String call() throws Exception {
+        // VspDetails vspDetails = vspManager.getVsp(itemId, version);
+        //
+        // try {
+        // healingManager.healItemVersion(itemId, version, ItemType.vsp, true);
+        // } catch (Exception e) {
+        // return (String
+        // .format("healing failed on vsp: %s with id: %s, versionId: %s, with
+        // message: %s",
+        // vspDetails.getName(), itemId, version.getId(), e.getMessage()));
+        // }
+        //
+        // return String.format("healed vsp: %s, with id: %s, versionId: %s",
+        // vspDetails.getName(), itemId, version.getId());
+        return null;
+    }
 }
