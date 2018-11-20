a7366ac136678028706bb8020d97a3e8545888c6
Sonsino, Ofir (os0695) <os0695@intl.att.com>
2018-11-20 16:59:17 +0200

    Sonar code smells
    Change-Id: Ia43e9d0ff3de1b02d259c5bb05678b4cc6eb8b9b
    Issue-ID: SDC-1941
    Signed-off-by: Sonsino, Ofir (os0695) <os0695@intl.att.com>

--- openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-api/src/main/java/org/openecomp/core/validation/ErrorMessageCode.java
+++ openecomp-be/lib/openecomp-sdc-validation-lib/openecomp-sdc-validation-api/src/main/java/org/openecomp/core/validation/ErrorMessageCode.java
@@ -21,8 +21,12 @@ public final class ErrorMessageCode {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         ErrorMessageCode that = (ErrorMessageCode) o;
 
