d6dfd6be381b2690e39bc4712931175d06ceaf27
shrikantawachar <shrikant.awachar@amdocs.com>
2019-01-16 12:37:16 +0530

    Allow custom plugins in SDC
    Allow custom plugins in SDC
    
    Change-Id: I09475a4f795734f7911e24652560f7d41ddb8d14
    Issue-ID: SDC-2022
    Signed-off-by: shrikantawachar <shrikant.awachar@amdocs.com>

--- catalog-fe/sdc-frontend/chef-repo/cookbooks/sdc-catalog-fe/recipes/FE_2_setup_configuration.rb
+++ catalog-fe/sdc-frontend/chef-repo/cookbooks/sdc-catalog-fe/recipes/FE_2_setup_configuration.rb
@@ -14,23 +14,6 @@ template "catalog-fe-config" do
    })
 end
 
-template "plugins-fe-config" do
-   path "#{ENV['JETTY_BASE']}/config/catalog-fe/plugins-configuration.yaml"
-   source "FE-plugins-configuration.yaml.erb"
-   owner "jetty"
-   group "jetty"
-   mode "0755"
-   variables({
-      :dcae_discovery_url     => node['Plugins']['DCAE']['dcae_discovery_url'],
-      :dcae_source_url        => node['Plugins']['DCAE']['dcae_source_url'],
-      :dcae_dt_discovery_url  => node['Plugins']['DCAE-TAB']['dcae_dt_discovery_url'],
-      :dcae_dt_source_url     => node['Plugins']['DCAE-TAB']['dcae_dt_source_url'],
-      :workflow_discovery_url => node['Plugins']['WORKFLOW']['workflow_discovery_url'],
-      :workflow_source_url    => node['Plugins']['WORKFLOW']['workflow_source_url']
-   })
-end
-
-
 template "onboarding-fe-config" do
     path "#{ENV['JETTY_BASE']}/config/onboarding-fe/onboarding_configuration.yaml"
     source "FE-onboarding-configuration.yaml.erb"
