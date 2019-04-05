51a521f3d731712a5df2ec2ee9f38c8835ff9607
Tomasz Golabek <tomasz.golabek@nokia.com>
2019-04-05 14:46:13 +0200

    Some unit tests for catalog-be
    Code coverage for some classes from catalog-be increased.
    Some refactor made if needed.
    
    Change-Id: I1b292cfbe4b0d2aa4fbe22871da3228aa434b823
    Issue-ID: SDC-2220
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>

---
+++ catalog-be/src/test/java/org/openecomp/sdc/be/auditing/impl/AuditingManagerTest.java
@@ -0,0 +1,75 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2019 Nokia Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.openecomp.sdc.be.auditing.impl;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.openecomp.sdc.be.auditing.api.AuditEventFactory;
+import org.openecomp.sdc.be.dao.api.ActionStatus;
+import org.openecomp.sdc.be.dao.cassandra.AuditCassandraDao;
+import org.openecomp.sdc.be.dao.cassandra.CassandraOperationStatus;
+import org.openecomp.sdc.be.dao.impl.AuditingDao;
+import org.openecomp.sdc.be.resources.data.auditing.AuditingGenericEvent;
+import org.openecomp.sdc.test.utils.TestConfigurationProvider;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuditingManagerTest {
+
+    private static final String MSG = "msg";
+    private String msg = "Any message";
+    private AuditingManager auditingManager;
+
+    @Mock
+    private AuditingGenericEvent auditEvent;
+    @Mock
+    private AuditingDao auditingDao;
+    @Mock
+    private AuditCassandraDao cassandraDao;
+    @Mock
+    private AuditEventFactory eventFactory;
+
+    @Before
+    public void setUp() throws Exception {
+        auditingManager = new AuditingManager(auditingDao, cassandraDao, new TestConfigurationProvider());
+        Mockito.when(eventFactory.getLogMessage()).thenReturn(msg);
+        Mockito.when(eventFactory.getDbEvent()).thenReturn(auditEvent);
+        Mockito.when(eventFactory.getAuditingEsType()).thenReturn(MSG);
+        Mockito.when(cassandraDao.saveRecord(auditEvent)).thenReturn(CassandraOperationStatus.OK);
+        Mockito.when(auditingDao.addRecord(auditEvent, MSG)).thenReturn(
+            ActionStatus.OK);
+    }
+
+    @Test
+    public void testShouldAuditEvent() {
+        String result = auditingManager.auditEvent(eventFactory);
+        assertThat(result, is(msg));
+        Mockito.verify(cassandraDao).saveRecord(auditEvent);
+        Mockito.verify(auditingDao).addRecord(auditEvent, MSG);
+    }
+
+
+}
--- catalog-be/src/test/java/org/openecomp/sdc/be/auditing/impl/category/AuditCategoryEventFuncTest.java
+++ catalog-be/src/test/java/org/openecomp/sdc/be/auditing/impl/category/AuditCategoryEventFuncTest.java
@@ -1,3 +1,24 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ * Modifications copyright (c) 2019 Nokia
+ * ================================================================================
+ */
 package org.openecomp.sdc.be.auditing.impl.category;
 
 import org.junit.Before;
@@ -19,6 +40,7 @@ import org.openecomp.sdc.be.resources.data.auditing.AuditingGenericEvent;
 import org.openecomp.sdc.be.resources.data.auditing.CategoryEvent;
 import org.openecomp.sdc.be.resources.data.auditing.model.CommonAuditData;
 import org.openecomp.sdc.common.util.ThreadLocalsHolder;
+import org.openecomp.sdc.test.utils.TestConfigurationProvider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -44,7 +66,7 @@ public class AuditCategoryEventFuncTest {
     @Before
     public void setUp() {
         init(esConfig);
-        auditingManager = new AuditingManager(auditingDao, cassandraDao);
+        auditingManager = new AuditingManager(auditingDao, cassandraDao, new TestConfigurationProvider());
         ThreadLocalsHolder.setUuid(REQUEST_ID);
     }
 
