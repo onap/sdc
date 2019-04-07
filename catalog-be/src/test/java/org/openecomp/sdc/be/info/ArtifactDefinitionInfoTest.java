215f9ae4d105d86d37a47efe5e69442fddf187eb
Tomasz Golabek <tomasz.golabek@nokia.com>
2019-04-07 15:05:51 +0200

    Some unit tests for catalog-be
    Code coverage for some classes from catalog-be increased.
    Some refactor made if needed.
    
    Change-Id: I33114eed03d4e176896fd803b54fb77787c34283
    Issue-ID: SDC-2220
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>

--- catalog-be/src/test/java/org/openecomp/sdc/be/info/ArtifactDefinitionInfoTest.java
+++ catalog-be/src/test/java/org/openecomp/sdc/be/info/ArtifactDefinitionInfoTest.java
@@ -1,122 +1,69 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ * Modifications copyright (c) 2019 Nokia
+ * ================================================================================
+ */
 package org.openecomp.sdc.be.info;
 
+import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.openecomp.sdc.be.model.ArtifactDefinition;
 
-
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class ArtifactDefinitionInfoTest {
 
-	private ArtifactDefinitionInfo createTestSubject() {
-		return new ArtifactDefinitionInfo(new ArtifactDefinition());
-	}
+	@Mock
+	private ArtifactDefinition artifactDefinition;
 
-	
+	private static final String VERSION = "VERSION";
+	private static final String DISPLAY_NAME = "DISPLAY NAME";
+	private static final String UUID = "1";
+	private static final byte[] PAYLOAD_DATA = "Test".getBytes();
+	private static final String NAME = "Name";
+
 	@Test
-	public void testGetUniqueId() throws Exception {
-		ArtifactDefinitionInfo testSubject;
-		String result;
-
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getUniqueId();
+	public void shouldHaveValidGettersAndSetters() {
+		assertThat(ArtifactDefinitionInfo.class, hasValidGettersAndSetters());
 	}
 
-	
 	@Test
-	public void testSetUniqueId() throws Exception {
-		ArtifactDefinitionInfo testSubject;
-		String uniqueId = "";
+	public void testCtor() {
+		Mockito.when(artifactDefinition.getPayloadData()).thenReturn(PAYLOAD_DATA);
+		Mockito.when(artifactDefinition.getArtifactName()).thenReturn(NAME);
+		Mockito.when(artifactDefinition.getArtifactDisplayName()).thenReturn(DISPLAY_NAME);
+		Mockito.when(artifactDefinition.getArtifactVersion()).thenReturn(VERSION);
+		Mockito.when(artifactDefinition.getArtifactUUID()).thenReturn(UUID);
 
-		// default test
-		testSubject = createTestSubject();
-		testSubject.setUniqueId(uniqueId);
+		ArtifactDefinitionInfo artifactDefinitionInfo = new ArtifactDefinitionInfo(artifactDefinition);
+		assertThat(artifactDefinitionInfo.getArtifactDisplayName(), is(DISPLAY_NAME));
+		assertThat(artifactDefinitionInfo.getArtifactName(), is(NAME));
+		assertThat(artifactDefinitionInfo.getArtifactVersion(), is(VERSION));
+		assertThat(artifactDefinitionInfo.getArtifactUUID(), is(UUID));
+
 	}
 
-	
-	@Test
-	public void testGetArtifactName() throws Exception {
-		ArtifactDefinitionInfo testSubject;
-		String result;
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getArtifactName();
-	}
-
-	
-	@Test
-	public void testSetArtifactName() throws Exception {
-		ArtifactDefinitionInfo testSubject;
-		String artifactName = "";
-
-		// default test
-		testSubject = createTestSubject();
-		testSubject.setArtifactName(artifactName);
-	}
-
-	
-	@Test
-	public void testGetArtifactDisplayName() throws Exception {
-		ArtifactDefinitionInfo testSubject;
-		String result;
-
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getArtifactDisplayName();
-	}
-
-	
-	@Test
-	public void testSetArtifactDisplayName() throws Exception {
-		ArtifactDefinitionInfo testSubject;
-		String artifactDisplayName = "";
-
-		// default test
-		testSubject = createTestSubject();
-		testSubject.setArtifactDisplayName(artifactDisplayName);
-	}
-
-	
-	@Test
-	public void testGetArtifactVersion() throws Exception {
-		ArtifactDefinitionInfo testSubject;
-		String result;
-
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getArtifactVersion();
-	}
-
-	
-	@Test
-	public void testSetArtifactVersion() throws Exception {
-		ArtifactDefinitionInfo testSubject;
-		String artifactVersion = "";
-
-		// default test
-		testSubject = createTestSubject();
-		testSubject.setArtifactVersion(artifactVersion);
-	}
-
-	
-	@Test
-	public void testGetArtifactUUID() throws Exception {
-		ArtifactDefinitionInfo testSubject;
-		String result;
-
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getArtifactUUID();
-	}
-
-	
-	@Test
-	public void testSetArtifactUUID() throws Exception {
-		ArtifactDefinitionInfo testSubject;
-		String artifactUUID = "";
-
-		// default test
-		testSubject = createTestSubject();
-		testSubject.setArtifactUUID(artifactUUID);
-	}
 }
--- catalog-be/src/test/java/org/openecomp/sdc/be/info/ArtifactTypesInfoTest.java
+++ catalog-be/src/test/java/org/openecomp/sdc/be/info/ArtifactTypesInfoTest.java
@@ -1,14 +1,41 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ * Modifications copyright (c) 2019 Nokia
+ * ================================================================================
+ */
 package org.openecomp.sdc.be.info;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanConstructor;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
 import static org.hamcrest.MatcherAssert.assertThat;
-
 
 public class ArtifactTypesInfoTest {
     @Test
     public void shouldHaveValidGettersAndSetters() {
         assertThat(ArtifactTypesInfo.class, hasValidGettersAndSetters());
+    }
+
+    @Test
+    public void testDefaultCtor() {
+        Assert.assertThat(ArtifactTypesInfo.class, hasValidBeanConstructor());
     }
 }
