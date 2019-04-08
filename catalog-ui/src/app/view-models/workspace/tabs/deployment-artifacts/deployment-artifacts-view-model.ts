161401f2f7aa830ee94f32800c0e895f2f05e877
Piotr Darosz <piotr.darosz@nokia.com>
2019-04-08 14:35:40 +0200

    Refactor hardcoded column config
    Move hardcoded GAB columns configuration to configuration.yaml file
    
    Change-Id: Ie70a1d44c9a04a37c3ba0d3b5f64ad2989b11166
    Issue-ID: SDC-2225
    Signed-off-by: Piotr Darosz <piotr.darosz@nokia.com>

--- catalog-ui/src/app/view-models/workspace/tabs/deployment-artifacts/deployment-artifacts-view-model.ts
+++ catalog-ui/src/app/view-models/workspace/tabs/deployment-artifacts/deployment-artifacts-view-model.ts
@@ -31,6 +31,8 @@ import {GenericArtifactBrowserComponent} from "../../../../ng2/components/logic/
 import {PathsAndNamesDefinition} from "../../../../models/paths-and-names";
 import {ModalService as ModalServiceSdcUI} from "sdc-ui/lib/angular/modals/modal.service";
 import {IModalConfig} from "sdc-ui/lib/angular/modals/models/modal-config";
+import {CacheService} from "../../../../services/cache-service";
+import {GabConfig} from "../../../../models/gab-config";
 
 interface IDeploymentArtifactsViewModelScope extends IWorkspaceViewModelScope {
     tableHeadersList:Array<any>;
@@ -66,6 +68,7 @@ export class DeploymentArtifactsViewModel {
         '$scope',
         '$templateCache',
         '$filter',
+        'Sdc.Services.CacheService',
         'ValidationUtils',
         'ArtifactsUtils',
         'ModalsHandler',
@@ -76,6 +79,7 @@ export class DeploymentArtifactsViewModel {
     constructor(private $scope:IDeploymentArtifactsViewModelScope,
                 private $templateCache:ng.ITemplateCacheService,
                 private $filter:ng.IFilterService,
+                private cacheService:CacheService,
                 private validationUtils:ValidationUtils,
                 private artifactsUtils:ArtifactsUtils,
                 private ModalsHandler:ModalsHandler,
@@ -278,6 +282,7 @@ export class DeploymentArtifactsViewModel {
         };
 
         this.$scope.openGenericArtifactBrowserModal = (artifact:ArtifactModel):void => {
+            let self = this;
             let modalConfig: IModalConfig = {
                 size: 'xl',
                 title: 'Generic Artifact Browser',
@@ -291,11 +296,17 @@ export class DeploymentArtifactsViewModel {
                     {text: "Cancel", size: "'x-small'", closeModal: true}]
             };
 
-            let pathsandnames: PathsAndNamesDefinition[] = [
-                {friendlyName: 'Action', path: 'event.action[2]'},
-                {friendlyName: 'Comment', path: 'event.comment'},
-                {friendlyName: 'Alarm Additional Information',
-                    path: 'event.structure.faultFields.structure.alarmAdditionalInformation.comment'}];
+            let uiConfiguration: any = this.cacheService.get('UIConfiguration');
+            let gabConfig: GabConfig = uiConfiguration.gab.find(config =>
+                config.artifactType === artifact.artifactType);
+            let pathsandnames: PathsAndNamesDefinition[] = [];
+
+            if(typeof uiConfiguration.gab === 'undefined' || typeof gabConfig == 'undefined') {
+                let msg = self.$filter('translate')("DEPLOYMENT_ARTIFACT_GAB_NO_CONFIG");
+                console.log(msg);
+            } else {
+                pathsandnames = gabConfig.pathsandnames;
+            }
 
             let modalInputs = {
                 pathsandnames: pathsandnames,
