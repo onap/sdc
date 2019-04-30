4f514b1c3f1c688345c750281097925ac22417ca
Andrei_Barcovschi <andrei.barcovschi@ericsson.com>
2019-04-30 15:13:34 +0100

    add automated fadeout to sdc splashscreen
    Change-Id: Id24486b4a58d5aa2ccd1dfc8be99bf747b4263c5
    Issue-ID: SDC-2258
    Signed-off-by: Andrei_Barcovschi <andrei.barcovschi@ericsson.com>

--- catalog-ui/src/app/view-models/welcome/welcome-view.ts
+++ catalog-ui/src/app/view-models/welcome/welcome-view.ts
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,7 +43,13 @@ export class WelcomeViewModel {
             this.loadImages(():void=> {
                 window.setTimeout(():void =>{
                     $(".sdc-welcome-new-page").addClass("animated fadeIn");
-                },1000);
+                    window.setTimeout(():void =>{
+                      $(".sdc-welcome-new-page").addClass("animated fadeOut");
+                      window.setTimeout(():void =>{
+                        this.$state.go("dashboard", {});
+                      },1000);
+                    },3000);
+                },0);
             });
         },0);
     }
@@ -73,5 +79,5 @@ export class WelcomeViewModel {
         };
         img.src = url;
     };
-    
+
 }
