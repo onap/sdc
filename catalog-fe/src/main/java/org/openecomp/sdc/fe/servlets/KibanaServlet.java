adb8333f9b451ea4291f17c0274672830d48712d
Piotr Borelowski <p.borelowski@partner.samsung.com>
2019-05-31 13:22:17 +0200

    Added unit tests for KibanaServlet
    Improve unit test coverage in sdc/catalog-fe
    Remove sonar code smells
    
    Issue-ID: SDC-2327
    Signed-off-by: Piotr Borelowski <p.borelowski@partner.samsung.com>
    Change-Id: I365ec437e999d7e0505a9345268405ac26495762

--- catalog-fe/src/main/java/org/openecomp/sdc/fe/servlets/KibanaServlet.java
+++ catalog-fe/src/main/java/org/openecomp/sdc/fe/servlets/KibanaServlet.java
@@ -4,12 +4,14 @@
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (c) 2019 Samsung
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +22,8 @@
 
 package org.openecomp.sdc.fe.servlets;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.eclipse.jetty.proxy.ProxyServlet;
 import org.openecomp.sdc.common.api.Constants;
 import org.openecomp.sdc.fe.config.Configuration;
@@ -27,55 +31,52 @@ import org.openecomp.sdc.fe.config.ConfigurationManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletRequest;
-
 public class KibanaServlet extends ProxyServlet {
-	private static final long serialVersionUID = 1L;
-	private static Logger log = LoggerFactory.getLogger(KibanaServlet.class.getName());
 
-	@Override
-	public String rewriteTarget(HttpServletRequest request) {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(KibanaServlet.class.getName());
 
-		String originalUrl = request.getRequestURI();
+    @Override
+    public String rewriteTarget(HttpServletRequest request) {
+        String originalUrl = request.getRequestURI();
+        String redirectedUrl = getModifiedUrl(request);
 
-		String redirectedUrl = getModifiedUrl(request);
+        log.debug("KibanaServlet Redirecting request from: {} , to: {}", originalUrl, redirectedUrl);
 
-		log.debug("KibanaServlet Redirecting request from: {} , to: {}", originalUrl, redirectedUrl);
+        return redirectedUrl;
+    }
 
-		return redirectedUrl;
-	}
+    public String getModifiedUrl(HttpServletRequest request) {
+        Configuration config = getConfiguration(request);
 
-	public String getModifiedUrl(HttpServletRequest request) {
-		Configuration config = getConfiguration(request);
-		if (config == null) {
-			log.error("failed to retrieve configuration.");
-		}
-		// String scheme = request.getScheme();
-		String contextPath = request.getContextPath(); // /mywebapp
-		String servletPath = request.getServletPath(); // /servlet/MyServlet
-		String pathInfo = request.getPathInfo(); // /a/b;c=123
-		String queryString = request.getQueryString(); // d=789
+        if (config == null) {
+            log.error("Failed to retrieve configuration.");
+            throw new NullPointerException("Failed to retrieve configuration.");
+        }
 
-		StringBuilder url = new StringBuilder();
-		url.append(config.getKibanaProtocol()).append("://").append(config.getKibanaHost());
-		url.append(":").append(config.getKibanaPort());
-		url.append(contextPath).append(servletPath);
+        String contextPath = request.getContextPath();
+        String servletPath = request.getServletPath();
+        String pathInfo = request.getPathInfo();
+        String queryString = request.getQueryString();
 
-		if (pathInfo != null) {
-			url.append(pathInfo);
-		}
-		if (queryString != null) {
-			url.append("?").append(queryString);
-		}
+        StringBuilder url = new StringBuilder();
+        url.append(config.getKibanaProtocol()).append("://").append(config.getKibanaHost());
+        url.append(":").append(config.getKibanaPort());
+        url.append(contextPath).append(servletPath);
 
-		String redirectedUrl = url.toString().replace("/sdc1/kibanaProxy/", "/");
-		return redirectedUrl;
+        if (pathInfo != null) {
+            url.append(pathInfo);
+        }
 
-	}
+        if (queryString != null) {
+            url.append("?").append(queryString);
+        }
 
-	private Configuration getConfiguration(HttpServletRequest request) {
-		Configuration config = ((ConfigurationManager) request.getSession().getServletContext()
-				.getAttribute(Constants.CONFIGURATION_MANAGER_ATTR)).getConfiguration();
-		return config;
-	}
+        return url.toString().replace("/sdc1/kibanaProxy/", "/");
+    }
+
+    private Configuration getConfiguration(HttpServletRequest request) {
+        return ((ConfigurationManager) request.getSession().getServletContext()
+                .getAttribute(Constants.CONFIGURATION_MANAGER_ATTR)).getConfiguration();
+    }
 }
