c040ed04e815243fab0691bc1fa21195cd83f6bf
Tomasz Golabek <tomasz.golabek@nokia.com>
2019-06-04 15:31:47 +0200

    Clean up MdcData
    Removed duplicated class. Added assertions to check the logic.
    
    Change-Id: Ia953ee0c88d71214cf620cdd286f6ecdd3f3d26d
    Issue-ID: SDC-2326
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>

--- catalog-fe/src/main/java/org/openecomp/sdc/fe/impl/MdcData.java
+++ catalog-fe/src/main/java/org/openecomp/sdc/fe/impl/MdcData.java
@@ -1,3 +1,24 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ * Modifications copyright (c) 2019 Nokia
+ * ================================================================================
+ */
 package org.openecomp.sdc.fe.impl;
 
 public class MdcData {
@@ -8,7 +29,6 @@ public class MdcData {
 		private Long transactionStartTime;
 
 		public MdcData(String serviceInstanceID, String userId, String remoteAddr, String localAddr, Long transactionStartTime) {
-			super();
 			this.serviceInstanceID = serviceInstanceID;
 			this.userId = userId;
 			this.remoteAddr = remoteAddr;
