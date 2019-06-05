70cd3ef0d7d6b88a5de45ef0cf252d06e9e7b5c2
avigaffa <avi.gaffa@amdocs.com>
2019-06-05 15:26:51 +0300

    Add lombok support to simple classes
    Change-Id: I2d1a7d63e7f80aa766ac71df9c808dcda35d68e3
    Issue-ID: SDC-2354
    Signed-off-by: avigaffa <avi.gaffa@amdocs.com>

--- openecomp-be/api/openecomp-sdc-rest-webapp/application-config-rest/application-config-rest-types/src/main/java/org/openecomp/sdcrests/applicationconfiguration/types/ApplicationConfigDto.java
+++ openecomp-be/api/openecomp-sdc-rest-webapp/application-config-rest/application-config-rest-types/src/main/java/org/openecomp/sdcrests/applicationconfiguration/types/ApplicationConfigDto.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,27 +20,17 @@
 
 package org.openecomp.sdcrests.applicationconfiguration.types;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by Talio on 8/10/2016.
  */
+@Getter
+@Setter
 public class ApplicationConfigDto {
 
-  private String key;
-  private String value;
+    private String key;
+    private String value;
 
-  public String getKey() {
-    return key;
-  }
-
-  public void setKey(String key) {
-    this.key = key;
-  }
-
-  public String getValue() {
-    return value;
-  }
-
-  public void setValue(String value) {
-    this.value = value;
-  }
 }
