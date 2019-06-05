cf0471084a98dfcd7e74f0c53be79539b8f9bce1
shrek2000 <orenkle@amdocs.com>
2019-06-05 14:23:10 +0300

    Add lombok support to simple classes
    Change-Id: I7060b515880159261fd228bdcfe5e75f84e2d99b
    Issue-ID: SDC-2354
    Signed-off-by: shrek2000 <orenkle@amdocs.com>

--- common/onap-tosca-datatype/src/main/java/org/onap/sdc/tosca/datatypes/model/ActivityDefinition.java
+++ common/onap-tosca-datatype/src/main/java/org/onap/sdc/tosca/datatypes/model/ActivityDefinition.java
@@ -1,5 +1,10 @@
 package org.onap.sdc.tosca.datatypes.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ActivityDefinition {
 
   private String delegate;
@@ -7,35 +12,4 @@ public class ActivityDefinition {
   private String call_operation;
   private String inline;
 
-  public String getDelegate() {
-    return delegate;
-  }
-
-  public void setDelegate(String delegate) {
-    this.delegate = delegate;
-  }
-
-  public String getSet_state() {
-    return set_state;
-  }
-
-  public void setSet_state(String set_state) {
-    this.set_state = set_state;
-  }
-
-  public String getCall_operation() {
-    return call_operation;
-  }
-
-  public void setCall_operation(String call_operation) {
-    this.call_operation = call_operation;
-  }
-
-  public String getInline() {
-    return inline;
-  }
-
-  public void setInline(String inline) {
-    this.inline = inline;
-  }
 }
