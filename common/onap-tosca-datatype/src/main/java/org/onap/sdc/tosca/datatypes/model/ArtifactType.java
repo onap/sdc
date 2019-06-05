bf2f1915da0c7a5459930331d97059b8fc0c6190
Oren  Kleks <orenkle@amdocs.com>
2019-06-05 17:10:53 +0300

    Add lombok support to simple classes
    Change-Id: Ie4c25fd87b3e302f7dd19f4d0156524398e1af55
    Issue-ID: SDC-2354
    Signed-off-by: shrek2000 <orenkle@amdocs.com>

--- common/onap-tosca-datatype/src/main/java/org/onap/sdc/tosca/datatypes/model/ArtifactType.java
+++ common/onap-tosca-datatype/src/main/java/org/onap/sdc/tosca/datatypes/model/ArtifactType.java
@@ -22,9 +22,12 @@ package org.onap.sdc.tosca.datatypes.model;
 
 import java.util.List;
 import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
 
 
-
+@Getter
+@Setter
 public class ArtifactType {
 
   private String derived_from;
@@ -36,59 +39,4 @@ public class ArtifactType {
   private Map<String, PropertyDefinition> properties;
 
 
-  public String getDerived_from() {
-    return derived_from;
-  }
-
-  public void setDerived_from(String derived_from) {
-    this.derived_from = derived_from;
-  }
-
-  public String getVersion() {
-    return version;
-  }
-
-  public void setVersion(String version) {
-    this.version = version;
-  }
-
-  public Map<String, String> getMetadata() {
-    return metadata;
-  }
-
-  public void setMetadata(Map<String, String> metadata) {
-    this.metadata = metadata;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public String getMime_type() {
-    return mime_type;
-  }
-
-  public void setMime_type(String mime_type) {
-    this.mime_type = mime_type;
-  }
-
-  public List<String> getFile_ext() {
-    return file_ext;
-  }
-
-  public void setFile_ext(List<String> file_ext) {
-    this.file_ext = file_ext;
-  }
-
-  public Map<String, PropertyDefinition> getProperties() {
-    return properties;
-  }
-
-  public void setProperties(Map<String, PropertyDefinition> properties) {
-    this.properties = properties;
-  }
 }
