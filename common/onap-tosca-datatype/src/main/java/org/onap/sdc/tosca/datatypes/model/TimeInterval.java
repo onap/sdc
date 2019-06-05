2fa3af99657c544812b16f14f359fdf39f67d241
shrek2000 <orenkle@amdocs.com>
2019-06-05 14:13:24 +0300

    Add lombok support to simple classes
    Change-Id: Ic3956c9c6f4fddf412a2216de3d79a2cb2d779b0
    Issue-ID: SDC-2354
    Signed-off-by: shrek2000 <orenkle@amdocs.com>

--- common/onap-tosca-datatype/src/main/java/org/onap/sdc/tosca/datatypes/model/TimeInterval.java
+++ common/onap-tosca-datatype/src/main/java/org/onap/sdc/tosca/datatypes/model/TimeInterval.java
@@ -1,24 +1,13 @@
 package org.onap.sdc.tosca.datatypes.model;
 
 import java.sql.Timestamp;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class TimeInterval {
   private Timestamp start_time;
   private Timestamp end_time;
 
-  public Timestamp getStart_time() {
-    return start_time;
-  }
-
-  public void setStart_time(Timestamp start_time) {
-    this.start_time = start_time;
-  }
-
-  public Timestamp getEnd_time() {
-    return end_time;
-  }
-
-  public void setEnd_time(Timestamp end_time) {
-    this.end_time = end_time;
-  }
 }
