50bd86960c27d28e7fb5c5e8b2498405f27385a7
avigaffa <avi.gaffa@amdocs.com>
2019-06-05 15:11:02 +0300

    Add lombok support to simple classes
    Change-Id: Iec40668537af0fad0d43a599af9ed65d4e5aefbd
    Issue-ID: SDC-2354
    Signed-off-by: avigaffa <avi.gaffa@amdocs.com>

--- openecomp-be/api/openecomp-sdc-rest-webapp/action-library-rest/action-library-rest-types/src/main/java/org/openecomp/sdcrests/action/types/ListResponseWrapper.java
+++ openecomp-be/api/openecomp-sdc-rest-webapp/action-library-rest/action-library-rest-types/src/main/java/org/openecomp/sdcrests/action/types/ListResponseWrapper.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,51 +20,30 @@
 
 package org.openecomp.sdcrests.action.types;
 
-import org.openecomp.sdc.action.types.OpenEcompComponent;
-
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.openecomp.sdc.action.types.OpenEcompComponent;
 
 /**
  * Defines DTO used for creating Response with list of {@link ActionResponseDto } or list of {@link
  * openEcompComponent }.
  */
+@Getter
+@Setter
 public class ListResponseWrapper {
 
-  List<ActionResponseDto> actionList;
-  List<OpenEcompComponent> componentList;
-  List<ActionResponseDto> versions;
+    List<ActionResponseDto> actionList;
+    List<OpenEcompComponent> componentList;
+    List<ActionResponseDto> versions;
 
 
-  public ListResponseWrapper() {
-    this.actionList = new ArrayList<>();
-  }
+    public ListResponseWrapper() {
+        this.actionList = new ArrayList<>();
+    }
 
-  public List<ActionResponseDto> getVersions() {
-    return versions;
-  }
-
-  public void setVersions(List<ActionResponseDto> versions) {
-    this.versions = versions;
-  }
-
-  public List<ActionResponseDto> getActionList() {
-    return actionList;
-  }
-
-  public void setActionList(List<ActionResponseDto> actionList) {
-    this.actionList = actionList;
-  }
-
-  public List<OpenEcompComponent> getComponentList() {
-    return componentList;
-  }
-
-  public void setComponentList(List<OpenEcompComponent> componentList) {
-    this.componentList = componentList;
-  }
-
-  public void add(ActionResponseDto exception) {
-    this.getActionList().add(exception);
-  }
+    public void add(ActionResponseDto exception) {
+        this.actionList.add(exception);
+    }
 }
