e29c8c1db646d9bd0d28adfdfd0379cde04239c6
noahs <noah.shogan@amdocs.com>
2019-06-23 11:55:42 +0300

    Add Lombok support to RelationshipType
    add @Data annotation
    
    Change-Id: I185a32166a6efe9d221c260a0f25d958dec2690e
    Issue-ID: SDC-2380
    Signed-off-by: noahs <noah.shogan@amdocs.com>

--- common/onap-tosca-datatype/src/main/java/org/onap/sdc/tosca/datatypes/model/RelationshipType.java
+++ common/onap-tosca-datatype/src/main/java/org/onap/sdc/tosca/datatypes/model/RelationshipType.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,83 +20,23 @@
 
 package org.onap.sdc.tosca.datatypes.model;
 
+import lombok.Data;
+
 import java.util.List;
 import java.util.Map;
 
+@Data
 public class RelationshipType {
 
-  private String derived_from;
-  private String version;
-  private Map<String, String> metadata;
-  private String description;
-  private Map<String, PropertyDefinition> properties;
-  private Map<String, AttributeDefinition> attributes;
-  private Map<String, Object> interfaces;
-  private List<String> valid_target_types;
-  //An optional list of one or more names of Capability Types that are valid targets
-  // for this relationship
+    private String derived_from;
+    private String version;
+    private Map<String, String> metadata;
+    private String description;
+    private Map<String, PropertyDefinition> properties;
+    private Map<String, AttributeDefinition> attributes;
+    private Map<String, Object> interfaces;
+    private List<String> valid_target_types;
+    //An optional list of one or more names of Capability Types that are valid targets
+    // for this relationship
 
-  public String getDerived_from() {
-    return derived_from;
-  }
-
-  public void setDerived_from(String derived_from) {
-    this.derived_from = derived_from;
-  }
-
-  public String getVersion() {
-    return version;
-  }
-
-  public void setVersion(String version) {
-    this.version = version;
-  }
-
-  public Map<String, String> getMetadata() {
-    return metadata;
-  }
-
-  public void setMetadata(Map<String, String> metadata) {
-    this.metadata = metadata;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public Map<String, PropertyDefinition> getProperties() {
-    return properties;
-  }
-
-  public void setProperties(Map<String, PropertyDefinition> properties) {
-    this.properties = properties;
-  }
-
-  public Map<String, AttributeDefinition> getAttributes() {
-    return attributes;
-  }
-
-  public void setAttributes(Map<String, AttributeDefinition> attributes) {
-    this.attributes = attributes;
-  }
-
-  public Map<String, Object> getInterfaces() {
-    return interfaces;
-  }
-
-  public void setInterfaces(Map<String, Object> interfaces) {
-    this.interfaces = interfaces;
-  }
-
-  public List<String> getValid_target_types() {
-    return valid_target_types;
-  }
-
-  public void setValid_target_types(List<String> valid_target_types) {
-    this.valid_target_types = valid_target_types;
-  }
 }
