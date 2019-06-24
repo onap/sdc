4318fb7e9965f82e27e7e7856be6c5a97117b628
m.kowalski3 <m.kowalski3@partner.samsung.com>
2019-06-24 13:30:43 +0200

    Add unit tests for ComponentDependencyModelErrorBuilder
    Issue-ID: SDC-2327
    Signed-off-by: Marcin Kowalski <m.kowalski3@partner.samsung.com>
    Change-Id: I48a1b56d3d193dd5a85d4331c31b1913d0997ec3

---
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/test/java/org/openecomp/sdc/vendorsoftwareproduct/errors/ComponentDependencyModelErrorBuilderTests.java
@@ -0,0 +1,52 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2019 Samsung. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdc.vendorsoftwareproduct.errors;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openecomp.sdc.common.errors.ErrorCategory;
+import org.openecomp.sdc.common.errors.ErrorCode;
+
+public class ComponentDependencyModelErrorBuilderTests {
+
+    @Test
+    public void testGetCyclicDependencyComponentErrorBuilder() {
+        //when
+        ErrorCode errorCode = ComponentDependencyModelErrorBuilder.getcyclicDependencyComponentErrorBuilder();
+
+        //then
+        assertEquals("CYCLIC_DEPENDENCY_IN_COMPONENTS", errorCode.id());
+        assertEquals(ErrorCategory.APPLICATION, errorCode.category());
+        assertEquals("Cyclic dependency exists between components.", errorCode.message());
+    }
+
+    @Test
+    public void testGetInvalidRelationTypeErrorBuilder() {
+        //when
+        ErrorCode errorCode = ComponentDependencyModelErrorBuilder.getInvalidRelationTypeErrorBuilder();
+
+        //then
+        assertEquals("INVALID_COMPONENT_RELATION_TYPE", errorCode.id());
+        assertEquals(ErrorCategory.APPLICATION, errorCode.category());
+        assertEquals("Invalid relation type for components.", errorCode.message());
+    }
+}
