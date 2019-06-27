9d1727691bd02cd423236b94d063f64a6f2493b8
m.kowalski3 <m.kowalski3@partner.samsung.com>
2019-06-27 15:42:55 +0200

    Add unit test for InformationArtifactCreationErrorBuilder
    Issue-ID: SDC-2327
    Signed-off-by: Marcin Kowalski <m.kowalski3@partner.samsung.com>
    Change-Id: I1c2f53de62ac9dcbc264b900769a68eab1749b49

---
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/test/java/org/openecomp/sdc/vendorsoftwareproduct/errors/InformationArtifactCreationErrorBuilderTest.java
@@ -0,0 +1,47 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2019 Samsung. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdc.vendorsoftwareproduct.errors;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openecomp.sdc.common.errors.ErrorCategory;
+import org.openecomp.sdc.common.errors.ErrorCode;
+
+public class InformationArtifactCreationErrorBuilderTest {
+
+
+    @Test
+    public void testBuild() {
+        //given
+        InformationArtifactCreationErrorBuilder informationArtifactCreationErrorBuilder =
+                new InformationArtifactCreationErrorBuilder("1");
+
+        //when
+        ErrorCode errorCode = informationArtifactCreationErrorBuilder.build();
+
+        //then
+        assertEquals(VendorSoftwareProductErrorCodes.HEAT_PACKAGE_FILE_CREATION, errorCode.id());
+        assertEquals(ErrorCategory.SYSTEM, errorCode.category());
+        assertEquals("Error while trying to information artifact file for vendor software product with Id 1.",
+                errorCode.message());
+    }
+}
