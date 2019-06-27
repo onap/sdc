002fa5e2884ab6ee2b682dbdd5bfe1e71e4c20ba
m.kowalski3 <m.kowalski3@partner.samsung.com>
2019-06-27 14:22:26 +0200

    Add unit test for ProcessManagerImpl
    Issue-ID: SDC-2327
    Signed-off-by: Marcin Kowalski <m.kowalski3@partner.samsung.com>
    Change-Id: I16dc6f67707d520e0e6678255ab70c726be596c5

--- openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/test/java/org/openecomp/sdc/vendorsoftwareproduct/impl/ProcessManagerImplTest.java
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/test/java/org/openecomp/sdc/vendorsoftwareproduct/impl/ProcessManagerImplTest.java
@@ -7,6 +7,10 @@
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
+ * ================================================================================
+ *  Modifications Copyright (c) 2019 Samsung
+ * ================================================================================
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -370,6 +374,11 @@ public class ProcessManagerImplTest {
     } catch (CoreException exception) {
       Assert.assertEquals(exception.code().id(), expectedErrorCode);
     }
+  }
+
+  @Test(expected = CoreException.class)
+  public void shouldReturnCoreException() {
+    processManager.getProcessArtifact(VSP_ID, VERSION, COMPONENT_ID, PROCESS1_ID);
   }
 
 }
