629e6e099ac6548206a601bbac3202e3b617b81d
noahs <noah.shogan@amdocs.com>
2019-06-30 10:28:41 +0300

    Add Lombok support to Repository
    add @Data annotation
    
    Change-Id: I0a56a2f1978035a00eb4edafd5ecc7db6a7301a4
    Issue-ID: SDC-2401
    Signed-off-by: noahs <noah.shogan@amdocs.com>

--- common/onap-tosca-datatype/src/main/java/org/onap/sdc/tosca/datatypes/model/Repository.java
+++ common/onap-tosca-datatype/src/main/java/org/onap/sdc/tosca/datatypes/model/Repository.java
@@ -1,32 +1,11 @@
 package org.onap.sdc.tosca.datatypes.model;
 
+import lombok.Data;
+
+@Data
 public class Repository {
 
-  private String description;
-  private String url;
-  private Credential credential;
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public String getUrl() {
-    return url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
-
-  public Credential getCredential() {
-    return credential;
-  }
-
-  public void setCredential(Credential credential) {
-    this.credential = credential;
-  }
+    private String description;
+    private String url;
+    private Credential credential;
 }
