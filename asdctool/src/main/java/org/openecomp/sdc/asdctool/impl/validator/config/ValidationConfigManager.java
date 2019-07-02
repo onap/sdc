57d8972d05f830c8e08d04767f0e3bc4a42fc5b8
k.kedron <k.kedron@partner.samsung.com>
2019-07-02 11:04:56 +0200

    Improved unit tests for ArtifactValidationUtils and Sonar fixes.
    Added new tests for ArtifactValidationUtils.
    Checkstyle in the ValidationConfigManager and ArtifactValidationUtils.
    Added private constructor to ValidationConfigManager.
    Added logger for ArtifactValidationUtils and simplified code.
    Updated the ValidationConfigManagerTest and ReportManagerTests
    to not influence on each other.
    
    Issue-ID: SDC-2327
    Signed-off-by: Krystian Kedron <k.kedron@partner.samsung.com>
    Change-Id: I9c7c725792737b6cfa6bba20eeeb3b1d4ef1e471

--- asdctool/src/main/java/org/openecomp/sdc/asdctool/impl/validator/config/ValidationConfigManager.java
+++ asdctool/src/main/java/org/openecomp/sdc/asdctool/impl/validator/config/ValidationConfigManager.java
@@ -3,13 +3,17 @@
  * SDC
  * ================================================================================
  * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (c) 2019 Samsung
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +28,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,37 +43,40 @@ public class ValidationConfigManager {
     private static String outputFullFilePath;
     private static String outputFilePath;
 
+    private ValidationConfigManager() {
+    }
+
     public static String getOutputFullFilePath() {
         return outputFullFilePath;
     }
+
     public static String getOutputFilePath() {
         return outputFilePath;
     }
 
     public static void setOutputFullFilePath(String outputPath) {
-    	ValidationConfigManager.outputFilePath = outputPath;
-        ValidationConfigManager.outputFullFilePath = outputPath+ "/reportOutput.txt";
+        ValidationConfigManager.outputFilePath = outputPath;
+        ValidationConfigManager.outputFullFilePath = outputPath + "/reportOutput.txt";
     }
-
-    
 
     public static String getCsvReportFilePath() {
         return csvReportFilePath;
     }
 
     public static void setCsvReportFilePath(String outputPath) {
-        ValidationConfigManager.csvReportFilePath = outputPath +"/csvSummary_"+System.currentTimeMillis()+".csv";
+        ValidationConfigManager.csvReportFilePath =
+            outputPath + "/csvSummary_" + System.currentTimeMillis() + ".csv";
     }
 
     private static String csvReportFilePath = "summary.csv";
 
-    public static Properties setValidationConfiguration(String path){
-        InputStream input = null;
+    public static Properties setValidationConfiguration(String path) {
+        InputStream input;
         try {
             input = new FileInputStream(path);
             prop.load(input);
         } catch (IOException ex) {
-            log.info("FileInputStream failed - " , ex);
+            log.info("FileInputStream failed - {}", ex);
         }
         return prop;
     }
