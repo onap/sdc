2869cfe85c1fbe841b5e7bf69ba65062a9007a87
k.kedron <k.kedron@partner.samsung.com>
2019-07-02 12:01:42 +0200

    Improved unit tests for the DaoUtils class.
    Issue-ID: SDC-2327
    Signed-off-by: Krystian Kedron <k.kedron@partner.samsung.com>
    Change-Id: I634fc1ccccbfa5b1fec5f2612782f00990438c6b

--- catalog-dao/src/test/java/org/openecomp/sdc/be/dao/utils/DaoUtilsTest.java
+++ catalog-dao/src/test/java/org/openecomp/sdc/be/dao/utils/DaoUtilsTest.java
@@ -1,39 +1,69 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Modifications Copyright (c) 2019 Samsung
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
 package org.openecomp.sdc.be.dao.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Collections;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 public class DaoUtilsTest {
 
-	@Test
-	public void testConvertToJson() throws Exception {
-		Object object = new Object();
-		String result;
+    private static Map<String, String> testObject = Collections.singletonMap("key", "value");
 
-		// test 1
-		result = DaoUtils.convertToJson(object);
-		Assert.assertEquals("{}", result);
-		
-		assertThatThrownBy(()->DaoUtils.convertToJson(null)).isInstanceOf(RuntimeException.class);
-	}
+    private static final String json = "{\"key\":\"value\"}";
 
-	@Test
-	public void testConvertFromJson() throws Exception {
-		Class clazz = Object.class;
-		String json = null;
-		Object result;
+    @Test
+    public void testConvertToJson() {
 
-		// default test
-		result = DaoUtils.convertFromJson(clazz, json);
-		Assert.assertEquals(null, result);
-		
-		try {
-			result = DaoUtils.convertFromJson(null, json);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+        String result = DaoUtils.convertToJson(testObject);
+        Assert.assertEquals(json, result);
+
+        try {
+            DaoUtils.convertToJson(null);
+            fail("The exception wasn't thrown!");
+        } catch (RuntimeException e) {
+
+            assertEquals("The object cannot be NULL!!!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testConvertFromJson() {
+        Class<Map> clazz = Map.class;
+
+        // default test
+        Map result = DaoUtils.convertFromJson(clazz, json);
+        assertEquals(testObject, result);
+
+        try {
+            DaoUtils.convertFromJson(null, json);
+            fail("The exception wasn't thrown!");
+        } catch (RuntimeException e) {
+
+            assertEquals("The Class cannot be NULL!!!", e.getMessage());
+        }
+    }
 }
