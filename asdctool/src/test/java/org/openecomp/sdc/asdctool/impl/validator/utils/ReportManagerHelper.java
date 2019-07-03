ccf67428ebd561f2a07c5610359f970698f875b2
k.kedron <k.kedron@partner.samsung.com>
2019-07-03 08:27:48 +0200

    Improved test for ReportManger, ArtifactValidationUtils and Sonar fixes.
    Add new ReportManagerHelper to read files with reports.
    Fixed log.info to print the types of exception in the messages.
    Removed redundant new lines in the report files.
    
    Issue-ID: SDC-2327
    Signed-off-by: Krystian Kedron <k.kedron@partner.samsung.com>
    Change-Id: If0135f01a05b0936dad681d16e846a6c6560cc80

---
+++ asdctool/src/test/java/org/openecomp/sdc/asdctool/impl/validator/utils/ReportManagerHelper.java
@@ -0,0 +1,66 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2019 Samsung. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdc.asdctool.impl.validator.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openecomp.sdc.asdctool.impl.validator.config.ValidationConfigManager;
+
+public class ReportManagerHelper {
+
+    private ReportManagerHelper() {
+    }
+
+    public static List<String> getReportOutputFileAsList() {
+        return readFileAsList(ValidationConfigManager.getOutputFullFilePath());
+    }
+
+    public static List<String> getReportCsvFileAsList() {
+        return readFileAsList(ValidationConfigManager.getCsvReportFilePath());
+    }
+
+    public static void cleanReports() {
+        cleanFile(ValidationConfigManager.getCsvReportFilePath());
+        cleanFile(ValidationConfigManager.getOutputFullFilePath());
+    }
+
+    private static List<String> readFileAsList(String filePath) {
+        try {
+            BufferedReader br = Files.newBufferedReader(Paths.get(filePath));
+            return br.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private static void cleanFile(String filePath) {
+        try {
+            Files.delete(Paths.get(filePath));
+        } catch (IOException ignored) {
+
+        }
+    }
+}
