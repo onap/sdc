533eb74d88fef53ce652e97c1251dac63f392760
m.kowalski3 <m.kowalski3@partner.samsung.com>
2019-07-09 15:03:08 +0200

    Add unit test for PackageInvalidErrorBuilder
    Issue-ID: SDC-2327
    Signed-off-by: Marcin Kowalski <m.kowalski3@partner.samsung.com>
    Change-Id: I3ca7aa5ac07acd472e9ddd359e74064bf29186c0

---
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/test/java/org/openecomp/sdc/vendorsoftwareproduct/errors/PackageInvalidErrorBuilderTest.java
@@ -0,0 +1,48 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2019 Samsung. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdc.vendorsoftwareproduct.errors;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openecomp.sdc.common.errors.ErrorCategory;
+import org.openecomp.sdc.common.errors.ErrorCode;
+import org.openecomp.sdc.versioning.dao.types.Version;
+
+public class PackageInvalidErrorBuilderTest {
+
+    @Test
+    public void testBuild() {
+        //given
+        PackageInvalidErrorBuilder packageInvalidErrorBuilder =
+                new PackageInvalidErrorBuilder("1", Version.valueOf("1.0"));
+
+        //when
+        ErrorCode errorCode = packageInvalidErrorBuilder.build();
+
+        //then
+        assertEquals(VendorSoftwareProductErrorCodes.PACKAGE_INVALID, errorCode.id());
+        assertEquals(ErrorCategory.APPLICATION, errorCode.category());
+        assertEquals(
+                "Package for vendor software product with Id 1 and version 1.0 is invalid (does not contain translated data).",
+                errorCode.message());
+    }
+}
