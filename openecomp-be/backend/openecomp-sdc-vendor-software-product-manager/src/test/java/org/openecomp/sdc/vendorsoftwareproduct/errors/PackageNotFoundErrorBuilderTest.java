5d32e6c9115286e0defd27bea8528ab390e937ab
m.kowalski3 <m.kowalski3@partner.samsung.com>
2019-07-10 12:51:42 +0200

    Add unit test for PackageNotFoundErrorBuilder
    Issue-ID: SDC-2327
    Signed-off-by: Marcin Kowalski <m.kowalski3@partner.samsung.com>
    Change-Id: I7b553f8eb03b7a1bd8d059ea12dcbdff79100f03

---
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/test/java/org/openecomp/sdc/vendorsoftwareproduct/errors/PackageNotFoundErrorBuilderTest.java
@@ -0,0 +1,52 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2019 Samsung. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdc.vendorsoftwareproduct.errors;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openecomp.sdc.common.errors.ErrorCategory;
+import org.openecomp.sdc.common.errors.ErrorCode;
+import org.openecomp.sdc.versioning.dao.types.Version;
+
+public class PackageNotFoundErrorBuilderTest {
+
+    @Test
+    public void testBuild() {
+        PackageNotFoundErrorBuilder packageNotFoundErrorBuilder = new PackageNotFoundErrorBuilder("1");
+
+        ErrorCode errorCode = packageNotFoundErrorBuilder.build();
+
+        assertEquals(VendorSoftwareProductErrorCodes.PACKAGE_NOT_FOUND, errorCode.id());
+        assertEquals(ErrorCategory.APPLICATION, errorCode.category());
+        assertEquals("Package for vendor software product with Id 1 does not exist.", errorCode.message());
+
+        PackageNotFoundErrorBuilder packageNotFoundErrorBuilder2nd =
+                new PackageNotFoundErrorBuilder("1", Version.valueOf("1.0"));
+
+        ErrorCode errorCode2nd = packageNotFoundErrorBuilder2nd.build();
+
+        assertEquals(VendorSoftwareProductErrorCodes.PACKAGE_NOT_FOUND, errorCode2nd.id());
+        assertEquals(ErrorCategory.APPLICATION, errorCode2nd.category());
+        assertEquals("Package for vendor software product with Id 1 and version 1.0 does not exist.",
+                errorCode2nd.message());
+    }
+}
