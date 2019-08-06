b0bfb079a00826eeb4634c86f4e887c544a363fa
r.bogacki <r.bogacki@samsung.com>
2019-08-06 15:45:17 +0200

    HTTPS calls for sdc-onboarding
    Implement HTTPS calls into sdc-onboarding
    -Added p12 keystore certificate.
    -Updated application configuration.
    -Added trust-store.
    
    Issue-ID: SDC-2498
    Signed-off-by: Robert Bogacki <r.bogacki@samsung.com>
    Signed-off-by: Krystian Kedron <k.kedron@partner.samsung.com>
    Change-Id: Ia96c42cddee5fd94014963c9d2f584b098693a9a

--- openecomp-be/dist/sdc-onboard-backend-docker/artifacts/chef-repo/cookbooks/sdc-onboard-backend/attributes/default.rb
+++ openecomp-be/dist/sdc-onboard-backend-docker/artifacts/chef-repo/cookbooks/sdc-onboard-backend/attributes/default.rb
@@ -5,9 +5,9 @@ default['FE'][:http_port] = 8181
 default['FE'][:https_port] = 9443
 default['disableHttp'] = true
 default['cassandra'][:truststore_password] = "Aa123456"
-default['jetty'][:keystore_pwd] = "OBF:1cp61iuj194s194u194w194y1is31cok"
-default['jetty'][:keymanager_pwd] = "OBF:1cp61iuj194s194u194w194y1is31cok"
-default['jetty'][:truststore_pwd] = "OBF:1cp61iuj194s194u194w194y1is31cok"
+default['jetty'][:keystore_pwd] = "rTIS;B4kM]2GHcNK2c3B4&Ng"
+default['jetty'][:keymanager_pwd] = "rTIS;B4kM]2GHcNK2c3B4&Ng"
+default['jetty'][:truststore_pwd] = "Y,f975ZNJfVZhV*{+Y[}pA?0"
 default['VnfRepo']['vnfRepoPort'] = 8702
 default['VnfRepo']['vnfRepoHost'] = "192.168.50.5"
 
@@ -18,7 +18,6 @@ default['cassandra']['cluster_name'] = "SDC-CS-"
 default['cassandra']['socket_read_timeout'] = 20000
 default['cassandra']['socket_connect_timeout'] = 20000
 default['cassandra']['janusgraph_connection_timeout'] = 10000
-
 
 #ExternalTesting
 default['EXTTEST']['ep1_config'] = "vtp,VTP,true,http://192.168.50.5:8702/onapapi/vnfsdk-marketplace,c.*"
