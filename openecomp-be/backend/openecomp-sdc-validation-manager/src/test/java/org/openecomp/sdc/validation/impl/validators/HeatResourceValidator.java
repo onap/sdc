77653cd1c57a7c401c24b00dd85616ce6a75858a
Tomasz Golabek <tomasz.golabek@nokia.com>
2019-08-20 14:06:08 +0200

    Unit tests - openecomp-sdc-validation-manager
    Tests for org.openecomp.sdc.validation
    
    Change-Id: If0acdb2f453b5cd60d0ec49870f1467cdd212492
    Issue-ID: SDC-2326
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>

--- openecomp-be/backend/openecomp-sdc-validation-manager/src/test/java/org/openecomp/sdc/validation/impl/validators/HeatResourceValidator.java
+++ openecomp-be/backend/openecomp-sdc-validation-manager/src/test/java/org/openecomp/sdc/validation/impl/validators/HeatResourceValidator.java
@@ -2,14 +2,14 @@
  * ============LICENSE_START=======================================================
  * SDC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2019 Nokia. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,10 +17,17 @@
  * limitations under the License.
  * ============LICENSE_END=========================================================
  */
+package org.openecomp.sdc.validation.impl.validators;
 
-package org.openecomp.sdc.validation.types;
+import org.openecomp.core.validation.types.GlobalValidationContext;
+import org.openecomp.sdc.validation.Validator;
 
-public enum ValidationFileStatus {
-  Success,
-  Failure
+/**
+ * Stub required for class creation in test scope
+ */
+public class HeatResourceValidator implements Validator {
+
+    @Override
+    public void validate(GlobalValidationContext globalContext) {
+    }
 }
