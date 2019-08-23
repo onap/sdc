de82b74d94ce8ea4be48c9279292d27523989421
Tomasz Golabek <tomasz.golabek@nokia.com>
2019-08-23 10:48:28 +0200

    unit tests - catalog-be
    Additional junit tests
    
    Change-Id: Id83ab89115bd7bec471f42d3b96a2054ff4ead53
    Issue-ID: SDC-2326
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>

---
+++ catalog-be/src/test/java/org/openecomp/sdc/be/auditing/impl/distribution/AuditDistributionEngineEventMigrationFactoryTest.java
@@ -0,0 +1,57 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.openecomp.sdc.be.auditing.impl.distribution;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.openecomp.sdc.be.model.User;
+import org.openecomp.sdc.be.resources.data.auditing.AuditingActionEnum;
+import org.openecomp.sdc.be.resources.data.auditing.model.CommonAuditData;
+import org.openecomp.sdc.be.resources.data.auditing.model.DistributionTopicData;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuditDistributionEngineEventMigrationFactoryTest {
+
+    @Mock
+    private CommonAuditData commonFields;
+    @Mock
+    private DistributionTopicData distributionTopicData;
+
+    @Test
+    public void shouldBuildUserNameExtended() {
+        User user = new User("firstName", "lastName", "userId", "emailAddress", "role",
+            1L);
+        String userName = AuditDistributionEngineEventMigrationFactory.buildUserNameExtended(user);
+        assertEquals(userName, "userId, firstName lastName, emailAddress, role");
+    }
+
+    @Test
+    public void shouldReturnDefaultValues() {
+        AuditDistributionEngineEventMigrationFactory auditDistributionEngineEventMigrationFactory = new AuditDistributionEngineEventMigrationFactory(
+            AuditingActionEnum.ACTIVATE_SERVICE_BY_API, commonFields, distributionTopicData,
+            "consumerId", "apiKey", "envName", "role", "1");
+        assertEquals(auditDistributionEngineEventMigrationFactory.getLogMessageParams().length, 0);
+        assertEquals(auditDistributionEngineEventMigrationFactory.getLogPattern(), "");
+    }
+}
---
+++ catalog-be/src/test/java/org/openecomp/sdc/be/components/validation/PropertyValidatorTest.java
@@ -0,0 +1,88 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.openecomp.sdc.be.components.validation;
+
+import static org.junit.Assert.assertTrue;
+
+import fj.data.Either;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.openecomp.sdc.be.components.impl.utils.ExceptionUtils;
+import org.openecomp.sdc.be.impl.ComponentsUtils;
+import org.openecomp.sdc.be.model.PropertyDefinition;
+import org.openecomp.sdc.be.model.cache.ApplicationDataTypeCache;
+import org.openecomp.sdc.be.model.operations.impl.PropertyOperation;
+import org.openecomp.sdc.exception.ResponseFormat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PropertyValidatorTest {
+
+    private static final String TEST = "test";
+    private static final String VALUE = "VALUE";
+    private static final String TYPE = "TYPE";
+
+    @Mock
+    PropertyOperation propertyOperation;
+    @Mock
+    ExceptionUtils exceptionUtils;
+    @Mock
+    ComponentsUtils componentsUtils;
+    @Mock
+    ApplicationDataTypeCache applicationDataTypeCache;
+
+    @Test
+    public void shouldValidateThinProperties() {
+        Mockito.when(propertyOperation.validateAndUpdatePropertyValue(
+            Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(Either.left(""));
+
+        PropertyValidator propertyValidator = new PropertyValidator(propertyOperation, componentsUtils,
+            applicationDataTypeCache, exceptionUtils);
+        List<PropertyDefinition> props = new ArrayList<>();
+        List<PropertyDefinition> dbProps = new ArrayList<>();
+        PropertyDefinition prop = new PropertyDefinition();
+        prop.setName(TEST);
+        prop.setValue(VALUE);
+        prop.setType(TYPE);
+        props.add(prop);
+        dbProps.add(prop);
+        propertyValidator.thinPropertiesValidator(props, dbProps, Collections.emptyMap());
+        Mockito.verify(propertyOperation).validateAndUpdatePropertyValue(TYPE, VALUE, null, Collections.emptyMap());
+    }
+
+    @Test
+    public void shouldIterateOverPropertiesOnInvalidType() {
+        PropertyValidator propertyValidator = new PropertyValidator(propertyOperation, componentsUtils,
+            applicationDataTypeCache, exceptionUtils);
+        List<PropertyDefinition> props = new ArrayList<>();
+        PropertyDefinition prop = new PropertyDefinition();
+        prop.setName(TEST);
+        prop.setValue(VALUE);
+        prop.setType(TYPE);
+        props.add(prop);
+        Either<Boolean, ResponseFormat> booleanResponseFormatEither = propertyValidator.iterateOverProperties(props);
+        assertTrue(booleanResponseFormatEither.isRight());
+    }
+}
