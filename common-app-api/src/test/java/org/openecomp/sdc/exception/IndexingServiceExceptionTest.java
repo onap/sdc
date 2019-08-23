57e13437774261d78aa6a5f51ffbb83de0499f35
Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
2019-08-23 11:12:50 +0200

    increasing test coverage in common-app-api exception catalog
    Issue-ID: SDC-2326
    Signed-off-by: Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
    Change-Id: I0dd6bc60c88acbcf1b3755627a7b10b874a549ba

---
+++ common-app-api/src/test/java/org/openecomp/sdc/exception/IndexingServiceExceptionTest.java
@@ -0,0 +1,54 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdc.exception;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IndexingServiceExceptionTest {
+
+    @Test(expected = RuntimeException.class)
+    public void validateOneArgConstructorsThrowsCorrectException() {
+        final String testMessage = "test error message";
+
+        try {
+            throw new IndexingServiceException(testMessage);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(),testMessage);
+            throw new RuntimeException("");
+        }
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void validateTwoArgConstructorsThrowsCorrectException() {
+        final String testMessage = "test error message";
+        final String testThrowableMessage = "test throwable";
+
+        try {
+            throw new IndexingServiceException(testMessage, new Throwable(testThrowableMessage));
+        } catch (Exception e) {
+            assertEquals(e.getMessage(),testMessage);
+            assertEquals(e.getCause().getMessage(),testThrowableMessage);
+            throw new RuntimeException("");
+        }
+    }
+}
