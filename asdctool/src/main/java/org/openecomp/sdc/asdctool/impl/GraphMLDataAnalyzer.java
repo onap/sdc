262b1980ba44a0403c2fe4e1b42197afc9ecc040
Arindam Mondal <arind.mondal@samsung.com>
2019-08-27 13:47:44 +0900

    General Sonar fixes
    +) Replace System.out with logger
    ++) Use try-with-resources to close the resource
    +++) Format to delete tab charachter
    ++++) flip equals to avoid potential NPE
    
    Issue-ID: SDC-2542
    Change-Id: If57f36072ec67e9622d4fe51bce8336094dc10c8
    Signed-off-by: arindamm <arind.mondal@samsung.com>

--- asdctool/src/main/java/org/openecomp/sdc/asdctool/impl/GraphMLDataAnalyzer.java
+++ asdctool/src/main/java/org/openecomp/sdc/asdctool/impl/GraphMLDataAnalyzer.java
@@ -41,204 +41,205 @@ import java.util.Set;
 
 public class GraphMLDataAnalyzer {
 
-	private static Logger log = LoggerFactory.getLogger(GraphMLDataAnalyzer.class);
+    private static Logger log = LoggerFactory.getLogger(GraphMLDataAnalyzer.class);
 
-	private static final String[] COMPONENT_SHEET_HEADER = { "uniqueId", "type", "name", "toscaResourceName",
-			"resourceType", "version", "deleted", "hasNonCalculatedReqCap" };
-	private static final String[] COMPONENT_INSTANCES_SHEET_HEADER = { "uniqueId", "name", "originUid", "originType",
-			"containerUid" };
+    private static final String[] COMPONENT_SHEET_HEADER = {"uniqueId", "type", "name", "toscaResourceName",
+            "resourceType", "version", "deleted", "hasNonCalculatedReqCap"};
+    private static final String[] COMPONENT_INSTANCES_SHEET_HEADER =
+            {"uniqueId", "name", "originUid", "originType", "containerUid"};
 
-	public String analyzeGraphMLData(String[] args) {
-		String result = null;
-		try {
-			String mlFileLocation = args[0];
-			result = _analyzeGraphMLData(mlFileLocation);
-			log.info("Analyzed ML file=" + mlFileLocation + ", XLS result=" + result);
-		} catch (Exception e) {
-			log.error("analyze GraphML Data failed - {}" , e);
-			return null;
-		}
-		return result;
-	}
+    public String analyzeGraphMLData(String[] args) {
+        String result = null;
+        try {
+            String mlFileLocation = args[0];
+            result = _analyzeGraphMLData(mlFileLocation);
+            log.info("Analyzed ML file=" + mlFileLocation + ", XLS result=" + result);
+        } catch (Exception e) {
+            log.error("analyze GraphML Data failed - {}", e);
+            return null;
+        }
+        return result;
+    }
 
-	private String _analyzeGraphMLData(String mlFileLocation) throws Exception {
-		// Parse ML file
-		SAXBuilder builder = new SAXBuilder();
-		File xmlFile = new File(mlFileLocation);
-		Document document = builder.build(xmlFile);
-		
-		// XLS data file name
-		String outputFile = mlFileLocation.replace(".graphml", ".xls");
-		Workbook wb = new HSSFWorkbook();
-		try(FileOutputStream fileOut = new FileOutputStream(outputFile)){
-			writeComponents(wb, document);
-			writeComponentInstances(wb, document);
-			wb.write(fileOut);
-		}catch(Exception e){
-			log.error("analyze GraphML Data failed - {}" , e);
-		}
-		return outputFile;	
-	}
+    private String _analyzeGraphMLData(String mlFileLocation) throws Exception {
+        // Parse ML file
+        SAXBuilder builder = new SAXBuilder();
+        File xmlFile = new File(mlFileLocation);
+        Document document = builder.build(xmlFile);
 
-	private void writeComponents(Workbook wb, Document document) {
-		Sheet componentsSheet = wb.createSheet("Components");
-		Row currentRow = componentsSheet.createRow(0);
-		for (int i = 0; i < COMPONENT_SHEET_HEADER.length; i++) {
-			currentRow.createCell(i).setCellValue(COMPONENT_SHEET_HEADER[i]);
-		}
+        // XLS data file name
+        String outputFile = mlFileLocation.replace(".graphml", ".xls");
+        
+        try (Workbook wb = new HSSFWorkbook(); FileOutputStream fileOut = new FileOutputStream(outputFile)) {
+            writeComponents(wb, document);
+            writeComponentInstances(wb, document);
+            wb.write(fileOut);
+        } catch (Exception e) {
+            log.error("analyze GraphML Data failed - {}", e);
+        }
+        return outputFile;
+    }
 
-		List<ComponentRow> components = getComponents(document);
-		int rowNum = 1;
-		for (ComponentRow row : components) {
-			currentRow = componentsSheet.createRow(rowNum++);
-			currentRow.createCell(0).setCellValue(row.getUniqueId());
-			currentRow.createCell(1).setCellValue(row.getType());
-			currentRow.createCell(2).setCellValue(row.getName());
-			currentRow.createCell(3).setCellValue(row.getToscaResourceName());
-			currentRow.createCell(4).setCellValue(row.getResourceType());
-			currentRow.createCell(5).setCellValue(row.getVersion());
-			currentRow.createCell(6).setCellValue(row.getIsDeleted() != null ? row.getIsDeleted().toString() : "false");
-			currentRow.createCell(7).setCellValue(row.getHasNonCalculatedReqCap());
-		}
-	}
+    private void writeComponents(Workbook wb, Document document) {
+        Sheet componentsSheet = wb.createSheet("Components");
+        Row currentRow = componentsSheet.createRow(0);
+        for (int i = 0; i < COMPONENT_SHEET_HEADER.length; i++) {
+            currentRow.createCell(i).setCellValue(COMPONENT_SHEET_HEADER[i]);
+        }
 
-	private void writeComponentInstances(Workbook wb, Document document) {
-		Sheet componentsSheet = wb.createSheet("ComponentInstances");
-		Row currentRow = componentsSheet.createRow(0);
-		for (int i = 0; i < COMPONENT_INSTANCES_SHEET_HEADER.length; i++) {
-			currentRow.createCell(i).setCellValue(COMPONENT_INSTANCES_SHEET_HEADER[i]);
-		}
-		List<ComponentInstanceRow> components = getComponentInstances(document);
-		int rowNum = 1;
-		for (ComponentInstanceRow row : components) {
-			currentRow = componentsSheet.createRow(rowNum++);
-			currentRow.createCell(0).setCellValue(row.getUniqueId());
-			currentRow.createCell(1).setCellValue(row.getName());
-			currentRow.createCell(2).setCellValue(row.getOriginUid());
-			currentRow.createCell(3).setCellValue(row.getOriginType());
-			currentRow.createCell(4).setCellValue(row.getContainerUid());
-		}
-	}
+        List<ComponentRow> components = getComponents(document);
+        int rowNum = 1;
+        for (ComponentRow row : components) {
+            currentRow = componentsSheet.createRow(rowNum++);
+            currentRow.createCell(0).setCellValue(row.getUniqueId());
+            currentRow.createCell(1).setCellValue(row.getType());
+            currentRow.createCell(2).setCellValue(row.getName());
+            currentRow.createCell(3).setCellValue(row.getToscaResourceName());
+            currentRow.createCell(4).setCellValue(row.getResourceType());
+            currentRow.createCell(5).setCellValue(row.getVersion());
+            currentRow.createCell(6)
+                    .setCellValue(row.getIsDeleted() != null ? row.getIsDeleted().toString() : "false");
+            currentRow.createCell(7).setCellValue(row.getHasNonCalculatedReqCap());
+        }
+    }
 
-	private List<ComponentRow> getComponents(Document document) {
-		List<ComponentRow> res = new ArrayList<>();
-		Element root = document.getRootElement();
-		ElementFilter filter = new ElementFilter("graph");
-		Element graph = root.getDescendants(filter).next();
-		filter = new ElementFilter("edge");
-		IteratorIterable<Element> edges = graph.getDescendants(filter);
-		Set<String> componentsHavingReqOrCap = new HashSet<>();
-		filter = new ElementFilter("data");
-		for (Element edge : edges) {
-			IteratorIterable<Element> dataNodes = edge.getDescendants(filter);
-			for (Element data : dataNodes) {
-				String attributeValue = data.getAttributeValue("key");
-				if( attributeValue.equals("labelE")) {
-					String edgeLabel = data.getText();
-					if (edgeLabel.equals("REQUIREMENT") || edgeLabel.equals("CAPABILITY")) {
-						componentsHavingReqOrCap.add(edge.getAttributeValue("source"));
-					}
-				}
-			}
-		}
+    private void writeComponentInstances(Workbook wb, Document document) {
+        Sheet componentsSheet = wb.createSheet("ComponentInstances");
+        Row currentRow = componentsSheet.createRow(0);
+        for (int i = 0; i < COMPONENT_INSTANCES_SHEET_HEADER.length; i++) {
+            currentRow.createCell(i).setCellValue(COMPONENT_INSTANCES_SHEET_HEADER[i]);
+        }
+        List<ComponentInstanceRow> components = getComponentInstances(document);
+        int rowNum = 1;
+        for (ComponentInstanceRow row : components) {
+            currentRow = componentsSheet.createRow(rowNum++);
+            currentRow.createCell(0).setCellValue(row.getUniqueId());
+            currentRow.createCell(1).setCellValue(row.getName());
+            currentRow.createCell(2).setCellValue(row.getOriginUid());
+            currentRow.createCell(3).setCellValue(row.getOriginType());
+            currentRow.createCell(4).setCellValue(row.getContainerUid());
+        }
+    }
 
-		filter = new ElementFilter("node");
-		IteratorIterable<Element> nodes = graph.getDescendants(filter);
-		filter = new ElementFilter("data");
-		for (Element element : nodes) {
-			IteratorIterable<Element> dataNodes = element.getDescendants(filter);
-			ComponentRow componentRow = new ComponentRow();
-			boolean isComponent = false;
-			for (Element data : dataNodes) {
-				String attributeValue = data.getAttributeValue("key");
-				switch (attributeValue) {
-				case "nodeLabel":
-					String nodeLabel = data.getText();
-					if (nodeLabel.equals("resource") || nodeLabel.equals("service")) {
-						isComponent = true;
-						componentRow.setType(nodeLabel);
-						String componentId = element.getAttributeValue("id");
-						componentRow.setHasNonCalculatedReqCap(componentsHavingReqOrCap.contains(componentId));
-					}
-					break;
-				case "uid":
-					componentRow.setUniqueId(data.getText());
-					break;
-				case "name":
-					componentRow.setName(data.getText());
-					break;
-				case "toscaResourceName":
-					componentRow.setToscaResourceName(data.getText());
-					break;
-				case "resourceType":
-					componentRow.setResourceType(data.getText());
-					break;
-				case "version":
-					componentRow.setVersion(data.getText());
-					break;
-				case "deleted":
-					componentRow.setIsDeleted(Boolean.parseBoolean(data.getText()));
-					break;
-				default:
-					break;
-				}
-			}
-			if (isComponent) {
-				res.add(componentRow);
-			}
-		}
-		return res;
-	}
+    private List<ComponentRow> getComponents(Document document) {
+        List<ComponentRow> res = new ArrayList<>();
+        Element root = document.getRootElement();
+        ElementFilter filter = new ElementFilter("graph");
+        Element graph = root.getDescendants(filter).next();
+        filter = new ElementFilter("edge");
+        IteratorIterable<Element> edges = graph.getDescendants(filter);
+        Set<String> componentsHavingReqOrCap = new HashSet<>();
+        filter = new ElementFilter("data");
+        for (Element edge : edges) {
+            IteratorIterable<Element> dataNodes = edge.getDescendants(filter);
+            for (Element data : dataNodes) {
+                String attributeValue = data.getAttributeValue("key");
+                if (attributeValue.equals("labelE")) {
+                    String edgeLabel = data.getText();
+                    if (edgeLabel.equals("REQUIREMENT") || edgeLabel.equals("CAPABILITY")) {
+                        componentsHavingReqOrCap.add(edge.getAttributeValue("source"));
+                    }
+                }
+            }
+        }
 
-	private List<ComponentInstanceRow> getComponentInstances(Document document) {
-		List<ComponentInstanceRow> res = new ArrayList<>();
-		Element root = document.getRootElement();
-		ElementFilter filter = new ElementFilter("graph");
-		Element graph = root.getDescendants(filter).next();
-		filter = new ElementFilter("node");
-		IteratorIterable<Element> nodes = graph.getDescendants(filter);
-		filter = new ElementFilter("data");
-		for (Element element : nodes) {
-			IteratorIterable<Element> dataNodes = element.getDescendants(filter);
-			ComponentInstanceRow componentInstRow = new ComponentInstanceRow();
-			boolean isComponentInst = false;
-			for (Element data : dataNodes) {
-				String attributeValue = data.getAttributeValue("key");
-				switch (attributeValue) {
-				case "nodeLabel":
-					String nodeLabel = data.getText();
-					if (nodeLabel.equals("resourceInstance")) {
-						isComponentInst = true;
-					}
-					break;
-				case "uid":
-					componentInstRow.setUniqueId(data.getText());
-					break;
-				case "name":
-					componentInstRow.setName(data.getText());
-					break;
-				case "originType":
-					componentInstRow.setOriginType(data.getText());
-					break;
-				default:
-					break;
-				}
-			}
-			if (isComponentInst) {
-				// Assuming the uid is in standard form of
-				// <container>.<origin>.<name>
-				String uniqueId = componentInstRow.getUniqueId();
-				if (uniqueId != null) {
-					String[] split = uniqueId.split("\\.");
-					if (split.length == 3) {
-						componentInstRow.setContainerUid(split[0]);
-						componentInstRow.setOriginUid(split[1]);
-					}
-				}
-				res.add(componentInstRow);
-			}
-		}
-		return res;
-	}
+        filter = new ElementFilter("node");
+        IteratorIterable<Element> nodes = graph.getDescendants(filter);
+        filter = new ElementFilter("data");
+        for (Element element : nodes) {
+            IteratorIterable<Element> dataNodes = element.getDescendants(filter);
+            ComponentRow componentRow = new ComponentRow();
+            boolean isComponent = false;
+            for (Element data : dataNodes) {
+                String attributeValue = data.getAttributeValue("key");
+                switch (attributeValue) {
+                    case "nodeLabel":
+                        String nodeLabel = data.getText();
+                        if (nodeLabel.equals("resource") || nodeLabel.equals("service")) {
+                            isComponent = true;
+                            componentRow.setType(nodeLabel);
+                            String componentId = element.getAttributeValue("id");
+                            componentRow.setHasNonCalculatedReqCap(componentsHavingReqOrCap.contains(componentId));
+                        }
+                        break;
+                    case "uid":
+                        componentRow.setUniqueId(data.getText());
+                        break;
+                    case "name":
+                        componentRow.setName(data.getText());
+                        break;
+                    case "toscaResourceName":
+                        componentRow.setToscaResourceName(data.getText());
+                        break;
+                    case "resourceType":
+                        componentRow.setResourceType(data.getText());
+                        break;
+                    case "version":
+                        componentRow.setVersion(data.getText());
+                        break;
+                    case "deleted":
+                        componentRow.setIsDeleted(Boolean.parseBoolean(data.getText()));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (isComponent) {
+                res.add(componentRow);
+            }
+        }
+        return res;
+    }
+
+    private List<ComponentInstanceRow> getComponentInstances(Document document) {
+        List<ComponentInstanceRow> res = new ArrayList<>();
+        Element root = document.getRootElement();
+        ElementFilter filter = new ElementFilter("graph");
+        Element graph = root.getDescendants(filter).next();
+        filter = new ElementFilter("node");
+        IteratorIterable<Element> nodes = graph.getDescendants(filter);
+        filter = new ElementFilter("data");
+        for (Element element : nodes) {
+            IteratorIterable<Element> dataNodes = element.getDescendants(filter);
+            ComponentInstanceRow componentInstRow = new ComponentInstanceRow();
+            boolean isComponentInst = false;
+            for (Element data : dataNodes) {
+                String attributeValue = data.getAttributeValue("key");
+                switch (attributeValue) {
+                    case "nodeLabel":
+                        String nodeLabel = data.getText();
+                        if (nodeLabel.equals("resourceInstance")) {
+                            isComponentInst = true;
+                        }
+                        break;
+                    case "uid":
+                        componentInstRow.setUniqueId(data.getText());
+                        break;
+                    case "name":
+                        componentInstRow.setName(data.getText());
+                        break;
+                    case "originType":
+                        componentInstRow.setOriginType(data.getText());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (isComponentInst) {
+                // Assuming the uid is in standard form of
+                // <container>.<origin>.<name>
+                String uniqueId = componentInstRow.getUniqueId();
+                if (uniqueId != null) {
+                    String[] split = uniqueId.split("\\.");
+                    if (split.length == 3) {
+                        componentInstRow.setContainerUid(split[0]);
+                        componentInstRow.setOriginUid(split[1]);
+                    }
+                }
+                res.add(componentInstRow);
+            }
+        }
+        return res;
+    }
 
 }
