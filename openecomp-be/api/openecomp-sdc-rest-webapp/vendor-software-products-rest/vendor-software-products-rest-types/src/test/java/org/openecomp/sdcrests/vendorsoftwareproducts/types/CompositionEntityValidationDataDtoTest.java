a27030ff62895b9d0de9ca857ad21a6e03a30237
Tomasz Golabek <tomasz.golabek@nokia.com>
2019-08-30 09:26:10 +0200

    unit tests - openecomp-be
    Additional junit tests for vendor-software-product-rest-types
    
    Change-Id: I757fe0eed085b6ce880dda143ad1fceac220aafe
    Issue-ID: SDC-2326
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>

---
+++ openecomp-be/api/openecomp-sdc-rest-webapp/vendor-software-products-rest/vendor-software-products-rest-types/src/test/java/org/openecomp/sdcrests/vendorsoftwareproducts/types/CompositionEntityValidationDataDtoTest.java
@@ -0,0 +1,50 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.openecomp.sdcrests.vendorsoftwareproducts.types;
+
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanEquals;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanHashCode;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToString;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class CompositionEntityValidationDataDtoTest {
+    @Test
+    public void shouldHaveValidGettersAndSetters() {
+        assertThat(CompositionEntityValidationDataDto.class, hasValidGettersAndSetters());
+    }
+
+    @Test
+    public void shouldHaveValidToString() {
+        assertThat(CompositionEntityValidationDataDto.class, hasValidBeanToString());
+    }
+
+    @Test
+    public void shouldHaveEquals() {
+        assertThat(CompositionEntityValidationDataDto.class, hasValidBeanEquals());
+    }
+
+    @Test
+    public void shouldHaveHashCode() {
+        assertThat(CompositionEntityValidationDataDto.class, hasValidBeanHashCode());
+    }
+}
