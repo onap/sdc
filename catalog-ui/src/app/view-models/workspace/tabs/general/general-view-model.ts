d59a40cfac5eed1cb3919b9047f55f25f6423020
Nilay Ranjan <nilay.ranjan@team.telstra.com>
2019-12-09 22:58:39 +0530

    Disabled manual service creation for Partner
    Changes to disable/hide Partner Domain Service
    category to disable manual creation of such services
    
    Issue-ID: SDC-2382
    Change-Id: Ie15732360d78542f41101e315fa4aa803531b5f2
    Signed-off-by: Atif Husain <atif.husain@team.telstra.com>

--- catalog-ui/src/app/view-models/workspace/tabs/general/general-view-model.ts
+++ catalog-ui/src/app/view-models/workspace/tabs/general/general-view-model.ts
@@ -65,6 +65,7 @@ export interface IGeneralScope extends IWorkspaceViewModelScope {
     browseFileLabel:string;
     componentCategories:componentCategories;
     instantiationTypes:Array<instantiationType>;
+    isHiddenCategorySelected: boolean;
 
     save():Promise<any>;
     revert():void;
@@ -344,6 +345,40 @@ export class GeneralViewModel {
         this.$scope.updateMenuComponentName(this.$scope.component.name);
     };
 
+    //Find if a category is applicable for External API or not
+    private isHiddenCategory = (category: string) => {
+        let result: boolean = false;
+        let items: Array<any> = new Array<any>();
+        items = this.$scope.sdcMenu.component_workspace_menu_option[this.$scope.component.getComponentSubType()];
+        items.forEach((item: any) => {
+            if ((item.hiddenCategories && item.hiddenCategories.indexOf(category) > -1)) {
+                result = true;
+            }
+        });
+        return result;
+    };
+
+    private filteredCategories = () => {
+        let tempCategories: Array<IMainCategory> = new Array<IMainCategory>();
+        this.$scope.categories.forEach((category) => {
+            if (!this.isHiddenCategory(category.name)
+                && this.$scope.isCreateMode()
+            ) {
+                tempCategories.push(category);
+            } else if ((ComponentState.NOT_CERTIFIED_CHECKOUT === this.$scope.component.lifecycleState)
+                && !this.isHiddenCategory(this.$scope.component.selectedCategory)
+                && !this.isHiddenCategory(category.name)
+            ) {
+                tempCategories.push(category);
+            } else if ((ComponentState.NOT_CERTIFIED_CHECKOUT === this.$scope.component.lifecycleState)
+                && this.isHiddenCategory(this.$scope.component.selectedCategory)) {
+                tempCategories.push(category);
+            }
+        });
+
+        return tempCategories;
+    };    
+   
     private initScopeMethods = ():void => {
 
         this.$scope.initCategoreis = ():void => {
@@ -353,6 +388,14 @@ export class GeneralViewModel {
             }
             if (this.$scope.componentType === ComponentType.SERVICE) {
                 this.$scope.categories = this.cacheService.get('serviceCategories');
+
+                //Remove categories from dropdown applicable for External API
+                if (this.$scope.isCreateMode() || (ComponentState.NOT_CERTIFIED_CHECKOUT === this.$scope.component.lifecycleState)) {
+                    this.$scope.categories = this.filteredCategories();
+                    //Flag to disbale category if service is created through External API
+                    this.$scope.isHiddenCategorySelected = this.isHiddenCategory(this.$scope.component.selectedCategory);
+                }
+                
             }
         };
 
