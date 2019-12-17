e7c3006cb059586d37bcc50b964b2162ff6fd5d1
ilanap <ilanap@amdocs.com>
2019-12-17 14:57:13 +0200

    Fix loader for unready plugins
    Checks whether plugin was ready and will reset loader and ready event when moving between windows if not.
    
    Issue-ID: SDC-2715
    Signed-off-by: ilanap <ilanap@amdocs.com>
    Change-Id: I9c3d637135153f0607a6588e5f588450c1708b99

--- catalog-ui/src/app/ng2/components/ui/plugin/plugin-frame.component.ts
+++ catalog-ui/src/app/ng2/components/ui/plugin/plugin-frame.component.ts
@@ -18,6 +18,7 @@ export class PluginFrameComponent implements OnInit {
     pluginUrl: string;
     private urlSearchParams: URLSearchParams;
     private isClosed: boolean;
+    private isReady: boolean;
     private isPluginCheckDone: boolean;
 
     constructor(private eventBusService: EventBusService,
@@ -45,6 +46,7 @@ export class PluginFrameComponent implements OnInit {
     private initPlugin() {
         this.pluginUrl = this.plugin.pluginSourceUrl;
         this.isClosed = false;
+        this.isReady = false;
 
         if (this.queryParams && !_.isEmpty(this.queryParams)) {
             _.forOwn(this.queryParams, (value, key) => {
@@ -58,6 +60,7 @@ export class PluginFrameComponent implements OnInit {
         let readyEvent = (eventData) => {
             if (eventData.originId === this.plugin.pluginId) {
                 if (eventData.type == "READY") {
+                    this.isReady = true;
                     this.onLoadingDone.emit();
                     this.eventBusService.off(readyEvent)
                 }
@@ -70,10 +73,13 @@ export class PluginFrameComponent implements OnInit {
         // before moving to a new state
         this.$scope.$on('$stateChangeStart', (event, toState, toParams, fromState, fromParams) => {
             if ((fromState.name !== toState.name) || (fromState.name === toState.name) && (toParams.path !== fromParams.path)) {
+                if (!this.isReady) {
+                    this.onLoadingDone.emit();
+                    this.eventBusService.off(readyEvent)
+                }
                 if (this.eventBusService.NoWindowOutEvents.indexOf(this.eventBusService.lastEventNotified) == -1) {
                     if (!this.isClosed) {
                         event.preventDefault();
-
                         this.eventBusService.notify("WINDOW_OUT").subscribe(() => {
                             this.isClosed = true;
                             this.eventBusService.unregister(this.plugin.pluginId);
