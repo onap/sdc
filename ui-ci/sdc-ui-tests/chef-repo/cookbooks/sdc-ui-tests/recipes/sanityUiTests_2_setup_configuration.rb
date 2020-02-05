0e6b06b2abac7e7f5246e5a85fc6569f5ef8348b
aribeiro <anderson.ribeiro@est.tech>
2020-02-05 09:11:21 +0000

    Fix for sanityUiTests
    Issue-ID: SDC-2757
    Signed-off-by: aribeiro <anderson.ribeiro@est.tech>
    Change-Id: I90d1bb36fb527dd2f610ad610bfa88eccbbcc6d7

--- ui-ci/sdc-ui-tests/chef-repo/cookbooks/sdc-ui-tests/recipes/sanityUiTests_2_setup_configuration.rb
+++ ui-ci/sdc-ui-tests/chef-repo/cookbooks/sdc-ui-tests/recipes/sanityUiTests_2_setup_configuration.rb
@@ -35,7 +35,7 @@ template "janusgraph.properties" do
    mode "0755"
    variables({
       :CASSANDRA_IP => node['Nodes']['CS'].join(",").gsub(/[|]/,''),
-      :CASSANDRA_PORT =>  node['cassandra'][[:cassandra_port],
+      :CASSANDRA_PORT =>  node['cassandra'][:cassandra_port],
       :CASSANDRA_PWD => node['cassandra'][:cassandra_password],
       :CASSANDRA_USR => node['cassandra'][:cassandra_user],
       :rep_factor => node['cassandra']['replication_factor'],
