b3e9914ce32f3e1b11396f88b3af9ae00c76479d
vasraz <vasyl.razinkov@est.tech>
2020-03-18 13:29:59 +0000

    Increase test coverage
    Change-Id: I5b666658061bf8838e1af952f3b808da6e113c7a
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Issue-ID: SDC-2833

--- catalog-model/src/test/java/org/openecomp/sdc/be/model/UploadNodeFilterCapabilitiesInfoTest.java
+++ catalog-model/src/test/java/org/openecomp/sdc/be/model/UploadNodeFilterCapabilitiesInfoTest.java
@@ -2,14 +2,14 @@
  * ============LICENSE_START=======================================================
  * SDC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2020, Nordix Foundation. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,23 +20,22 @@
 
 package org.openecomp.sdc.be.model;
 
-import org.openecomp.sdc.be.datatypes.elements.InputsValueDataDefinition;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class PropertyValueDefinition extends InputsValueDataDefinition implements IOperationParameter {
+public class UploadNodeFilterCapabilitiesInfoTest {
 
-    public PropertyValueDefinition() {
-        super();
+    private UploadNodeFilterCapabilitiesInfo createTestSubject() {
+        return new UploadNodeFilterCapabilitiesInfo();
     }
 
-    public PropertyValueDefinition(String name, String value) {
-        super(name, value);
+    @Test
+    public void testBuildDummyCatalogUpdateTimestamp() {
+        UploadNodeFilterCapabilitiesInfo testSubject;
 
-    }
-
-    @Override
-    public boolean isDefinition() {
-        // TODO Auto-generated method stub
-        return false;
+        // default test
+        testSubject = createTestSubject();
+        Assert.assertNotNull(testSubject);
     }
 
 }
