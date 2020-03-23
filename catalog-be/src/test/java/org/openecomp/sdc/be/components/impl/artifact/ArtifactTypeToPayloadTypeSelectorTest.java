17efae17c4f516b95cb1bd15379694119c3dd018
Baumel, Dvir (db384r) <dvir.baumel@intl.att.com>
2020-03-23 16:21:05 +0200

    Upgrade Junit to version 5.6
    Issue-ID: SDC-2844
    Change-Id: I4716bf7920f9bb79c450ebfd48e9d4fba54fdfdd
    Signed-off-by: Baumel, Dvir (db384r) <dvir.baumel@intl.att.com>

--- catalog-be/src/test/java/org/openecomp/sdc/be/components/impl/artifact/ArtifactTypeToPayloadTypeSelectorTest.java
+++ catalog-be/src/test/java/org/openecomp/sdc/be/components/impl/artifact/ArtifactTypeToPayloadTypeSelectorTest.java
@@ -23,13 +23,19 @@
 package org.openecomp.sdc.be.components.impl.artifact;
 
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.openecomp.sdc.common.api.ArtifactTypeEnum;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 import static org.openecomp.sdc.be.components.impl.artifact.ArtifactTypeToPayloadTypeSelector.getPayloadType;
@@ -38,75 +44,67 @@ import static org.openecomp.sdc.be.components.impl.artifact.PayloadTypeEnum.HEAT
 import static org.openecomp.sdc.be.components.impl.artifact.PayloadTypeEnum.NOT_DEFINED;
 import static org.openecomp.sdc.be.components.impl.artifact.PayloadTypeEnum.XML;
 
-@RunWith(Parameterized.class)
+@ExtendWith(MockitoExtension.class)
 public class ArtifactTypeToPayloadTypeSelectorTest {
 
     private static final String ANY_EXTENSION = "anyExtension";
     private static final String JSON = "json";
     private static final String YAML = "yaml";
     private static final String YML = "yml";
-    private String artifactType;
-    private String extension;
-    private PayloadTypeEnum expectedPayloadTypeEnum;
 
-    public ArtifactTypeToPayloadTypeSelectorTest(String artifactType, String extension, PayloadTypeEnum expectedPayloadTypeEnum) {
-        this.artifactType = artifactType;
-        this.extension = extension;
-        this.expectedPayloadTypeEnum = expectedPayloadTypeEnum;
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of(ArtifactTypeEnum.HEAT.getType(), ANY_EXTENSION, HEAT_YAML),
+                Arguments.of(ArtifactTypeEnum.HEAT.getType().toLowerCase(), ANY_EXTENSION, HEAT_YAML),
+                Arguments.of(ArtifactTypeEnum.HEAT_VOL.getType(), ANY_EXTENSION, HEAT_YAML),
+                Arguments.of(ArtifactTypeEnum.HEAT_VOL.getType().toLowerCase(), ANY_EXTENSION, HEAT_YAML),
+                Arguments.of(ArtifactTypeEnum.HEAT_NET.getType(), ANY_EXTENSION, HEAT_YAML),
+                Arguments.of(ArtifactTypeEnum.HEAT_NET.getType().toLowerCase(), ANY_EXTENSION, HEAT_YAML),
+                Arguments.of(ArtifactTypeEnum.HEAT_ENV.getType(), ANY_EXTENSION, HEAT_ENV),
+                Arguments.of(ArtifactTypeEnum.HEAT_ENV.getType().toLowerCase(), ANY_EXTENSION, HEAT_ENV),
+                Arguments.of(ArtifactTypeEnum.YANG_XML.getType(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.YANG_XML.getType().toLowerCase(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.VNF_CATALOG.getType(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.VNF_CATALOG.getType().toLowerCase(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.VF_LICENSE.getType(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.VF_LICENSE.getType().toLowerCase(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.VENDOR_LICENSE.getType(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.VENDOR_LICENSE.getType().toLowerCase(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.MODEL_INVENTORY_PROFILE.getType(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.MODEL_INVENTORY_PROFILE.getType().toLowerCase(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.MODEL_QUERY_SPEC.getType(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.UCPE_LAYER_2_CONFIGURATION.getType(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.UCPE_LAYER_2_CONFIGURATION.getType().toLowerCase(), ANY_EXTENSION, XML),
+                Arguments.of(ArtifactTypeEnum.DCAE_INVENTORY_JSON.getType(), JSON, PayloadTypeEnum.JSON),
+                Arguments.of(ArtifactTypeEnum.DCAE_INVENTORY_JSON.getType().toLowerCase(), JSON, PayloadTypeEnum.JSON),
+                Arguments.of(ArtifactTypeEnum.DCAE_INVENTORY_TOSCA.getType(), YAML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.DCAE_INVENTORY_TOSCA.getType().toLowerCase(), YAML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.DCAE_INVENTORY_TOSCA.getType(), YML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.DCAE_INVENTORY_TOSCA.getType().toLowerCase(), YML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.VES_EVENTS.getType(), YAML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.VES_EVENTS.getType().toLowerCase(), YAML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.VES_EVENTS.getType(), YML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.VES_EVENTS.getType().toLowerCase(), YML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.LIFECYCLE_OPERATIONS.getType(), YAML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.LIFECYCLE_OPERATIONS.getType().toLowerCase(), YAML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.LIFECYCLE_OPERATIONS.getType(), YML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.LIFECYCLE_OPERATIONS.getType().toLowerCase(), YML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.PM_DICTIONARY.getType(), YAML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.PM_DICTIONARY.getType().toLowerCase(), YAML, PayloadTypeEnum.YAML ),
+                Arguments.of(ArtifactTypeEnum.PM_DICTIONARY.getType(), YML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.PM_DICTIONARY.getType().toLowerCase(), YML, PayloadTypeEnum.YAML),
+                Arguments.of(ArtifactTypeEnum.ANSIBLE_PLAYBOOK.getType(), ANY_EXTENSION, NOT_DEFINED),
+                Arguments.of(ArtifactTypeEnum.ANSIBLE_PLAYBOOK.getType().toLowerCase(), ANY_EXTENSION, NOT_DEFINED)
+        );
     }
 
-    @Parameters(name = "{index}: {0}, {1}")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-                { ArtifactTypeEnum.HEAT.getType(), ANY_EXTENSION, HEAT_YAML },
-                { ArtifactTypeEnum.HEAT.getType().toLowerCase(), ANY_EXTENSION, HEAT_YAML },
-                { ArtifactTypeEnum.HEAT_VOL.getType(), ANY_EXTENSION, HEAT_YAML },
-                { ArtifactTypeEnum.HEAT_VOL.getType().toLowerCase(), ANY_EXTENSION, HEAT_YAML },
-                { ArtifactTypeEnum.HEAT_NET.getType(), ANY_EXTENSION, HEAT_YAML },
-                { ArtifactTypeEnum.HEAT_NET.getType().toLowerCase(), ANY_EXTENSION, HEAT_YAML },
-                { ArtifactTypeEnum.HEAT_ENV.getType(), ANY_EXTENSION, HEAT_ENV },
-                { ArtifactTypeEnum.HEAT_ENV.getType().toLowerCase(), ANY_EXTENSION, HEAT_ENV },
-                { ArtifactTypeEnum.YANG_XML.getType(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.YANG_XML.getType().toLowerCase(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.VNF_CATALOG.getType(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.VNF_CATALOG.getType().toLowerCase(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.VF_LICENSE.getType(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.VF_LICENSE.getType().toLowerCase(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.VENDOR_LICENSE.getType(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.VENDOR_LICENSE.getType().toLowerCase(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.MODEL_INVENTORY_PROFILE.getType(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.MODEL_INVENTORY_PROFILE.getType().toLowerCase(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.MODEL_QUERY_SPEC.getType(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.MODEL_QUERY_SPEC.getType().toLowerCase(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.UCPE_LAYER_2_CONFIGURATION.getType(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.UCPE_LAYER_2_CONFIGURATION.getType().toLowerCase(), ANY_EXTENSION, XML },
-                { ArtifactTypeEnum.DCAE_INVENTORY_JSON.getType(), JSON, PayloadTypeEnum.JSON },
-                { ArtifactTypeEnum.DCAE_INVENTORY_JSON.getType().toLowerCase(), JSON, PayloadTypeEnum.JSON },
-                { ArtifactTypeEnum.DCAE_INVENTORY_TOSCA.getType(), YAML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.DCAE_INVENTORY_TOSCA.getType().toLowerCase(), YAML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.DCAE_INVENTORY_TOSCA.getType(), YML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.DCAE_INVENTORY_TOSCA.getType().toLowerCase(), YML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.VES_EVENTS.getType(), YAML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.VES_EVENTS.getType().toLowerCase(), YAML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.VES_EVENTS.getType(), YML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.VES_EVENTS.getType().toLowerCase(), YML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.LIFECYCLE_OPERATIONS.getType(), YAML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.LIFECYCLE_OPERATIONS.getType().toLowerCase(), YAML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.LIFECYCLE_OPERATIONS.getType(), YML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.LIFECYCLE_OPERATIONS.getType().toLowerCase(), YML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.PM_DICTIONARY.getType(), YAML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.PM_DICTIONARY.getType().toLowerCase(), YAML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.PM_DICTIONARY.getType(), YML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.PM_DICTIONARY.getType().toLowerCase(), YML, PayloadTypeEnum.YAML },
-                { ArtifactTypeEnum.ANSIBLE_PLAYBOOK.getType(), ANY_EXTENSION, NOT_DEFINED },
-                { ArtifactTypeEnum.ANSIBLE_PLAYBOOK.getType().toLowerCase(), ANY_EXTENSION, NOT_DEFINED }
-        });
-    }
-
-    @Test
-    public void testCorrectPayloadTypeEnumSelectedForArtifactTypeAndExtension() {
+    @ParameterizedTest
+    @MethodSource("data")
+    public void testCorrectPayloadTypeEnumSelectedForArtifactTypeAndExtension(String artifactType, String extension, PayloadTypeEnum expectedPayloadTypeEnum) {
         PayloadTypeEnum payloadType = getPayloadType(artifactType, extension);
         assertEquals(expectedPayloadTypeEnum, payloadType);
     }
-
 }
+
+
+
