34e13e96dd65bffc59450f919d877d20f27156ea
vasraz <vasyl.razinkov@est.tech>
2020-03-24 12:29:04 +0000

    Increase test coverage
    Change-Id: I90606d768c55ad13aa9df62bc4d3f533206db1bc
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Issue-ID: SDC-2833

--- catalog-model/src/main/java/org/openecomp/sdc/be/model/Product.java
+++ catalog-model/src/main/java/org/openecomp/sdc/be/model/Product.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,10 +20,9 @@
 
 package org.openecomp.sdc.be.model;
 
+import java.util.List;
 import org.openecomp.sdc.be.datatypes.elements.ProductMetadataDataDefinition;
 import org.openecomp.sdc.be.datatypes.enums.ComponentTypeEnum;
-
-import java.util.List;
 
 public class Product extends Component {
 
@@ -45,10 +44,12 @@ public class Product extends Component {
         getProductMetadataDefinition().setFullName(fullName);
     }
 
+    @Override
     public String getInvariantUUID() {
         return getProductMetadataDefinition().getInvariantUUID();
     }
 
+    @Override
     public void setInvariantUUID(String invariantUUID) {
         getProductMetadataDefinition().setInvariantUUID(invariantUUID);
     }
