a22105ea1e73575bd312450956e6cb6cb51b93a9
k.kedron <k.kedron@partner.samsung.com>
2020-03-25 15:29:42 +0100

    Updated the sdc cert
    Issue-ID: SDC-2855
    Signed-off-by: Krystian Kedron <k.kedron@partner.samsung.com>
    Change-Id: I1ee3f155ca50edee4a47b8585d95f9a3f848b94a

--- openecomp-be/dist/sdc-onboard-backend-docker/artifacts/chef-repo/cookbooks/sdc-onboard-backend/attributes/default.rb
+++ openecomp-be/dist/sdc-onboard-backend-docker/artifacts/chef-repo/cookbooks/sdc-onboard-backend/attributes/default.rb
@@ -5,9 +5,9 @@ default['FE'][:http_port] = 8181
 default['FE'][:https_port] = 9443
 default['disableHttp'] = true
 default['cassandra'][:truststore_password] = "Aa123456"
-default['jetty'][:keystore_pwd] = "!ppJ.JvWn0hGh)oVF]([Kv)^"
-default['jetty'][:keymanager_pwd] = "!ppJ.JvWn0hGh)oVF]([Kv)^"
-default['jetty'][:truststore_pwd] = "].][xgtze]hBhz*wy]}m#lf*"
+default['jetty'][:keystore_pwd] = "?(kP!Yur![*!Y5!E^f(ZKc31"
+default['jetty'][:keymanager_pwd] = "?(kP!Yur![*!Y5!E^f(ZKc31"
+default['jetty'][:truststore_pwd] = "z+KEj;t+,KN^iimSiS89e#p0"
 
 default['VnfRepo']['vnfRepoPort'] = 8702
 default['VnfRepo']['vnfRepoHost'] = "192.168.50.5"
