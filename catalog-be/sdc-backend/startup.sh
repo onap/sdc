eacb2c8c5a6dfee7fd1502c43ae373c9b0ae123b
k.kedron <k.kedron@partner.samsung.com>
2020-03-25 15:29:42 +0100

    Updated the sdc cert
    Issue-ID: SDC-2855
    Signed-off-by: Krystian Kedron <k.kedron@partner.samsung.com>
    Change-Id: I1ee3f155ca50edee4a47b8585d95f9a3f848b94a
    (cherry picked from commit a22105ea1e73575bd312450956e6cb6cb51b93a9)

--- catalog-be/sdc-backend/startup.sh
+++ catalog-be/sdc-backend/startup.sh
@@ -7,7 +7,7 @@ export JAVA_OPTIONS=" -Dconfig.home=${JETTY_BASE}/config \
        -Dartifactgenerator.config=${JETTY_BASE}/config/catalog-be/Artifact-Generator.properties \
 	   -Donboarding_configuration.yaml=${JETTY_BASE}/config/onboarding-be/onboarding_configuration.yaml \
        -Djavax.net.ssl.trustStore=${JETTY_BASE}/etc/org.onap.sdc.trust.jks \
-       -Djavax.net.ssl.trustStorePassword=].][xgtze]hBhz*wy]}m#lf* \
+       -Djavax.net.ssl.trustStorePassword=z+KEj;t+,KN^iimSiS89e#p0 \
        -Djetty.console-capture.dir=${JETTY_BASE}/logs \
        ${JAVA_OPTIONS} "
 
