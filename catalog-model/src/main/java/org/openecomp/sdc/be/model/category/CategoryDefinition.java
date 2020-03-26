ebe564dc3a0f67013b5a5e88e61b2c36860c98c8
vasraz <vasyl.razinkov@est.tech>
2020-03-26 17:25:54 +0000

    Increase test coverage
    Change-Id: If9b371042a42213f38828f9628714d076261f25c
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Issue-ID: SDC-2833

--- catalog-model/src/main/java/org/openecomp/sdc/be/model/category/CategoryDefinition.java
+++ catalog-model/src/main/java/org/openecomp/sdc/be/model/category/CategoryDefinition.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,11 +20,18 @@
 
 package org.openecomp.sdc.be.model.category;
 
-import org.openecomp.sdc.be.datatypes.category.CategoryDataDefinition;
-
 import java.util.ArrayList;
 import java.util.List;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.openecomp.sdc.be.datatypes.category.CategoryDataDefinition;
 
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class CategoryDefinition extends CategoryDataDefinition {
 
     private List<SubCategoryDefinition> subcategories;
@@ -37,24 +44,11 @@ public class CategoryDefinition extends CategoryDataDefinition {
         super(c);
     }
 
-    public List<SubCategoryDefinition> getSubcategories() {
-        return subcategories;
-    }
-
-    public void setSubcategories(List<SubCategoryDefinition> subcategories) {
-        this.subcategories = subcategories;
-    }
-
     public void addSubCategory(SubCategoryDefinition subcategory) {
         if (subcategories == null) {
             subcategories = new ArrayList<>();
         }
         subcategories.add(subcategory);
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + " CategoryDefinition [subcategories=" + subcategories + "]";
     }
 
 }
--- catalog-model/src/test/java/org/openecomp/sdc/be/model/category/SubCategoryDefinitionTest.java
+++ catalog-model/src/test/java/org/openecomp/sdc/be/model/category/SubCategoryDefinitionTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,58 +20,23 @@
 
 package org.openecomp.sdc.be.model.category;
 
-import java.util.List;
-
 import org.junit.Test;
-
+import org.assertj.core.api.Assertions;
 
 public class SubCategoryDefinitionTest {
 
-	private SubCategoryDefinition createTestSubject() {
-		return new SubCategoryDefinition();
-	}
+    private SubCategoryDefinition createTestSubject() {
+        return new SubCategoryDefinition();
+    }
 
-	
-	@Test
-	public void testGetGroupings() throws Exception {
-		SubCategoryDefinition testSubject;
-		List<GroupingDefinition> result;
+    @Test
+    public void testAddGrouping() throws Exception {
+        final SubCategoryDefinition testSubject;
+        final GroupingDefinition groupingDefinition = new GroupingDefinition();
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getGroupings();
-	}
-
-	
-	@Test
-	public void testSetGroupings() throws Exception {
-		SubCategoryDefinition testSubject;
-		List<GroupingDefinition> groupingDefinitions = null;
-
-		// default test
-		testSubject = createTestSubject();
-		testSubject.setGroupings(groupingDefinitions);
-	}
-
-	
-	@Test
-	public void testAddGrouping() throws Exception {
-		SubCategoryDefinition testSubject;
-		GroupingDefinition groupingDefinition = null;
-
-		// default test
-		testSubject = createTestSubject();
-		testSubject.addGrouping(groupingDefinition);
-	}
-
-	
-	@Test
-	public void testToString() throws Exception {
-		SubCategoryDefinition testSubject;
-		String result;
-
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.toString();
-	}
+        // default test
+        testSubject = createTestSubject();
+        testSubject.addGrouping(groupingDefinition);
+		Assertions.assertThat(testSubject.getGroupings()).isNotNull().isNotEmpty();
+    }
 }
