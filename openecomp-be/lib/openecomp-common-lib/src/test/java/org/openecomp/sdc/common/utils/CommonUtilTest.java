611fc672e4b1f1e8271f7377e7b314c53f0c8b41
Dmitry Puzikov <d.puzikov2@partner.samsung.com>
2020-04-03 17:40:28 +0200

    More tests added
    Change-Id: I4b6ec51eb13d3553b308f794fb22b7761993f0ab
    Issue-ID: SDC-2869
    Signed-off-by: Dmitry Puzikov <d.puzikov2@partner.samsung.com>

--- openecomp-be/lib/openecomp-common-lib/src/test/java/org/openecomp/sdc/common/utils/CommonUtilTest.java
+++ openecomp-be/lib/openecomp-common-lib/src/test/java/org/openecomp/sdc/common/utils/CommonUtilTest.java
@@ -16,15 +16,29 @@
 
 package org.openecomp.sdc.common.utils;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
 import static org.onap.sdc.tosca.services.CommonUtil.DEFAULT;
 import static org.onap.sdc.tosca.services.CommonUtil.UNDERSCORE_DEFAULT;
 import static org.testng.Assert.assertTrue;
 
+import com.google.common.io.Files;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import org.openecomp.core.utilities.file.FileContentHandler;
+import org.openecomp.core.utilities.orchestration.OnboardingTypesEnum;
+import org.openecomp.sdc.common.errors.CoreException;
+import org.openecomp.sdc.common.zip.exception.ZipException;
 import org.testng.annotations.Test;
 
 public class CommonUtilTest {
+    private static final String VALID_ZIP_FILE_PATH = "src/test/resources/valid.zip";
+    private static final String VALID_CSAR_FILE_PATH = "src/test/resources/valid.zip";
+    private static final String VALID_ZIP_WITH_DIR_FILE_PATH = "src/test/resources/zip_with_dir.zip";
 
     @Test
     public void testGetObjectAsMap() {
@@ -33,4 +47,66 @@ public class CommonUtilTest {
         assertTrue(CommonUtil.getObjectAsMap(obj).containsKey(UNDERSCORE_DEFAULT));
     }
 
+    @Test
+    public void testValidateAndUploadFileContentZip() throws IOException {
+        byte[] file = getFileAsBytes(VALID_ZIP_FILE_PATH);
+
+        FileContentHandler fch = CommonUtil.validateAndUploadFileContent(OnboardingTypesEnum.ZIP, file);
+
+        assertThat(fch, notNullValue(FileContentHandler.class));
+        assertThat(fch.containsFile("file.one.yaml"), is(true));
+        assertThat(fch.containsFile("file.two.yaml"), is(true));
+    }
+
+    @Test
+    public void testValidateAndUploadFileContentCsar() throws IOException {
+        byte[] file = getFileAsBytes(VALID_CSAR_FILE_PATH);
+
+        FileContentHandler fch = CommonUtil.validateAndUploadFileContent(OnboardingTypesEnum.CSAR, file);
+
+        assertThat(fch, notNullValue(FileContentHandler.class));
+        assertThat(fch.containsFile("file.one.yaml"), is(true));
+        assertThat(fch.containsFile("file.two.yaml"), is(true));
+    }
+
+    @Test(expectedExceptions={CoreException.class})
+    public void testValidateNoFolders() throws IOException {
+        byte[] file = getFileAsBytes(VALID_ZIP_WITH_DIR_FILE_PATH);
+
+        FileContentHandler fch = CommonUtil.validateAndUploadFileContent(OnboardingTypesEnum.ZIP, file);
+
+        fail("Should throw CoreException");
+    }
+
+    @Test
+    public void testGetZipContent() throws ZipException {
+        byte[] file = getFileAsBytes(VALID_ZIP_WITH_DIR_FILE_PATH);
+
+        FileContentHandler fch = CommonUtil.getZipContent(file);
+
+        assertThat(fch, notNullValue(FileContentHandler.class));
+        assertThat(fch.getFileList().size(), is(2));
+        assertThat(fch.getFolderList().size(), is(1));
+    }
+
+    @Test
+    public void testValidateAllFilesYaml() throws ZipException {
+        byte[] file = getFileAsBytes(VALID_ZIP_WITH_DIR_FILE_PATH);
+        FileContentHandler fch = CommonUtil.getZipContent(file);
+
+        boolean result = CommonUtil.validateAllFilesYml(fch);
+
+        assertThat(result, is(true));
+    }
+
+    private byte[] getFileAsBytes(String fileName) {
+        byte[] data = null;
+        try {
+            File file = new File(fileName);
+            data = Files.toByteArray(file);
+        } catch (IOException e) {
+            fail("Couldn't read test file");
+        }
+        return data;
+    }
 }
