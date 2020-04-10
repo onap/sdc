32bfdf3b900badd803d90c4d95155f6d051235a8
andre.schmid <andre.schmid@est.tech>
2020-04-10 14:13:23 +0100

    Fix GAB problem in ui-ci tests
    Generic Artifact Browser is failing in tests
    due mainly to a removal of the default constructor
    in GAB class.
    
    Change-Id: Id10b8d8d81c224e2658002b9c928e8aef7ca8e32
    Issue-ID: SDC-2903
    Signed-off-by: andre.schmid <andre.schmid@est.tech>

--- catalog-be/src/main/java/org/openecomp/sdc/be/info/GenericArtifactQueryInfo.java
+++ catalog-be/src/main/java/org/openecomp/sdc/be/info/GenericArtifactQueryInfo.java
@@ -23,11 +23,13 @@ package org.openecomp.sdc.be.info;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class GenericArtifactQueryInfo {
     private Set<String> fields;
     private String parentId;
