ca5c8d2e88ba32150299752ffa468ed5d30a2cc3
Francis Toth <francis.toth@yoppworks.com>
2020-04-14 09:00:31 -0400

    Encapsulate Report state in its own class
    Change-Id: I8aff0aa5f8229a36e00858a1bd40b3baedb7e220
    Signed-off-by: Francis Toth <francis.toth@yoppworks.com>
    Issue-ID: SDC-2499

--- asdctool/src/test/java/org/openecomp/sdc/asdctool/impl/validator/tasks/artifacts/ServiceArtifactValidationTaskTest.java
+++ asdctool/src/test/java/org/openecomp/sdc/asdctool/impl/validator/tasks/artifacts/ServiceArtifactValidationTaskTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,27 +22,26 @@ package org.openecomp.sdc.asdctool.impl.validator.tasks.artifacts;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.openecomp.sdc.asdctool.impl.validator.utils.Report;
 import org.openecomp.sdc.be.dao.jsongraph.GraphVertex;
-
-import java.util.HashMap;
 
 import static org.mockito.Mockito.mock;
 
 public class ServiceArtifactValidationTaskTest {
 
-	private ServiceArtifactValidationTask createTestSubject() {
-		ArtifactValidationUtils artifactValidationUtilsMock = mock(ArtifactValidationUtils.class);
-		return new ServiceArtifactValidationTask(artifactValidationUtilsMock);
-	}
+  private ServiceArtifactValidationTask createTestSubject() {
+    ArtifactValidationUtils artifactValidationUtilsMock = mock(ArtifactValidationUtils.class);
+    return new ServiceArtifactValidationTask(artifactValidationUtilsMock);
+  }
 
-	@Test
-	public void testValidate() {
-		GraphVertex vertex = null;
-		ArtifactsVertexResult actual = createTestSubject().validate(new HashMap<>(), vertex, null);
-		// TODO: Formerly, there was no assertion in this test
-		// One has been added but the expected result is unknown
-		// This requires a complete refactoring.
-		Assertions.assertThat(actual).isEqualTo(null);
-	}
-
+  @Test
+  public void testValidate() {
+    GraphVertex vertex = null;
+    // TODO: Fix these nulls
+    ArtifactsVertexResult actual = createTestSubject().validate(Report.make(null, null), vertex);
+    // TODO: Formerly, there was no assertion in this test
+    // One has been added but the expected result is unknown
+    // This requires a complete refactoring.
+    Assertions.assertThat(actual).isEqualTo(null);
+  }
 }
--- asdctool/src/test/java/org/openecomp/sdc/asdctool/impl/validator/tasks/artifacts/VfArtifactValidationTaskTest.java
+++ asdctool/src/test/java/org/openecomp/sdc/asdctool/impl/validator/tasks/artifacts/VfArtifactValidationTaskTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,28 +22,27 @@ package org.openecomp.sdc.asdctool.impl.validator.tasks.artifacts;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.openecomp.sdc.asdctool.impl.validator.utils.Report;
 import org.openecomp.sdc.asdctool.impl.validator.utils.VertexResult;
 import org.openecomp.sdc.be.dao.jsongraph.GraphVertex;
-
-import java.util.HashMap;
 
 import static org.mockito.Mockito.mock;
 
 public class VfArtifactValidationTaskTest {
 
-	private VfArtifactValidationTask createTestSubject() {
-		ArtifactValidationUtils artifactValidationUtilsMock = mock(ArtifactValidationUtils.class);
-		return new VfArtifactValidationTask(artifactValidationUtilsMock);
-	}
+  private VfArtifactValidationTask createTestSubject() {
+    ArtifactValidationUtils artifactValidationUtilsMock = mock(ArtifactValidationUtils.class);
+    return new VfArtifactValidationTask(artifactValidationUtilsMock);
+  }
 
-	@Test
-	public void testValidate() {
-		GraphVertex vertex = null;
-		VertexResult validate = createTestSubject().validate(new HashMap<>(), vertex, null);
-		// TODO: Formerly, there was no assertion in this test
-		// One has been added but the expected result is unknown
-		// This requires a complete refactoring.
-		Assertions.assertThat(validate).isEqualTo(null);
-	}
-
+  @Test
+  public void testValidate() {
+    GraphVertex vertex = null;
+    // TODO: Fix these nulls
+    VertexResult validate = createTestSubject().validate(Report.make(null, null), vertex);
+    // TODO: Formerly, there was no assertion in this test
+    // One has been added but the expected result is unknown
+    // This requires a complete refactoring.
+    Assertions.assertThat(validate).isEqualTo(null);
+  }
 }
