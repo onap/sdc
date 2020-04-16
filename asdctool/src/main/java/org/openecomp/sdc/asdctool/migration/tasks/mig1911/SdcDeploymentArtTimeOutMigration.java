f3faec290954593571602769e2153f44b307d387
Chris André <chris.andre@yoppworks.com>
2020-04-16 16:29:05 -0400

    Formatting code following project's code style
    Issue-ID: SDC-2904
    Signed-off-by: Chris Andre <chris.andre@yoppworks.com>
    Change-Id: I537175a0e33a74e7df20e6de475cb074a384ed30

--- asdctool/src/main/java/org/openecomp/sdc/asdctool/migration/tasks/mig1911/SdcDeploymentArtTimeOutMigration.java
+++ asdctool/src/main/java/org/openecomp/sdc/asdctool/migration/tasks/mig1911/SdcDeploymentArtTimeOutMigration.java
@@ -73,7 +73,8 @@ public class SdcDeploymentArtTimeOutMigration extends InstanceMigrationBase impl
     public MigrationResult migrate() {
         StorageOperationStatus status = updateDeploymentArtifactTimeOut();
         return status == StorageOperationStatus.OK ?
-                MigrationResult.success() : MigrationResult.error("failed to update instance deployment artifact timeOut. Error : " + status);
+            MigrationResult.success()
+            : MigrationResult.error("failed to update instance deployment artifact timeOut. Error : " + status);
     }
 
     protected StorageOperationStatus updateDeploymentArtifactTimeOut() {
@@ -81,7 +82,9 @@ public class SdcDeploymentArtTimeOutMigration extends InstanceMigrationBase impl
         propertiesToMatch.put(GraphPropertyEnum.COMPONENT_TYPE, ComponentTypeEnum.SERVICE.name());
         Map<GraphPropertyEnum, Object> propertiesNotToMatch = new EnumMap<>(GraphPropertyEnum.class);
         propertiesNotToMatch.put(GraphPropertyEnum.IS_DELETED, true);
-        Either<List<GraphVertex>, JanusGraphOperationStatus> byCriteria = janusGraphDao.getByCriteria(VertexTypeEnum.TOPOLOGY_TEMPLATE, propertiesToMatch, propertiesNotToMatch, JsonParseFlagEnum.ParseAll);
+        Either<List<GraphVertex>, JanusGraphOperationStatus> byCriteria = janusGraphDao
+            .getByCriteria(VertexTypeEnum.TOPOLOGY_TEMPLATE, propertiesToMatch, propertiesNotToMatch,
+                JsonParseFlagEnum.ParseAll);
         return byCriteria.either(this::proceed, this::handleError);
     }
 
@@ -90,22 +93,28 @@ public class SdcDeploymentArtTimeOutMigration extends InstanceMigrationBase impl
         StorageOperationStatus status = StorageOperationStatus.NOT_FOUND;
         GraphVertex containerV = getVertexById(containerVorig.getUniqueId());
         try {
-            Either<GraphVertex, JanusGraphOperationStatus> childVertex = janusGraphDao.getChildVertex(containerV, EdgeLabelEnum.INST_DEPLOYMENT_ARTIFACTS, JsonParseFlagEnum.ParseAll);
+            Either<GraphVertex, JanusGraphOperationStatus> childVertex = janusGraphDao
+                .getChildVertex(containerV, EdgeLabelEnum.INST_DEPLOYMENT_ARTIFACTS, JsonParseFlagEnum.ParseAll);
             GraphVertex instDeployArt = childVertex.left().value();
-            Collection<MapArtifactDataDefinition> values = (Collection<MapArtifactDataDefinition>) instDeployArt.getJson().values();
-                List<ArtifactDataDefinition> artifactDataDefinitionsList = values.stream().map(f -> f.getMapToscaDataDefinition().values()).flatMap(f -> f.stream().filter(isRelevantArtifact())).collect(Collectors.toList());
-                artifactDataDefinitionsList.forEach(t -> t.setTimeout(defaultTimeOut));
-                status =  updateVertexAndCommit(instDeployArt);
+            Collection<MapArtifactDataDefinition> values = (Collection<MapArtifactDataDefinition>) instDeployArt
+                .getJson().values();
+            List<ArtifactDataDefinition> artifactDataDefinitionsList = values.stream()
+                .map(f -> f.getMapToscaDataDefinition().values()).flatMap(f -> f.stream().filter(isRelevantArtifact()))
+                .collect(Collectors.toList());
+            artifactDataDefinitionsList.forEach(t -> t.setTimeout(defaultTimeOut));
+            status = updateVertexAndCommit(instDeployArt);
 
         } catch (NullPointerException e) {
-            log.error("Null Pointer Exception occurred - this mean we have zombie vertex, migration task will continue anyway", e);
+            log.error(
+                "Null Pointer Exception occurred - this mean we have zombie vertex, migration task will continue anyway",
+                e);
             status = StorageOperationStatus.OK;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             //it is happy flow as well
             log.error("Exception occurred:", e);
-            log.error("Migration task will continue anyway, please find below vertex details related to this exception", e);
-            if (containerV != null){
+            log.error("Migration task will continue anyway, please find below vertex details related to this exception",
+                e);
+            if (containerV != null) {
                 log.error("containerV.getUniqueId() {} ---> ", containerV.getUniqueId());
             }
 
@@ -113,14 +122,13 @@ public class SdcDeploymentArtTimeOutMigration extends InstanceMigrationBase impl
         } finally {
             if (status != StorageOperationStatus.OK) {
                 janusGraphDao.rollback();
-                log.info("failed to update vertex ID {} ",  containerV.getUniqueId());
+                log.info("failed to update vertex ID {} ", containerV.getUniqueId());
                 if (status == StorageOperationStatus.NOT_FOUND) {
                     //it is happy flow as well
                     status = StorageOperationStatus.OK;
                 }
-            }
-            else{
-                log.info("vertex ID {} successfully updated",  containerV.getUniqueId());
+            } else {
+                log.info("vertex ID {} successfully updated", containerV.getUniqueId());
             }
 
         }
@@ -129,8 +137,10 @@ public class SdcDeploymentArtTimeOutMigration extends InstanceMigrationBase impl
 
     private static Predicate<ArtifactDataDefinition> isRelevantArtifact() {
 
-        return p -> ((p.getArtifactType().equals(ArtifactTypeEnum.HEAT.getType()) || p.getArtifactType().equals(ArtifactTypeEnum.HEAT_VOL.getType()) || p.getArtifactType().equals(ArtifactTypeEnum.HEAT_NET.getType()))
-                && p.getTimeout() != defaultTimeOut);
+        return p -> ((p.getArtifactType().equals(ArtifactTypeEnum.HEAT.getType()) || p.getArtifactType()
+            .equals(ArtifactTypeEnum.HEAT_VOL.getType()) || p.getArtifactType()
+            .equals(ArtifactTypeEnum.HEAT_NET.getType()))
+            && p.getTimeout() != defaultTimeOut);
 
     }
 
