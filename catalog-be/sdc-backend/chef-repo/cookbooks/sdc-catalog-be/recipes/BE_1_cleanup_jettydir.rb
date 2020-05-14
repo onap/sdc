fcbf6d2541c6d033e4979784ce2373f0c5db3932
vasraz <vasyl.razinkov@est.tech>
2020-05-14 16:09:22 +0100

    Support setting SDC-BE configuration at deployment time
    Change-Id: Id1ebcb01d81b8e0cd681dcb304aa77bb3248d69c
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Issue-ID: SDC-3076

--- catalog-be/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_1_cleanup_jettydir.rb
+++ catalog-be/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_1_cleanup_jettydir.rb
@@ -1,24 +1,29 @@
-directory "BE_tempdir_creation" do
-  path "#{ENV['JETTY_BASE']}/temp"
-  owner 'jetty'
-  group 'jetty'
-  mode '0755'
-  action :create
+unless Dir.exist? "#{ENV['JETTY_BASE']}/temp"
+  directory "BE_tempdir_creation" do
+    path "#{ENV['JETTY_BASE']}/temp"
+    owner 'jetty'
+    group 'jetty'
+    mode '0755'
+    action :create
+  end
 end
 
-directory "BE_create_config_dir" do
-  path "#{ENV['JETTY_BASE']}/config"
-  owner 'jetty'
-  group 'jetty'
-  mode '0755'
-  action :create
+unless Dir.exist? "#{ENV['JETTY_BASE']}/config"
+  directory "BE_create_config_dir" do
+    path "#{ENV['JETTY_BASE']}/config"
+    owner 'jetty'
+    group 'jetty'
+    mode '0755'
+    action :create
+  end
 end
 
-
-directory "BE_create_catalog-be" do
-  path "#{ENV['JETTY_BASE']}/config/catalog-be"
-  owner 'jetty'
-  group 'jetty'
-  mode '0755'
-  action :create
+unless Dir.exist? "#{ENV['JETTY_BASE']}/config/catalog-be"
+  directory "BE_create_catalog-be" do
+    path "#{ENV['JETTY_BASE']}/config/catalog-be"
+    owner 'jetty'
+    group 'jetty'
+    mode '0755'
+    action :create
+  end
 end
