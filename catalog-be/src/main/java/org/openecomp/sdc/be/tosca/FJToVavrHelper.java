d1ede4ed8f516b373a6e0fc0e146df719b002f92
Francis Toth <francis.toth@yoppworks.com>
2020-05-20 12:19:26 -0400

    Refactor CsarUtils::writeComponentInterface
    This commit aims to decouple the zip file writing logic from the CsarUtils::writeComponentInterface function. This should provide a higher level of testability and maintenance thanks to inversion of control. An extra helper has also been added to help the migration from the FJ library to VAVR.
    
    Signed-off-by: Francis Toth <francis.toth@yoppworks.com>
    Change-Id: I4f2df4749cae3082906a80acc39045ac8887ec39
    Issue-ID: SDC-2812

---
+++ catalog-be/src/main/java/org/openecomp/sdc/be/tosca/FJToVavrHelper.java
@@ -0,0 +1,48 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdc.be.tosca;
+
+import io.vavr.control.Try;
+import java.util.function.Function;
+
+/**
+ * Helper class providing facilities for migrating from FJ to VAVR
+ */
+public final class FJToVavrHelper {
+
+    private FJToVavrHelper() {}
+
+    public interface Try0 {
+        /**
+         * Converts a {@link fj.data.Either} to a {@link io.vavr.control.Try}
+         *
+         * @param e
+         * @param onError
+         * @return
+         */
+        static <L, R> Try<L> fromEither(fj.data.Either<L, R> e, Function<R, Throwable> onError) {
+            return e.either(
+                Try::success,
+                r -> Try.failure(onError.apply(r))
+            );
+        }
+    }
+}
