01630f7047201e028e81d402bfdda31173b85fe0
vasraz <vasyl.razinkov@est.tech>
2020-05-28 11:44:09 +0100

    Support setting SDC-BE configuration at deployment time
    Change-Id: I4b384bc911630d6041a0c5c235d3aaf3ac148090
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Issue-ID: SDC-3076

--- catalog-be/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_7_logback.rb
+++ catalog-be/sdc-backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_7_logback.rb
@@ -1,7 +1,8 @@
 cookbook_file "#{ENV['JETTY_BASE']}/config/catalog-be/logback.xml" do
   source "logback.xml"
-  mode 0755
+  mode 0644
   owner "jetty"
   group "jetty"
+  action :create_if_missing
 end
  
