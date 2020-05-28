455e37bf6e595119a184d0043a2896bc46a82675
Pratik Raj <rajpratik71@gmail.com>
2020-05-28 11:06:41 +0530

    Issue-ID: SDC-3089 optimization in git clone using --depth
    optimize the git clone using --depth flag in term of size of clone
    and also in term's of time taken to fetch the files and commit history
    of whole repository .
    
    More detail can be found at blog
    https://www.atlassian.com/git/tutorials/big-repositories
    
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>
    Change-Id: Ib806bf0c172bd304abe9a138e2f85fcf14e07675

--- sdc-os-chef/scripts/k8s/build_nsenter_exec.sh
+++ sdc-os-chef/scripts/k8s/build_nsenter_exec.sh
@@ -9,7 +9,7 @@ docker run --name nsenter -it ubuntu:14.04 bash
 apt-get update
 apt-get install git build-essential libncurses5-dev libslang2-dev gettext zlib1g-dev libselinux1-dev debhelper lsb-release pkg-config po-debconf autoconf automake autopoint libtool bison
 
-git clone git://git.kernel.org/pub/scm/utils/util-linux/util-linux.git util-linux
+git clone --depth 1 git://git.kernel.org/pub/scm/utils/util-linux/util-linux.git util-linux
 cd util-linux/
 
 ./autogen.sh
