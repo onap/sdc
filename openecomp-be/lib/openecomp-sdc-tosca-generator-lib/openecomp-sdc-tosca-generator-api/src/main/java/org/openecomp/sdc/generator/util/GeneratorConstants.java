ada10a6e126e7698b17f4a70de631f4478a10dca
ChrisC <christophe.closset@intl.att.com>
2020-06-15 11:28:14 +0200

    Fix Sonar issues
    Hide implicit public constructor in utility class
    
    Issue-ID: SDC-3116
    Signed-off-by: ChrisC <christophe.closset@intl.att.com>
    Change-Id: I4edd2ac37aab2fd587b5d46391dd669181385748

--- openecomp-be/lib/openecomp-sdc-tosca-generator-lib/openecomp-sdc-tosca-generator-api/src/main/java/org/openecomp/sdc/generator/util/GeneratorConstants.java
+++ openecomp-be/lib/openecomp-sdc-tosca-generator-lib/openecomp-sdc-tosca-generator-api/src/main/java/org/openecomp/sdc/generator/util/GeneratorConstants.java
@@ -20,27 +20,26 @@
 
 package org.openecomp.sdc.generator.util;
 
-public class GeneratorConstants {
-  public static final String ALLOWED_FLAVORS_PROPERTY = "allowed_flavors";
-  public static final String IMAGES_PROPERTY = "images";
-  public static final String RELEASE_VENDOR = "releaseVendor";
-  public static final String VNF_CONFIG_NODE_TEMPLATE_ID_SUFFIX = "_VNF_Configuration";
-  public static final String VNF_NODE_TEMPLATE_ID_SUFFIX = "_VNF";
-  public static final String PORT_TYPE_INTERNAL_NODE_TEMPLATE_SUFFIX = "_lan";
-  public static final String PORT_TYPE_EXTERNAL_NODE_TEMPLATE_SUFFIX = "_wan";
-  public static final String PORT_NODE_TEMPLATE_ID_SUFFIX = "_port";
+public final class GeneratorConstants {
+    public static final String ALLOWED_FLAVORS_PROPERTY = "allowed_flavors";
+    public static final String IMAGES_PROPERTY = "images";
+    public static final String RELEASE_VENDOR = "releaseVendor";
+    public static final String VNF_CONFIG_NODE_TEMPLATE_ID_SUFFIX = "_VNF_Configuration";
+    public static final String VNF_NODE_TEMPLATE_ID_SUFFIX = "_VNF";
+    public static final String PORT_TYPE_INTERNAL_NODE_TEMPLATE_SUFFIX = "_lan";
+    public static final String PORT_TYPE_EXTERNAL_NODE_TEMPLATE_SUFFIX = "_wan";
+    public static final String PORT_NODE_TEMPLATE_ID_SUFFIX = "_port";
+    public static final String TOSCA_SERVICE_TEMPLATE_FILE_NAME_SUFFIX = "ServiceTemplate.yaml";
 
-  public static final String TOSCA_SERVICE_TEMPLATE_FILE_NAME_SUFFIX = "ServiceTemplate.yaml";
+    //Manual VSP propeties
+    public static final String NUM_CPUS = "num_cpus";
+    public static final String DISK_SIZE = "disk_size";
+    public static final String MEM_SIZE = "mem_size";
+    public static final String NUM_CPUS_PROP_DESC_PREFIX  = "Number of cpu for ";
+    public static final String DISK_SIZE_PROP_DESC_PREFIX  = "Disk size for ";
+    public static final String MEM_SIZE_PROP_DESC_PREFIX  = "Memory size for ";
 
-  //Manual VSP propeties
-  public static final String NUM_CPUS = "num_cpus";
-  public static final String DISK_SIZE = "disk_size";
-  public static final String MEM_SIZE = "mem_size";
-
-  public static final String NUM_CPUS_PROP_DESC_PREFIX  = "Number of cpu for ";
-  public static final String DISK_SIZE_PROP_DESC_PREFIX  = "Disk size for ";
-  public static final String MEM_SIZE_PROP_DESC_PREFIX  = "Memory size for ";
-
-
-
+    // prevent utility class instantiation
+    private GeneratorConstants() {
+    }
 }
