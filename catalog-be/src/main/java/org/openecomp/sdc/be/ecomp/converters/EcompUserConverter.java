19364d0f1cf587675f439c5fe77502f0a4c771c9
sebdet <sebastien.determe@intl.att.com>
2020-06-24 14:10:34 +0200

    Fix sonar issues
    Fix sonar/checkstyle issues in sdc code
    
    Issue-ID: SDC-3158
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    Change-Id: I33638b39d39b7e918437cb9f3e0f15d52fa92218

--- catalog-be/src/main/java/org/openecomp/sdc/be/ecomp/converters/EcompUserConverter.java
+++ catalog-be/src/main/java/org/openecomp/sdc/be/ecomp/converters/EcompUserConverter.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,16 +21,15 @@
 package org.openecomp.sdc.be.ecomp.converters;
 
 import fj.data.Either;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import org.onap.portalsdk.core.onboarding.exception.PortalAPIException;
 import org.onap.portalsdk.core.restful.domain.EcompRole;
 import org.onap.portalsdk.core.restful.domain.EcompUser;
 import org.openecomp.sdc.be.dao.utils.UserStatusEnum;
 import org.openecomp.sdc.be.model.User;
 import org.openecomp.sdc.be.user.Role;
-
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 
 public final class EcompUserConverter {
 
@@ -60,7 +59,7 @@ public final class EcompUserConverter {
         for (Role role : Role.values()) {
             if (role.name().equals(asdcUser.getRole()) || role.toString().equals(asdcUser.getRole())) {
                 convertedRole.setName(role.name());
-                convertedRole.setId(new Long(role.ordinal()));
+                convertedRole.setId((long) role.ordinal());
                 break;
             }
         }
