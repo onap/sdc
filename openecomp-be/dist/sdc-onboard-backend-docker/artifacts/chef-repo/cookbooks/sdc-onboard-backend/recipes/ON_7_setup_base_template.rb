39844ef9a786cf0ccb4102d6b19d521f74682939
sebdet <sebastien.determe@intl.att.com>
2020-09-21 22:13:05 +0200

    Use integration-java11 Dockerfile
    Use onap/integration-java11:7.1.0 as a source for the images requiring JDK 11 in SDC
    
    Issue-ID: SDC-3298
    Change-Id: Ifee3d2942dcb9c078f2ebb686bdabc0ecca857b3
    Signed-off-by: xuegao <xue.gao@intl.att.com>

--- openecomp-be/dist/sdc-onboard-backend-docker/artifacts/chef-repo/cookbooks/sdc-onboard-backend/recipes/ON_7_setup_base_template.rb
+++ openecomp-be/dist/sdc-onboard-backend-docker/artifacts/chef-repo/cookbooks/sdc-onboard-backend/recipes/ON_7_setup_base_template.rb
@@ -1,14 +1,14 @@
 cookbook_file "#{ENV['JETTY_BASE']}/resources/base_template.yaml" do
   source "base_template.yaml"
   mode 0644
-  owner "jetty"
-  group "jetty"
+  owner "#{ENV['JETTY_USER']}"
+  group "#{ENV['JETTY_GROUP']}"
 end
  
 cookbook_file "#{ENV['JETTY_BASE']}/resources/base_template.env" do
   source "base_template.env"
   mode 0644
-  owner "jetty"
-  group "jetty"
+  owner "#{ENV['JETTY_USER']}"
+  group "#{ENV['JETTY_GROUP']}"
 end
  
