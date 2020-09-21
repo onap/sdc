15f4a1b70d3cf8ea39f17db290ed3710f2eea930
sebdet <sebastien.determe@intl.att.com>
2020-09-21 22:13:05 +0200

    Use integration-java11 Dockerfile
    Use onap/integration-java11:7.1.0 as a source for the images requiring JDK 11 in SDC
    
    Issue-ID: SDC-3298
    Change-Id: Ifee3d2942dcb9c078f2ebb686bdabc0ecca857b3
    Signed-off-by: xuegao <xue.gao@intl.att.com>

--- utils/webseal-simulator/sdc-simulator/startup.sh
+++ utils/webseal-simulator/sdc-simulator/startup.sh
@@ -1,24 +1,19 @@
 #!/bin/sh
 
-cd /root/chef-solo
-chef-solo -c solo.rb -E ${ENVNAME}
-rc=$?
-if [ $rc -ne 0 ]; then
-    echo "Chef exaction failed."
-    exit $rc;
-fi
-
-JAVA_OPTIONS=" ${JAVA_OPTIONS} \
+JAVA_OPTIONS=" $JAVA_OPTIONS \
 		-Xdebug -agentlib:jdwp=transport=dt_socket,address=5000,server=y,suspend=n -Xmx128m -Xms128m -Xss1m \
-                -Dconfig.home=${JETTY_BASE}/config/sdc-simulator \
-                -Dlog.home=${JETTY_BASE}/logs \
-                -Dlogback.configurationFile=${JETTY_BASE}/config/sdc-simulator/logback.xml \
-                -Djavax.net.ssl.trustStore=${JETTY_BASE}/etc/org.onap.sdc.trust.jks \
+                -Dconfig.home=$JETTY_BASE/config/sdc-simulator \
+                -Dlog.home=$JETTY_BASE/logs \
+                -Dlogback.configurationFile=$JETTY_BASE/config/sdc-simulator/logback.xml \
+                -Djavax.net.ssl.trustStore=$JETTY_BASE/etc/org.onap.sdc.trust.jks \
                 -Djavax.net.ssl.trustStorePassword=z+KEj;t+,KN^iimSiS89e#p0 \
-                -Djetty.console-capture.dir=${JETTY_BASE}/logs"
+                -Djetty.console-capture.dir=$JETTY_BASE/logs"
 
 
-cd /var/lib/jetty
+cd $JETTY_BASE/chef-solo
+chef-solo -c solo.rb -E ${ENVNAME}
 
-java $JAVA_OPTIONS -jar "$JETTY_HOME/start.jar"
+cd $JETTY_HOME
+
+java $JAVA_OPTIONS -jar "${JETTY_HOME}/start.jar"
 
