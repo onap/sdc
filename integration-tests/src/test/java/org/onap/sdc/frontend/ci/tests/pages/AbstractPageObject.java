290420e92809093050a493f34be3cc214459162f
sebdet <sebastien.determe@intl.att.com>
2020-10-20 11:02:53 +0200

    Increase timeout for Selenium tests
    Increase 2 timeouts that seems to give issue on the LF infra.
    
    Issue-ID: SDC-3324
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    Change-Id: Ibc7a9275c1fba7af35bfbf0b39101f2d9f540871

--- integration-tests/src/test/java/org/onap/sdc/frontend/ci/tests/pages/AbstractPageObject.java
+++ integration-tests/src/test/java/org/onap/sdc/frontend/ci/tests/pages/AbstractPageObject.java
@@ -37,7 +37,7 @@ public abstract class AbstractPageObject implements PageObject {
 
     public AbstractPageObject(final WebDriver webDriver) {
         this.webDriver = webDriver;
-        timeoutInSeconds = 10;
+        timeoutInSeconds = 20;
     }
 
     /**
