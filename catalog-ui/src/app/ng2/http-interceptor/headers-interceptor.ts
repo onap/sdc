4d5658a33023eec3c521993effeeff526dffbc6b
xuegao <xue.gao@intl.att.com>
2020-12-09 16:01:22 +0100

    Add basic auth
    Adding basic auth for SDC apis.
    
    Issue-ID: OJSI-90
    Signed-off-by: xuegao <xue.gao@intl.att.com>
    Change-Id: Ie84e6bab8d8526f7f4d21a36bba52d8fe9abebbb
    Signed-off-by: xuegao <xue.gao@intl.att.com>

--- catalog-ui/src/app/ng2/http-interceptor/headers-interceptor.ts
+++ catalog-ui/src/app/ng2/http-interceptor/headers-interceptor.ts
@@ -5,20 +5,26 @@ import { ButtonType } from 'onap-ui-angular/dist/common';
 import { Observable } from 'rxjs/Observable';
 import { ServerErrorResponse } from '../../models/server-error-response';
 import { Cookie2Service } from '../services/cookie.service';
+import { CredentialService } from "../services/credential.service";
 import { HttpHelperService } from '../services/http-hepler.service';
 import { TranslateService } from '../shared/translator/translate.service';
 
 @Injectable()
 export class HeadersInterceptor implements HttpInterceptor {
 
-    constructor(private injector: Injector, private cookieService: Cookie2Service, private httpHelperService: HttpHelperService) {}
+    constructor(private injector: Injector, private cookieService: Cookie2Service, private credentialService: CredentialService, private httpHelperService: HttpHelperService) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
         let authReq = req.clone({ headers: req.headers.set(this.cookieService.getUserIdSuffix(), this.cookieService.getUserId())
             .set('Content-Type', 'application/json; charset=UTF-8')
             .set(this.cookieService.getUserIdSuffix(), this.cookieService.getUserId())
             .set(this.cookieService.getUserIdSuffix(), this.cookieService.getUserId())
         });
+
+        if (this.credentialService.basicAuthEnabled()) {
+            authReq = authReq.clone({ headers: authReq.headers.set('Authorization', this.credentialService.getBasicAuth())});
+        }
 
         const uuidValue = this.httpHelperService.getUuidValue(authReq.url);
         if (uuidValue !== '') {
