ec7bc5a7f57e0d864cd64930dbece2f6b94ae4de
Maciej Malewski <maciej.malewski@nokia.com>
2020-12-10 14:38:00 +0100

    Integration tests for validating PM_Dictionary.
    Checking compliance content of the PM_Dictionary file against it's schema.
    
    Issue-ID: SDC-3390
    Change-Id: I8e20f977c7d6838005bc84fc6c7c8ab197152a53
    Signed-off-by: Maciej Malewski <maciej.malewski@nokia.com>

---
+++ integration-tests/src/test/java/org/onap/sdc/backend/ci/tests/validation/pmdictionary/CsarValidationTest.java
@@ -0,0 +1,73 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.sdc.backend.ci.tests.validation.pmdictionary;
+
+import org.junit.jupiter.api.Test;
+import org.openecomp.core.utilities.file.FileContentHandler;
+import org.openecomp.sdc.datatypes.error.ErrorLevel;
+import org.openecomp.sdc.datatypes.error.ErrorMessage;
+import org.openecomp.sdc.vendorsoftwareproduct.exception.OnboardPackageException;
+import org.openecomp.sdc.vendorsoftwareproduct.impl.orchestration.csar.validation.ValidatorFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class CsarValidationTest {
+
+    @Test
+    public void shouldNotReturnErrors_whenPnfCsarIsValid() throws OnboardPackageException, IOException {
+        //given
+        FileContentHandler pnfFileContent = CsarLoader.load("validPnfCompliantWithSOL004.csar", "/Files/PNFs/validation/pmdictionary/validPnfCompliantWithSOL004.csar");
+
+        //when
+        Map<String, List<ErrorMessage>> errorsMap = ValidatorFactory.getValidator(pnfFileContent).validateContent(pnfFileContent);
+
+        //then
+        assertThat(errorsMap, is(anEmptyMap()));
+    }
+
+    @Test
+    public void shouldReturnError_whenPMDictionaryContentIsNotCompliantWithSchema() throws OnboardPackageException, IOException {
+        //given
+        String expectedErrorMessage = "Document number: 1, Path: /pmMetaData/, Message: Key not found: pmHeader";
+        FileContentHandler pnfFileContent = CsarLoader.load("invalidPnfCompliantWithSOL004.csar", "/Files/PNFs/validation/pmdictionary/invalidPnfCompliantWithSOL004.csar");
+
+        //when
+        Map<String, List<ErrorMessage>> errorMap = ValidatorFactory.getValidator(pnfFileContent).validateContent(pnfFileContent);
+        List<ErrorMessage> errorList = errorMap.get("uploadFile");
+
+        //then
+        assertThat(errorList, is(not(empty())));
+        assertThat(getActualErrorMessage(errorList), is(equalTo(expectedErrorMessage)));
+        assertThat(getActualErrorLevel(errorList), is(ErrorLevel.ERROR));
+    }
+
+    private String getActualErrorMessage(List<ErrorMessage> errorList) {
+        return errorList.get(0).getMessage();
+    }
+
+    private ErrorLevel getActualErrorLevel(List<ErrorMessage> errorList) {
+        return errorList.get(0).getLevel();
+    }
+}
