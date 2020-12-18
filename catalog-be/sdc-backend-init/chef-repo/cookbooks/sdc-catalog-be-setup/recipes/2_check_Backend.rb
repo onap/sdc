8641b065f07fd12b16ce697a5889edf8703c3c73
xuegao <xue.gao@intl.att.com>
2020-12-18 10:37:23 +0100

    Add basic auth header
    Add basic auth header for sdc-backend-init python scripts.
    
    Issue-ID: OJSI-273
    Signed-off-by: xuegao <xue.gao@intl.att.com>
    Change-Id: I3559d5792509db0f65b202a731545083c7c91c96

--- catalog-be/sdc-backend-init/chef-repo/cookbooks/sdc-catalog-be-setup/recipes/2_check_Backend.rb
+++ catalog-be/sdc-backend-init/chef-repo/cookbooks/sdc-catalog-be-setup/recipes/2_check_Backend.rb
@@ -1,3 +1,4 @@
+require 'base64'
 if node['disableHttp']
   protocol = "https"
   https_flag = "--https"
@@ -8,9 +9,21 @@ else
   be_port = node['BE']['http_port']
 end
 
+if node['basic_auth']
+  basic_auth_enabled = node['basic_auth']['enabled']
+  basic_auth_user = node['basic_auth']['user_name']
+  basic_auth_pass = node['basic_auth']['user_pass']
+  if basic_auth_enabled
+    basic_auth_config = "--header " + Base64.encode64(basic_auth_user + ":" + basic_auth_pass)
+  else
+    # set default user configuration file
+    basic_auth_config = ""
+  end
+end
+
 bash "executing-check_backend_health" do
    code <<-EOH
-     sdccheckbackend -i #{node['Nodes']['BE']} -p #{be_port} #{https_flag}
+     sdccheckbackend -i #{node['Nodes']['BE']} -p #{be_port} #{basic_auth_config} #{https_flag}
      rc=$?
      if [[ $rc != 0 ]]; then exit $rc; fi
    EOH
