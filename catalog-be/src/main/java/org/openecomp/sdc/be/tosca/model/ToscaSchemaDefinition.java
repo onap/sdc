3b87ab94a2729d971c8e7f1220767031119300ce
andre.schmid <andre.schmid@est.tech>
2021-02-02 19:18:22 +0000

    Improve import and export VFC TOSCA attributes
    Improve the import and export VFC TOSCA attributes, addressing the
    following concerns:
    - brings the import and export logic very close to the properties logic,
    as they are very similar structures
    - fix import/export default values for complex types and list of simple
    or complex types
    - fix export of unnecessary/empty attribute entries
    - creation of attributes during the initialization (base types)
    - inheritance of parent attributes
    
    Change-Id: Ic733e3455fc256595b5c2b1f48e19a13be27b5cc
    Issue-ID: SDC-3466
    Signed-off-by: andre.schmid <andre.schmid@est.tech>

--- catalog-be/src/main/java/org/openecomp/sdc/be/tosca/model/ToscaSchemaDefinition.java
+++ catalog-be/src/main/java/org/openecomp/sdc/be/tosca/model/ToscaSchemaDefinition.java
@@ -20,24 +20,12 @@
 
 package org.openecomp.sdc.be.tosca.model;
 
-public class EntrySchema {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ToscaSchemaDefinition {
     private String type;
     private String description;
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
 }
