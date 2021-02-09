6c04b1359c84633486b589abfdf2689514f2dee8
Jan Malkiewicz <jan.malkiewicz@nokia.com>
2021-02-09 08:39:23 +0100

    Add validation of manifest for helm packages.
    For ONAP native zip packages added validation of manifest file.
    HELM package is only valid if:
     * isBase flag is set to 'true' for exactly one helm entry
     * isBase flag is present for all helm entries
    Zip package is considered to be a helm package if it contains HELM entries but does not contain any HEAT entries (however it may contains entries of other types).
    
    Refactored method OnboardingPackageProcessor.processPackage():
     * simplified logic
     * enhanced exception handling
    
    Issue-ID: SDC-3185
    Signed-off-by: Jan Malkiewicz <jan.malkiewicz@nokia.com>
    Change-Id: Ica3b1f1504ce4fc3a671c4b8fa8de2bf0236bd77

--- openecomp-be/lib/openecomp-common-lib/src/main/java/org/openecomp/sdc/common/errors/Messages.java
+++ openecomp-be/lib/openecomp-common-lib/src/main/java/org/openecomp/sdc/common/errors/Messages.java
@@ -8,7 +8,7 @@
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,OnboardingPackageProcessorTest
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -33,6 +33,7 @@ public enum Messages {
     PACKAGE_PROCESS_INTERNAL_PACKAGE_ERROR("Could not process internal package '%s'"),
     PACKAGE_INVALID_ERROR("Invalid package content '%s'"),
     PACKAGE_MISSING_INTERNAL_PACKAGE("Missing expected internal package"),
+    COULD_NOT_READ_MANIFEST_FILE("Could not read manifest file: %s [%s]"),
     INVALID_ZIP_FILE("Invalid zip file"),
     INVALID_CSAR_FILE("Invalid csar file"),
     CSAR_FILE_NOT_FOUND("Each CSAR file must contain %s file."),
