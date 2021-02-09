8897722148fd0e5da815b312cb30832cc9fc3637
Jan Malkiewicz <jan.malkiewicz@nokia.com>
2021-02-09 14:00:11 +0100

    Fix maven build
    Fix problem with loading resources by jersey.
    
    Issue-ID: SDC-3185
    Signed-off-by: Jan Malkiewicz <jan.malkiewicz@nokia.com>
    Change-Id: Ibefa76b6b120d7befbf4e08c9f9463b156e7a6d5

--- catalog-be/src/main/java/org/openecomp/sdc/be/externalapi/servlet/CrudExternalServlet.java
+++ catalog-be/src/main/java/org/openecomp/sdc/be/externalapi/servlet/CrudExternalServlet.java
@@ -39,6 +39,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.HeaderParam;
@@ -47,8 +48,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
 import org.apache.commons.lang3.StringUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -102,7 +106,8 @@ import org.springframework.stereotype.Controller;
 @Tag(name = "SDC External APIs")
 @Server(url = "/sdc")
 @Controller
-public class CrudExternalServlet extends AbstractValidationsServlet {
+@Singleton
+public class CrudExternalServlet extends AbstractValidationsServlet implements Feature {
 
     @Context
     private HttpServletRequest request;
@@ -644,4 +649,8 @@ public class CrudExternalServlet extends AbstractValidationsServlet {
         }
     }
 
+    @Override
+    public boolean configure(FeatureContext context) {
+        return true;
+    }
 }
