b35dcfcf91f3e791fa6c52d4c1387e0a7bd79ed7
vasraz <vasyl.razinkov@est.tech>
2021-02-18 14:59:02 +0000

    Fix locally failing TCs in catalog-be
    Migrate to Junit 5 TCs that extends JerseyTest
    
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: I4c81e70836ee5e634291d74cd1773d221dce3702
    Issue-ID: SDC-3377

--- catalog-be/src/test/java/org/openecomp/sdc/be/externalapi/servlet/AssetsDataServletTest.java
+++ catalog-be/src/test/java/org/openecomp/sdc/be/externalapi/servlet/AssetsDataServletTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,19 @@
 package org.openecomp.sdc.be.externalapi.servlet;
 
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
 import fj.data.Either;
+import java.util.Arrays;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import org.apache.http.HttpStatus;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -29,8 +41,10 @@ import org.glassfish.jersey.test.JerseyTest;
 import org.glassfish.jersey.test.TestProperties;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.stubbing.Answer;
 import org.openecomp.sdc.be.components.impl.ComponentInstanceBusinessLogic;
@@ -62,20 +76,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.Arrays;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-
-public class AssetsDataServletTest extends JerseyTest {
+class AssetsDataServletTest extends JerseyTest {
 
     private static final HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
     private static final HttpSession session = Mockito.mock(HttpSession.class);
@@ -95,11 +96,11 @@ public class AssetsDataServletTest extends JerseyTest {
     private static final ResourceAssetMetadata resourceAssetMetadata = new ResourceAssetMetadata();
     private static final LifecycleBusinessLogic lifecycleBusinessLogic = Mockito.mock(LifecycleBusinessLogic.class);
     private static final UserBusinessLogic userBusinessLogic = Mockito.mock(UserBusinessLogic.class);
-    private static final ComponentInstanceBusinessLogic componentInstanceBusinessLogic = Mockito.mock(ComponentInstanceBusinessLogic.class);
+    private static final ComponentInstanceBusinessLogic componentInstanceBusinessLogic = Mockito
+        .mock(ComponentInstanceBusinessLogic.class);
 
 
-
-    @BeforeClass
+    @BeforeAll
     public static void setup() {
         ExternalConfiguration.setAppName("catalog-be");
         when(request.getSession()).thenReturn(session);
@@ -108,7 +109,8 @@ public class AssetsDataServletTest extends JerseyTest {
         when(request.getRequestURL()).thenReturn(new StringBuffer("sdc/v1/catalog/resources"));
 
         when(session.getServletContext()).thenReturn(servletContext);
-        when(servletContext.getAttribute(Constants.WEB_APPLICATION_CONTEXT_WRAPPER_ATTR)).thenReturn(webAppContextWrapper);
+        when(servletContext.getAttribute(Constants.WEB_APPLICATION_CONTEXT_WRAPPER_ATTR))
+            .thenReturn(webAppContextWrapper);
         when(webAppContextWrapper.getWebAppContext(servletContext)).thenReturn(webApplicationContext);
 
         when(webApplicationContext.getBean(ServletUtils.class)).thenReturn(servletUtils);
@@ -119,24 +121,31 @@ public class AssetsDataServletTest extends JerseyTest {
 
         when(resource.getName()).thenReturn("MockVFCMT");
         when(resource.getSystemName()).thenReturn("mockvfcmt");
-        Either<Resource, ResponseFormat>  eitherRet = Either.left(resource);
-        when(componentsUtils.convertJsonToObjectUsingObjectMapper(Mockito.any(), Mockito.any(), Mockito.eq(Resource.class), Mockito.any(), Mockito.eq(ComponentTypeEnum.RESOURCE))).thenReturn(eitherRet);
+        Either<Resource, ResponseFormat> eitherRet = Either.left(resource);
+        when(componentsUtils
+            .convertJsonToObjectUsingObjectMapper(Mockito.any(), Mockito.any(), Mockito.eq(Resource.class),
+                Mockito.any(), Mockito.eq(ComponentTypeEnum.RESOURCE))).thenReturn(eitherRet);
 
         when(webApplicationContext.getBean(ResourceImportManager.class)).thenReturn(resourceImportManager);
         when(webApplicationContext.getBean(ElementBusinessLogic.class)).thenReturn(elementBusinessLogic);
         when(categoryDefinition.getName()).thenReturn("Template");
         when(subCategoryDefinition.getName()).thenReturn("Monitoring Template");
         when(categoryDefinition.getSubcategories()).thenReturn(Arrays.asList(subCategoryDefinition));
-        when(elementBusinessLogic.getAllResourceCategories()).thenReturn(Either.left(Arrays.asList(categoryDefinition)));
-        when(resourceBusinessLogic.createResource(Mockito.eq(resource), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(resource);
+        when(elementBusinessLogic.getAllResourceCategories())
+            .thenReturn(Either.left(Arrays.asList(categoryDefinition)));
+        when(resourceBusinessLogic
+            .createResource(Mockito.eq(resource), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()))
+            .thenReturn(resource);
         when(webApplicationContext.getBean(AssetMetadataConverter.class)).thenReturn(assetMetadataConverter);
         when(request.isUserInRole(anyString())).thenReturn(true);
 
-        Mockito.doReturn(Either.left(resourceAssetMetadata)).when(assetMetadataConverter).convertToSingleAssetMetadata(Mockito.eq(resource), Mockito.anyString(),
+        Mockito.doReturn(Either.left(resourceAssetMetadata)).when(assetMetadataConverter)
+            .convertToSingleAssetMetadata(Mockito.eq(resource), Mockito.anyString(),
                 Mockito.eq(true));
 
         String appConfigDir = "src/test/resources/config/catalog-be";
-        ConfigurationSource configurationSource = new FSConfigurationSource(ExternalConfiguration.getChangeListener(), appConfigDir);
+        ConfigurationSource configurationSource = new FSConfigurationSource(ExternalConfiguration.getChangeListener(),
+            appConfigDir);
         ConfigurationManager configurationManager = new ConfigurationManager(configurationSource);
 
         org.openecomp.sdc.be.config.Configuration configuration = new org.openecomp.sdc.be.config.Configuration();
@@ -145,53 +154,66 @@ public class AssetsDataServletTest extends JerseyTest {
         configurationManager.setConfiguration(configuration);
     }
 
-
-    private static void mockResponseFormat() {
-        when(componentsUtils.getResponseFormat(Mockito.any(ActionStatus.class), Mockito.any(String[].class))).thenAnswer((Answer<ResponseFormat>) invocation -> {
-            ResponseFormat ret;
-            final ActionStatus actionStatus = invocation.getArgument(0);
-            switch( actionStatus ){
-            case CREATED :{
-                ret = new ResponseFormat(HttpStatus.SC_CREATED);
-                break;
-            }
-            default :{
-                ret = new ResponseFormat(HttpStatus.SC_INTERNAL_SERVER_ERROR);
-                break;
-            }
-            }
-            return ret;
-        });
+    @BeforeEach
+    public void before() throws Exception {
+        super.setUp();
     }
 
+    @AfterEach
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    private static void mockResponseFormat() {
+        when(componentsUtils.getResponseFormat(Mockito.any(ActionStatus.class), Mockito.any(String[].class)))
+            .thenAnswer((Answer<ResponseFormat>) invocation -> {
+                ResponseFormat ret;
+                final ActionStatus actionStatus = invocation.getArgument(0);
+                switch (actionStatus) {
+                    case CREATED: {
+                        ret = new ResponseFormat(HttpStatus.SC_CREATED);
+                        break;
+                    }
+                    default: {
+                        ret = new ResponseFormat(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+                        break;
+                    }
+                }
+                return ret;
+            });
+    }
 
 
     @Test
-    public void createVfcmtHappyScenario() {
+    void createVfcmtHappyScenario() {
         final JSONObject createRequest = buildCreateJsonRequest();
-        Response response = target().path("/v1/catalog/resources").request(MediaType.APPLICATION_JSON).header(Constants.X_ECOMP_INSTANCE_ID_HEADER, "mockXEcompInstanceId").header(Constants.USER_ID_HEADER, "mockAttID")
-                .post(Entity.json(createRequest.toJSONString()), Response.class);
-        assertEquals(response.getStatus(), HttpStatus.SC_CREATED);
+        Response response = target().path("/v1/catalog/resources").request(MediaType.APPLICATION_JSON)
+            .header(Constants.X_ECOMP_INSTANCE_ID_HEADER, "mockXEcompInstanceId")
+            .header(Constants.USER_ID_HEADER, "mockAttID")
+            .post(Entity.json(createRequest.toJSONString()), Response.class);
+        assertEquals(HttpStatus.SC_CREATED, response.getStatus());
 
     }
+
     private static final String BASIC_CREATE_REQUEST = "{\r\n" +
-            "  \"name\": \"VFCMT_1\",\r\n" +
-            "  \"description\": \"VFCMT Description\",\r\n" +
-            "  \"resourceType\" : \"VFCMT\",\r\n" +
-            "  \"category\": \"Template\",\r\n" +
-            "  \"subcategory\": \"Monitoring Template\",\r\n" +
-            "  \"vendorName\" : \"DCAE\",\r\n" +
-            "  \"vendorRelease\" : \"1.0\",\r\n" +
-            "  \"tags\": [\r\n" +
-            "    \"VFCMT_1\"\r\n" +
-            "  ],\r\n" +
-            "  \"icon\" : \"defaulticon\",\r\n" +
-            "  \"contactId\": \"cs0008\"\r\n" +
-            "}";
+        "  \"name\": \"VFCMT_1\",\r\n" +
+        "  \"description\": \"VFCMT Description\",\r\n" +
+        "  \"resourceType\" : \"VFCMT\",\r\n" +
+        "  \"category\": \"Template\",\r\n" +
+        "  \"subcategory\": \"Monitoring Template\",\r\n" +
+        "  \"vendorName\" : \"DCAE\",\r\n" +
+        "  \"vendorRelease\" : \"1.0\",\r\n" +
+        "  \"tags\": [\r\n" +
+        "    \"VFCMT_1\"\r\n" +
+        "  ],\r\n" +
+        "  \"icon\" : \"defaulticon\",\r\n" +
+        "  \"contactId\": \"cs0008\"\r\n" +
+        "}";
+
     private JSONObject buildCreateJsonRequest() {
 
         JSONParser parser = new JSONParser();
-        return (JSONObject) FunctionalInterfaces.swallowException( () -> parser.parse(BASIC_CREATE_REQUEST));
+        return (JSONObject) FunctionalInterfaces.swallowException(() -> parser.parse(BASIC_CREATE_REQUEST));
 
     }
 
@@ -200,14 +222,16 @@ public class AssetsDataServletTest extends JerseyTest {
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         forceSet(TestProperties.CONTAINER_PORT, "0");
         return new ResourceConfig()
-                .register(new CrudExternalServlet(userBusinessLogic, componentInstanceBusinessLogic,componentsUtils,servletUtils,resourceImportManager, elementBusinessLogic, assetMetadataConverter, lifecycleBusinessLogic, resourceBusinessLogic, serviceBusinessLogic))
-                .register(new AbstractBinder() {
+            .register(new CrudExternalServlet(userBusinessLogic, componentInstanceBusinessLogic, componentsUtils,
+                servletUtils, resourceImportManager, elementBusinessLogic, assetMetadataConverter,
+                lifecycleBusinessLogic, resourceBusinessLogic, serviceBusinessLogic))
+            .register(new AbstractBinder() {
 
-                    @Override
-                    protected void configure() {
-                        bind(request).to(HttpServletRequest.class);
-                    }
-                })
-                .property("contextConfig", context);
+                @Override
+                protected void configure() {
+                    bind(request).to(HttpServletRequest.class);
+                }
+            })
+            .property("contextConfig", context);
     }
 }
