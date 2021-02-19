4820dd452c3b361c70c3cd298d7d14b53c153ca0
vasraz <vasyl.razinkov@est.tech>
2021-02-19 13:13:27 +0000

    Fix locally failing TCs in catalog-be
    Migrate to Junit 5 TCs that extends JerseyTest
    
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: I4c81e70836ee5e634291d74cd1773d221dce3702
    Issue-ID: SDC-3377
    Signed-off-by: aribeiro <anderson.ribeiro@est.tech>

--- catalog-be/src/test/java/org/openecomp/sdc/cucumber/runners/RunTenantIsolationTest.java
+++ catalog-be/src/test/java/org/openecomp/sdc/cucumber/runners/RunTenantIsolationTest.java
@@ -25,7 +25,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 import org.openecomp.sdc.be.components.BeConfDependentTest;
 
-// TODO - investigate NPE
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:cucumber/tenantIsolation.feature", glue = "org.openecomp.sdc.be.components.distribution.engine")
 public class RunTenantIsolationTest extends BeConfDependentTest {
