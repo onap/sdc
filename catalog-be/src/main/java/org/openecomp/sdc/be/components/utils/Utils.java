ab938528c9f67d3ea3daaed98fc718b0ec32f947
vasraz <vasyl.razinkov@est.tech>
2021-03-02 13:34:57 +0000

    Update some dependencies to latest version
    Change-Id: Ife0406cd4bbf0bec2b3d6ba4d0f8d12ba0f2340c
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Issue-ID: SDC-3149

--- catalog-be/src/main/java/org/openecomp/sdc/be/components/utils/Utils.java
+++ catalog-be/src/main/java/org/openecomp/sdc/be/components/utils/Utils.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,36 +20,34 @@
 
 package org.openecomp.sdc.be.components.utils;
 
-import org.apache.tinkerpop.shaded.minlog.Log;
-
-import javax.validation.constraints.NotNull;
 import java.security.SecureRandom;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.tinkerpop.shaded.minlog.Log;
 
 public class Utils {
 
     private static final Pattern COUNTER_PATTERN = Pattern.compile("\\d+$");
     private static final SecureRandom random = new SecureRandom();
 
+    private Utils() {
+    }
 
-    private Utils() {}
-
-    public static int getNextCounter(@NotNull List<String> existingValues) {
-        if (existingValues.isEmpty()) {
+    public static int getNextCounter(final List<String> existingValues) {
+        if (CollectionUtils.isEmpty(existingValues)) {
             return 0;
         }
         try {
             return 1 + existingValues.stream()
-                    .map(COUNTER_PATTERN::matcher)
-                    .filter(Matcher::find)
-                    .map(matcher -> matcher.group(0))
-                    .mapToInt(Integer::parseInt)
-                    .max()
-                    .orElse(0);
-        }
-        catch (Exception e) {
+                .map(COUNTER_PATTERN::matcher)
+                .filter(Matcher::find)
+                .map(matcher -> matcher.group(0))
+                .mapToInt(Integer::parseInt)
+                .max()
+                .orElse(0);
+        } catch (Exception e) {
             Log.warn("Failed in retrieivng counter from existing value: ", e);
             return random.nextInt(100) + 50;
         }
