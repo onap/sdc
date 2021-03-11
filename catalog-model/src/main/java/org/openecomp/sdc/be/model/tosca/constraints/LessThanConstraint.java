91e06705fd721ce96041401051982fa3862b1feb
xuegao <xue.gao@intl.att.com>
2021-03-11 17:22:46 +0100

    Adding unit tests
    Adding unit tests to improve test coverage.
    
    Issue-ID: SDC-3428
    Signed-off-by: xuegao <xue.gao@intl.att.com>
    Change-Id: I57da2ff92839cb78985e5f3d3c13dc575c1b6c17

--- catalog-model/src/main/java/org/openecomp/sdc/be/model/tosca/constraints/LessThanConstraint.java
+++ catalog-model/src/main/java/org/openecomp/sdc/be/model/tosca/constraints/LessThanConstraint.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +20,7 @@
 
 package org.openecomp.sdc.be.model.tosca.constraints;
 
+import lombok.AllArgsConstructor;
 import org.openecomp.sdc.be.model.PropertyConstraint;
 import org.openecomp.sdc.be.model.tosca.ToscaType;
 import org.openecomp.sdc.be.model.tosca.constraints.exception.ConstraintFunctionalException;
@@ -29,14 +30,11 @@ import org.openecomp.sdc.be.model.tosca.constraints.exception.PropertyConstraint
 
 import javax.validation.constraints.NotNull;
 
+@AllArgsConstructor
 public class LessThanConstraint extends AbstractComparablePropertyConstraint {
 
     @NotNull
     private String lessThan;
-
-    public LessThanConstraint(String lessThan) {
-        this.lessThan = lessThan;
-    }
 
     @Override
     public void initialize(ToscaType propertyType) throws ConstraintValueDoNotMatchPropertyTypeException {
--- common-app-api/src/main/java/org/openecomp/sdc/common/api/HealthCheckWrapper.java
+++ common-app-api/src/main/java/org/openecomp/sdc/common/api/HealthCheckWrapper.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,43 +20,18 @@
 
 package org.openecomp.sdc.common.api;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class HealthCheckWrapper {
-
-	String sdcVersion;
-	String siteMode;
-	List<HealthCheckInfo> componentsInfo;
-
-	public HealthCheckWrapper(List<HealthCheckInfo> componentsInfo, String sdcVersion, String siteMode) {
-		super();
-		this.componentsInfo = componentsInfo;
-		this.sdcVersion = sdcVersion;
-		this.siteMode = siteMode;
-	}
-
-	public List<HealthCheckInfo> getComponentsInfo() {
-		return componentsInfo;
-	}
-
-	public void setComponentsInfo(List<HealthCheckInfo> componentsInfo) {
-		this.componentsInfo = componentsInfo;
-	}
-
-	public String getSdcVersion() {
-		return sdcVersion;
-	}
-
-	public void setSdcVersion(String sdcVersion) {
-		this.sdcVersion = sdcVersion;
-	}
-
-	public String getSiteMode() {
-		return siteMode;
-	}
-
-	public void setSiteMode(String siteMode) {
-		this.siteMode = siteMode;
-	}
+	private List<HealthCheckInfo> componentsInfo;
+	private String sdcVersion;
+	private String siteMode;
 
 }
