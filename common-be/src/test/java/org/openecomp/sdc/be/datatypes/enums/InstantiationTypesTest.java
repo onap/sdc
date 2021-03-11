b4027b7244e8f0ef34c4afa5223a5acb858f1d2a
xuegao <xue.gao@intl.att.com>
2021-03-11 20:25:30 +0100

    Adding unit tests
    Adding unit tests to improve test coverage.
    
    Issue-ID: SDC-3428
    Signed-off-by: xuegao <xue.gao@intl.att.com>
    Change-Id: I1d9ea0a4a8a1855414a611c57930a4e0decb3b89

---
+++ common-be/src/test/java/org/openecomp/sdc/be/datatypes/enums/InstantiationTypesTest.java
@@ -0,0 +1,35 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2021 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdc.be.datatypes.enums;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Test;
+
+public class InstantiationTypesTest {
+
+    @Test
+    public void testContainsName() {
+        assertTrue(InstantiationTypes.containsName("A-la-carte"));
+        assertTrue(InstantiationTypes.containsName("Macro"));
+        assertFalse(InstantiationTypes.containsName("test"));
+    }
+}
