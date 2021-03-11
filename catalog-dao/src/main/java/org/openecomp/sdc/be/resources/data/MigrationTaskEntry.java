57975ddbe96fd6a16986fd9525f3784733ee71c2
xuegao <xue.gao@intl.att.com>
2021-03-11 16:18:22 +0100

    Adding unit tests
    Adding unit tests to improve test coverage.
    
    Issue-ID: SDC-3428
    Signed-off-by: xuegao <xue.gao@intl.att.com>
    Change-Id: Icea75db44575c2ea9e6b51a34730e83b0d35f03f

--- catalog-dao/src/main/java/org/openecomp/sdc/be/resources/data/MigrationTaskEntry.java
+++ catalog-dao/src/main/java/org/openecomp/sdc/be/resources/data/MigrationTaskEntry.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,9 +24,13 @@ import com.datastax.driver.mapping.annotations.ClusteringColumn;
 import com.datastax.driver.mapping.annotations.Column;
 import com.datastax.driver.mapping.annotations.PartitionKey;
 import com.datastax.driver.mapping.annotations.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
+@Getter
+@Setter
 @Table(keyspace = "sdcrepository", name = "migrationTasks")
 public class MigrationTaskEntry {
 
@@ -55,68 +59,4 @@ public class MigrationTaskEntry {
 
     @Column(name = "description")
     private String description;
-
-    public void setMajorVersion(Long majorVersion) {
-        this.majorVersion = majorVersion;
-    }
-
-    public void setMinorVersion(Long minorVersion) {
-        this.minorVersion = minorVersion;
-    }
-
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
-
-    public void setTaskStatus(String taskStatus) {
-        this.taskStatus = taskStatus;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public void setExecutionTime(double executionTime) {
-        this.executionTime = executionTime;
-    }
-
-    public Long getMajorVersion() {
-        return majorVersion;
-    }
-
-    public Long getMinorVersion() {
-        return minorVersion;
-    }
-
-    public Date getTimestamp() {
-        return timestamp;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public String getTaskStatus() {
-        return taskStatus;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public double getExecutionTime() {
-        return executionTime;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- catalog-dao/src/main/java/org/openecomp/sdc/be/resources/data/auditing/DistributionNotificationEvent.java
+++ catalog-dao/src/main/java/org/openecomp/sdc/be/resources/data/auditing/DistributionNotificationEvent.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +25,9 @@ import com.datastax.driver.mapping.annotations.ClusteringColumn;
 import com.datastax.driver.mapping.annotations.Column;
 import com.datastax.driver.mapping.annotations.PartitionKey;
 import com.datastax.driver.mapping.annotations.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import org.openecomp.sdc.be.resources.data.auditing.model.CommonAuditData;
 import org.openecomp.sdc.be.resources.data.auditing.model.OperationalEnvAuditData;
 import org.openecomp.sdc.be.resources.data.auditing.model.ResourceCommonInfo;
@@ -36,6 +39,8 @@ import java.util.Date;
 import java.util.TimeZone;
 import java.util.UUID;
 
+@Getter
+@Setter
 @Table(keyspace = AuditingTypesConstants.AUDIT_KEYSPACE, name = AuditingTypesConstants.DISTRIBUTION_NOTIFICATION_EVENT_TYPE)
 public class DistributionNotificationEvent extends AuditingGenericEvent {
 
@@ -43,7 +48,7 @@ public class DistributionNotificationEvent extends AuditingGenericEvent {
     protected UUID timebaseduuid;
 
     @ClusteringColumn
-    protected Date timestamp1;
+    @Setter(AccessLevel.NONE)protected Date timestamp1;
 
     @Column(name = "request_id")
     protected String requestId;
@@ -120,6 +125,10 @@ public class DistributionNotificationEvent extends AuditingGenericEvent {
         this.timestamp1 = parseDateFromString(timestamp);
     }
 
+    public void setTimestamp1(Date timestamp) {
+        this.timestamp1 = timestamp;
+    }
+
     @Override
     public void fillFields() {
         fields.put(AuditingFieldsKey.AUDIT_REQUEST_ID.getDisplayName(), getRequestId());
@@ -142,142 +151,6 @@ public class DistributionNotificationEvent extends AuditingGenericEvent {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormatPattern);
         simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         fields.put(AuditingFieldsKey.AUDIT_TIMESTAMP.getDisplayName(), simpleDateFormat.format(timestamp1));
-    }
-
-    public String getResourceName() {
-        return resourceName;
-    }
-
-    public void setResourceName(String resourceName) {
-        this.resourceName = resourceName;
-    }
-
-    public String getResourceType() {
-        return resourceType;
-    }
-
-    public void setResourceType(String resourceType) {
-        this.resourceType = resourceType;
-    }
-
-    public String getCurrVersion() {
-        return currVersion;
-    }
-
-    public void setCurrVersion(String currVersion) {
-        this.currVersion = currVersion;
-    }
-
-    public String getCurrState() {
-        return currState;
-    }
-
-    public void setCurrState(String currState) {
-        this.currState = currState;
-    }
-
-    public String getTopicName() {
-        return topicName;
-    }
-
-    public void setTopicName(String topicName) {
-        this.topicName = topicName;
-    }
-
-    public UUID getTimebaseduuid() {
-        return timebaseduuid;
-    }
-
-    public void setTimebaseduuid(UUID timebaseduuid) {
-        this.timebaseduuid = timebaseduuid;
-    }
-
-    public Date getTimestamp1() {
-        return timestamp1;
-    }
-
-    public void setTimestamp1(Date timestamp1) {
-        this.timestamp1 = timestamp1;
-    }
-
-    public String getRequestId() {
-        return requestId;
-    }
-
-    public void setRequestId(String requestId) {
-        this.requestId = requestId;
-    }
-
-    public String getServiceInstanceId() {
-        return serviceInstanceId;
-    }
-
-    public void setServiceInstanceId(String serviceInstanceId) {
-        this.serviceInstanceId = serviceInstanceId;
-    }
-
-    public String getAction() {
-        return action;
-    }
-
-    public void setAction(String action) {
-        this.action = action;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public String getModifier() {
-        return modifier;
-    }
-
-    public void setModifier(String modifier) {
-        this.modifier = modifier;
-    }
-
-    public String getDid() {
-        return did;
-    }
-
-    public void setDid(String did) {
-        this.did = did;
-    }
-
-    public String getVnfWorkloadContext() {
-        return vnfWorkloadContext;
-    }
-
-    public void setVnfWorkloadContext(String vnfWorkloadContext) {
-        this.vnfWorkloadContext = vnfWorkloadContext;
-    }
-
-    public String getEnvId() {
-        return envId;
-    }
-
-    public void setEnvId(String envId) {
-        this.envId = envId;
-    }
-
-    public String getTenant() {
-        return tenant;
-    }
-
-    public void setTenant(String tenant) {
-        this.tenant = tenant;
     }
 
     @Override
