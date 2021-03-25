ccd5142b9739f77c6f5ea25b53cb5304fc5ffd63
xuegao <xue.gao@intl.att.com>
2021-03-25 16:00:32 +0100

    Improve test coverage
    Add unit tests to improve test coverage.
    
    Issue-ID: SDC-3428
    Change-Id: Ifd9e6eb25fa3ec9f4f93d283277574120edff835
    Signed-off-by: xuegao <xue.gao@intl.att.com>

--- openecomp-be/lib/openecomp-sdc-translator-lib/openecomp-sdc-translator-core/src/main/java/org/openecomp/sdc/translator/datatypes/heattotosca/unifiedmodel/composition/NodeTemplateInformation.java
+++ openecomp-be/lib/openecomp-sdc-translator-lib/openecomp-sdc-translator-core/src/main/java/org/openecomp/sdc/translator/datatypes/heattotosca/unifiedmodel/composition/NodeTemplateInformation.java
@@ -19,37 +19,20 @@
  */
 package org.openecomp.sdc.translator.datatypes.heattotosca.unifiedmodel.composition;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.onap.sdc.tosca.datatypes.model.NodeTemplate;
 
 /**
  * Created by Talio on 4/4/2017.
  */
+@Getter
+@Setter
+@AllArgsConstructor
 public class NodeTemplateInformation {
 
     UnifiedCompositionEntity unifiedCompositionEntity;
     private NodeTemplate nodeTemplate;
-
-    public NodeTemplateInformation() {
-    }
-
-    public NodeTemplateInformation(UnifiedCompositionEntity unifiedCompositionEntity, NodeTemplate nodeTemplate) {
-        this.unifiedCompositionEntity = unifiedCompositionEntity;
-        this.nodeTemplate = nodeTemplate;
-    }
-
-    public UnifiedCompositionEntity getUnifiedCompositionEntity() {
-        return unifiedCompositionEntity;
-    }
-
-    public void setUnifiedCompositionEntity(UnifiedCompositionEntity unifiedCompositionEntity) {
-        this.unifiedCompositionEntity = unifiedCompositionEntity;
-    }
-
-    public NodeTemplate getNodeTemplate() {
-        return nodeTemplate;
-    }
-
-    public void setNodeTemplate(NodeTemplate nodeTemplate) {
-        this.nodeTemplate = nodeTemplate;
-    }
 }
