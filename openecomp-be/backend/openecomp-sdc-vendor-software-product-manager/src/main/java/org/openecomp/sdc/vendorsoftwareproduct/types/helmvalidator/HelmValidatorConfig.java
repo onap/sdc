65c407460e56f475b4e6fe5218e72bbf11604ecb
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2021-04-21 13:59:12 +0200

    Integrate helm validator with SDC-BE
    - Read helm validator configuration
    - Add call to helm validator during CNF validation
    - Add JUnit tests
    - Fix display message when CNF upload is unsuccessful
    
    Issue-ID: SDC-3185
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>
    Change-Id: If197d557e6ddef4a07bef986d7cf133aedcb2cc5
    Signed-off-by: Piotr Marcinkiewicz <piotr.marcinkiewicz@nokia.com>

---
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/types/helmvalidator/HelmValidatorConfig.java
@@ -0,0 +1,107 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 Nokia
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+package org.openecomp.sdc.vendorsoftwareproduct.types.helmvalidator;
+
+public final class HelmValidatorConfig {
+    private final String validatorUrl;
+    private final String version;
+    private final boolean isEnabled;
+    private final boolean isDeployable;
+    private final boolean isLintable;
+    private final boolean isStrictLintable;
+
+    private HelmValidatorConfig(String validatorUrl, String version, boolean isEnabled, boolean isDeployable,
+        boolean isLintable, boolean isStrictLintable) {
+        this.validatorUrl = validatorUrl;
+        this.version = version;
+        this.isEnabled = isEnabled;
+        this.isDeployable = isDeployable;
+        this.isLintable = isLintable;
+        this.isStrictLintable = isStrictLintable;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public boolean isDeployable() {
+        return isDeployable;
+    }
+
+    public boolean isLintable() {
+        return isLintable;
+    }
+
+    public boolean isStrictLintable() {
+        return isStrictLintable;
+    }
+
+    public String getValidatorUrl() {
+        return validatorUrl;
+    }
+
+    public static class HelmValidationConfigBuilder {
+
+        private String validatorUrl;
+        private String version;
+        private boolean enabled;
+        private boolean deployable;
+        private boolean lintable;
+        private boolean strictLintable;
+
+        public HelmValidationConfigBuilder setVersion(String version) {
+            this.version = version;
+            return this;
+        }
+
+        public HelmValidationConfigBuilder setEnabled(boolean enabled) {
+            this.enabled = enabled;
+            return this;
+        }
+
+        public HelmValidationConfigBuilder setDeployable(boolean deployable) {
+            this.deployable = deployable;
+            return this;
+        }
+
+        public HelmValidationConfigBuilder setLintable(boolean lintable) {
+            this.lintable = lintable;
+            return this;
+        }
+
+        public HelmValidationConfigBuilder setStrictLintable(boolean strictLintable) {
+            this.strictLintable = strictLintable;
+            return this;
+        }
+
+        public HelmValidationConfigBuilder setValidatorUrl(String validatorUrl) {
+            this.validatorUrl = validatorUrl;
+            return this;
+        }
+
+        public HelmValidatorConfig build() {
+            return new HelmValidatorConfig(validatorUrl, version, enabled, deployable, lintable, strictLintable);
+        }
+    }
+
+}
