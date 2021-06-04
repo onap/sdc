5910e886fc01e05888317837d91535831ce419ae
sebdet <sebastien.determe@intl.att.com>
2021-06-04 17:28:49 +0200

    Dead Code removal
    Removal of dead code found during Config manager analysis
    
    Issue-ID: SDC-3616
    Change-Id: I50c15dfcba7518c32af953fe481ef013aa393ca5
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- integration-tests/src/test/java/org/onap/sdc/frontend/ci/tests/execute/AmdocsComplexService/PathValidations.java
+++ integration-tests/src/test/java/org/onap/sdc/frontend/ci/tests/execute/AmdocsComplexService/PathValidations.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * SDC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017, 2021 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,28 +22,16 @@ package org.onap.sdc.frontend.ci.tests.execute.AmdocsComplexService;
 
 import com.aventstack.extentreports.Status;
 import com.clearspring.analytics.util.Pair;
+import java.util.*;
 import org.apache.commons.lang.RandomStringUtils;
 import org.json.JSONObject;
 import org.onap.sdc.frontend.ci.tests.datatypes.DataTestIdEnum;
-import org.onap.sdc.backend.ci.tests.datatypes.HeatMetaFirstLevelDefinition;
 import org.onap.sdc.backend.ci.tests.datatypes.ServiceReqDetails;
-import org.onap.sdc.backend.ci.tests.datatypes.VendorSoftwareProductObject;
 import org.onap.sdc.backend.ci.tests.datatypes.http.RestResponse;
-import org.onap.sdc.backend.ci.tests.execute.devCI.ArtifactFromCsar;
-import org.onap.sdc.frontend.ci.tests.pages.DeploymentArtifactPage;
-import org.onap.sdc.frontend.ci.tests.utilities.DownloadManager;
-import org.onap.sdc.frontend.ci.tests.utilities.FileHandling;
 import org.onap.sdc.frontend.ci.tests.utilities.GeneralUIUtils;
-import org.onap.sdc.frontend.ci.tests.utilities.OnboardingUiUtils;
-import org.onap.sdc.frontend.ci.tests.execute.setup.ArtifactsCorrelationManager;
 import org.onap.sdc.frontend.ci.tests.execute.setup.ExtentTestActions;
 import org.onap.sdc.frontend.ci.tests.execute.setup.SetupCDTest;
-import org.onap.sdc.frontend.ci.tests.pages.ResourceGeneralPage;
 import org.openqa.selenium.WebElement;
-
-import java.io.File;
-import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNotSame;
@@ -55,46 +43,6 @@ public class PathValidations {
     public static final int NUMBER_OF_LINKS = 5;
     public static final int NUMBER_OF_LINES = 3;
     public static final int NUMBER_OF_LINES_TO_DELETE = 1;
-
-    public static String[] validateServiceExtendedPath(String vspName) throws Exception {
-        PathUtilities.linkVFs(vspName, NUMBER_OF_LINKS);
-        PathUtilities.openCreatePath();
-        String pathName = "name1";
-        PathUtilities.insertValues(pathName, "pathProtocol1", "pathPortNumbers1");
-        PathUtilities.selectFirstLineParam();
-        PathValidations.extendPath(NUMBER_OF_LINES);
-        //delete line
-        PathUtilities.deleteLines(NUMBER_OF_LINES_TO_DELETE, NUMBER_OF_LINES);
-        GeneralUIUtils.clickOnElementByTestId(DataTestIdEnum.ComplexServiceAmdocs.CREATE_BUTTON.getValue());
-        ExtentTestActions.log(Status.INFO, "path has been created");
-        PathValidations.checkPathFilter(pathName, true);
-        GeneralUIUtils.findElementsByXpath("//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.PATH_MENU_BUTTON.getValue() + "']/parent::*").get(0).click();
-        GeneralUIUtils.clickOnElementByTestId(DataTestIdEnum.ComplexServiceAmdocs.PATH_LIST_BUTTON.getValue());
-        String PathListName1 = GeneralUIUtils.findByText(pathName).getText();
-        // edit path
-        String newPathName = "name2";
-        PathUtilities.editPathName(pathName, newPathName);
-
-        GeneralUIUtils.findElementsByXpath("//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.PATH_MENU_BUTTON.getValue() + "']/parent::*").get(0).click();
-        GeneralUIUtils.clickOnElementByTestId(DataTestIdEnum.ComplexServiceAmdocs.PATH_LIST_BUTTON.getValue());
-        String PathListName2 = GeneralUIUtils.findByText(newPathName).getText();
-
-        // assert names changed
-        assertNotSame("path name expected to change after edit but did not", PathListName1, PathListName2);
-
-        // delete path
-        int paths_before_deletion = GeneralUIUtils.findElementsByXpath("//*[text()='" + newPathName + "']/parent::*//span").size();
-        GeneralUIUtils.findElementsByXpath(newPathName).get(1).click();
-        GeneralUIUtils.ultimateWait();
-        int paths_after_deletion = GeneralUIUtils.findElementsByXpath("//*[text()='" + newPathName + "']/parent::*//span").size();
-        assertNotSame("path expected to be deleted but did not", paths_after_deletion, paths_before_deletion);
-
-        GeneralUIUtils.clickOnElementByTestId(DataTestIdEnum.PropertiesAssignmentScreen.INPUT_DELETE_DIALOG_CLOSE.getValue());
-        SetupCDTest.getExtendTest().log(Status.INFO, "Path has been created");
-        // check that path got deleted in the path filter list
-        PathValidations.checkPathFilter(newPathName, false);
-        return new String[]{pathName, newPathName};
-    }
 
     public static String[] validateComplexExtendedPath(String[] services) throws Exception {
         PathUtilities.linkServices(services[0], services[1], NUMBER_OF_LINKS);
@@ -111,7 +59,9 @@ public class PathValidations {
         String PathListName1 = GeneralUIUtils.findByText(pathName).getText();
         String newPathName = "name2";
         PathUtilities.editPathName(pathName, newPathName);
-        GeneralUIUtils.findElementsByXpath("//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.PATH_MENU_BUTTON.getValue() + "']/parent::*").get(0).click();
+        GeneralUIUtils.findElementsByXpath(
+                "//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.PATH_MENU_BUTTON.getValue() +
+                        "']/parent::*").get(0).click();
         GeneralUIUtils.ultimateWait();
         GeneralUIUtils.clickOnElementByTestId(DataTestIdEnum.ComplexServiceAmdocs.PATH_LIST_BUTTON.getValue());
         String PathListName2 = GeneralUIUtils.findByText(newPathName).getText();
@@ -120,14 +70,17 @@ public class PathValidations {
         assertNotSame("path name expected to change after edit but did not", PathListName1, PathListName2);
 
         // delete path
-        int paths_before_deletion = GeneralUIUtils.findElementsByXpath("//*[text()='" + newPathName + "']/parent::*//span").size();
+        int paths_before_deletion =
+                GeneralUIUtils.findElementsByXpath("//*[text()='" + newPathName + "']/parent::*//span").size();
         GeneralUIUtils.findElementsByXpath("//*[text()='" + newPathName + "']/parent::*//span").get(1).click();
         GeneralUIUtils.ultimateWait();
 
-        int paths_after_deletion = GeneralUIUtils.findElementsByXpath("//*[text()='" + newPathName + "']/parent::*//span").size();
+        int paths_after_deletion =
+                GeneralUIUtils.findElementsByXpath("//*[text()='" + newPathName + "']/parent::*//span").size();
         assertNotSame("path expected to be deleted but did not", paths_after_deletion, paths_before_deletion);
 
-        GeneralUIUtils.clickOnElementByTestId(DataTestIdEnum.PropertiesAssignmentScreen.INPUT_DELETE_DIALOG_CLOSE.getValue());
+        GeneralUIUtils
+                .clickOnElementByTestId(DataTestIdEnum.PropertiesAssignmentScreen.INPUT_DELETE_DIALOG_CLOSE.getValue());
         PathValidations.validateServicePath(services[2], pathName);
         PathValidations.validateServicePath(services[2], newPathName);
         SetupCDTest.getExtendTest().log(Status.INFO, "Paths have been validated");
@@ -137,7 +90,8 @@ public class PathValidations {
     public static void createPathNumOfRows(int numOfPathRows) throws Exception {
         final int numberOfRandomCharacters = 8;
         PathUtilities.openCreatePath();
-        PathUtilities.insertValues("extended" + RandomStringUtils.randomAlphanumeric(numberOfRandomCharacters), "pathProtocol1", "pathPortNumbers1");
+        PathUtilities.insertValues("extended" + RandomStringUtils.randomAlphanumeric(numberOfRandomCharacters),
+                "pathProtocol1", "pathPortNumbers1");
         PathUtilities.selectFirstLineParam();
         extendPath(numOfPathRows);
         GeneralUIUtils.clickOnElementByTestId(DataTestIdEnum.ComplexServiceAmdocs.CREATE_BUTTON.getValue());
@@ -150,10 +104,16 @@ public class PathValidations {
             String index = Integer.toString(i + 2);
             GeneralUIUtils.clickOnElementByTestId(DataTestIdEnum.ComplexServiceAmdocs.EXTEND_BUTTON.getValue());
 
-            List<WebElement> linkSrcs = GeneralUIUtils.findElementsByXpath("//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.LINK_SOURCE.getValue() + "']//select");
-            List<WebElement> linkSrcCPs = GeneralUIUtils.findElementsByXpath("//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.LINK_SOURCE_CP.getValue() + "']//select");
-            List<WebElement> linkTargets = GeneralUIUtils.findElementsByXpath("//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.LINK_TARGET.getValue() + "']//select");
-            List<WebElement> linkTargetCPs = GeneralUIUtils.findElementsByXpath("//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.LINK_TARGET_CP.getValue() + "']//select");
+            List<WebElement> linkSrcs = GeneralUIUtils.findElementsByXpath(
+                    "//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.LINK_SOURCE.getValue() + "']//select");
+            List<WebElement> linkSrcCPs = GeneralUIUtils.findElementsByXpath(
+                    "//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.LINK_SOURCE_CP.getValue() +
+                            "']//select");
+            List<WebElement> linkTargets = GeneralUIUtils.findElementsByXpath(
+                    "//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.LINK_TARGET.getValue() + "']//select");
+            List<WebElement> linkTargetCPs = GeneralUIUtils.findElementsByXpath(
+                    "//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.LINK_TARGET_CP.getValue() +
+                            "']//select");
             for (int j = 0; j < i + 2; j++) {
                 validateExtendedPathDisabledButtons(linkSrcs, j, "Source should be disabled. open bug to UI team");
                 check = linkSrcCPs.get(j).getAttribute("class");
@@ -165,9 +125,11 @@ public class PathValidations {
                 check = linkTargetCPs.get(j).getAttribute("class");
                 validateElementDisabledAttribute(check, "Target connection point");
             }
-            List<WebElement> choices = GeneralUIUtils.findElementsByXpath("//*[" + index + "]/*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.LINK_TARGET.getValue() + "']//option");
+            List<WebElement> choices = GeneralUIUtils.findElementsByXpath("//*[" + index + "]/*[@data-tests-id='" +
+                    DataTestIdEnum.ComplexServiceAmdocs.LINK_TARGET.getValue() + "']//option");
             choices.get((new Random()).nextInt(choices.size())).click();
-            choices = GeneralUIUtils.findElementsByXpath("//*[" + index + "]/*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.LINK_TARGET_CP.getValue() + "']//option");
+            choices = GeneralUIUtils.findElementsByXpath("//*[" + index + "]/*[@data-tests-id='" +
+                    DataTestIdEnum.ComplexServiceAmdocs.LINK_TARGET_CP.getValue() + "']//option");
             choices.get((new Random()).nextInt(choices.size())).click();
         }
     }
@@ -176,7 +138,8 @@ public class PathValidations {
         assertEquals(param + " of last lines should be disabled", check.contains("disabled"), true);
     }
 
-    public static void validateExtendedPathDisabledButtons(List<WebElement> linkSrcs, int i, String errMsg) throws Exception {
+    public static void validateExtendedPathDisabledButtons(List<WebElement> linkSrcs, int i, String errMsg)
+            throws Exception {
         String check = linkSrcs.get(i).getAttribute("class");
         assertEquals(errMsg, check.contains("disabled"), true);
     }
@@ -197,9 +160,11 @@ public class PathValidations {
 
     public static void ValidateAndDeletePathFromPathList(String pathName) throws Exception {
         // count paths
-        List<WebElement> path_list_name = GeneralUIUtils.getWebElementsListByTestID(DataTestIdEnum.ComplexServiceAmdocs.PATH_LIST_NAME.getValue());
+        List<WebElement> path_list_name = GeneralUIUtils
+                .getWebElementsListByTestID(DataTestIdEnum.ComplexServiceAmdocs.PATH_LIST_NAME.getValue());
         // delete paths
-        List<WebElement> path_delete = GeneralUIUtils.getWebElementsListByTestID(DataTestIdEnum.ComplexServiceAmdocs.PATH_LIST_DELETE.getValue());
+        List<WebElement> path_delete = GeneralUIUtils
+                .getWebElementsListByTestID(DataTestIdEnum.ComplexServiceAmdocs.PATH_LIST_DELETE.getValue());
         int paths_before_deletion = 0;
         for (int i = 0; i < path_list_name.size(); i++) {
             if (path_list_name.get(i).getText().equals(pathName)) {
@@ -210,7 +175,8 @@ public class PathValidations {
         int paths_after_deletion = 0;
         List<WebElement> path_list_name2;
         try {
-            path_list_name2 = GeneralUIUtils.getWebElementsListByTestID(DataTestIdEnum.ComplexServiceAmdocs.PATH_LIST_NAME.getValue());
+            path_list_name2 = GeneralUIUtils
+                    .getWebElementsListByTestID(DataTestIdEnum.ComplexServiceAmdocs.PATH_LIST_NAME.getValue());
             for (WebElement path_list_element : path_list_name2) {
                 if (path_list_element.getText().equals(pathName)) {
                     paths_after_deletion++;
@@ -223,7 +189,9 @@ public class PathValidations {
     }
 
     public static void checkPathFilter(String pathName, boolean isFound) throws Exception {
-        List<WebElement> pathFilterList = GeneralUIUtils.findElementsByXpath("//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.SERVICE_PATH_SELECTOR.getValue() + "']//option");
+        List<WebElement> pathFilterList = GeneralUIUtils.findElementsByXpath(
+                "//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.SERVICE_PATH_SELECTOR.getValue() +
+                        "']//option");
         GeneralUIUtils.ultimateWait();
         if (isFound) {
             for (int i = 0; i < pathFilterList.size(); i++) {
@@ -243,7 +211,9 @@ public class PathValidations {
     }
 
     public static void AssertCreatePath() throws Exception {
-        String check = GeneralUIUtils.getWebElementByTestID(DataTestIdEnum.ComplexServiceAmdocs.CREATE_BUTTON.getValue()).getAttribute("disabled");
+        String check =
+                GeneralUIUtils.getWebElementByTestID(DataTestIdEnum.ComplexServiceAmdocs.CREATE_BUTTON.getValue())
+                        .getAttribute("disabled");
         assertEquals("create button path should be disabled", check, "true");
     }
 
@@ -254,18 +224,22 @@ public class PathValidations {
         // assert path is in the list
         GeneralUIUtils.findByText(pathName).getText();
         GeneralUIUtils.ultimateWait();
-        GeneralUIUtils.clickOnElementByTestId(DataTestIdEnum.PropertiesAssignmentScreen.INPUT_DELETE_DIALOG_CLOSE.getValue());
+        GeneralUIUtils
+                .clickOnElementByTestId(DataTestIdEnum.PropertiesAssignmentScreen.INPUT_DELETE_DIALOG_CLOSE.getValue());
 //        GeneralUIUtils.ultimateWait();
         SetupCDTest.getExtendTest().log(Status.INFO, "Path is in the path list");
 //        GeneralUIUtils.ultimateWait();
     }
 
     public static void AssertExtendPath() throws Exception {
-        String check = GeneralUIUtils.getWebElementsListByTestID(DataTestIdEnum.ComplexServiceAmdocs.EXTEND_BUTTON.getValue()).get(0).getAttribute("class");
+        String check =
+                GeneralUIUtils.getWebElementsListByTestID(DataTestIdEnum.ComplexServiceAmdocs.EXTEND_BUTTON.getValue())
+                        .get(0).getAttribute("class");
         assertEquals("Extend button should be disabled", check, "disabled");
     }
 
-    public static Pair<RestResponse, ServiceReqDetails> validateServicePath(String serviceName, String name) throws Exception {
+    public static Pair<RestResponse, ServiceReqDetails> validateServicePath(String serviceName, String name)
+            throws Exception {
         Pair<RestResponse, ServiceReqDetails> servicePaths = PathUtilities.getServicePathsAPI(serviceName);
         String response = servicePaths.left.getResponse();
 
@@ -290,7 +264,8 @@ public class PathValidations {
 
     public static void ValidateThereIsNoErrorMessage() throws Exception {
         try {
-            GeneralUIUtils.findElementsByXpath("//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.OK.getValue() + "']");
+            GeneralUIUtils.findElementsByXpath(
+                    "//*[@data-tests-id='" + DataTestIdEnum.ComplexServiceAmdocs.OK.getValue() + "']");
             throw new Exception("element should not be found");
         } catch (Exception e) {
             if (e.getMessage().equals("element should not be found")) {
@@ -312,14 +287,16 @@ public class PathValidations {
         try {
             GeneralUIUtils.clickOnElementByTestId(DataTestIdEnum.ComplexServiceAmdocs.OK.getValue());
         } catch (Exception e) {
-            throw new Exception("space in beggining or end does not count. when creating another path with duplicate name, expected error did not appear");
+            throw new Exception(
+                    "space in beggining or end does not count. when creating another path with duplicate name, expected error did not appear");
         }
         PathUtilities.openPathList();
         PathUtilities.editPathName(secondPathName, "           " + firstPathName);
         try {
             GeneralUIUtils.clickOnElementByTestId(DataTestIdEnum.ComplexServiceAmdocs.OK.getValue());
         } catch (Exception e) {
-            throw new Exception("space in beggining or end does not count. when creating another path with duplicate name, expected error did not appear");
+            throw new Exception(
+                    "space in beggining or end does not count. when creating another path with duplicate name, expected error did not appear");
         }
         PathUtilities.openPathList();
         PathUtilities.editPathProtocol(secondPathName, "pathProtocol2");
@@ -333,28 +310,4 @@ public class PathValidations {
         PathUtilities.openPathList();
         PathUtilities.editPathName(pathName, "newName2");
     }
-
-    public static void importAndVerifyVSP(VendorSoftwareProductObject createVendorSoftwareProduct, String filepath, String vnfFile) throws Exception {
-        DownloadManager.downloadCsarByNameFromVSPRepository(createVendorSoftwareProduct.getName(), createVendorSoftwareProduct.getVspId());
-        File latestFilefromDir = FileHandling.getLastModifiedFileNameFromDir();
-
-        OnboardingUiUtils.importVSP(createVendorSoftwareProduct);
-
-        ResourceGeneralPage.getLeftMenu().moveToDeploymentArtifactScreen();
-
-        // Verify deployment artifacts
-        Map<String, Object> combinedMap = ArtifactFromCsar.combineHeatArtifacstWithFolderArtifacsToMap(latestFilefromDir.getAbsolutePath());
-
-        LinkedList<HeatMetaFirstLevelDefinition> deploymentArtifacts = ((LinkedList<HeatMetaFirstLevelDefinition>) combinedMap.get("Deployment"));
-        ArtifactsCorrelationManager.addVNFartifactDetails(vnfFile, deploymentArtifacts);
-
-        List<String> heatEnvFilesFromCSAR = deploymentArtifacts.stream().filter(e -> e.getType().equals("HEAT_ENV")).
-                map(e -> e.getFileName()).
-                collect(Collectors.toList());
-
-        OnboardingUiUtils.validateDeploymentArtifactsVersion(deploymentArtifacts, heatEnvFilesFromCSAR);
-
-        DeploymentArtifactPage.verifyArtifactsExistInTable(filepath, vnfFile);
-    }
-
 }
