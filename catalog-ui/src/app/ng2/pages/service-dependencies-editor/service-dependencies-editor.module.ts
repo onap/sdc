39c71e222e4e83afde562aed1fc81523493e082d
davsad <david.sadlier@est.tech>
2021-06-08 07:17:07 +0100

    Adding type safety to the service dependency editor.
    Issue-ID: SDC-3725
    
    
    Signed-off-by: davsad <david.sadlier@est.tech>
    Change-Id: I63d77837fb0df24f5ee12baa5b852a76ce5f55e3

--- catalog-ui/src/app/ng2/pages/service-dependencies-editor/service-dependencies-editor.module.ts
+++ catalog-ui/src/app/ng2/pages/service-dependencies-editor/service-dependencies-editor.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { FormElementsModule } from 'app/ng2/components/ui/form-components/form-elements.module';
 import { UiElementsModule } from 'app/ng2/components/ui/ui-elements.module';
 import { ServiceDependenciesEditorComponent } from './service-dependencies-editor.component';
+import { PropertyTableModule } from 'app/ng2/components/logic/properties-table/property-table.module';
 
 @NgModule({
     declarations: [
@@ -13,7 +14,8 @@ import { ServiceDependenciesEditorComponent } from './service-dependencies-edito
         CommonModule,
         FormsModule,
         FormElementsModule,
-        UiElementsModule
+        UiElementsModule,
+        PropertyTableModule
     ],
     exports: [],
     entryComponents: [
