cc9405b4079bf57dd416c48be6456baf052e55a4
vasraz <vasyl.razinkov@est.tech>
2021-06-19 13:53:06 +0100

    Remove usage of outdated library JMockit (catalog-dao)
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Issue-ID: SDC-3379
    Change-Id: I6b46049b71eafbfedfd3d40c68c743efff13ef54

--- catalog-dao/src/test/java/org/openecomp/sdc/be/dao/cassandra/schema/tables/DistribStatusEventTableDescTest.java
+++ catalog-dao/src/test/java/org/openecomp/sdc/be/dao/cassandra/schema/tables/DistribStatusEventTableDescTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,46 +21,30 @@
 package org.openecomp.sdc.be.dao.cassandra.schema.tables;
 
 import com.datastax.driver.core.DataType;
-import mockit.Deencapsulation;
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.junit.Test;
-import org.openecomp.sdc.be.dao.cassandra.schema.tables.DistribStatusEventTableDesc.DSEFieldsDescription;
-
-import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openecomp.sdc.be.resources.data.auditing.AuditingTypesConstants;
 
-public class DistribStatusEventTableDescTest {
+class DistribStatusEventTableDescTest {
 
-	private DistribStatusEventTableDesc createTestSubject() {
-		return new DistribStatusEventTableDesc();
-	}
+    private DistribStatusEventTableDesc createTestSubject() {
+        return new DistribStatusEventTableDesc();
+    }
 
-	@Test
-	public void testUpdateColumnDistribDescription() throws Exception {
-		DistribStatusEventTableDesc testSubject;
-		Map<String, ImmutablePair<DataType, Boolean>> columns = new HashMap<>();
+    @Test
+    void testUpdateColumnDistribDescription() throws Exception {
+        final DistribStatusEventTableDesc testSubject = createTestSubject();
+        final Map<String, ImmutablePair<DataType, Boolean>> columnDescription = testSubject.getColumnDescription();
+        Assertions.assertNotNull(columnDescription);
+        Assertions.assertEquals(10, columnDescription.size());
+    }
 
-		// default test
-		testSubject = createTestSubject();
-		Deencapsulation.invoke(testSubject, "updateColumnDistribDescription", columns);
-	}
+    @Test
+    void testGetTableName() throws Exception {
+        final DistribStatusEventTableDesc testSubject = createTestSubject();
+        Assertions.assertEquals(AuditingTypesConstants.DISTRIBUTION_STATUS_EVENT_TYPE, testSubject.getTableName());
+    }
 
-	@Test
-	public void testGetTableName() throws Exception {
-		DistribStatusEventTableDesc testSubject;
-		String result;
-
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getTableName();
-	}
-	
-	@Test
-	public void testDSEFieldsDescription() throws Exception {
-		DSEFieldsDescription testSubject = DistribStatusEventTableDesc.DSEFieldsDescription.CONSUMER_ID;
-		
-		testSubject.getName();
-		testSubject.getType();
-		testSubject.isIndexed();
-	}
 }
