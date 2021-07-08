5a72493a725889cd9ef2a0a9912558443533e2d7
marine <marine.de.dorlodot@intl.att.com>
2021-07-08 11:57:27 +0200

    Exposing new parameters in chef config for SDC
    Issue-ID: SDC-3633
    
    Change-Id: Ib59ecbe151b3d7fb5b52241102f5c7dfc520003b
    Signed-off-by: xuegao <xue.gao@intl.att.com>

--- catalog-be/src/main/docker/backend/chef-repo/cookbooks/sdc-catalog-be/attributes/default.rb
+++ catalog-be/src/main/docker/backend/chef-repo/cookbooks/sdc-catalog-be/attributes/default.rb
@@ -35,6 +35,9 @@ default['cassandra']['socket_connect_timeout'] = 20000
 default['cassandra']['janusgraph_connection_timeout'] = 10000
 default['cassandra'][:truststore_password] = "Aa123456"
 default['cassandra']['janusgraph_config_file'] = "#{ENV['JETTY_BASE']}/config/catalog-be/janusgraph.properties"
+default['cassandra'][:db_cache] = true
+default['cassandra'][:read_consistency_level] = "ONE"
+default['cassandra'][:write_consistency_level] = "ALL"
 
 
 #Onboard
