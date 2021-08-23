5a0703ffad1492ec6b6c78143f63dca83ee030d2
andre.schmid <andre.schmid@est.tech>
2021-08-23 19:41:50 +0100

    Dynamically load CSAR validators for models
    Change-Id: I88ece0936e8a2814ef13dfa23eecda56de3dc6fe
    Issue-ID: SDC-3683
    Signed-off-by: andre.schmid <andre.schmid@est.tech>

--- openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/test/java/org/openecomp/sdc/vendorsoftwareproduct/impl/orchestration/csar/validation/ValidatorFactoryTest.java
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/test/java/org/openecomp/sdc/vendorsoftwareproduct/impl/orchestration/csar/validation/ValidatorFactoryTest.java
@@ -20,7 +20,10 @@
 
 package org.openecomp.sdc.vendorsoftwareproduct.impl.orchestration.csar.validation;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.openecomp.sdc.be.test.util.TestResourcesHandler.getResourceBytesOrFail;
 import static org.openecomp.sdc.tosca.csar.ManifestTokenType.ATTRIBUTE_VALUE_SEPARATOR;
 import static org.openecomp.sdc.tosca.csar.ToscaMetaEntry.CREATED_BY_ENTRY;
@@ -36,17 +39,20 @@ import static org.openecomp.sdc.vendorsoftwareproduct.impl.orchestration.csar.va
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import org.junit.Before;
-import org.junit.Test;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openecomp.core.utilities.file.FileContentHandler;
 
-public class ValidatorFactoryTest {
+class ValidatorFactoryTest {
 
     private String metaFile;
     private FileContentHandler handler;
+    private ValidatorFactory validatorFactory;
 
-    @Before
-    public void setUp(){
+    @BeforeEach
+    void setUp(){
+        validatorFactory = new ValidatorFactory();
         handler = new FileContentHandler();
         metaFile = new StringBuilder()
             .append(TOSCA_META_FILE_VERSION_ENTRY.getName())
@@ -58,49 +64,49 @@ public class ValidatorFactoryTest {
             .toString();
     }
 
-    @Test(expected = IOException.class)
-    public void testGivenEmptyMetaFile_thenIOExceptionIsThrown() throws IOException{
+    @Test
+    void testGivenEmptyMetaFile_thenIOExceptionIsThrown() {
         handler.addFile(TOSCA_META_PATH_FILE_NAME, "".getBytes(StandardCharsets.UTF_8));
         handler.addFile(TOSCA_DEFINITION_FILEPATH, "".getBytes());
         handler.addFile(TOSCA_CHANGELOG_FILEPATH, "".getBytes(StandardCharsets.UTF_8));
         handler.addFile(TOSCA_MANIFEST_FILEPATH, "".getBytes(StandardCharsets.UTF_8));
 
-        ValidatorFactory.getValidator(handler);
+        assertThrows(IOException.class, () -> validatorFactory.getValidator(handler));
     }
 
     @Test
-    public void testGivenEmptyBlock0_thenONAPCsarValidatorIsReturned() throws IOException {
+    void testGivenEmptyBlock0_thenONAPCsarValidatorIsReturned() throws IOException {
         handler.addFile(TOSCA_META_PATH_FILE_NAME, " ".getBytes(StandardCharsets.UTF_8));
         handler.addFile(TOSCA_DEFINITION_FILEPATH, "".getBytes());
         handler.addFile(TOSCA_CHANGELOG_FILEPATH, "".getBytes(StandardCharsets.UTF_8));
         handler.addFile(TOSCA_MANIFEST_FILEPATH, "".getBytes(StandardCharsets.UTF_8));
 
-        assertEquals(ONAPCsarValidator.class, ValidatorFactory.getValidator(handler).getClass());
+        assertEquals(ONAPCsarValidator.class, validatorFactory.getValidator(handler).getClass());
     }
 
 
     @Test
-    public void testGivenNonSOL004MetaDirectoryCompliantMetaFile_thenONAPCSARValidatorIsReturned() throws IOException {
+    void testGivenNonSOL004MetaDirectoryCompliantMetaFile_thenONAPCSARValidatorIsReturned() throws IOException {
         metaFile = metaFile +
                 ENTRY_DEFINITIONS.getName() + ATTRIBUTE_VALUE_SEPARATOR.getToken() + TOSCA_DEFINITION_FILEPATH;
         handler.addFile(TOSCA_META_PATH_FILE_NAME, metaFile.getBytes(StandardCharsets.UTF_8));
 
-        assertEquals(ONAPCsarValidator.class, ValidatorFactory.getValidator(handler).getClass());
+        assertEquals(ONAPCsarValidator.class, validatorFactory.getValidator(handler).getClass());
     }
 
     @Test
-    public void testGivenSOL004MetaDirectoryCompliantMetafile_thenONAPCsarValidatorIsReturned() throws IOException {
+    void testGivenSOL004MetaDirectoryCompliantMetafile_thenONAPCsarValidatorIsReturned() throws IOException {
         metaFile = metaFile +
             ENTRY_DEFINITIONS.getName() + ATTRIBUTE_VALUE_SEPARATOR.getToken() + TOSCA_DEFINITION_FILEPATH + "\n"
             + ETSI_ENTRY_MANIFEST.getName() + ATTRIBUTE_VALUE_SEPARATOR.getToken() + TOSCA_MANIFEST_FILEPATH + "\n"
             + ETSI_ENTRY_CHANGE_LOG.getName() + ATTRIBUTE_VALUE_SEPARATOR.getToken() + TOSCA_CHANGELOG_FILEPATH + "\n";
         handler.addFile(TOSCA_META_PATH_FILE_NAME, metaFile.getBytes(StandardCharsets.UTF_8));
 
-       assertEquals(SOL004MetaDirectoryValidator.class, ValidatorFactory.getValidator(handler).getClass());
+       assertEquals(SOL004MetaDirectoryValidator.class, validatorFactory.getValidator(handler).getClass());
     }
 
     @Test
-    public void testGivenMultiBlockMetadataWithSOL00CompliantMetaFile_thenSOL004MetaDirectoryValidatorReturned()
+    void testGivenMultiBlockMetadataWithSOL00CompliantMetaFile_thenSOL004MetaDirectoryValidatorReturned()
             throws IOException {
         handler.addFile(TOSCA_META_PATH_FILE_NAME,
             getResourceBytesOrFail("validation.files/metafile/metaFileWithMultipleBlocks.meta"));
@@ -108,8 +114,20 @@ public class ValidatorFactoryTest {
         handler.addFile(TOSCA_CHANGELOG_FILEPATH, "".getBytes(StandardCharsets.UTF_8));
         handler.addFile(TOSCA_MANIFEST_FILEPATH, "".getBytes(StandardCharsets.UTF_8));
 
-        assertEquals(SOL004MetaDirectoryValidator.class, ValidatorFactory.getValidator(handler).getClass());
+        assertEquals(SOL004MetaDirectoryValidator.class, validatorFactory.getValidator(handler).getClass());
 
+    }
+
+    @Test
+    void testGetValidators() {
+        final List<Validator> validatorList = validatorFactory.getValidators("test");
+        assertFalse(validatorList.isEmpty());
+        assertEquals(2, validatorList.size());
+        assertTrue(validatorList.get(0) instanceof TestModelValidator1);
+        assertTrue(validatorList.get(1) instanceof TestModelValidator2);
+
+        final List<Validator> validatorList1 = validatorFactory.getValidators("test1");
+        assertTrue(validatorList1.isEmpty());
     }
 
 }
