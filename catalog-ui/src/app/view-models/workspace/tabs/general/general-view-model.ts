47743742519f3fdd9f60e0e155e9eee3a97754cc
aribeiro <anderson.ribeiro@est.tech>
2021-09-09 12:18:46 +0100

    Update category init file to include models
    Issue-ID: SDC-3721
    Signed-off-by: aribeiro <anderson.ribeiro@est.tech>
    Change-Id: I99bef55551bd585e8608c2e7e8d0f9a6cf0ad48f

--- catalog-ui/src/app/view-models/workspace/tabs/general/general-view-model.ts
+++ catalog-ui/src/app/view-models/workspace/tabs/general/general-view-model.ts
@@ -822,7 +822,7 @@ export class GeneralViewModel {
         // reload categories
         this.$scope.initCategories();
         this.$scope.categories = this.$scope.categories.filter(category =>
-            !modelName ? category.models === null : category.models !== null && category.models.indexOf(modelName) !== -1);
+            !modelName ? category.models.indexOf(DEFAULT_MODEL_NAME) !== -1 : category.models !== null && category.models.indexOf(modelName) !== -1);
     }
 
     private setUnsavedChanges = (hasChanges: boolean): void => {
