3c9fd259a8d709f103d1a05a4d29381b6d74e5ea
aribeiro <anderson.ribeiro@est.tech>
2021-12-14 15:09:50 +0000

    Identify SOL004 packages
    Issue-ID: SDC-3819
    Signed-off-by: MichaelMorris <michael.morris@est.tech>
    Change-Id: I7ea36ebc27753e8068791cffc3340db30adc4662
    Signed-off-by: aribeiro <anderson.ribeiro@est.tech>

--- openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/impl/orchestration/csar/validation/ValidatorFactory.java
+++ openecomp-be/backend/openecomp-sdc-vendor-software-product-manager/src/main/java/org/openecomp/sdc/vendorsoftwareproduct/impl/orchestration/csar/validation/ValidatorFactory.java
@@ -49,7 +49,7 @@ public class ValidatorFactory {
     public Validator getValidator(final FileContentHandler fileContentHandler) throws IOException {
         final ETSIService etsiService = new ETSIServiceImpl(null);
         if (!etsiService.isSol004WithToscaMetaDirectory(fileContentHandler)) {
-            return new ONAPCsarValidator();
+            return etsiService.isEtsiPackage(fileContentHandler) ? new EtsiSol004Version251Validator() : new ONAPCsarValidator();
         }
         if (!etsiService.getHighestCompatibleSpecificationVersion(fileContentHandler).isLowerThan(ETSI_VERSION_2_7_1)) {
             if (etsiService.hasCnfEnhancements(fileContentHandler)) {
