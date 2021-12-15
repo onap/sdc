b80595dcf4f84d1b50156cab745fff3fd026b0ba
andre.schmid <andre.schmid@est.tech>
2021-12-15 19:44:06 +0000

    Onboarding upload control
    Brings the initial structure to control asynchronously a VSP package
    upload during the onboarding.
    Instead of blocking the UI, the upload and processing status will be
    controlled by the backend, so the frontend can query it and control the
    behaviour of the UI.
    Updates the upload endpoint to obtain/verify an upload lock, and creates
    a second endpoint to check for the upload status.
    
    Change-Id: If1c43fb4f0b11e1d8a5627578bafc75f266393c2
    Issue-ID: SDC-3826, SDC-3827
    Signed-off-by: andre.schmid <andre.schmid@est.tech>

---
+++ openecomp-be/api/openecomp-sdc-rest-webapp/vendor-software-products-rest/vendor-software-products-rest-services/src/main/java/org/openecomp/sdcrests/vsp/rest/OrchestrationTemplateCandidateUploadManagerController.java
@@ -0,0 +1,65 @@
+/*
+ * -
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 Nordix Foundation.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdcrests.vsp.rest;
+
+import static org.openecomp.sdcrests.common.RestConstants.USER_ID_HEADER_PARAM;
+import static org.openecomp.sdcrests.common.RestConstants.USER_MISSING_ERROR_MSG;
+import static org.openecomp.sdcrests.vsp.rest.OrchestrationTemplateCandidateUploadManagerController.URL;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.springframework.validation.annotation.Validated;
+
+@Path(URL)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "SDCE-1 APIs")
+@Tag(name = "Package Upload Manager")
+@Validated
+public interface OrchestrationTemplateCandidateUploadManagerController extends VspEntities {
+
+    String URL = "/v1.0/vendor-software-products/{vspId}/versions/{versionId}/orchestration-template-candidate/upload";
+
+    /**
+     * Gets the latest package upload status for a Vendor Software Product version.
+     *
+     * @param vspId     the vsp id
+     * @param versionId the vsp version id
+     * @param user      the username accessing the API
+     * @return if successful, an OK response with the latest VspUploadStatus information
+     */
+    @GET
+    @Path("/")
+    Response getLatestStatus(@Parameter(description = "Vendor Software Product id") @PathParam("vspId") String vspId,
+                             @Parameter(description = "Vendor Software Product version id") @PathParam("versionId") String versionId,
+                             @NotNull(message = USER_MISSING_ERROR_MSG) @HeaderParam(USER_ID_HEADER_PARAM) String user);
+
+}
