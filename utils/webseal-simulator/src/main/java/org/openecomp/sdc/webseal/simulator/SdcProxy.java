5917402b920fcbd7b6ce32a1dae01341c375edc3
vasraz <vasyl.razinkov@est.tech>
2022-04-13 12:50:49 +0100

    Fix encoding/decoding issue
    Change-Id: If7a770548702cb9e6bb2d9e866c99c0396f74ee4
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Issue-ID: SDC-3967

--- utils/webseal-simulator/src/main/java/org/openecomp/sdc/webseal/simulator/SdcProxy.java
+++ utils/webseal-simulator/src/main/java/org/openecomp/sdc/webseal/simulator/SdcProxy.java
@@ -23,10 +23,8 @@ package org.openecomp.sdc.webseal.simulator;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -204,7 +202,7 @@ public class SdcProxy extends HttpServlet {
         return contextPaths;
     }
 
-    private String getUri(HttpServletRequest request, Map<String, String[]> requestParameters) throws UnsupportedEncodingException {
+    private String getUri(HttpServletRequest request, Map<String, String[]> requestParameters) {
         String suffix = request.getRequestURI();
         if (getContextPaths().stream().anyMatch(request.getRequestURI()::contains)) {
             suffix = alignUrlProxy(suffix);
@@ -316,7 +314,7 @@ public class SdcProxy extends HttpServlet {
         return SDC1 + requestURI;
     }
 
-    private StringBuilder alignUrlParameters(Map<String, String[]> requestParameters) throws UnsupportedEncodingException {
+    private StringBuilder alignUrlParameters(Map<String, String[]> requestParameters) {
         final var query = new StringBuilder();
         for (final Entry<String, String[]> entry : requestParameters.entrySet()) {
             for (final String value : entry.getValue()) {
@@ -325,7 +323,7 @@ public class SdcProxy extends HttpServlet {
                 } else {
                     query.append("&");
                 }
-                query.append(String.format("&%s=%s", URLEncoder.encode(entry.getKey(), "UTF-8"), URLEncoder.encode(value, "UTF-8")));
+                query.append(String.format("&%s=%s", entry.getKey(), value));
             }
         }
         return query;
