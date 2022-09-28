d74a76388e5dfb7851de5f0ffdaeb19588c26065
vasraz <vasyl.razinkov@est.tech>
2022-09-28 17:53:28 +0100

    Implement API for fetching single data type
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: Icebf660d67d503250e4f6d4d7fd5a2e169cdc951
    Issue-ID: SDC-4184

---
+++ catalog-be/src/main/java/org/openecomp/sdc/be/servlets/DataTypeServlet.java
@@ -0,0 +1,92 @@
+/*
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2022 Nordix Foundation. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdc.be.servlets;
+
+import com.jcabi.aspects.Loggable;
+import fj.data.Either;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.openecomp.sdc.be.components.impl.aaf.AafPermission;
+import org.openecomp.sdc.be.components.impl.aaf.PermissionAllowed;
+import org.openecomp.sdc.be.dao.api.ActionStatus;
+import org.openecomp.sdc.be.dao.janusgraph.JanusGraphOperationStatus;
+import org.openecomp.sdc.be.impl.ComponentsUtils;
+import org.openecomp.sdc.be.model.operations.impl.DataTypeOperation;
+import org.openecomp.sdc.be.resources.data.DataTypeData;
+import org.openecomp.sdc.be.user.UserBusinessLogic;
+import org.openecomp.sdc.common.api.Constants;
+import org.openecomp.sdc.common.log.wrappers.Logger;
+import org.springframework.stereotype.Controller;
+
+@Loggable(prepend = true, value = Loggable.DEBUG, trim = false)
+@Path("/v1/catalog/data-types")
+@Tag(name = "SDCE-2 APIs")
+@Server(url = "/sdc2/rest")
+@Controller
+public class DataTypeServlet extends BeGenericServlet {
+
+    private static final Logger log = Logger.getLogger(DataTypeServlet.class);
+    private final DataTypeOperation dataTypeOperation;
+
+    public DataTypeServlet(final UserBusinessLogic userAdminManager, final ComponentsUtils componentsUtils,
+                           final DataTypeOperation dataTypeOperation) {
+        super(userAdminManager, componentsUtils);
+        this.dataTypeOperation = dataTypeOperation;
+    }
+
+    @GET
+    @Path("{dataTypeUid}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Get data types", method = "GET", summary = "Returns data types", responses = {
+        @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class))),
+        @ApiResponse(responseCode = "200", description = "datatype"),
+        @ApiResponse(responseCode = "403", description = "Restricted operation"),
+        @ApiResponse(responseCode = "400", description = "Invalid content / Missing content"),
+        @ApiResponse(responseCode = "404", description = "Data types not found")})
+    @PermissionAllowed(AafPermission.PermNames.INTERNAL_ALL_VALUE)
+    public Response fetchDataType(@Context final HttpServletRequest request,
+                                  @HeaderParam(value = Constants.USER_ID_HEADER) String userId,
+                                  @PathParam("dataTypeUid") String dataTypeUid) {
+        final String url = request.getMethod() + " " + request.getRequestURI();
+        log.debug("Start handle request of {} - modifier id is {}", url, userId);
+        final Either<DataTypeData, JanusGraphOperationStatus> dataTypeByUid = dataTypeOperation.getDataTypeByUid(dataTypeUid);
+        if (dataTypeByUid.isRight()) {
+            return buildErrorResponse(getComponentsUtils().getResponseFormat(ActionStatus.DATA_TYPE_NOT_FOUND, dataTypeUid));
+        }
+        return buildOkResponse(getComponentsUtils().getResponseFormat(ActionStatus.OK), dataTypeByUid.left().value().getDataTypeDataDefinition());
+    }
+
+}
--- catalog-model/src/main/java/org/openecomp/sdc/be/model/operations/impl/DataTypeOperation.java
+++ catalog-model/src/main/java/org/openecomp/sdc/be/model/operations/impl/DataTypeOperation.java
@@ -71,38 +71,38 @@ public class DataTypeOperation extends AbstractOperation {
         }
 
         final List<DataTypeData> allDataTypeNodesWithModel = getAllDataTypesWithModel();
-        if(CollectionUtils.isNotEmpty(allDataTypeNodesWithModel)) {
+        if (CollectionUtils.isNotEmpty(allDataTypeNodesWithModel)) {
             dataTypesFound.addAll(allDataTypeNodesWithModel);
         }
         return dataTypesFound;
     }
-    
+
     public Map<String, List<String>> getAllDataTypeUidsToModels() {
         final Map<String, List<String>> dataTypesFound = new HashMap<>();
         final Either<List<DataTypeData>, JanusGraphOperationStatus> getAllDataTypesWithNullModel =
             janusGraphGenericDao.getByCriteria(NodeTypeEnum.DataType, null, DataTypeData.class);
 
         final var dataTypesValidated = validateDataType(getAllDataTypesWithNullModel, null);
-        
-        for (DataTypeData dataType: dataTypesValidated) {
-            if (!dataTypesFound.containsKey(dataType.getUniqueId())){
+
+        for (DataTypeData dataType : dataTypesValidated) {
+            if (!dataTypesFound.containsKey(dataType.getUniqueId())) {
                 dataTypesFound.put(dataType.getUniqueId(), new ArrayList<>());
             }
             dataTypesFound.get(dataType.getUniqueId()).add(null);
         }
-        
+
         modelOperation.findAllModels()
             .forEach(model -> {
-                for (DataTypeData dataType: getAllDataTypesWithModel(model.getName())) {
-                    if (!dataTypesFound.containsKey(dataType.getUniqueId())){
+                for (DataTypeData dataType : getAllDataTypesWithModel(model.getName())) {
+                    if (!dataTypesFound.containsKey(dataType.getUniqueId())) {
                         dataTypesFound.put(dataType.getUniqueId(), new ArrayList<>());
                     }
                     dataTypesFound.get(dataType.getUniqueId()).add(model.getName());
                 }
-        });
+            });
         return dataTypesFound;
     }
-    
+
     private List<DataTypeData> getAllDataTypesWithModel(final String modelName) {
         final Either<List<DataTypeData>, JanusGraphOperationStatus> getAllDataTypesByModel = janusGraphGenericDao
             .getByCriteriaForModel(NodeTypeEnum.DataType, null, modelName, DataTypeData.class);
@@ -129,7 +129,7 @@ public class DataTypeOperation extends AbstractOperation {
         if (getDataTypes.isRight()) {
             final var status = getDataTypes.right().value();
             if (LOGGER.isErrorEnabled()) {
-                final var errorMsg= String.format("Failed to fetch data types from database with model %s. Status is %s", modelName, status);
+                final var errorMsg = String.format("Failed to fetch data types from database with model %s. Status is %s", modelName, status);
                 LOGGER.error(String.valueOf(EcompLoggerErrorCode.UNKNOWN_ERROR), DataTypeOperation.class.getName(), errorMsg);
                 BeEcompErrorManager.getInstance().logInternalConnectionError(DataTypeOperation.class.getName(), errorMsg, ErrorSeverity.ERROR);
             }
@@ -150,6 +150,17 @@ public class DataTypeOperation extends AbstractOperation {
             traversal.V(dataTypeVertex).out(GraphEdgeLabels.PROPERTY.getProperty()).drop().iterate();
             dataTypeVertex.remove();
         });
+    }
+
+    public Either<DataTypeData, JanusGraphOperationStatus> getDataTypeByUid(final String uniqueId) {
+        final Either<DataTypeData, JanusGraphOperationStatus> dataTypes = janusGraphGenericDao
+            .getNode(UniqueIdBuilder.getKeyByNodeType(NodeTypeEnum.DataType), uniqueId, DataTypeData.class);
+        if (dataTypes.isRight()) {
+            final JanusGraphOperationStatus status = dataTypes.right().value();
+            LOGGER.debug("Failed to fetch data types '{}' from database. Status is: {}", uniqueId, status);
+            return Either.right(status);
+        }
+        return dataTypes;
     }
 
 }
