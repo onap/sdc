34af7e28405eae76332360d863c7708f93de733f
vasraz <vasyl.razinkov@est.tech>
2022-09-30 17:07:24 +0100

    Fix bug 'X-Frame-Options not configured: Lack of clickjacking protection'
    Add new Filter (XFrameOptionsHeaderFilter)
    
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: Ic8151df64e4b95b3d59b44a5f74dd12210f55e87
    Issue-ID: SDC-4192

---
+++ common-app-api/src/main/java/org/openecomp/sdc/common/filters/XFrameOptionsHeaderFilter.java
@@ -0,0 +1,56 @@
+/*
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2022 Nordix Foundation. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdc.common.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+public class XFrameOptionsHeaderFilter implements Filter {
+
+    private String permittedAncestors = null;
+
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+        permittedAncestors = filterConfig.getInitParameter("permittedAncestors");
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Content-Security-Policy",
+            "frame-ancestors 'self' " + (StringUtils.isNotBlank(permittedAncestors) ? permittedAncestors : ""));
+        chain.doFilter(request, httpServletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        permittedAncestors = null;
+    }
+
+}
