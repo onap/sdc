152b931fe8e6c1065ae1f312ebc270366a247e36
JvD_Ericsson <jeff.van.dam@est.tech>
2022-10-13 19:13:37 +0100

    Create UI Component for viewing property constraints
    Issue-ID: SDC-4219
    Signed-off-by: JvD_Ericsson <jeff.van.dam@est.tech>
    Change-Id: Ia0e40e2ed2fd954749a74ce307f1474380f9ad71

--- catalog-ui/src/app/view-models/forms/property-forms/component-property-form/property-form-view-model.ts
+++ catalog-ui/src/app/view-models/forms/property-forms/component-property-form/property-form-view-model.ts
@@ -458,6 +458,10 @@ export class PropertyFormViewModel {
             }
         }
 
+        this.$scope.onConstraintChange = (constraints: any[]): void => {
+            console.log(constraints);
+        }
+
         this.$scope.onGetFunctionValidFunction = (toscaGetFunction: ToscaGetFunction): void => {
             this.$scope.editPropertyModel.property.toscaFunction = toscaGetFunction;
         }
