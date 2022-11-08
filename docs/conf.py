9001e71e5e8f0114ca290375680d961e26855e37
MichaelMorris <michael.morris@est.tech>
2022-11-08 14:14:18 +0000

    Fix doc configuration on kohn branch
    Signed-off-by: MichaelMorris <michael.morris@est.tech>
    Issue-ID: SDC-4242
    Change-Id: Iae21bb2ea386a78d53fd188121e83ab9ddba1bff

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,46 @@
-from docs_conf.conf import *
+project = "onap"
+release = "kohn"
+version = "kohn"
 
-master_doc = 'index'
+author = "Open Network Automation Platform"
+# yamllint disable-line rule:line-length
+copyright = "ONAP. Licensed under Creative Commons Attribution 4.0 International License"
+
+pygments_style = "sphinx"
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+  "style_nav_header_background": "white",
+  "sticky_navigation": "False" }
+html_logo = "_static/logo_onap_2017.png"
+html_favicon = "_static/favicon.ico"
+html_static_path = ["_static"]
+html_show_sphinx = False
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.graphviz',
+    'sphinxcontrib.blockdiag',
+    'sphinxcontrib.seqdiag',
+    'sphinxcontrib.swaggerdoc',
+    'sphinxcontrib.plantuml'
+]
+
+#
+# Map to 'latest' if this file is used in 'latest' (master) 'doc' branch.
+# Change to {releasename} after you have created the new 'doc' branch.
+#
+
+branch = 'kohn'
 
 intersphinx_mapping = {}
+doc_url = 'https://docs.onap.org/projects'
+master_doc = 'index'
+
+exclude_patterns = ['.tox']
+
+spelling_lang = "en_GB"
+spelling_word_list_filename='spelling_wordlist.txt'
+spelling_exclude_patterns=['release-notes.rst']
 
 linkcheck_ignore = [
     'http://localhost',
