ab6d8edbe6215f67a33f1fbe7207b9ee0ab4b044
MichaelMorris <michael.morris@est.tech>
2022-11-17 14:55:16 -0800

    Fix SDC-BE not logging to STDOUT
    Signed-off-by: MichaelMorris <michael.morris@est.tech>
    Issue-ID: SDC-3534
    Change-Id: Ia3e20b7f43ece61cf28038d59af51264db2b7dec

--- catalog-be/src/main/docker/backend/startup.sh
+++ catalog-be/src/main/docker/backend/startup.sh
@@ -8,8 +8,7 @@ export JAVA_OPTIONS="$JAVA_OPTIONS -Dconfig.home=$JETTY_BASE/config \
        -Dartifactgenerator.config=$JETTY_BASE/config/catalog-be/Artifact-Generator.properties \
 	     -Donboarding_configuration.yaml=$JETTY_BASE/config/onboarding-be/onboarding_configuration.yaml \
        -Djavax.net.ssl.trustStore=$JETTY_BASE/etc/org.onap.sdc.trust.jks \
-       -Djavax.net.ssl.trustStorePassword=z+KEj;t+,KN^iimSiS89e#p0 \
-       -Djetty.console-capture.dir=$JETTY_BASE/logs"
+       -Djavax.net.ssl.trustStorePassword=z+KEj;t+,KN^iimSiS89e#p0" 
 
 cd $JETTY_BASE/chef-solo
 chef-solo -c solo.rb -E ${ENVNAME}
