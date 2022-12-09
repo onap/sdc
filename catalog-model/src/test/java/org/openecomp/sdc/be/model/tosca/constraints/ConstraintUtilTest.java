3fe707463fdfffa4e54a97ccdca81a4c6d9bb697
vasraz <vasyl.razinkov@est.tech>
2022-12-09 12:36:05 +0000

    Add support comparable type constraints for scalar values
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: I57234399f23721506d308dfb8351067845ebe892
    Issue-ID: SDC-4305

--- catalog-model/src/test/java/org/openecomp/sdc/be/model/tosca/constraints/ConstraintUtilTest.java
+++ catalog-model/src/test/java/org/openecomp/sdc/be/model/tosca/constraints/ConstraintUtilTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,39 +20,47 @@
 
 package org.openecomp.sdc.be.model.tosca.constraints;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 import org.openecomp.sdc.be.model.tosca.ToscaType;
+import org.openecomp.sdc.be.model.tosca.constraints.exception.ConstraintValueDoNotMatchPropertyTypeException;
 
-public class ConstraintUtilTest {
+class ConstraintUtilTest {
 
-	@Test
-	public void testCheckStringType() throws Exception {
-		ToscaType propertyType = ToscaType.STRING;
+    @Test
+    void testCheckStringType() throws Exception {
+        assertDoesNotThrow(() -> ConstraintUtil.checkStringType(ToscaType.STRING));
+        assertThrows(ConstraintValueDoNotMatchPropertyTypeException.class, () -> ConstraintUtil.checkStringType(ToscaType.SCALAR_UNIT));
+    }
 
-		// default test
-		ConstraintUtil.checkStringType(propertyType);
-	}
+    @Test
+    void testCheckComparableType() throws Exception {
+        assertDoesNotThrow(() -> ConstraintUtil.checkComparableType(ToscaType.INTEGER));
+        assertThrows(ConstraintValueDoNotMatchPropertyTypeException.class, () -> ConstraintUtil.checkComparableType(ToscaType.SCALAR_UNIT));
+    }
 
-	
-	@Test
-	public void testCheckComparableType() throws Exception {
-		ToscaType propertyType = ToscaType.INTEGER;
+    @Test
+    void testConvertToComparable() throws Exception {
+        assertTrue(ConstraintUtil.convertToComparable(ToscaType.BOOLEAN, "true") instanceof Comparable);
+        assertThrows(IllegalArgumentException.class, () -> ConstraintUtil.convertToComparable(ToscaType.SCALAR_UNIT, "value"));
+    }
 
-		// default test
-		ConstraintUtil.checkComparableType(propertyType);
-	}
+    @Test
+    void testParseToCollection() throws Exception {
+        List<Object> list = ConstraintUtil.parseToCollection("[\"color\",\"type\"]", new TypeReference<List<Object>>() {
+        });
+        assertTrue(list instanceof List);
+        assertEquals(2, list.size());
 
-	
-	@Test
-	public void testConvertToComparable() throws Exception {
-		ToscaType propertyType = ToscaType.BOOLEAN;
-		String value = "";
-		Comparable result;
-
-		// default test
-		result = ConstraintUtil.convertToComparable(propertyType, value);
-	}
-
-	
+        assertThrows(ConstraintValueDoNotMatchPropertyTypeException.class,
+            () -> ConstraintUtil.parseToCollection("", new TypeReference<List<Object>>() {
+            }));
+    }
 
 }
