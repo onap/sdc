a55467240c5bb7b9548689dbc5e9e43278e1b9b0
vasraz <vasyl.razinkov@est.tech>
2022-12-15 12:55:10 +0000

    Improve message for constraints different errors
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: I8babb10de4f0f25a49998fda077a7bb2554f86ba
    Issue-ID: SDC-4305

--- catalog-ui/src/app/ng2/components/logic/properties-table/dynamic-property/dynamic-property.component.ts
+++ catalog-ui/src/app/ng2/components/logic/properties-table/dynamic-property/dynamic-property.component.ts
@@ -113,6 +113,7 @@ export class DynamicPropertyComponent {
     ngOnChanges() {
         this.propType = this.property.derivedDataType;
         this.propPath = (this.property instanceof PropertyFEModel) ? this.property.name : this.property.propertiesName;
+        this.rootProperty = (this.rootProperty) ? this.rootProperty : <PropertyFEModel>this.property;
         this.propertyTestsId = this.getPropertyTestsId(); 
     }
 
