9a767465537da9045aff1573cc60ab3a0966f1c1
eschcam <cameron.scholes@est.tech>
2023-01-06 12:17:56 +0000

    Add validation for int and float constraints
    Issue-ID: SDC-4316
    Signed-off-by: eschcam <cameron.scholes@est.tech>
    Change-Id: I6d6172743779291597305583f2a7f4f2145f57fb

--- catalog-ui/src/app/view-models/forms/property-forms/component-property-form/property-form-view-model.ts
+++ catalog-ui/src/app/view-models/forms/property-forms/component-property-form/property-form-view-model.ts
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -466,6 +466,8 @@ export class PropertyFormViewModel {
         }
 
         this.$scope.onConstraintChange = (constraints: any): void => {
+            console.log('$scope.onConstraintChange', constraints);
+
             if (!this.$scope.invalidMandatoryFields) {
                 this.$scope.footerButtons[0].disabled = !constraints.valid;
             } else {
