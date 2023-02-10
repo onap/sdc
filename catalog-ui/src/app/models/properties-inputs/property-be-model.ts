1fb7f51f3d159a68abea96b1bd292117be4ef314
KrupaNagabhushan <krupa.nagabhushan@est.tech>
2023-02-10 18:41:01 +0000

    Provide input name when declaring service property as input
    Issue-ID: SDC-4385
    Signed-off-by: KrupaNagabhushan <krupa.nagabhushan@est.tech>
    Change-Id: I2b71e04b97ba69195380d2aa29d9d98a3bd5e981

--- catalog-ui/src/app/models/properties-inputs/property-be-model.ts
+++ catalog-ui/src/app/models/properties-inputs/property-be-model.ts
@@ -69,6 +69,7 @@ export class PropertyBEModel {
     parentPropertyType: string;
     subPropertyInputPath: string;
     inputPath: string;
+    inputName: string;
     toscaPresentation: ToscaPresentationData;
     metadata: Metadata;
     propertyConstraints: any;
@@ -103,6 +104,7 @@ export class PropertyBEModel {
             this.toscaPresentation = property.toscaPresentation;
             this.getPolicyValues = property.getPolicyValues;
             this.inputPath = property.inputPath;
+            this.inputName = property.inputName;
             this.metadata = property.metadata;
             if (property.toscaFunction) {
                 this.toscaFunction = property.toscaFunction;
---
+++ catalog-ui/src/app/ng2/pages/properties-assignment/declare-input/declare-input.component.ts
@@ -0,0 +1,40 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2023 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'declare-input',
+  templateUrl: './declare-input.component.html'
+})
+export class DeclareInputComponent implements OnInit {
+  inputNameForm: FormControl = new FormControl(undefined);
+  formGroup: FormGroup = new FormGroup({
+    'inputName': this.inputNameForm,
+  });
+  inputName: string;
+  constructor() { }
+  ngOnInit() {
+  }
+  setInputName(event) {
+    this.inputName = event.target.value;
+    this.inputNameForm.setValue(this.inputName);
+  }
+
+}
