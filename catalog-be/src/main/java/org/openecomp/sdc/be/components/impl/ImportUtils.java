2d322a1ff1f7aa24be85ab86763780c288a2fc28
vasraz <vasyl.razinkov@est.tech>
2023-03-06 10:19:29 +0000

    Add backend support for VFC property metadata
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: I0c446b537934ffd19c1162326e075e13845db23e
    Issue-ID: SDC-4424

--- catalog-be/src/main/java/org/openecomp/sdc/be/components/impl/ImportUtils.java
+++ catalog-be/src/main/java/org/openecomp/sdc/be/components/impl/ImportUtils.java
@@ -40,6 +40,8 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.text.StringEscapeUtils;
 import org.onap.sdc.tosca.datatypes.model.EntrySchema;
@@ -83,6 +85,7 @@ import org.yaml.snakeyaml.nodes.Tag;
 import org.yaml.snakeyaml.representer.Representer;
 import org.yaml.snakeyaml.resolver.Resolver;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Component
 public final class ImportUtils {
 
@@ -90,9 +93,6 @@ public final class ImportUtils {
     private static final Yaml strictYamlLoader = new YamlLoader().getStrictYamlLoader();
     private static final Logger log = Logger.getLogger(ImportUtils.class);
     private static ComponentsUtils componentsUtils;
-
-    private ImportUtils() {
-    }
 
     @Autowired
     public static void setComponentsUtils(ComponentsUtils cu) {
@@ -361,6 +361,7 @@ public final class ImportUtils {
         setJsonStringField(propertyValue, TypeUtils.ToscaTagNamesEnum.VALUE, propertyDef.getType(), propertyDef::setValue);
         setFieldBoolean(propertyValue, TypeUtils.ToscaTagNamesEnum.IS_PASSWORD, pass -> propertyDef.setPassword(Boolean.parseBoolean(pass)));
         setField(propertyValue, TypeUtils.ToscaTagNamesEnum.STATUS, propertyDef::setStatus);
+        setFieldMap(propertyValue, ToscaTagNamesEnum.METADATA, propertyDef::setMetadata);
         setSchema(propertyValue, propertyDef);
         setPropertyConstraints(propertyValue, propertyDef);
         return propertyDef;
@@ -770,6 +771,7 @@ public final class ImportUtils {
         }
     }
 
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class Constants {
 
         public static final String FIRST_NON_CERTIFIED_VERSION = "0.1";
@@ -790,7 +792,5 @@ public final class ImportUtils {
         public static final String VF_DESCRIPTION = "Nested VF in service";
         public static final String TENANT = "tenant";
 
-        private Constants() {
-        }
     }
 }
