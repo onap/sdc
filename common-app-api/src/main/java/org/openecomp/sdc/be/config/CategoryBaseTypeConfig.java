abbdafe672e5cf9e1e9eca5c96dc032a88513669
shikha0203 <shivani.khare@est.tech>
2023-03-22 11:52:31 +0000

    Enable using substitution mapping type directly
    Issue-ID: SDC-4435
    Signed-off-by: shikha0203 <shivani.khare@est.tech>
    Change-Id: Ia3120eb6f03ad861dc87de64a2cb81a61e048f4e

--- common-app-api/src/main/java/org/openecomp/sdc/be/config/CategoryBaseTypeConfig.java
+++ common-app-api/src/main/java/org/openecomp/sdc/be/config/CategoryBaseTypeConfig.java
@@ -34,5 +34,6 @@ public class CategoryBaseTypeConfig {
     private boolean required;
     private List<String> baseTypes;
     private String defaultBaseType;
+    private boolean doNotExtendBaseType;
 
 }
