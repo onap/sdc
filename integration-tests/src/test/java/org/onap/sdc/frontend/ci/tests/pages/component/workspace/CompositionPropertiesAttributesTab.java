d5a0ae25d3946026350143261ff0fd9138e0eef7
eschcam <cameron.scholes@est.tech>
2023-04-21 14:37:50 +0100

    Create integration test to verify setting instance integer properties
    Issue-ID: SDC-4478
    Signed-off-by: eschcam <cameron.scholes@est.tech>
    Change-Id: I27138a92e9e3e2603a3a01f0255d8dbb12549de4

---
+++ integration-tests/src/test/java/org/onap/sdc/frontend/ci/tests/pages/component/workspace/CompositionPropertiesAttributesTab.java
@@ -0,0 +1,40 @@
+package org.onap.sdc.frontend.ci.tests.pages.component.workspace;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.onap.sdc.frontend.ci.tests.pages.AbstractPageObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CompositionPropertiesAttributesTab extends AbstractPageObject {
+
+    private WebElement wrapperElement;
+
+    public CompositionPropertiesAttributesTab(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    @Override
+    public void isLoaded() {
+        wrapperElement = waitForElementVisibility(By.xpath(XpathSelector.PROPERTIES_TAB.xPath));
+    }
+
+    public void clickOnProperty(String propertyName) {
+        final WebElement propElement = wrapperElement.findElement(By.xpath(XpathSelector.PROPERTY.formatXPath(propertyName)));
+        propElement.click();
+    }
+
+    @AllArgsConstructor
+    @Getter
+    private enum XpathSelector {
+        PROPERTIES_TAB("//properties-tab"),
+        PROPERTY("//span[@data-tests-id='%s']");
+
+        private final String xPath;
+
+        public String formatXPath(Object value) {
+            return String.format(xPath, value);
+        }
+    }
+}
