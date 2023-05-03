526f8d340f0d2e812f90360e9e8882986bf0011b
vasraz <vasyl.razinkov@est.tech>
2023-05-03 16:57:48 +0100

    Support SIP TLS
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: Icbadd04cfa87302491c59f2e4a39ef92aaafcaa3
    Issue-ID: SDC-4483

--- common-app-api/src/main/java/org/openecomp/sdc/common/listener/AppContextListener.java
+++ common-app-api/src/main/java/org/openecomp/sdc/common/listener/AppContextListener.java
@@ -39,6 +39,7 @@ public class AppContextListener implements ServletContextListener {
 
     private static Logger log = Logger.getLogger(AppContextListener.class.getName());
 
+    @Override
     public void contextInitialized(ServletContextEvent context) {
         log.debug("ServletContextListener initialized ");
         log.debug("After read values from Manifest {}", getManifestInfo(context.getServletContext()));
@@ -57,6 +58,7 @@ public class AppContextListener implements ServletContextListener {
         ExternalConfiguration.listenForChanges();
     }
 
+    @Override
     public void contextDestroyed(ServletContextEvent context) {
         log.debug("ServletContextListener destroyed");
         ExternalConfiguration.stopListenForFileChanges();
