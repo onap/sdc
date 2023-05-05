dc3b608984a181a4f334fdd6574e0338da977a15
vasraz <vasyl.razinkov@est.tech>
2023-05-05 19:45:34 +0100

    Add FileChangeWatcher for SIP TLS
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: Id92cd441d88f4946be20975e01813749c830b2f9
    Issue-ID: SDC-4483

---
+++ catalog-be/src/main/java/org/openecomp/sdc/be/components/impl/FileAlterationListenerComponent.java
@@ -0,0 +1,83 @@
+package org.openecomp.sdc.be.components.impl;
+
+import nl.altindag.ssl.util.KeyManagerUtils;
+import nl.altindag.ssl.util.KeyStoreUtils;
+import nl.altindag.ssl.util.TrustManagerUtils;
+import org.apache.commons.io.monitor.FileAlterationListener;
+import org.apache.commons.io.monitor.FileAlterationListenerAdaptor;
+import org.apache.commons.io.monitor.FileAlterationMonitor;
+import org.apache.commons.io.monitor.FileAlterationObserver;
+import org.onap.config.api.JettySSLUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.security.KeyStore;
+import java.util.stream.Stream;
+
+@Component
+public class FileAlterationListenerComponent {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileAlterationListenerComponent.class);
+
+    private final SwappableSslService swappableSslService;
+
+    @Autowired
+    public FileAlterationListenerComponent(final SwappableSslService swappableSslService) {
+        this.swappableSslService = swappableSslService;
+        runListener();
+    }
+
+    private void runListener() {
+        final JettySSLUtils.JettySslConfig sslConfig = JettySSLUtils.getSSLConfig();
+        final String keystoreLocation = sslConfig.getKeystorePath();
+        final String keystorePass = sslConfig.getKeystorePass();
+        final String truststoreLocation = sslConfig.getTruststorePath();
+        final String truststorePass = sslConfig.getTruststorePass();
+        final String keystoreType = sslConfig.getKeystoreType();
+        final String truststoreType = sslConfig.getTruststoreType();
+        final FileAlterationMonitor monitor = new FileAlterationMonitor();
+        final FileAlterationListener listener = new FileAlterationListenerAdaptor() {
+            @Override
+            public void onFileChange(final File file) {
+                LOGGER.error("Something changed: {}", file);
+                final Path path = Path.of(String.valueOf(file));
+                if (file.getAbsolutePath().endsWith(keystoreLocation)) {
+                    try {
+                        LOGGER.error("it was key store file: {}", keystoreLocation);
+                        final char[] passCharArray = keystorePass.toCharArray();
+                        final KeyStore keystore = KeyStoreUtils.loadKeyStore(path, passCharArray, keystoreType);
+                        FileAlterationListenerComponent.this.swappableSslService.updateKeyManager(KeyManagerUtils.createKeyManager(keystore, passCharArray));
+                        LOGGER.error("KeyManager updated successfully");
+                    } catch (Exception e) {
+                        LOGGER.error("Failed to reload Key Store", e);
+                    }
+                } else if (file.getAbsolutePath().endsWith(truststoreLocation)) {
+                    try {
+                        LOGGER.error("it was trust store file: {}", truststoreLocation);
+                        final KeyStore truststore = KeyStoreUtils.loadKeyStore(path, truststorePass.toCharArray(), truststoreType);
+                        FileAlterationListenerComponent.this.swappableSslService.updateTrustManager(TrustManagerUtils.createTrustManager(truststore));
+                        LOGGER.error("TrustManager updated successfully");
+                    } catch (Exception e) {
+                        LOGGER.error("Failed to reload Trust Store", e);
+                    }
+                }
+            }
+        };
+
+        Stream.of(Path.of(sslConfig.getJettyBase(), keystoreLocation).getParent(), Path.of(sslConfig.getJettyBase(), truststoreLocation).getParent())
+                .distinct().forEach(path -> {
+                    final FileAlterationObserver observer = new FileAlterationObserver(path.toFile());
+                    observer.addListener(listener);
+                    monitor.addObserver(observer);
+                });
+        try {
+            monitor.start();
+        } catch (final Exception e) {
+            LOGGER.error("Failed to start monitor ", e);
+        }
+    }
+
+}
