c40b44b15d4fbb2e974290a62cd977cbe46a11f2
MichaelMorris <michael.morris@est.tech>
2023-08-29 09:34:04 +0100

    Escape '<' char before jsoup validation
    Signed-off-by: MichaelMorris <michael.morris@est.tech>
    Issue-ID: SDC-4606
    Change-Id: I3ef6f1c63dd8e86e0ed97ce4c0ba69339dd7d44a

--- common-app-api/src/main/java/org/openecomp/sdc/common/util/NoHtmlValidator.java
+++ common-app-api/src/main/java/org/openecomp/sdc/common/util/NoHtmlValidator.java
@@ -32,6 +32,7 @@ public class NoHtmlValidator implements ConstraintValidator<NoHtml, String> {
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext ctx) {
-        return value == null || Jsoup.isValid(value, Safelist.none());
+        final String valueAsHtmlBody = Jsoup.parseBodyFragment(value).body().html();
+        return value == null || Jsoup.isValid(valueAsHtmlBody, Safelist.none());
     }
 }
