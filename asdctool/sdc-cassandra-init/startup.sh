1b0e1ec3c55df90c6908a915d81ddb64905e3f8a
Khantwal <anamika.khantwal@accenture.com>
2024-11-20 15:19:44 +0530

    Remove chef for asdctool
    - Remove chef from asdctool config and handle executions through kubernetes resources
    
    Issue-ID: SDC-4700
    Change-Id: I09dcf729c9172f30d4f83cf36705628856cd8915
    Signed-off-by: Anamika Khantwal <anamika.khantwal@accenture.com>

--- asdctool/sdc-cassandra-init/startup.sh
+++ asdctool/sdc-cassandra-init/startup.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
-cd /home/sdc/chef-solo
-chef-solo -c solo.rb  -E ${ENVNAME}
-rc=$?
-if [ $rc -ne 0 ]; then exit $rc; fi
+mkdir -p /tmp/writable-config /tmp/writable-config/sdctool /tmp/writable-config/tools /home/sdc/asdctool/logs/SDC/SDC-TOOL
+
+chmod -R 770 /home/sdc/asdctool/logs/SDC/SDC-TOOL
+
+cp -r /tmp/config/cassandra-db-scripts-common/* /tmp/writable-config/
+cp -r /tmp/tools/* /tmp/writable-config/tools
+cp -r /tmp/sdctool/* /tmp/writable-config/sdctool
+cp -r /tmp/writable-config/janusgraph.properties /tmp/writable-config/sdctool/config
+cp -r /tmp/writable-config/configuration.yaml /tmp/writable-config/sdctool/config
+
+sh -x /tmp/writable-config/change_cassandra_user.sh
+sh -x /tmp/writable-config/create_dox_keyspace.sh
+
+cd /tmp/writable-config/tools/build/scripts
+sh -x /tmp/writable-config/tools/build/scripts/onboard-db-schema-creation.sh
+
+chmod -R 770 /tmp/writable-config/sdctool
+
+# Update java options in schema scripts
+sed -i 's/java \(.*\) -cp/java \1 -Djava.io.tmpdir=\/tmp\/writable-config\/tmp -cp/' \
+    /tmp/writable-config/sdctool/scripts/{schemaCreation.sh,janusGraphSchemaCreation.sh,sdcSchemaFileImport.sh}
+
+sh -x /tmp/writable-config/create-alter-dox-db.sh
+sh -x /tmp/writable-config/sdctool/scripts/schemaCreation.sh /tmp/writable-config/sdctool/config
+sh -x /tmp/writable-config/sdctool/scripts/janusGraphSchemaCreation.sh /tmp/writable-config/sdctool/config
+sh -x /tmp/writable-config/importconformance.sh
