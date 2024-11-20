d58a29fc752b319b015462c8d255aab115f7ebab
Lodha <jinendra.lodha@accenture.com>
2024-11-20 16:54:20 +0530

    Remove chef for catalog-fe - Remove chef from catalog-fe config and handle executions through kubernetes resources.
    Issue-ID: SDC-4699
    
    Change-Id: Ie90b879c927d6513cd5959304f2c36f3535c57fe
    Signed-off-by: jinendra.lodha <jinendra.lodha@accenture.com>

---
+++ catalog-fe/sdc-frontend/scripts/cleanup_jettydir.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#Create temp directory
+mkdir -p "$JETTY_BASE/temp"
+chown "$JETTY_USER:$JETTY_GROUP" "$JETTY_BASE/temp"
+chmod 0755 "$JETTY_BASE/temp"
+
+# Create config directory
+mkdir -p "$JETTY_BASE/config"
+chown "$JETTY_USER:$JETTY_GROUP" "$JETTY_BASE/config"
+chmod 0755 "$JETTY_BASE/config"
+
+# Create onboarding-be directory
+mkdir -p "$JETTY_BASE/config/onboarding-be"
+chown -R "$JETTY_USER:$JETTY_GROUP" "$JETTY_BASE/config/onboarding-be"
+chmod -R 0755 "$JETTY_BASE/config/onboarding-be"
+
+
+# Create onboarding-be directory
+mkdir -p "$JETTY_BASE/config/onboarding-be"
+chown -R "$JETTY_USER:$JETTY_GROUP" "$JETTY_BASE/config/catalog-fe"
+chmod -R 0755 "$JETTY_BASE/config/catalog-fe"
