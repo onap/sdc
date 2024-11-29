b35b07b5489a26e908a8045ef24402ecd8059493
Khantwal <anamika.khantwal@accenture.com>
2024-11-29 16:33:18 +0530

    Remove chef for asdctool and sdc-os-chef
    - Remove chef from asdctool config and handle executions through shell scripts
    
    Issue-ID: SDC-4700
    Change-Id: Iff84d4caa22fb0ac3be2358608c0698a9c2da418
    Signed-off-by: Anamika Khantwal <anamika.khantwal@accenture.com>

---
+++ asdctool/sdc-cassandra-init/scripts/cassandra-env_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export CASSANDRA_IP=${CASSANDRA_IP:-"sdc-cs"}
+export CS_PORT=${CS_PORT:-9042}
+export CS_PASSWORD=${CS_PASSWORD:-"onap123#@!"}
+export SDC_USER=${SDC_USER:-"asdc_user"}
+export SDC_PASSWORD=${SDC_PASSWORD:-"Aa1234%^!"}
+export CASSANDRA_PASS=${CS_PASSWORD:-"onap123@#!"}
+export DC_NAME=${DC_NAME:-"dc1"}
+export cqlversion=${cqlversion:-"3.4.4"}
+export DISABLE_HTTP="false"
