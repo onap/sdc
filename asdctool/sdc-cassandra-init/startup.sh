be53592cdb3f5b753e7b00973cb743ceac298809
Khantwal <anamika.khantwal@accenture.com>
2024-11-29 16:33:18 +0530

    Remove chef for asdctool and sdc-os-chef
    - Remove chef from asdctool config and handle executions through shell scripts
    
    Issue-ID: SDC-4700
    Change-Id: Iff84d4caa22fb0ac3be2358608c0698a9c2da418
    Signed-off-by: Anamika Khantwal <anamika.khantwal@accenture.com>

--- asdctool/sdc-cassandra-init/startup.sh
+++ asdctool/sdc-cassandra-init/startup.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-cd /home/sdc/chef-solo
-chef-solo -c solo.rb  -E ${ENVNAME}
-rc=$?
-if [ $rc -ne 0 ]; then exit $rc; fi
+cd /home/sdc/scripts
+sh -x conditional_test.sh
+sh -x /home/sdc/scripts/change_cassandra_user.sh
+mkdir -p /tmp/config
+sh -x /home/sdc/scripts/create_dox_keyspace.sh
+cd /home/sdc/tools/build/scripts
+sh -x /home/sdc/tools/build/scripts/onboard-db-schema-creation.sh
+cd /home/sdc/scripts
+sh -x /home/sdc/scripts/create-alter-dox-db.sh
+cd /home/sdc/sdctool/scripts
+sh -x /home/sdc/sdctool/scripts/schemaCreation.sh /home/sdc/sdctool/config
+sh -x /home/sdc/sdctool/scripts/janusGraphSchemaCreation.sh /home/sdc/sdctool/config
+cd /home/sdc/scripts
+sh -x /home/sdc/scripts/importconformance.sh
