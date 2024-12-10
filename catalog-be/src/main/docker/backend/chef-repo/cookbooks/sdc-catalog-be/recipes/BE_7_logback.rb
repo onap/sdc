bcd34513db53fe20ed49efd8001ad1f8fa44be3f
bhoopender.singh@t-systems.com <bhoopender.singh@t-systems.com>
2024-12-10 09:52:09 +0100

    chef removal changes for catalog-be
    - Removed chef from catalog-be module with kubernetes resources.
    
    Issue-ID: SDC-4697
    Change-Id: Ife1b8ad8c0a978d574787e395daf978364a0fc41
    Signed-off-by: bhoopender.singh@t-systems.com <bhoopender.singh@t-systems.com>

--- catalog-be/src/main/docker/backend/chef-repo/cookbooks/sdc-catalog-be/recipes/BE_7_logback.rb
+++
@@ -1,8 +0,0 @@
-cookbook_file "#{ENV['JETTY_BASE']}/config/catalog-be/logback.xml" do
-  source "logback.xml"
-  mode 0644
-  owner "#{ENV['JETTY_USER']}"
-  group "#{ENV['JETTY_GROUP']}"
-  action :create_if_missing
-end
- 
