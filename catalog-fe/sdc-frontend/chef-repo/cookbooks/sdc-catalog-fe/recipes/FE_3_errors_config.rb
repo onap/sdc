205f76b6c7ce4b779dc27a78b69de0a538f021a2
Lodha <jinendra.lodha@accenture.com>
2024-12-16 12:41:47 +0530

    Remove chef for catalog-fe
    - Remove chef from catalog-fe config and handle executions through kubernetes resources.
    
    Issue-ID: SDC-4699
    Change-Id: I53229c6fe4d5ff9ba09ceda6cd9532f899c7702e
    Signed-off-by: jinendra.lodha <jinendra.lodha@accenture.com>

--- catalog-fe/sdc-frontend/chef-repo/cookbooks/sdc-catalog-fe/recipes/FE_3_errors_config.rb
+++
@@ -1,7 +0,0 @@
-cookbook_file "#{ENV['JETTY_BASE']}/config/catalog-fe/ecomp-error-configuration.yaml" do
-  source "FE-ecomp-error-configuration.yaml"
-  mode 0755
-  owner "#{ENV['JETTY_USER']}"
-  group "#{ENV['JETTY_GROUP']}"
-end
- 
--- catalog-fe/sdc-frontend/startup.sh
+++ catalog-fe/sdc-frontend/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sh -x /app/jetty/env_variables.sh
+
 JAVA_OPTIONS="$JAVA_OPTIONS \
                -Dconfig.home=$JETTY_BASE/config \
                -Dlog.home=$JETTY_BASE/logs \
@@ -9,11 +11,6 @@ JAVA_OPTIONS="$JAVA_OPTIONS \
                -Djavax.net.ssl.trustStore=$JETTY_BASE/etc/org.onap.sdc.trust.jks \
                -Djavax.net.ssl.trustStorePassword=z+KEj;t+,KN^iimSiS89e#p0"
 
-cd $JETTY_BASE/chef-solo
-chef-solo -c solo.rb -E ${ENVNAME}
-
 cd $JETTY_HOME
-echo "etc/rewrite-root-to-sdc1.xml" >> $JETTY_HOME/start.d/rewrite.ini
-echo "jetty.httpConfig.sendServerVersion=false" >> $JETTY_HOME/start.d/start.ini
 
 java $JAVA_OPTIONS -jar "${JETTY_HOME}/start.jar"
