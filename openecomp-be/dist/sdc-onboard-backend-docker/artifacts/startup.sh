643f392a8b8b855f48342bf5a2c429368d8f023d
rohit.raj@t-systems.com <rohit.raj@t-systems.com>
2025-01-15 08:32:04 +0100

    Chef removal for openecomp-be module. - Removal of Chef recipies in the module. - Introduction of shell scripts in the module to replace some recipes. - Generation of module configuration using Charts. - Introduction of config files in integration tests to simulate files generated by Charts in the module.
    Issue-ID: SDC-4698
    Change-Id: I847919438c572ea1e3e330224b5240a2ef59bf6b
    Signed-off-by: rohit.raj@t-systems.com <rohit.raj@t-systems.com>

--- openecomp-be/dist/sdc-onboard-backend-docker/artifacts/startup.sh
+++ openecomp-be/dist/sdc-onboard-backend-docker/artifacts/startup.sh
@@ -3,17 +3,13 @@
 JAVA_OPTIONS="$JAVA_OPTIONS \
             -Dcom.datastax.driver.USE_NATIVE_CLOCK=false \
             -Dconfig.home=$JETTY_BASE/config \
+	    -Duser.dir=$JETTY_BASE \
             -Dlog.home=$JETTY_BASE/logs \
             -Dlogback.configurationFile=$JETTY_BASE/config/onboarding-be/logback.xml \
             -Dconfiguration.yaml=$JETTY_BASE/config/onboarding-be/onboarding_configuration.yaml \
             -Dfeatures.properties=$JETTY_BASE/config/onboarding-be/features.properties \
             -XX:+HeapDumpOnOutOfMemoryError \
             -Dconfig.location=$JETTY_BASE/config/onboarding-be/."
-
-cd $JETTY_BASE
-
-cd $JETTY_BASE/chef-solo
-chef-solo -c solo.rb -E ${ENVNAME}
 
 cd $JETTY_HOME
 echo "jetty.httpConfig.sendServerVersion=false" >> $JETTY_HOME/start.d/start.ini
