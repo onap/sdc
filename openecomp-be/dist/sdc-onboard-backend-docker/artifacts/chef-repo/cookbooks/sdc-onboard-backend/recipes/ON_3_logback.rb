a1a66eb6125a69e8592ecc05708068e827a21057
rohit.raj@t-systems.com <rohit.raj@t-systems.com>
2025-01-15 08:32:04 +0100

    Chef removal for openecomp-be module.
    - Chef removal changes.
    
    Issue-ID: SDC-4698
    Change-Id: I847919438c572ea1e3e330224b5240a2ef59bf6b
    Signed-off-by: rohit.raj@t-systems.com <rohit.raj@t-systems.com>

--- openecomp-be/dist/sdc-onboard-backend-docker/artifacts/chef-repo/cookbooks/sdc-onboard-backend/recipes/ON_3_logback.rb
+++
@@ -1,7 +0,0 @@
-cookbook_file "#{ENV['JETTY_BASE']}/config/onboarding-be/logback.xml" do
-  source "logback.xml"
-  mode 0755
-  owner "#{ENV['JETTY_USER']}"
-  group "#{ENV['JETTY_GROUP']}"
-end
- 
