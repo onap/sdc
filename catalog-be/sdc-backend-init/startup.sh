f1439b3b088489cdfa246358cdf56ebd8f69e3dd
bhoopender.singh@t-systems.com <bhoopender.singh@t-systems.com>
2025-01-19 19:49:35 +0100

    chef removal changes from catalog-be.
    - Removed chef from catalog-be module with kubernetes resources.
    
    Issue-ID: SDC-4697
    Change-Id: Ic3450a59a9ecb179566b579494c87ef8d12a6616
    Signed-off-by: bhoopender.singh@t-systems.com <bhoopender.singh@t-systems.com>

--- catalog-be/sdc-backend-init/startup.sh
+++ catalog-be/sdc-backend-init/startup.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-cd /home/"${user}"/chef-solo || exit $?
-chef-solo -c solo.rb -E "${ENVNAME}"
+set -e
+
+
+log_script() {
+    script_name=$1
+    sh "$script_name" 2>&1 | sed "s|^|[$script_name] |" || \
+    echo "$script_name failed. Continuing with other scripts..." >&2
+}
+
+log_script "/home/onap/create_consumer_and_user.sh"
+log_script "/home/onap/check_backend.sh"
+log_script "/home/onap/import_normatives.sh"
+
+echo "Done"
