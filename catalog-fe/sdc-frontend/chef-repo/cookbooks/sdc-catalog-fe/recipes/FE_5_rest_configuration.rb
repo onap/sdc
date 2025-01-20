877119d1b67b950736eab851693aed9dc8c9ec22
Khantwal <anamika.khantwal@accenture.com>
2025-01-20 12:04:48 +0530

    Chef removal changes from Catalog-fe
    - Removed chef from catalog-fe module with kubernetes resources.
    
    Issue-ID: SDC-4699
    Change-Id: I5389c99d3a3c78eb4ffba34ca339ae72566c6ece
    Signed-off-by: Anamika Khantwal <anamika.khantwal@accenture.com>

--- catalog-fe/sdc-frontend/chef-repo/cookbooks/sdc-catalog-fe/recipes/FE_5_rest_configuration.rb
+++
@@ -1,7 +0,0 @@
-cookbook_file "#{ENV['JETTY_BASE']}/config/catalog-fe/rest-configuration-info.yaml" do
-  source "FE-rest-configuration.yaml"
-  mode 0755
-  owner "#{ENV['JETTY_USER']}"
-  group "#{ENV['JETTY_GROUP']}"
-end
- 
