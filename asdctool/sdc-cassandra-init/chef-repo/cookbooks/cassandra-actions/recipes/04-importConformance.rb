90f27d1eabc48e94e2d84a7c20f9462e36823fdb
Lodha <jinendra.lodha@accenture.com>
2025-01-22 16:13:34 +0530

    Remove chef for asdc_tool, sdc_os_chef
    - Remove chef from sdc-os-chef and asdctool config and handle executions
      through kubernetes resources.
    
    Issue-ID: SDC-4699
    Change-Id: Ib76ee4e2c76c090d722ed32ad4e8c6ee54401971
    Signed-off-by: jinendra.lodha <jinendra.lodha@accenture.com>

--- asdctool/sdc-cassandra-init/chef-repo/cookbooks/cassandra-actions/recipes/04-importConformance.rb
+++
@@ -1,9 +0,0 @@
-working_directory =  "/tmp"
-cl_release=node['version'].split('.')[0..2].join('.').split('-')[0]
-printf("\033[33mcl_release=[%s]\n\033[0m", cl_release)
-
-execute "import-Conformance" do
-  command "conf_dir=/tmp/sdctool/config && tosca_dir=/tmp/sdctool/tosca && cl_version=`grep 'toscaConformanceLevel:' $conf_dir/configuration.yaml |awk '{print $2}'` && cd /tmp/sdctool/scripts && /bin/chmod +x sdcSchemaFileImport.sh && echo \"execute /tmp/sdctool/scripts/sdcSchemaFileImport.sh ${tosca_dir} #{cl_release} ${cl_version} ${conf_dir} onap\" && ./sdcSchemaFileImport.sh ${tosca_dir} #{cl_release} ${cl_version} ${conf_dir} onap"
-  cwd "#{working_directory}"
-  action :run
-end
