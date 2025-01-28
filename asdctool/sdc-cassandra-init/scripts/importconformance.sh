889f76026e0bff8d6030514f669ec3e2df62925d
Lodha <jinendra.lodha@accenture.com>
2025-01-28 22:55:30 +0530

    Chef removal changes for asdc_tool
    - Removal of Chef recipes in the module.
    - Introduction of shell scripts in the module to replace some recipes.
    - Generation of module configuration using Charts.
    
    Issue-ID: SDC-4699
    Change-Id: I5c493296b548b2188efb83c50f0323c38276d17a
    Signed-off-by: jinendra.lodha <jinendra.lodha@accenture.com>

---
+++ asdctool/sdc-cassandra-init/scripts/importconformance.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Set the working directory
+working_directory="/home/sdc"
+
+# Extract the cl_release version
+version="1.13.7"  # Example version string; replace with actual value
+cl_release=$(echo $version | cut -d. -f1-3 | cut -d- -f1)
+echo -e "\033[33mcl_release=[$cl_release]\033[0m"
+
+# Execute the import-Conformance command
+conf_dir="/home/sdc/sdctool/config"
+tosca_dir="/home/sdc/sdctool/tosca"
+cl_version=$(grep 'toscaConformanceLevel:' $conf_dir/configuration.yaml | awk '{print $2}')
+
+cd /home/sdc/sdctool/scripts
+chmod +x sdcSchemaFileImport.sh
+
+echo "execute /home/sdc/sdctool/scripts/sdcSchemaFileImport.sh ${tosca_dir} ${cl_release} ${cl_version} ${conf_dir} onap"
+./sdcSchemaFileImport.sh ${tosca_dir} ${cl_release} ${cl_version} ${conf_dir} onap
