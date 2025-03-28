6af0c31022a2643612e62cfa665e34aabccc9cab
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-03-28 16:45:21 +0100

    Make sdc-onboarding-be a spring-boot app
    - define main class in onboarding-rest-war module
    - keep war packaging for now (until it is migrated to jar at
      a later stage)
    - remove <bean> declarations from beans-services.xml when they
      are already instantiated by spring boots context scanning
    
    Issue-ID: SDC-4727
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
    Change-Id: I1d58e609a798357ded26589a1f4a58dcd5536d67

---
+++ openecomp-be/api/openecomp-sdc-rest-webapp/onboarding-rest-war/src/test/java/org/openecomp/server/SdcOnboardingBackendTest.java
@@ -0,0 +1,13 @@
+package org.openecomp.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SdcOnboardingBackendTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
