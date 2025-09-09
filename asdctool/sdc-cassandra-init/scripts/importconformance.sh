24f0f37ecbbcfd7b4a486453c777b3a57d83dd3c
ankita.singhal <ankita.singhal@t-systems.com>
2025-09-09 07:28:19 +0200

    [SDC]: Bump snapshot version to 1.14.4-SNAPSHOT
    - Permissions Issue Fix for SDC-Fe init containers.
    - Datatype changes for sdc and 3gpp maxNumberOfues
    
    Issue-ID: SDC-4769
    Change-Id: I53206ef71e67f7c29ed0fea6497d12afdb9ee877
    Signed-off-by: ankita.singhal <ankita.singhal@t-systems.com>

--- asdctool/sdc-cassandra-init/scripts/importconformance.sh
+++ asdctool/sdc-cassandra-init/scripts/importconformance.sh
@@ -4,7 +4,7 @@
 working_directory="/home/sdc"
 
 # Extract the cl_release version
-version="1.14.3"
+version="1.14.4"
 cl_release=$(echo $version | cut -d. -f1-3 | cut -d- -f1)
 echo -e "\033[33mcl_release=[$cl_release]\033[0m"
 
