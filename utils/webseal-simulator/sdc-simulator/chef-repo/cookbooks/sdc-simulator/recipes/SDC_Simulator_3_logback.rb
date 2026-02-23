d6e009a88e83e9ee2307536505e64481f819e7c7
Fiete Ostkamp <fiete.ostkamp@telekom.de>
2026-02-23 10:20:33 +0100

    Get rid of Chef in sdc-simulator (also shortens pipeline duration by ~1>
    - remove Chef workbook that was used in sdc-simulator
    - replace with shell scripts and envsubst
    - use eclipse-temurin base image and remove obsolete
      packages from the image
    - this should speed up the pipeline duration by at least 10
      minutes (since that is how long it took to build these
      ruby gems)
    
    Issue-ID: SDC-4787
    Change-Id: I8f514030c208ea07304fba2c742e6e7d4c26d185
    Signed-off-by: Fiete Ostkamp <fiete.ostkamp@telekom.de>

--- utils/webseal-simulator/sdc-simulator/chef-repo/cookbooks/sdc-simulator/recipes/SDC_Simulator_3_logback.rb
+++
@@ -1,7 +0,0 @@
-cookbook_file "log4j2.properties" do
-   path "#{ENV['JETTY_BASE']}/config/sdc-simulator/log4j2.properties"
-   source "log4j2.properties"
-   owner "#{ENV['JETTY_USER']}"
-   group "#{ENV['JETTY_GROUP']}"
-   mode "0755"
-end
---
+++ utils/webseal-simulator/sdc-simulator/scripts/create_directories.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Create required directories for sdc-simulator
+mkdir -p "$JETTY_BASE/temp"
+mkdir -p "$JETTY_BASE/config/sdc-simulator"
+mkdir -p "$JETTY_BASE/etc"
+mkdir -p "$JETTY_BASE/logs"
